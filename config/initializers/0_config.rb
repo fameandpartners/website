562d6fa9172a83d0444ef874434070ec8cf371f7
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2013-05-21 14:49:28 +0400

    Add some social service`s links

--- config/initializers/0_config.rb
+++ config/initializers/0_config.rb
@@ -10,9 +10,10 @@ configatron.aws.bucket = ""
 configatron.aws.access_key = ""
 configatron.aws.secret_key = ""
 
-configatron.links.twitter = ''
-configatron.links.facebook = ''
-configatron.links.google = ''
+configatron.links.twitter = 'https://twitter.com/fameandpartners'
+configatron.links.facebook = 'http://www.facebook.com/FameandPartners'
+configatron.links.yputube = 'http://www.youtube.com/user/fameandpartners'
+configatron.links.google = 'https://plus.google.com/b/117433562249244184743/117433562249244184743/about/p/pub'
 configatron.links.pinterest = ''
 configatron.links.rss = ''
 
