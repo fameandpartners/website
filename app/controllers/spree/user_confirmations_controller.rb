089da3113aa76f3414c34329faa36100e56d2df5
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2013-06-25 13:39:19 +0400

    Redirect with addition params after confirmation

--- app/controllers/spree/user_confirmations_controller.rb
+++ app/controllers/spree/user_confirmations_controller.rb
@@ -1,2 +1,5 @@
 class Spree::UserConfirmationsController < Devise::ConfirmationsController
+  def after_confirmation_path_for(resource_name, resource)
+    root_path(user_addition_params)
+  end
 end
