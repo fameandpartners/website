d94d16912dd3f75118a53b7b1d925c7ffff533ba
Ilya Gorenburg <ilya.gorenburg@gmail.com>
2013-06-25 12:43:25 +0400

    account page

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ FameAndPartners::Application.routes.draw do
 
   # Static for ecommerce
   #match '/products' => 'pages#products'
+  match '/account' => 'pages#account'
   match '/product' => 'pages#product'
   match '/cart' => 'pages#cart'
   match '/quick-view' => 'pages#quick-view'
