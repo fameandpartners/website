3f152849275f0bbfb36118883c6957bfecfc7cf6
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2013-06-26 16:47:24 +0400

    Fix routes for production

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
   layout 'spree/layouts/spree_application'
-  respond_to :html 
+  respond_to :html
 
   def fb_auth
     if params[:prom]
       session[:sign_up_reason] = 'custom_dress'
     end
 
-    redirect_to spree.spree_user_omniauth_authorize_url(:provider => :facebook)
+    redirect_to spree.user_omniauth_authorize_url(:provider => :facebook)
   end
 end
