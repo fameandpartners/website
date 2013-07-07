a316c8f0caf3cd6e9b42173a260431cbf1878ea7
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2013-07-07 20:19:37 +0400

    Voting on the index page

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,6 +2,11 @@ class BlogController < BlogBaseController
   def index
     @promo_banners        = Blog::PromoBanner.published.all
     @latest_photos        = Blog::CelebrityPhoto.latest
+    if current_spree_user.present?
+      @photo_votes = Blog::CelebrityPhotoVote.where(
+        user_id: current_spree_user.id, celebrity_photo_id: @latest_photos.map(&:id)
+      )
+    end
     generate_breadcrumbs
   end
 
