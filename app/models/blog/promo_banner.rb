c58d78a4a56e92e4782667ac0c55678e263c3779
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2013-07-22 08:10:36 +0400

    Promo banners should use correct url

--- app/models/blog/promo_banner.rb
+++ app/models/blog/promo_banner.rb
@@ -2,13 +2,14 @@ class Blog::PromoBanner < ActiveRecord::Base
   attr_accessible :title, :url, :position, :published, :photo, :description
 
   belongs_to :user, class_name: Spree::User
-  has_attached_file :photo
+  has_attached_file :photo, styles: { preview: "1078x434#"}
 
   validates :title, :url, :user_id, presence: true
-  validates_attachment_presence :photo, styles: { preview: "1078x434#"}
+  validates_attachment_presence :photo
 
   scope :published, where(published: true)
   before_post_process :randomize_file_name
+  before_create
 
   def state
     if published?
@@ -19,7 +20,9 @@ class Blog::PromoBanner < ActiveRecord::Base
   end
 
   def randomize_file_name
-    extension = File.extname(photo_file_name).downcase
-    self.photo.instance_write(:file_name, "#{SecureRandom.hex(16)}#{extension}")
+    if photo.present?
+      extension = File.extname(photo_file_name).downcase
+      self.photo.instance_write(:file_name, "#{SecureRandom.hex(16)}#{extension}")
+    end
   end
 end
