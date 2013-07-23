ac6f46915ea5bbaff50839a1c8c0727f17740be8
Evgeniy <malleus.petrov@gmail.com>
2013-07-24 00:44:23 +0400

    order edit / summary fixes

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -13,4 +13,20 @@ module OrdersHelper
     state = order.shipment_state.present? ? order.shipment_state : 'pending'
     t("shipment_states.#{state}")
   end
+
+  def show_order_adjustment_total(order)
+    if order.display_adjustment_total && order.display_adjustment_total.money.cents > 0
+      content_tag('span', order.currency) + order.display_adjustment_total
+    else
+      'Free'
+    end
+  end
+
+  def show_order_shipment_total(order)
+    if order.shipment && order.shipment.display_amount && order.shipment.display_amount.money.cents > 0
+      content_tag('span', order.currency) + order.shipment.display_amount
+    else
+      'Free'
+    end
+  end
 end
