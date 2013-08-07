7e77198b488f88f5cf8b994e793be7a1bb9b630c
Evgeniy <malleus.petrov@gmail.com>
2013-08-07 18:03:55 +0400

    add after deploy symlink

---
+++ deploy/after_symlink.rb
@@ -0,0 +1,4 @@
+%w(spree).each do |folder| 
+  run "echo 'release_path: #{release_path}/public/#{folder}' >> #{shared_path}/#{spree}" 
+  run "ln -nfs #{shared_path}/#{folder} #{release_path}/public/#{folder}" 
+end 
