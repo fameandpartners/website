fb51cb844ef8503aca57b7f7b4bfe82fdf749bbe
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2013-08-07 18:41:41 +0400

    Fix deploy hook

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,4 +1,4 @@
 %w(spree).each do |folder| 
-  run "echo 'release_path: #{release_path}/public/#{folder}' >> #{shared_path}/#{spree}" 
+  run "echo 'release_path: #{release_path}/public/#{folder}' >> #{shared_path}/#{folder}"
   run "ln -nfs #{shared_path}/#{folder} #{release_path}/public/#{folder}" 
 end 
