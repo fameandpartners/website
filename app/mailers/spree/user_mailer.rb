12e79e4589df2b4eb5f0ba3c543564774b1cb4d2
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2013-08-08 00:31:04 +0400

    Add email views

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -10,7 +10,7 @@ class Spree::UserMailer < ActionMailer::Base
   end
 
   def reset_password_instructions(user)
-    @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => user.reset_password_token)
+    @resource = user
 
     mail(:to => user.email,
          :subject => Spree::Config[:site_name] + ' ' + I18n.t(:password_reset_instructions))
