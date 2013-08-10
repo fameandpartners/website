2d7742be8e6a761b83609412a31bf53998f105ce
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2013-08-10 16:32:40 +0400

    Add the ability to create a product and select "on demand"

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -58,4 +58,21 @@ Spree::Product.class_eval do
     variants_including_master.update_all(:deleted_at => Time.now)
     update_index
   end
+
+  private
+
+  def build_variants_from_option_values_hash
+    ensure_option_types_exist_for_values_hash
+    values = option_values_hash.values
+    values = values.inject(values.shift) { |memo, value| memo.product(value).map(&:flatten) }
+
+    values.each do |ids|
+      variant = variants.create({
+                                  :option_value_ids => ids,
+                                  :price => master.price,
+                                  :on_demand => on_demand
+                                }, :without_protection => true)
+    end
+    save
+  end
 end
