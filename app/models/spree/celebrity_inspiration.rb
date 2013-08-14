1657115288916880bd81cdac72f8e00bffa221df
eltonstewart <elton.stewart@gmail.com>
2013-08-15 00:44:03 +1000

    changed celeb inspiration photo upload dimensions

--- app/models/spree/celebrity_inspiration.rb
+++ app/models/spree/celebrity_inspiration.rb
@@ -3,7 +3,7 @@ class Spree::CelebrityInspiration < ActiveRecord::Base
 
   attr_accessible :photo, :celebrity_name, :celebrity_description
 
-  has_attached_file :photo, styles: { preview: "460x590#", small: "230x295#", thumbnail: "100x100#"}
+  has_attached_file :photo, styles: { preview: "418x600#", small: "209x280#", thumbnail: "104x150#"}
 
   belongs_to :product, class_name: 'Spree::Product', foreign_key: :spree_product_id
 end
