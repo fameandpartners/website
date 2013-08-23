aeb449a2f3c5111ea76544c66cc5909d2fb6aab3
Evgeniy <malleus.petrov@gmail.com>
2013-08-23 18:05:06 +0400

    trying to avoid issue with double opening

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -86,6 +86,8 @@ Spree::CheckoutController.class_eval do
         return
       end
 
+      @order.reload
+
       if @order.state == 'complete' || @order.completed?
         flash.notice = t(:order_processed_successfully)
         flash[:commerce_tracking] = 'nothing special'
