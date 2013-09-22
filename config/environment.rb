515102b28a13ab4d06eff2adb2db2b04e2ad927f
Iskander Haziev <gvalmon@gmail.com>
2013-09-22 13:50:10 +0400

    reconnect sidekiq on passenger fork

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,14 @@ require File.expand_path('../application', __FILE__)
 FameAndPartners::Application.initialize!
 
 ActiveRecord::Base.include_root_in_json = true
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    if forked and defined?(Sidekiq)
+      Rails.logger.error('INFO: reconnect sidekiq')
+      Sidekiq.redis do |r|
+        r.client.reconnect
+      end
+    end
+  end
+end
