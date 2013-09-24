01e4e18564e38bcccf5b4b6b15075a1ed0b615df
Pavel Feklistov <p.feklistov@gmail.com>
2013-09-24 16:37:54 +0300

    fix show mum

--- app/mailers/spree/product_mailer.rb
+++ app/mailers/spree/product_mailer.rb
@@ -11,12 +11,16 @@ class Spree::ProductMailer < ActionMailer::Base
   def send_to_friend(product, friend_info)
     @product = product
     @friend_info = friend_info
-    @user_name = friend_info[:sender_name].titleize || friend_info[:sender_email]
-
+    @user_name = friend_info[:sender_name].to_s.titleize
+    subject = if @user_name.present?
+      "#{@user_name} wants you to approve of her dress at #{Spree::Config[:site_name]}"
+    else
+      "Review this dress at #{Spree::Config[:site_name]}"
+    end
     mail(
       from: friend_info[:sender_email],
       to: friend_info[:email],
-      subject: "#{@user_name} wants you to approve of her dress at #{Spree::Config[:site_name]}"
+      subject: subject
     )
   end
 end
