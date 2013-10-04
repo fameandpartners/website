6f33a806ebf4386ae725344f52d426fa46c13825
Pavel Feklistov <p.feklistov@gmail.com>
2013-10-04 11:25:57 +0300

    fame chain form

---
+++ app/controllers/fame_chains_controller.rb
@@ -0,0 +1,23 @@
+class FameChainsController < ApplicationController
+  before_filter :set_seo_meta
+
+  def new
+    @fame_chain = FameChain.new
+  end
+
+  def create
+    @fame_chain = FameChain.new(params[:fame_chain])
+    if @fame_chain.send_request
+      flash[:notice] = 'Yours request was successfully sent'
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def set_seo_meta
+    @title = "Join The Fame Chain - Fame & Partners"
+  end
+end
