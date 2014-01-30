644745fd6094eedff590253a48c639566db7ea7e
Evgeniy <malleus.petrov@gmail.com>
2014-01-30 14:43:51 +0400

    admin control panel moodboard items fixes

--- app/controllers/spree/admin/moodboard_items_controller.rb
+++ app/controllers/spree/admin/moodboard_items_controller.rb
@@ -2,7 +2,6 @@ module Spree
   module Admin
     class MoodboardItemsController < BaseController
       include Spree::Admin::ImagesHelper
-      respond_to :json
 
       def index
         render 'index', locals: { items: product.moodboard_items, product: product }
@@ -18,6 +17,30 @@ module Spree
           redirect_to admin_product_moodboard_items_path(product_id: product.permalink)
         else
           render 'index', locals: { items: product.moodboard_items, product: product, item: item }
+        end
+      end
+
+      def edit
+        item = product.moodboard_items.find(params[:id])
+        render 'edit', locals: { product: product, item: item }
+      end
+
+      def update
+        item = product.moodboard_items.find(params[:id])
+        item.assign_attributes(params[:moodboard_item])
+
+        respond_to do |format|
+          format.html do
+            item.save
+            render 'index', locals: { items: product.moodboard_items, product: product, item: item }
+          end
+          format.js do
+            if item.save
+              render 'update', locals: { product: product, item: item }
+            else
+              render 'edit', locals: { product: product, item: item }
+            end
+          end
         end
       end
 
