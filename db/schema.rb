55a4dc2b133dea23999dcbd588fe1f4873650e0f
Artem Budiansky <bydiansky@gmail.com>
2014-02-11 17:11:28 +0200

    new mailer logos

--- db/schema.rb
+++ db/schema.rb
@@ -296,17 +296,6 @@ ActiveRecord::Schema.define(:version => 20140210090904) do
 
   add_index "data_migrations", ["version"], :name => "unique_data_migrations", :unique => true
 
-  create_table "inspirations", :force => true do |t|
-    t.integer  "spree_product_id"
-    t.string   "name"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-  end
-
   create_table "line_item_personalizations", :force => true do |t|
     t.integer  "line_item_id"
     t.integer  "product_id"
