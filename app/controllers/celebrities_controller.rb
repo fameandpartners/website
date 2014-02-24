eb0a9d479296d8191ddfba9d43be7af11f0eaf7d
Evgeniy <malleus.petrov@gmail.com>
2014-02-25 03:01:55 +0400

    show celebrity accessories with products

--- app/controllers/celebrities_controller.rb
+++ app/controllers/celebrities_controller.rb
@@ -5,19 +5,6 @@ class CelebritiesController < ApplicationController
     @controller_action_id = 'products'
     @celebrity = Celebrity.find_by_slug(params[:id])
 
-=begin
-    @primary_image = @celebrity.primary_image
-    @secondary_images = @celebrity.secondary_images
-    @style_profile = @celebrity.style_profile
-    @products = @celebrity.products.active
-    @left_column, @right_column = @secondary_images.first(6).in_groups(2)
-=end
+    @products = @celebrity.products
   end
-
-  private
-
-  def colors
-    @colors ||= Products::ColorsSearcher.new(@products.to_a).retrieve_colors
-  end
-  helper_method :colors
 end
