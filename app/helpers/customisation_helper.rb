b6322a252a2857a0cc3bf3033a0c9817086165ab
eltonstewart <elton.stewart@gmail.com>
2014-03-06 19:01:20 +1100

    customisation fixes

--- app/helpers/customisation_helper.rb
+++ app/helpers/customisation_helper.rb
@@ -2,8 +2,6 @@ module CustomisationHelper
   def product_customisation_value_image(customisation_value)
     if customisation_value.image.present?
       image_url = customisation_value.image.url
-    elsif Rails.env.development?
-      image_url = 'http://placehold.it/240x240'
     end
     if image_url.present?
       image_tag(image_url, alt: customisation_value.presentation, title: customisation_value.presentation)
