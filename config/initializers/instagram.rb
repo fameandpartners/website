3fbd965fe6b81deedf8f6ca87b21ddb4e7adb8ac
Evgeniy <malleus.petrov@gmail.com>
2014-03-12 18:37:32 +0400

    updated accounts

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,2 +1,3 @@
-configatron.instagram.client_id = "b3dedbb2a4c04265ab09999305e1eea3"
-configatron.instagram.user_id   = "628069819" # should be 'self', and client id - from f&p app
+configatron.instagram.client_id     = "73f4b340c22e4244940e9b7077f31990"
+configatron.instagram.client_secret = "4ae5a96c0ca6499997247a5eade4bea1"
+configatron.instagram.user_id       = "628069819" # should be 'self', and client id - from f&p app
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-configatron.twitter.consumer_key        = "9CrsZ73XUMF19Sqv9akhw"
-configatron.twitter.consumer_secret     = "0Xj2nCqmWh6GnujKUddY6yJYkMHVTcPVTDXdF5BFwYQ"
-configatron.twitter.access_token        = "193256975-g07S2EKhBXymooIt0wzkNcVV8PNPNXlGdBbxFjtM"
-configatron.twitter.access_token_secret = "XuWMmeTO2giLOhHv77Uh80G8IjfR112TFF0TfbIILRqKc"
+configatron.twitter.consumer_key        = "fv7x8oXZZf2x8um15QlkA"
+configatron.twitter.consumer_secret     = "zpBn2XyP5CzLdme069ahOgPJeA3Te3NY2byAUZUeHM"
+configatron.twitter.access_token        = '1445555088-fLkQuFD3BMK7OhNemLxNy1aKjRezJ19teIZaUS5'
+configatron.twitter.access_token_secret = 'YzotL48zIYTccIFPiiUBh7ZtoF9sFNSwHLSpAx7X6hnP1'
