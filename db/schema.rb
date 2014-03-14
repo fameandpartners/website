9b19adfe2180b0fdb0c3a8f39c8efb9027b27568
eltonstewart <elton.stewart@gmail.com>
2014-03-14 18:09:25 +1100

    blog edits

--- db/schema.rb
+++ db/schema.rb
@@ -165,12 +165,14 @@ ActiveRecord::Schema.define(:version => 20140313131540) do
   add_index "blog_posts", ["user_id"], :name => "index_blog_posts_on_user_id"
 
   create_table "blog_preferences", :force => true do |t|
-    t.text     "value"
     t.string   "key"
+    t.text     "value"
     t.string   "value_type"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "blog_preferences", ["key"], :name => "index_blog_preferences_on_key"
 
   create_table "blog_promo_banners", :force => true do |t|
     t.string   "url"
@@ -248,6 +250,8 @@ ActiveRecord::Schema.define(:version => 20140313131540) do
     t.datetime "created_at",                           :null => false
     t.datetime "updated_at",                           :null => false
   end
+
+  add_index "celebrity_moodboard_items", ["side"], :name => "index_celebrity_moodboard_items_on_side"
 
   create_table "celebrity_product_accessories", :force => true do |t|
     t.integer  "celebrity_id"
@@ -343,23 +347,6 @@ ActiveRecord::Schema.define(:version => 20140313131540) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.decimal  "price",                 :precision => 8, :scale => 2
-  end
-
-  create_table "data_migrations", :id => false, :force => true do |t|
-    t.string "version", :null => false
-  end
-
-  add_index "data_migrations", ["version"], :name => "unique_data_migrations", :unique => true
-
-  create_table "inspirations", :force => true do |t|
-    t.integer  "spree_product_id"
-    t.string   "name"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
   end
 
   create_table "line_item_personalizations", :force => true do |t|
@@ -504,13 +491,13 @@ ActiveRecord::Schema.define(:version => 20140313131540) do
   create_table "product_videos", :force => true do |t|
     t.integer  "spree_product_id"
     t.integer  "spree_option_value_id"
-    t.boolean  "is_master",             :default => false
+    t.boolean  "is_master",                            :default => false
     t.string   "color"
-    t.string   "url"
+    t.string   "url",                   :limit => 512
     t.string   "video_id"
     t.integer  "position"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
+    t.datetime "created_at",                                              :null => false
+    t.datetime "updated_at",                                              :null => false
   end
 
   create_table "questions", :force => true do |t|
