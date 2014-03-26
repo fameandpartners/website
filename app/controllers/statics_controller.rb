bcded948cb14e3e8caa1cd3c818cd6911994a1e7
eltonstewart <elton.stewart@gmail.com>
2014-03-26 15:41:23 +1100

    enabled nylonxfame

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -6,11 +6,5 @@ class StaticsController < ApplicationController
   # enable showing of display banner
   before_filter :display_marketing_banner
 
-  def nylonxfame
-    if Rails.env.production?
-      redirect_to root_path
-    else
-      render layout: 'statics_fullscreen'
-    end
-  end
+  
 end
