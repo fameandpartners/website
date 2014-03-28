09523a19414ea8eb50fb5c4c8078fd0f7793d7ef
eltonstewart <elton.stewart@gmail.com>
2014-03-28 19:57:30 +1100

    added celebrities landing page

--- app/controllers/celebrities_controller.rb
+++ app/controllers/celebrities_controller.rb
@@ -1,6 +1,10 @@
 class CelebritiesController < ApplicationController
   layout 'spree/layouts/spree_application'
 
+  def index
+
+  end
+
   def show
     @controller_action_id = 'products'
     @celebrity = Celebrity.find_by_slug(params[:id])
