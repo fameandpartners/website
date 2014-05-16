8a73da985210f05912a1fc0400ed1f883cac0a4f
eltonstewart <elton.stewart@gmail.com>
2014-05-16 15:19:22 +1000

    added page titles to many pages improved some titles

--- app/controllers/celebrities_controller.rb
+++ app/controllers/celebrities_controller.rb
@@ -9,6 +9,9 @@ class CelebritiesController < ApplicationController
     @controller_action_id = 'products'
     @celebrity = Celebrity.find_by_slug(params[:id])
 
+    @title = @celebrity.full_name + " Celebrity Style - " + default_seo_title
+    @description = "25% off dresses inspired by " + @celebrity.full_name + ". " + ActionController::Base.helpers.strip_tags(@celebrity.body) + " " + default_meta_description
+
     @products = @celebrity.products
   end
 end
--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -8,7 +8,57 @@ class StaticsController < ApplicationController
 
   def nylonxfame
     render layout: 'statics_fullscreen'
+    @title = "About Fame & Partners"
   end
 
+  def about
+    @title = "About Us - " + default_seo_title
+    @description = "We design beautiful dresses for prom and many other occasions. We are a passionate team of fashionista's based in Sydney and NYC. " + default_meta_description
+  end
+
+  def team
+    @title = "Our Team - " + default_seo_title
+    @description = "Our great team. " + default_meta_description
+  end
+
+  def ecom_terms
+    @title = "Terms & Conditions - " + default_seo_title
+    @description = "Terms & Conditions. " + default_meta_description
+  end
+
+  def ecom_privacy
+    @title = "Privacy - " + default_seo_title
+    @description = "Privacy. " + default_meta_description
+  end
+
+  def legal
+    @title = "Legal - " + default_seo_title
+    @description = "Legal. " + default_meta_description
+  end
+
+  def faqs
+    @title = "FAQs - " + default_seo_title
+    @description = "FAQs. " + default_meta_description
+  end
+
+  def how_it_works
+    @title = "How it Works - " + default_seo_title
+    @description = "How it works. " + default_meta_description
+  end
+  
+  def nyemix
+    @title = "MUSIC MIX / I LIKE MY MUSIC LOUD - " + default_seo_title
+    @description = "Music Mix - Do the songs match the dress? Get a discount off dresses. " + default_meta_description
+  end
+
+  def renxfame
+    @title = "Rachel et Nicole Design Collaboration - " + default_seo_title
+    @description = "Rachel et Nicole Design Collaboration. We are so proud of this insanely adorable collaboration between Fame and Rachel et Nicole. " + default_meta_description
+  end
+
+  def lilyxfame
+    @title = "Lily Patchett Design Collaboration - " + default_seo_title
+    @description = "Lily Patchett Design Collaboration. " + default_meta_description
+  end
   
 end
