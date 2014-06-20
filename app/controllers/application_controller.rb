0075d9f87e7eee1555a03f90a1fbbc990fdc4384
eltonstewart <elton.stewart@gmail.com>
2014-06-20 13:14:35 +1000

    fixed page titles rem taxon name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -245,13 +245,17 @@ class ApplicationController < ActionController::Base
   helper_method :custom_dresses_path
 
   def set_product_show_page_title(product, info = "")
-    range_taxonomy ||= Spree::Taxonomy.where(name: 'Range').first
+    prefix = "#{product.short_description} #{info} #{@product.name}"
+    self.title = [prefix, default_seo_title].join(' - ')
+    description([prefix, default_meta_description].join(' - '))
 
-    if range_taxonomy.present? && range_taxon = @product.taxons.where(taxonomy_id: range_taxonomy.id).first
-      prefix = "#{product.short_description} #{info} #{@product.name} in #{range_taxon.name}"
-      self.title = [prefix, default_seo_title].join(' - ')
-      description([prefix, default_meta_description].join(' - '))
-    end
+#    range_taxonomy ||= Spree::Taxonomy.where(name: 'Range').first
+
+#    if range_taxonomy.present? && range_taxon = @product.taxons.where(taxonomy_id: range_taxonomy.id).first
+#      prefix = "#{product.short_description} #{info} #{@product.name} in #{range_taxon.name}"
+#      self.title = [prefix, default_seo_title].join(' - ')
+#     description([prefix, default_meta_description].join(' - '))
+#    end
   end
 
   helper_method :current_site_version, :site_versions_enabled?
