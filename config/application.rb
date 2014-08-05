e686afb6915c9247862a8c68fc04b855e16a1ae0
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2014-08-05 10:59:49 +0400

    Revert changes in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -80,9 +80,9 @@ module FameAndPartners
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
 
-    # redis_namespace = ['fame_and_partners', Rails.env, 'cache'].join('_')
-    #
-    # config.cache_store = :redis_store, "redis://10.100.94.127:6379/0/#{redis_namespace}"
+    redis_namespace = ['fame_and_partners', Rails.env, 'cache'].join('_')
+
+    config.cache_store = :redis_store, "redis://10.100.94.127:6379/0/#{redis_namespace}"
 
     config.generators do |generator|
       generator.test_framework :rspec
