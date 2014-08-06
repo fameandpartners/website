ca003bbd8e12b22407a4294f94a7d5c929d79fc5
eltonstewart <elton.stewart@gmail.com>
2014-08-06 14:52:05 +1000

    trying to change s3 to relative urls

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -8,7 +8,8 @@ if Rails.env.production?
       :access_key_id => configatron.aws.s3.access_key_id,
       :secret_access_key => configatron.aws.s3.secret_access_key
     },
-    s3_host_alias: configatron.aws.host
+    s3_host_alias: configatron.aws.host,
+    :s3_protocol => ""
   )
 else
   Paperclip::Attachment.default_options.merge!(
