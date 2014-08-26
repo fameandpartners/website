603c11a79b6b71f0f758391acb900868a114bbad
railsfactory-padmavathy <padmavathy@railsfactory.org>
2014-08-26 15:36:53 +0800

    finished homepage cms

--- app/models/spree/banner_box_decorator.rb
+++ app/models/spree/banner_box_decorator.rb
@@ -1,3 +1,22 @@
 Spree::BannerBox.class_eval do
-  attr_accessible :is_small
+  attr_accessible :is_small, :css_class, :title
+  has_attached_file :attachment,
+                :url  => "/spree/banners/:id/:style_:basename.:extension",
+                :path => ":rails_root/public/spree/banners/:id/:style_:basename.:extension",
+                :styles => lambda {|a|{
+                  :mini => "80x80#",
+                  :small => "358x500#",
+                  :big => "1140x500#",
+                  :custom => "#{a.instance.attachment_width}x#{a.instance.attachment_height}#"
+                }},
+                :convert_options => { :all => '-strip -auto-orient' }
+                
+  def self.big_banner
+    self.where('is_small =? and enabled =?',false,true).order('updated_at DESC').first
+  end  
+
+  def self.small_banner
+    self.where('is_small =? and enabled =?',true, true).order('position ASC')
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140901062407) do
+ActiveRecord::Schema.define(:version => 20140904044908) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
@@ -637,6 +637,8 @@ ActiveRecord::Schema.define(:version => 20140901062407) do
     t.datetime "created_at",                                 :null => false
     t.datetime "updated_at",                                 :null => false
     t.boolean  "is_small"
+    t.text     "css_class"
+    t.text     "title"
   end
 
   create_table "spree_calculators", :force => true do |t|
