c5925b0e2c531430bb046556976d27fe8a15b095
eltonstewart <elton.stewart@gmail.com>
2014-08-28 16:16:59 +1000

    increase number dresses in other plus size dresses section

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -21,7 +21,7 @@ class StaticsController < ApplicationController
     currency = current_currency
 
     @plus_size_dresses = get_products_from_edit('plus-size', currency, user, 11)
-    @plus_size_dresses_other = get_products_from_edit('plus-size-other', currency, user, 8)
+    @plus_size_dresses_other = get_products_from_edit('plus-size-other', currency, user, 16)
   end
 
   def nylonxfame
