b10cfeb03ab7653559fa68d7031f5265411ddf17
Marko Kacanski <marko.kacanski@gmail.com>
2014-11-04 21:23:35 +0100

    Moves previously unmoved taxons to style
    The error was caught when inspecting page title for 'split' taxon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141030224601) do
+ActiveRecord::Schema.define(:version => 20141104194804) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
