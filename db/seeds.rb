15e290d8dc2cbf9a067aa6b16798b95ce4e5900c
Evgeniy <malleus.petrov@gmail.com>
2014-11-17 22:29:07 +0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,18 @@ Spree::Core::Engine.load_seed if defined?(Spree::Core)
 user = Spree::User.new
 user.first_name = 'John'
 user.last_name = 'Doe'
-user.email = 'john.doe@userroot.com'
-user.password = '123456'
-user.password_confirmation = '123456'
+user.email = 'user@example.com'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.save
+puts "User user@example.com/password created"
+
+user = Spree::User.new
+user.first_name = 'John'
+user.last_name = 'Doe'
+user.email = 'admin@example.com'
+user.password = 'password'
+user.password_confirmation = 'password'
 user.save
 user.spree_roles << Spree::Role.find_by_name('admin')
+puts "Admin User admin@example.com/password created"
