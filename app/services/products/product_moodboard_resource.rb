201d20c4611e78978fc01671aeb8c0fd02da2657
Evgeniy <malleus.petrov@gmail.com>
2014-11-27 22:34:48 +0400

    updated moodboard resource : returns only data, not objects [faster, cleaner, cacheable ]

--- app/services/products/product_moodboard_resource.rb
+++ app/services/products/product_moodboard_resource.rb
@@ -15,23 +15,28 @@ class Products::ProductMoodboardResource
 
   private
 
+  def convert_item(item)
+    OpenStruct.new(image_url: item.image.url, source: item.source)
+  end
+
   def moodboard_items
     @moodboard_items ||= product.moodboard_items.active.to_a
   end
 
   def song_items
-    @song_items ||= moodboard_items.select{|item| item.item_type == 'song' }
+    @song_items ||= moodboard_items.select{|item| item.item_type == 'song' }.map{|item| convert_item(item)}
   end
 
   def parfume_items
-    @parfume_items ||= moodboard_items.select{|item| item.item_type == 'parfume' }
+    @parfume_items ||= moodboard_items.select{|item| item.item_type == 'parfume' }.map{|item| convert_item(item)}
   end
 
   def other_items
     @other_items ||= begin
       limit = 6 - (song_items.size + parfume_items.size )
       limit = 0 if limit <= 0
-      moodboard_items.select{|item| item.item_type != 'parfume' && item.item_type != 'song' }[0...limit]
+      items = moodboard_items.select{|item| item.item_type != 'parfume' && item.item_type != 'song' }[0...limit]
+      items.map{|item| convert_item(item)}
     end
   end
 end
