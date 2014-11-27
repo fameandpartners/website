0a5fd82474a13c5d1c5285f1a31921ad6702585e
Evgeniy <malleus.petrov@gmail.com>
2014-11-28 00:50:32 +0400

    added new palette to bridesmaid/colours

--- app/controllers/bridesmaid/colours_controller.rb
+++ app/controllers/bridesmaid/colours_controller.rb
@@ -28,6 +28,21 @@ class Bridesmaid::ColoursController < Bridesmaid::BaseController
       (bridesmaid_user_profile.colors || []).map{|c| c[:original_code]} || palette_colors.first
     end
 
+    # palette from wireframe
+    def palette_colors
+      @palette_colors ||= begin
+        colors  = %w{392E37 3D303C 413644 3A2E3E 2B2838 242231 22202D 231E2A 171621 10121B 0E1018 }
+        colors += %w{74555C A77B7B 9B7A7B 7C6976 685F6F 5B5666 4A4958 393B4D 292E3F 212736 1F202C }
+        colors += %w{DAA992 FDF4E8 F3DAC6 CEA99B B09590 9E8783 8F7674 7A6262 58474D 3F343C 2F2930 }
+        colors += %w{E9BC8E FDF5E7 F6E3C3 E6BF96 CEA184 B38B75 9C7965 896C5C 715A50 5E4B47 4E403E }
+        colors += %w{A3642E C3894E CE8B47 B8773A A16A35 946134 83572F 78502A 654425 5B3C25 523622 }
+        colors += %w{8C4510 A25112 A75713 984C11 884610 7A3F0F 6C380E 5D330D 46260A 3D220A 3A1E0A }
+        colors += %w{251101 331905 422107 4A2509 4D280A 522A09 502A0A 4D290B 4A260B 43240B 3B2009 }
+        colors.map{|code| "##{code}"}
+      end
+    end
+=begin
+    # grey colors palette
     def palette_colors
       @palette_colors ||= begin
         colors  = %w{111111 131313 161616 191919 222222 232323 272727 303030 333333 363636}
@@ -38,6 +53,18 @@ class Bridesmaid::ColoursController < Bridesmaid::BaseController
         colors.map{|code| "##{code}"}
       end
     end
+
+    # avaialble_color_options_palette
+    def palette_colors
+      @palette_colors ||= begin
+        option_type = Spree::OptionType.where(name: 'dress-color').first
+        option_type.option_values.
+          select{|option_value| option_value.value.present? }.
+          sort_by{|option_value| option_value.hsv_value }.
+          collect{|option_value| option_value.value }.compact
+      end
+    end
+=end
 
     def product_colors
       @product_colors ||= begin
