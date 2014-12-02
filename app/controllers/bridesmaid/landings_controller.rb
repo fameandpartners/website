79f752e35ded819f85afb90f1b16957ff464afbe
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2014-12-02 20:47:12 +0400

    Small changes for landing pages & add some validations to controller

--- app/controllers/bridesmaid/landings_controller.rb
+++ app/controllers/bridesmaid/landings_controller.rb
@@ -8,7 +8,22 @@ class Bridesmaid::LandingsController < Bridesmaid::BaseController
 
     user   = Spree::User.find_by_slug!(params[:user_slug])
     event  = BridesmaidParty::Event.find_by_spree_user_id!(user.id)
-    member = event.members.find_by_token!(params[:token])
+
+    if spree_user_signed_in? && params[:token].blank?
+      if current_spree_user.id == user.id
+        redirect_to bridesmaid_party_path
+      elsif !event.members.map(&:spree_user_id).include?(current_spree_user.id)
+        raise Bridesmaid::Errors::MoodboardAccessDenied
+      end
+    elsif params[:token].present?
+      member = event.members.find_by_token(params[:token])
+
+      if member.blank?
+        raise Bridesmaid::Errors::MoodboardAccessDenied
+      end
+    else
+      raise Bridesmaid::Errors::MoodboardAccessDenied
+    end
 
     @bride = user
   end
