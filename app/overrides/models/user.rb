60dbdfcb38d362f3116a4a281d7899d82cec327a
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2014-12-03 03:05:00 +0400

    Add ability to auth bridesmaids using token

--- app/overrides/models/user.rb
+++ app/overrides/models/user.rb
@@ -1,7 +1,7 @@
 Spree::User.class_eval do
   ALLOWED_AVATAR_FILE_TYPES = %w( image/jpg image/jpeg image/png image/gif )
   ALLOWED_AVATAR_FILE_SIZE = 0..1.megabytes
-  SIGN_UP_REASON = %w( custom_dress style_quiz workshop competition campaign_style_call customise_dress)
+  SIGN_UP_REASON = %w( custom_dress style_quiz workshop competition campaign_style_call customise_dress bridesmaid_party )
   SIGN_UP_VIA = %w( Email Facebook )
 
   attr_accessible :avatar, :slug, :description, :newsletter, :dob
@@ -61,5 +61,4 @@ Spree::User.class_eval do
     extension = File.extname(avatar_file_name).downcase
     self.avatar.instance_write(:file_name, "#{SecureRandom.hex(16)}#{extension}")
   end
-
 end
