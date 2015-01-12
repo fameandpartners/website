a651e6286aba970d0fe5cc0a217a99475b7986cb
Artem Budiansky <bydiansky@gmail.com>
2015-01-12 23:50:22 +0400

    markup bridesmaid promo mailer

--- app/mailers/bridesmaid_party_email_marketing_mailer.rb
+++ app/mailers/bridesmaid_party_email_marketing_mailer.rb
@@ -5,11 +5,11 @@ class BridesmaidPartyEmailMarketingMailer < ActionMailer::Base
   default :from => configatron.noreply, :template_path => 'mailers/bridesmaid_party_email_marketing_mailer'
 
   def share_completed_bridesmaid_profile(user_id, options = {})
-    mail(to: 'user@example.com', subject: 'It’s time to reveal your secret!')
+    mail(to: 'user@example.com', subject: "It’s time to reveal your secret!")
   end
 
   def bridesmaid_member_not_purchased(user_id, options = {})
-    mail(to: 'user@example.com', subject: 'Forgetting something? Tick "Bridesmaids dresses" off your list & purchase with 15% OFF!')
+    mail(to: 'user@example.com', subject: "Forgetting something? Tick 'Bridesmaids dresses' off your list & purchase with 15% OFF!")
   end
 
   def concierge_service_offer(user_id, options = {})
@@ -17,14 +17,14 @@ class BridesmaidPartyEmailMarketingMailer < ActionMailer::Base
   end
 
   def reminder_to_brides(user_id, options = {})
-    mail(to: 'user@example.com', subject: 'Bridesmaid Party Email Marketing')
+    mail(to: 'user@example.com', subject: "Now you have the ring, it's time for a dress! 15% OFF your engagement dress!")
   end
 
   def promo_for_bride_with_bridesmaids(user_id, options = {})
-    mail(to: 'user@example.com', subject: 'Bridesmaid Party Email Marketing')
+    mail(to: 'user@example.com', subject: "Help has arrived. Take 15% OFF your bridesmaids’ dresses & take away the stress!")
   end
 
   def free_styling_lesson_for_maid_of_honour(user_id, options = {})
-    mail(to: 'user@example.com', subject: 'Bridesmaid Party Email Marketing')
+    mail(to: 'user@example.com', subject: "Bridesmaid Party Email Marketing")
   end
 end
