2f268da1b017d8357b96175011fa043351bf7e88
Evgeniy <malleus.petrov@gmail.com>
2015-01-13 13:05:24 +0400

    backup & custom field changer fro campaignmonitor

--- lib/campaign_monitor/field_type_updater.rb
+++ lib/campaign_monitor/field_type_updater.rb
@@ -1,18 +1,37 @@
 # this file needed to update field type from 'multiselect' => 'text'
 # unfortunately, campaign monitor doesn't have api to change custom field settings [ other than name ]
 # usage
-#   load File.join(Rails.root, 'lib', 'campaign_monitor', 'field_type_updater')
+#   load File.join(Rails.root, 'lib', 'campaign_monitor', 'field_type_updater.rb')
 #   FieldTypeUpdater.new.update
+#{
+#  "FieldName"=>"Sign up reason",
+#   "Key"=>"[Signupreason]",
+#   "DataType"=>"MultiSelectOne",
+#   "FieldOptions"=> ["Custom dress", "Style quiz", "Workshop", "Competition", "Dolly campaign", "Customise dress", "Campaign Style Call", "Newsletter Modal"],
+# "VisibleInPreferenceCenter"=>false
+#},
 
 class FieldTypeUpdater
   attr_reader :list_id, :api_key
 
-  def initialize
-    #@api_key = configatron.campaign_monitor.api_key
-    #@list_id = configatron.campaign_monitor.list_id
-    @api_key = 'f9d32d1a8b31a53fb4b09a9c064e6562'
-    @list_id = '63175b5b43d65a82201cba1df16daae3'
+  def production
+    true
   end
+
+  def initialize
+    if production
+      # real
+      @api_key = '3f7e4ac86b143e32a5c7b46b83641143'
+      @list_id = 'cc9be877f40c64cf389f6e3ea95daa0a'
+    else
+      # evgenp
+      @api_key = 'f9d32d1a8b31a53fb4b09a9c064e6562'
+      @list_id = '63175b5b43d65a82201cba1df16daae3'
+    end
+
+    init
+  end
+
 
   def update
     field_name = 'Sign up reason'
@@ -23,7 +42,7 @@ class FieldTypeUpdater
 
     # update field A with signup reason value
     copy_values(from: field_name, to: holder_field_name)
-
+    
     # remove old custom field
     delete_field(field_name)
 
@@ -39,8 +58,8 @@ class FieldTypeUpdater
 
   private
 
-    def list
-      @list ||= CreateSend::List.new('63175b5b43d65a82201cba1df16daae3')
+    def init
+      CreateSend.api_key api_key
     end
 
     def list
@@ -55,8 +74,16 @@ class FieldTypeUpdater
       source = get_field_id_by_name(options[:from])
       target = get_field_id_by_name(options[:to])
 
-      each_subscriber do |subscriber_data|
-        value = subscriber_data.CustomFields.find{|f| f.Key == source}.Value
+      start_time = Time.now.to_f
+
+      each_subscriber do |subscriber_data, index|
+        puts "#{ (Time.now.to_f - start_time).to_s }: processing #{ index } record" if index % 100 == 0
+
+        value = subscriber_data.CustomFields.find{|f| f.Key == source}.Value rescue ''
+        current_value = subscriber_data.CustomFields.find{|f| f.Key == target}.Value rescue ''
+
+        next if (value.blank? || current_value == value)
+
         custom_fields = [{ "Key" => target, "Value" => value }]
 
         subscriber = CreateSend::Subscriber.new(list_id, subscriber_data.EmailAddress)
@@ -84,8 +111,9 @@ class FieldTypeUpdater
       subscribers_count = list.stats["TotalActiveSubscribers"]
       pages_count = (subscribers_count / 1000.0).ceil
       (1..pages_count).each do |page_num|
-        list.active('', page_num, 1_000)['Results'].each do |subscriber|
-          original_block.call(subscriber)
+        list.active('', page_num, 1_000)['Results'].each_with_index do |subscriber, index|
+          subscriber_position = 1000 * (page_num - 1) + index
+          original_block.call(subscriber, subscriber_position)
         end
       end
     end
