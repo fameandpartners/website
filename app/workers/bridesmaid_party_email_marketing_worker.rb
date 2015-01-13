c0f9a0909ca11c96691a7fd5898910043d141e9a
Evgeniy <malleus.petrov@gmail.com>
2015-01-14 02:44:55 +0400

    have finished bridesmaid email marketing chain

--- app/workers/bridesmaid_party_email_marketing_worker.rb
+++ app/workers/bridesmaid_party_email_marketing_worker.rb
@@ -1,6 +1,6 @@
 class BridesmaidPartyEmailMarketingWorker < BaseEmailMarketingWorker
   def perform(code, user_id, options = {})
-    return if already_send?(code, user_id)
+    return if already_sent?(code, user_id)
     send_email(code, user_id, options)
     mark_as_sent!(code, user_id)
   rescue Exception => e
@@ -9,15 +9,15 @@ class BridesmaidPartyEmailMarketingWorker < BaseEmailMarketingWorker
 
   private
 
-  def already_sent?(code, user_id)
-    EmailNotification.where(code: code, spree_user_id: user_id).exists?
-  end
+    def already_sent?(code, user_id)
+      EmailNotification.where(code: code, spree_user_id: user_id).exists?
+    end
 
-  def mark_as_sent!(code, user_id)
-    EmailNotification.where(code: code, spree_user_id: user_id).first_or_create
-  end
+    def mark_as_sent!(code, user_id)
+      EmailNotification.where(code: code, spree_user_id: user_id).first_or_create
+    end
 
-  def send_email(code, user_id, options = {})
-    BridesmaidPartyEmailMarketingMailer.send(code, user_id, options).deliver
-  end
+    def send_email(code, user_id, options = {})
+      BridesmaidPartyEmailMarketingMailer.send(code, user_id, options).deliver
+    end
 end
