f010b9fc1d07020e0fcbcc7515aa29202dc4cf2c
Evgeniy <malleus.petrov@gmail.com>
2015-01-13 19:32:52 +0400

    finished promo for bride email

--- app/mailers/bridesmaid_party_email_marketing_mailer.rb
+++ app/mailers/bridesmaid_party_email_marketing_mailer.rb
@@ -39,10 +39,18 @@ class BridesmaidPartyEmailMarketingMailer < ActionMailer::Base
 
   def promo_for_bride_with_bridesmaids(user_id, options = {})
     user = Spree::User.find(user_id)
-    mail(
-      to: user.email,
-      subject: "Help has arrived. Take 15% OFF your bridesmaids' dresses & take away the stress!"
-    )
+    @bridesmaids_count = options[:bridesmaids_count]
+    @bridesmaids_count ||= user.bridesmaid_party_events.order('bridesmaids_count desc').first.try(:bridesmaids_count).to_i
+
+    if @bridesmaids_count < 3
+      subject = "Help has arrived. Take 15% OFF your bridesmaids' dresses & take away the stress!"
+    elsif @bridesmaids_count < 5
+      subject = "Help has arrived. Take 20% OFF your bridesmaids' dresses & take away the stress!"
+    else
+      subject = "Help has arrived. Take 25% OFF your bridesmaids' dresses & take away the stress!"
+    end
+
+    mail(to: user.email, subject: subject)
   end
 
   def free_styling_lesson_for_maid_of_honour(user_id, options = {})
