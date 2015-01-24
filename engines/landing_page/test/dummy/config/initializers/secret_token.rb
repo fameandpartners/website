06e5b0324c80e04e3c40cab03f10a8f7fd138336
Toby Hede <tobyhede@info-architects.net>
2015-01-25 09:15:39 +1100

    Delete test app

--- engines/landing_page/test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'c36146ec4187480cae5d970341cfac08120ce2702734172176487c33f9e20fa05915dcfc4873501faeacc5a62532db26c82cc508f400658fc8560e9332b8053c'
--- engines/landing_page/test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount LandingPage::Engine => "/landing_page"
-end
