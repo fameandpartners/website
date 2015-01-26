cfb32a092f9cd8dcbc73d76ae68d242137100d6f
Evgeniy <malleus.petrov@gmail.com>
2015-01-26 13:52:44 +0400

    [feat][mailer] added timeout processing errors to bridesmaid emails delivery
    if email didn't delivered due to timeout (smtp server too busy ), then we can safely re-schedule work. or reset 'delivered' state

--- app/workers/bridesmaid_party_email_marketing_worker.rb
+++ app/workers/bridesmaid_party_email_marketing_worker.rb
@@ -1,8 +1,10 @@
 class BridesmaidPartyEmailMarketingWorker < BaseEmailMarketingWorker
   def perform(code, user_id, options = {})
     return if already_sent?(code, user_id)
-    send_email(code, user_id, options)
     mark_as_sent!(code, user_id)
+    send_email(code, user_id, options)
+  rescue Timeout::Error => e
+    mark_as_not_delivered!(code, user_id)
   rescue Exception => e
     log_mailer_error(e)
   end
@@ -15,6 +17,10 @@ class BridesmaidPartyEmailMarketingWorker < BaseEmailMarketingWorker
 
     def mark_as_sent!(code, user_id)
       EmailNotification.where(code: code, spree_user_id: user_id).first_or_create
+    end
+
+    def mark_as_not_delivered!(code, user_id)
+      EmailNotification.where(code: code, spree_user_id: user_id).delete_all
     end
 
     def send_email(code, user_id, options = {})
