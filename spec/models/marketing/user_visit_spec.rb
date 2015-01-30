8ea0d483e96d2b0ca1c6811ce7d39d6c8a9a2a6a
Evgeniy <malleus.petrov@gmail.com>
2015-01-31 02:17:15 +0400

    fix typo for spec

--- spec/models/marketing/user_visit_spec.rb
+++ spec/models/marketing/user_visit_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe UserVisit, :type => :model do
+RSpec.describe Marketing::UserVisit, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
