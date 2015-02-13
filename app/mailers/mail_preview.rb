56c9f05dec39f4689cfd083169655a76171bd59e
Toby Hede <tobyhede@info-architects.net>
2015-02-13 14:46:45 +1100

    [fix][mail preview] Hide mail_preview in production env
    Class has dependency on gem only in dev & test environments

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,8 +1,10 @@
-class MailPreview < MailView
+if Rails.env.development? || Rails.env.test?
+  class MailPreview < MailView
 
-  # Stub-like
-  def marketing_email
-    user = Struct.new(:email, :name).new('name@example.com', 'Jill Smith')
-    mail = Spree::CompetitionsMailer.marketing_email(user.email)
+    # Stub-like
+    def marketing_email
+      user = Struct.new(:email, :name).new('name@example.com', 'Jill Smith')
+      mail = Spree::CompetitionsMailer.marketing_email(user.email)
+    end
   end
 end
