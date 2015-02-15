d849a23aec4d04fddb5a9cbec790ce2f1694f74f
Evgeniy <malleus.petrov@gmail.com>
2015-02-15 23:53:23 +0300

    added custom errors for products collection/details

--- app/controllers/products/base_controller.rb
+++ app/controllers/products/base_controller.rb
@@ -1,2 +1,8 @@
 class Products::BaseController < ApplicationController
+  rescue_from Errors::ProductInactive, with: :redirect_to_default_collection
+  rescue_from Errors::ProductNotFound, with: :redirect_to_default_collection
+
+  def redirect_to_default_collection
+    redirect_to dresses_path, notice: "Sorry, we can't found this dress"
+  end
 end
