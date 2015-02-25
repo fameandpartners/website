797625d8c33f0bf729ccae9c3e359a261d7dd794
Evgeniy <malleus.petrov@gmail.com>
2015-02-25 19:51:47 +0300

    [collection][collection details] update banner title / text / image for collection page - it can be slighty differs from previous code, but i have limited code-read abilities

--- app/services/products/collection_details.rb
+++ app/services/products/collection_details.rb
@@ -38,11 +38,11 @@ class Products::CollectionDetails
     OpenStruct.new(
       title: title,
       description: description,
-      footer: title,
+      #footer: title,
       banner: OpenStruct.new(
-        title: title,
-        description: description,
-        image: ''
+        title: banner_title,
+        description: banner_description,
+        image: banner_image
       )
     )
   end
@@ -59,8 +59,18 @@ class Products::CollectionDetails
         sale_all:   "View all dresses on sale",
         sale:       "View all dresses with %{ discount }% off",
         default:    "Shop and customize the best %{color} %{style} %{event} dress trends %{event} at Fame & Partners."
+      },
+      banner_title: {
+        #default:    "Fame & Partners Formal Dresses",
+        sale_all:   "All Dresses on Sale",
+        sale:       "Sale %{ discount }% Off",
+        default:    "%{color} %{style} %{event} dresses"
+      },
+      banner_description: {
+        sale_all:   'View all dresses on sale',
+        sale:       "View all dresses with %{ discount }% off",
+        default:    "High fashion dresses."
       }
-
     }
   end
 
@@ -72,6 +82,18 @@ class Products::CollectionDetails
 
     def description
       @description ||= get_description
+    end
+
+    def banner_title
+      @banner_title ||= get_banner_title
+    end
+
+    def banner_description
+      @banner_description ||= get_banner_description
+    end
+
+    def banner_image
+      @banner_image ||= get_banner_image
     end
 
     # formal color or not.
@@ -164,5 +186,82 @@ class Products::CollectionDetails
         style: style.present? ? style.name.titleize : '',
         event: event.present? ? event.name.titleize : 'any event'
       }
+
+      default_description.gsub!(/\s{1,}/, ' ').capitalize # remove double spaces
+    end
+
+
+    # shortly
+    #   - if sales : show sale-specific
+    #
+    #   The way titles should work:
+    #     Black prom dresses //colour
+    #     Black strapless formal dresses //colour + style
+    #     Black strapless homecoming dresses // colour + style + event
+    #
+    def get_banner_title
+      if discount.present?
+        if discount.to_sym == :all
+          return templates[:banner_title][:sale_all]
+        else
+          return (templates[:banner_title][:sale] % discount.to_i)
+        end
+      end
+
+      if edits.present?
+        return edits.name.capitalize
+      end
+
+      if collection_taxons.present?
+        taxon = collection_taxons.find{|t| t.banner.present? && t.banner.title.present? }
+        return taxon.banner.title if taxon.present?
+      end
+
+      default_banner_title = templates[:banner_title][:default] % {
+        color: is_formal_dress_color?(color) ? color.name.titleize : nil,
+        style: style.present? ? style.name.titleize : '',
+        event: event.present? ? event.name.titleize : 'any event'
+      }
+
+      default_banner_title.gsub!(/\s{1,}/, ' ').capitalize # remove double spaces
+    end
+
+    def get_banner_description
+      if discount.present?
+        if discount.to_sym == :all
+          return templates[:banner_description][:sale_all]
+        else
+          return (templates[:banner_description][:sale] % discount.to_i)
+        end
+      end
+
+      text = if edits.present?
+        edits.banner.try(:description)
+      elsif collection.present?
+        collection.banner.try(:description)
+      else
+        empty_collection.banner.try(:description)
+      end
+
+      # todo: find text
+      return text if text.present?
+
+      if is_formal_dress_color?(color)
+        I18n.t(:subtitle, scope: [:collection, :colors, color.name.to_s.parameterize.underscore])
+      else
+        templates[:banner_description][:default]
+      end
+    end
+
+    def get_banner_image
+      return nil if is_formal_dress_color?(color)
+
+      if edits.present?
+        edits.banner.image
+      elsif collection.present?
+        collection.banner.image
+      else
+        empty_collection.banner.image
+      end
     end
 end
