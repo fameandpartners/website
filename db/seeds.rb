f472820bcaafe5d0122c21b183b0f403e606760d
Evgeniy <malleus.petrov@gmail.com>
2015-03-07 01:19:13 +0300

    commented out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+puts "-" * 100
+puts "sorry, but seeds for this project obsoleted hundreds years ago"
+puts "-" * 100
+=begin
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 
 user = Spree::User.where(email: 'user@example.com').first_or_initialize
@@ -17,3 +21,4 @@ user.password_confirmation = 'password'
 user.save
 user.spree_roles << Spree::Role.find_by_name('admin')
 puts "Admin User admin@example.com/password created"
+=end
