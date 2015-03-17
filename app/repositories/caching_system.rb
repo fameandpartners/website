a4b0d62bf466bc1a31588f8ce6be7417bb6ff085
Toby Hede <tobyhede@info-architects.net>
2015-03-17 14:32:01 +1100

    Code loading

--- app/repositories/caching_system.rb
+++ app/repositories/caching_system.rb
@@ -1,55 +1,55 @@
 require 'active_support/concern'
 
-module Repositories
-  module CachingSystem
-    extend ActiveSupport::Concern
+module Repositories; end
 
-    # global settings
-    # Repositories::CachingSystem.cache_fetch_params
-    def self.cache_fetch_params(options = {})
-      result = { 
-        force: options.delete(:force)
-      }
+module Repositories::CachingSystem
+  extend ActiveSupport::Concern
 
-      if Rails.env.development?
-        result[:expires_in] = configatron.cache.expire.quickly
-      else
-        result[:expires_in] = configatron.cache.expire.long
-      end
+  # global settings
+  # Repositories::CachingSystem.cache_fetch_params
+  def self.cache_fetch_params(options = {})
+    result = {
+      force: options.delete(:force)
+    }
 
-      result
+    if Rails.env.development?
+      result[:expires_in] = configatron.cache.expire.quickly
+    else
+      result[:expires_in] = configatron.cache.expire.long
     end
 
-    included do
-      # do something here
-    end
+    result
+  end
 
-    module ClassMethods
-      def cache_results(method_name)
-        # note: in without cache we should reset cache!
-        define_method "#{ method_name }_with_cache" do |options = {}|
-          Rails.cache.fetch(cache_key, cache_fetch_params(options)) do
-            call_method("#{ method_name }_without_cache", options)
-          end
+  included do
+    # do something here
+  end
+
+  module ClassMethods
+    def cache_results(method_name)
+      # note: in without cache we should reset cache!
+      define_method "#{ method_name }_with_cache" do |options = {}|
+        Rails.cache.fetch(cache_key, cache_fetch_params(options)) do
+          call_method("#{ method_name }_without_cache", options)
         end
-        alias_method_chain method_name, :cache
       end
+      alias_method_chain method_name, :cache
     end
+  end
 
-    def call_method(name, *arguments)
-      if self.class.instance_method(name).arity == 0
-        send(name)
-      else
-        send(name, *arguments)
-      end
+  def call_method(name, *arguments)
+    if self.class.instance_method(name).arity == 0
+      send(name)
+    else
+      send(name, *arguments)
     end
+  end
 
-    def reset_cache!
-      Rails.cache.delete(cache_key)
-    end
+  def reset_cache!
+    Rails.cache.delete(cache_key)
+  end
 
-    def cache_fetch_params(options = {})
-      Repositories::CachingSystem.cache_fetch_params(options)
-    end
+  def cache_fetch_params(options = {})
+    Repositories::CachingSystem.cache_fetch_params(options)
   end
 end
