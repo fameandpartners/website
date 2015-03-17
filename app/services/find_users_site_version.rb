b48808ec582cda25d53fcb41756a7df1a2224270
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-03-17 15:45:59 +1100

    Correct spelling of chosen

--- app/services/find_users_site_version.rb
+++ app/services/find_users_site_version.rb
@@ -9,31 +9,31 @@ class FindUsersSiteVersion
   end
 
   def get
-    sv_choosen_by_user || sv_choosen_by_cookie || sv_choosen_by_location || sv_choosen_by_param || SiteVersion.default
+    sv_chosen_by_user || sv_chosen_by_cookie || sv_chosen_by_location || sv_chosen_by_param || SiteVersion.default
   end
 
   private
 
-  def sv_choosen_by_user
+  def sv_chosen_by_user
     if user && user.site_version_id.present?
       SiteVersion.find_by_id(user.site_version_id)
     end
   end
 
-  def sv_choosen_by_cookie
+  def sv_chosen_by_cookie
     if cookie_param.present?
       SiteVersion.find_by_permalink(cookie_param)
     end
   end
 
-  def sv_choosen_by_location
+  def sv_chosen_by_location
     version_permalink = fetch_user_country_code
     site_version = SiteVersion.find_by_permalink(version_permalink)
 
     site_version
   end
 
-  def sv_choosen_by_param
+  def sv_chosen_by_param
     if url_param.present?
       SiteVersion.find_by_permalink(url_param)
     end
--- spec/services/find_users_site_version_spec.rb
+++ spec/services/find_users_site_version_spec.rb
@@ -6,26 +6,26 @@ describe FindUsersSiteVersion do
 
     let!(:site_version) { create(:site_version) }
 
-    it "returns site version choosen by user previously" do
+    it "returns site version chosen by user previously" do
       user = create(:spree_user, site_version_id: site_version.id)
 
       service = FindUsersSiteVersion.new(user: user)
       expect(service.get).to eq(site_version)
     end
 
-    it "returns site version choosen by guest user" do
+    it "returns site version chosen by guest user" do
       sv = create(:site_version)
 
       service = FindUsersSiteVersion.new(cookie_param: sv.permalink)
       expect(service.get).to eq(sv)
     end
 
-    it "returns site version choosen by remote ip"
+    it "returns site version chosen by remote ip"
 
-    it "returns site version choosen by passing a param" do
+    it "returns site version chosen by passing a param" do
       sv = create(:site_version)
       service = FindUsersSiteVersion.new(url_param: sv.permalink)
-      allow(service).to receive(:sv_choosen_by_location).and_return(nil)
+      allow(service).to receive(:sv_chosen_by_location).and_return(nil)
 
       expect(service.get).to eq(sv)
     end
@@ -35,7 +35,7 @@ describe FindUsersSiteVersion do
       allow(SiteVersion).to receive(:default).and_return(sv)
 
       service = FindUsersSiteVersion.new({})
-      allow(service).to receive(:sv_choosen_by_location).and_return(nil)
+      allow(service).to receive(:sv_chosen_by_location).and_return(nil)
 
       expect(service.get).to eq(sv)
     end
