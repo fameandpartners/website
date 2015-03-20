58d9eea760b5952eac1411c28b7eb0df81aabf2a
Evgeniy <malleus.petrov@gmail.com>
2015-03-20 19:43:29 +0300

    [fix][products] replaed empty places with coloured box for missing moodboard item

--- app/presenters/products/presenter.rb
+++ app/presenters/products/presenter.rb
@@ -102,10 +102,14 @@ module Products
     #  [ song  | item | item ]
     #  [ celeb | item | item ]
     def inspiration
-      [ 
+      result = [ 
        moodboard.items.slice(0,2) || [],
        moodboard.items.slice(2,2) || []
-      ].compact      
+      ].compact
+      # fill to have exactly two items in row
+      result[0] += Array.new(2 - result[0].size)
+      result[1] += Array.new(2 - result[1].size)
+      result
     end
 
     def preorder?
