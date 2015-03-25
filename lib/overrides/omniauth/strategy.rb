e69af08dd4060547bff9bd5d87c0da74dd13ed2f
Evgeniy <malleus.petrov@gmail.com>
2015-03-25 22:31:04 +0300

    [fix] oauth2 paths fix

--- lib/overrides/omniauth/strategy.rb
+++ lib/overrides/omniauth/strategy.rb
@@ -3,6 +3,7 @@ module OmniAuth
     class Facebook
       option :client_options, {
          site: 'https://graph.facebook.com/v2.0',
+         token_url: '/oauth/access_token',
          authorize_url: "https://www.facebook.com/v2.0/dialog/oauth"
       }
 
