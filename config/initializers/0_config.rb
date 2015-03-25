77da03455a7069ef73646399afb05cdc2110aa78
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-03-25 15:02:25 +1100

    [fix][s3] seriously AWS?

--- config/initializers/0_config.rb
+++ config/initializers/0_config.rb
@@ -71,7 +71,7 @@ when :preproduction
   configatron.aws.s3.access_key_id = 'AKIAJ7U3MBOEHSMUAOHQ'
   configatron.aws.s3.secret_access_key = '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'
 
-  configatron.aws.host = "s3-us-west-2.amazonaws.com/preprod-fameandpartners/"
+  configatron.aws.host = "s3-us-west-2.amazonaws.com/preprod-fameandpartners"
 
   redis_host = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env][:hosts]
   configatron.redis_options = { namespace: 'fame_and_partners', url: "redis://#{redis_host}/0" }
