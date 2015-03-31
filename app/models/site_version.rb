f28da730ccfbf1897fc40677b99c9bd9e5295764
Evgeniy <malleus.petrov@gmail.com>
2015-03-31 15:44:53 +0300

    [feat][site_versions] cache exchange rate in classvar - probably, we can hardcode it to 1.0

--- app/models/site_version.rb
+++ app/models/site_version.rb
@@ -160,16 +160,22 @@ class SiteVersion < ActiveRecord::Base
     #   get_exchange_rate(EUR, USD) => is EUR -> AUD -> USD
     #   base / rate_to_default_currency * rate_to_required_currency
     def get_exchange_rate(from, to)
-      if from == Spree::Config.currency
-        rate_to_default_currency = 1
-      else
-        rate_to_default_currency = SiteVersion.where(currency: from).first.try(:exchange_rate) || 1
-      end
-      rate_to_required_currency = SiteVersion.where(currency: to).first.try(:exchange_rate) || 1
+      rate_to_default_currency = (from == Spree::Config.currency) ? 1.0 : currency_rates[from]
+      rate_to_required_currency = currency_rates[to]
 
       return (1.0 / rate_to_default_currency) * rate_to_required_currency
     rescue Exception => e
-      1
+      1.0
+    end
+
+    def currency_rates
+      @exchange_rates ||= begin
+        result = HashWithIndifferentAccess.new(){ 1.0 }
+        SiteVersion.all.each do |site_version|
+          result[site_version.currency] = site_version.exchange_rate.to_f
+        end
+        result
+      end
     end
   end
 end
