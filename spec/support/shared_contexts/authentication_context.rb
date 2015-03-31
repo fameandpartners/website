17b5ef84db716f702804951de0995dd471f1e6d5
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-03-31 11:28:56 +1100

    [fix][spec] remove old expected login page context

--- spec/support/shared_contexts/authentication_context.rb
+++ spec/support/shared_contexts/authentication_context.rb
@@ -1,17 +1,14 @@
 shared_context 'authenticated_user' do
 
   let(:user) { create(:spree_user, :skip_welcome_email => true) }
-  
-  before do 
+
+  before do
     visit '/us/login'
     within('#password-credentials') do
       fill_in 'Email', :with => user.email
       fill_in 'Password', :with => user.password
     end
-    click_button 'Log-in'        
-    expect(page).to_not have_content 'Invalid email or password.'        
-    expect(page).to have_content 'sign out'
-    expect(page).to have_content user.full_name
+    click_button 'Login'
+    expect(page).to_not have_content 'Invalid email or password.'
   end
-
 end
