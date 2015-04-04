e57dbcfdb37d4572d7beb9b9faad2b901af2bf81
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-04-04 19:28:36 +1100

    [feat][marketing] add known Promo Codes to modal generator

--- app/controllers/spree/admin/modals_controller.rb
+++ app/controllers/spree/admin/modals_controller.rb
@@ -4,8 +4,12 @@ module Spree
 
       def index
       end
+
+      helper_method :promo_codes
+
+      def promo_codes
+        @promo_codes ||= Spree::Promotion.where('usage_limit > 1').where('expires_at < ?', Date.current ).collect { |x| [x.code, "#{x.code} - #{x.description}"] }
+      end
     end
   end
 end
-
-
