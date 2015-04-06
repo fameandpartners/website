97c9457fb80348af64c185262a337e4dd2820885
Evgeniy <malleus.petrov@gmail.com>
2015-04-07 02:46:54 +0300

    deactived style_quiz on main site. will work on separate page

--- config/initializers/features.rb
+++ config/initializers/features.rb
@@ -1,10 +1,11 @@
 # Turns on global features
 require 'redis'
-if Rails.env.production?
-  Features.deactivate(:style_quiz)
-else
-  Features.activate(:style_quiz)
-end
+#if Rails.env.production?
+#  Features.deactivate(:style_quiz)
+#else
+#  Features.activate(:style_quiz)
+#end
+Features.deactivate(:style_quiz)
 Features.deactivate(:collection_content)
 Features.deactivate(:maintenance)
 Features.deactivate(:express_deliveries)
