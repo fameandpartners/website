993d5a9a523a028f8db75ff736d5ecf1d8f8ce2a
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-04-07 15:03:43 +1000

    [feat][payments] add spree NAB gateway class, basic tests

--- spec/integration/nab_transact_integration_spec.rb
+++ spec/integration/nab_transact_integration_spec.rb
@@ -7,40 +7,67 @@ require 'spec_helper'
 
 describe 'NABTransact' do
   context 'Fame Test Credentials' do
-    let(:one_dollar) { 100 }
+    # This just lets us track test transactions a bit easier in the NAB interface.
+    let(:whodunnit) { "#{ENV['USER']}@#{Socket.gethostname}-#{Process.pid}" }
 
-    let(:test_username) { 'G9C0010'  }
-    let(:test_password) { '3kbn8AO1' }
+    let(:one_dollar)      { 100 }
+    let(:order_reference) { "#{gateway_slug}-#{whodunnit}" }
+    let(:test_username)   { 'G9C0010'  }
+    let(:test_password)   { '3kbn8AO1' }
+    let(:test_mode)       { true }
 
-    let(:order_reference_number) { '3682261795659690329' }
-
-
-    let(:valid_test_card) {
+    let(:visa_test_card) {
       ActiveMerchant::Billing::CreditCard.new(
           :first_name => 'Fame',
           :last_name  => 'Babe',
           :month      => '9',
-          :year       => '2016',
+          :year       => 3.years.from_now.year.to_s,
           :brand      => 'visa',
           :number     => '4444333322221111'
       )
     }
-    let(:gateway) {
-      ActiveMerchant::Billing::NabTransactGateway.new login: test_username, password: test_password, test: true
-    }
 
     subject(:response) do
-      gateway.purchase(one_dollar, valid_test_card, order_id: order_reference_number)
+      gateway.purchase(one_dollar, valid_test_card, order_id: order_reference)
     end
 
+    shared_examples :processes_test_transactions do
+      it 'processes a test transaction' do
+        expect(response.params['response_code']).to     eq '00'
+        expect(response.params['response_text']).to     eq 'Approved'
+        expect(response.params['merchant_id']).to       eq test_username
+        expect(response.params['amount']).to            eq one_dollar.to_s
+        expect(response.params['purchase_order_no']).to eq order_reference
+        expect( Date.strptime(response.params['settlement_date'], "%Y%m%d")).to be_within(1.day).of(Date.today)
+      end
+    end
 
-    it 'can process test transactions' do
-      expect(response.params['response_code']).to     eq '00'
-      expect(response.params['response_text']).to     eq 'Approved'
-      expect(response.params['merchant_id']).to       eq test_username
-      expect(response.params['amount']).to            eq one_dollar.to_s
-      expect(response.params['purchase_order_no']).to eq order_reference_number
-      expect( Date.strptime(response.params['settlement_date'], "%Y%m%d")).to be_within(1.day).of(Date.today)
+    context ActiveMerchant::Billing::NabTransactGateway do
+      let(:gateway_slug) { "AM:B:NTGW" }
+
+      let(:gateway) {
+        ActiveMerchant::Billing::NabTransactGateway.new(
+            login:    test_username,
+            password: test_password,
+            test:     test_mode
+        )
+      }
+
+      include_examples :processes_test_transactions
+    end
+
+    context 'Spree::Gateway::NabTransactGateway' do
+      let(:gateway_slug) { "S:G:NTGW" }
+
+      let(:gateway) {
+        Spree::Gateway::NabTransactGateway.new(
+            preferred_login:     test_username,
+            preferred_password:  test_password,
+            preferred_test_mode: test_mode
+        )
+      }
+
+      include_examples :processes_test_transactions
     end
   end
 end
