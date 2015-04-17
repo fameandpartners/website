2aa9b2714b1a487cc25278b35a90b2986675c6fb
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-04-17 21:02:26 +1000

    formatting

--- app/repositories/user_cart.rb
+++ app/repositories/user_cart.rb
@@ -1,25 +1,25 @@
 module Repositories
-class UserCart
-  attr_reader :accessor, :order, :site_version
+  class UserCart
+    attr_reader :accessor, :order, :site_version
 
-  def initialize(options = {})
-    @accessor = options[:accessor]
-    @order    = options[:order]
-    @site_version = options[:site_version]
-  end
+    def initialize(options = {})
+      @accessor     = options[:accessor]
+      @order        = options[:order]
+      @site_version = options[:site_version]
+    end
 
-  def read
-    OpenStruct.new(
-      items: order_items
-    )
-  end
+    def read
+      OpenStruct.new(
+        items: order_items
+      )
+    end
 
-  private
+    private
 
     def order_items
       order.line_items.map do |line_item|
         Repositories::CartItem.new(line_item: line_item).read
       end
     end
-end
+  end
 end
