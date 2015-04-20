8bb39a3806fe3f7fc915af91c922bfee839d013b
Tiago Amaro <tiagocis@gmail.com>
2015-04-20 19:25:24 -0300

    [test] tests for collection SEO data for the products/collection_controller#show method

--- spec/controllers/products/collections_controller_spec.rb
+++ spec/controllers/products/collections_controller_spec.rb
@@ -35,4 +35,44 @@ describe Products::CollectionsController, :type => :controller do
       end
     end
   end
+
+  describe 'private methods' do
+    let(:collection_details) { double('Collection Details', meta_title: 'My Title', seo_description: 'My Description') }
+    let(:collection_double) { double('Collection', details: collection_details) }
+
+    describe '#set_collection_seo_meta_data' do
+      before(:each) do
+        controller.instance_variable_set(:@collection, collection_double)
+      end
+
+      describe 'sets the @title, @description, @status and @canonical of a collection' do
+        it 'has the @title and @description' do
+          controller.send(:set_collection_seo_meta_data)
+
+          expect(controller.instance_variable_get(:@title)).to eq('My Title | - Fame & Partners')
+          expect(controller.instance_variable_get(:@description)).to eq('My Description')
+        end
+
+        context 'collection has options (a collection was found)' do
+          it 'has @status :ok and @canonical is nil' do
+            controller.instance_variable_set(:@collection_options, {})
+            controller.send(:set_collection_seo_meta_data)
+
+            expect(controller.instance_variable_get(:@status)).to eq(:ok)
+            expect(controller.instance_variable_get(:@canonical)).to be_nil
+          end
+        end
+
+        context 'collection has no options (a invalid collection was queried)' do
+          it 'has @status :not_found and @canonical has the dresses_path' do
+            controller.instance_variable_set(:@collection_options, nil)
+            controller.send(:set_collection_seo_meta_data)
+
+            expect(controller.instance_variable_get(:@status)).to eq(:not_found)
+            expect(controller.instance_variable_get(:@canonical)).to eq(dresses_path)
+          end
+        end
+      end
+    end
+  end
 end
