5e9c372f86ff3a06d1d410e95060ee26a17808ce
Toby Hede <tobyhede@info-architects.net>
2015-04-21 10:50:46 +1000

    [fix] Enhance collection spec to not depend on spree config

--- app/controllers/products/collections_controller.rb
+++ app/controllers/products/collections_controller.rb
@@ -36,9 +36,6 @@ class Products::CollectionsController < Products::BaseController
 
     @collection = collection_resource.read
 
-    @title = "#{@collection.details.meta_title} #{default_seo_title}"
-    @description  = @collection.details.seo_description
-
     respond_to do |format|
       format.html { render :show, status: @status }
       format.json do
@@ -55,8 +52,8 @@ class Products::CollectionsController < Products::BaseController
 
     def set_collection_seo_meta_data
       # set title / meta description / HTTP status / canonical for the page
-      title(@collection.details.meta_title, default_seo_title)
-      @description = @collection.details.seo_description
+      @title = "#{@collection.details.meta_title} #{default_seo_title}"
+      @description  = @collection.details.seo_description
       @status = @collection_options ? :ok : :not_found
       @canonical = dresses_path if @status == :not_found
     end
