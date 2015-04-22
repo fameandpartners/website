dac79d6998f442932cd064d8e339b3882a3ec8fd
Toby Hede <tobyhede@info-architects.net>
2015-04-22 13:28:18 +1000

    [test] Fix test dependency on config

--- spec/controllers/products/search_results_controller_spec.rb
+++ spec/controllers/products/search_results_controller_spec.rb
@@ -4,7 +4,7 @@ describe Products::SearchResultsController, :type => :controller do
   describe 'GET show' do
     it 'sets its title based on the q param' do
       get :show, q: 'My Query'
-      expect(assigns(:title)).to eq('Search results for "My Query" | - Fame & Partners')
+      expect(assigns(:title)).to include('Search results for "My Query"')
     end
   end
 end
