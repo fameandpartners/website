0a92df6a2587cb0b49af2b51c633a2d46efc9ed7
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-04-24 00:37:38 +1000

    [copy][email] new subject for shipping email, style tweaks

--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -1,3 +1,10 @@
 Spree::ShipmentMailer.class_eval do
   add_template_helper(OrdersHelper)
+
+  def shipped_email(shipment, resend = false)
+    @shipment = shipment.is_a?(Spree::Shipment) ? shipment : Spree::Shipment.find(shipment)
+    subject = (resend ? "[#{t(:resend).upcase}] " : '')
+    subject += "Hey babe, your dress is on it's way - Order: ##{@shipment.order.number}"
+    mail(:to => @shipment.order.email, :from => from_address, :subject => subject)
+  end
 end
