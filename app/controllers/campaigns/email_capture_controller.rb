5b95a9912a5577f9278378aff345b1c9dd154445
Evgeniy <malleus.petrov@gmail.com>
2015-04-28 15:33:16 +0300

    [chore][campaign-monitor] removed dead code

--- app/controllers/campaigns/email_capture_controller.rb
+++ app/controllers/campaigns/email_capture_controller.rb
@@ -1,6 +1,4 @@
 class Campaigns::EmailCaptureController < ApplicationController
-  #respond_to :html, :js
-
   def new
     respond_to do |format|
       format.js { render partial: 'form', layout: false, status: :ok }
@@ -20,21 +18,4 @@ class Campaigns::EmailCaptureController < ApplicationController
   rescue Exception => e
     render :json => { status: 'invalid' }, status: :error
   end
-
-#  # create
-#  def create
-#    @participation = Participation.new(:email => params[:email], :content => params[:email])
-#
-#    if @participation.valid?
-#      CampaignMonitor.delay.synchronize(@participation.email, nil, Signupreason: "email_capture_#{@participation.content}")
-#      render :json => { status: 'ok' }, status: :ok
-#    else
-#      render :json => { status: 'invalid' }, status: :error
-#    end
-#
-#    # respond_to do |format|
-#    #   format.html { render partial: 'form', layout: false }
-#    #   format.js   { render: 'json' => {   }, status: :ok }
-#    # end
-#  end
 end
