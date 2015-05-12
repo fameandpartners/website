8efe11e9f8d04f90196e3bc2f295373fec136344
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-05-12 10:43:42 +1000

    [fix][db] migrations and factories for Fabric models
    SQUASH fix tests

--- spec/models/fabric_card_spec.rb
+++ spec/models/fabric_card_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe FabricCard, :type => :model do
-  it { is_expected.to validate_presence_of :name          }
-  it { is_expected.to validate_presence_of :sku_component }
+  it { is_expected.to validate_presence_of :name }
 end
