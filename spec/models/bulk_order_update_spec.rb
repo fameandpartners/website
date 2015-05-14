b2f44095947c0701fb017788b0c03c5261d49afe
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-05-14 17:40:46 +1000

    [fix][specs] class namespace

--- spec/models/bulk_order_update_spec.rb
+++ spec/models/bulk_order_update_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe BulkOrderUpdate, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+module Admin
+  RSpec.describe BulkOrderUpdate, :type => :model do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
 end
