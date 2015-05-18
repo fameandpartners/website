0554dd6901fec369b3410b0d9eabe0bd55230e66
Tiago Amaro <tiagocis@gmail.com>
2015-05-18 18:23:27 -0300

    [ref] simplify locales regex

--- app/services/localize_url_service.rb
+++ app/services/localize_url_service.rb
@@ -1,8 +1,9 @@
 class LocalizeUrlService
   class << self
     def localize_url(url, site_version)
-      locales_regex = SiteVersion.permalinks.join('|')
-      url_without_locale = url.gsub(/\/(#{locales_regex})/, '')
+      locales_paths = SiteVersion.permalinks.map { |sv| "/#{sv}" }
+      locales_regex = Regexp.union(locales_paths)
+      url_without_locale = url.gsub(locales_regex, '')
       uri = URI.parse(url_without_locale)
       URI.join("#{uri.scheme}://#{uri.host}:#{uri.port}", "./#{site_version.to_param}/", "./#{uri.path}").to_s
     end
