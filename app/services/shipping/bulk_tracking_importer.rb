f1cdeabea911a18452d75b7a91de00444a03a0cd
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-05-18 20:51:47 +1000

    refactor import cruft to simpler methods

--- app/services/shipping/bulk_tracking_importer.rb
+++ app/services/shipping/bulk_tracking_importer.rb
@@ -19,35 +19,31 @@ module Shipping
 
       new_bulk_update = Admin::BulkOrderUpdate.new(user: username, filename: original_filename)
 
-      # headers = ["ORDER RELEASE DATE  下单日期",
-      #  "ORDER NO. 订单号",
-      #  "STYLE  款号",
-      #  "SIZE   尺码                                                      AU/US",
-      #  "QUANTITY 数量",
-      #  "COLOR 颜色",
-      #  "PARCEL SENT DATE  寄件日期",
-      #  "TRACKING NO. 运单号",
-      #  "客人姓名",
-      #  "地址",
-      #  "电话"]
-      #
-      mapped_headers = {}
-      header_detectors.map do |name, detector|
-        next unless header = csv.headers.detect { |h| h[detector] }
-        mapped_headers[name] = header
-      end
+      header_mapping = mapped_headers(csv)
 
       csv.each_with_index do |row, idx|
-        attributes = mapped_headers.each_with_object({}) { |(name, header), attrs|
-          attrs[name] = row[header].to_s.strip
-        }
-
+        attributes = extract_attributes_from_row(row, header_mapping)
         attributes.merge!(row_number: idx + 1)
 
         new_bulk_update.line_item_updates << Admin::LineItemUpdate.new(attributes)
       end
 
       new_bulk_update
+    end
+
+    private
+
+    def extract_attributes_from_row(row, mapping)
+      mapping.each_with_object({}) do |(attr_name, header), attrs|
+        attrs[attr_name] = row[header].to_s.strip
+      end
+    end
+
+    def mapped_headers(csv)
+      header_detectors.each_with_object({}) do |(name, detector), mapping|
+        next unless header = csv.headers.detect { |h| h[detector] }
+        mapping[name] = header
+      end
     end
 
     def header_detectors
@@ -64,5 +60,18 @@ module Shipping
         raw_line_item:   /line item/i,
       }
     end
+
+    # headers = ["ORDER RELEASE DATE  下单日期",
+    #  "ORDER NO. 订单号",
+    #  "STYLE  款号",
+    #  "SIZE   尺码 AU/US",
+    #  "QUANTITY 数量",
+    #  "COLOR 颜色",
+    #  "PARCEL SENT DATE  寄件日期",
+    #  "TRACKING NO. 运单号",
+    #  "客人姓名",
+    #  "地址",
+    #  "电话"]
+    #
   end
 end
