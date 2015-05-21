d5f28fcb62757b21cff1b6f5c68083b6f07f7e79
Toby Hede <tobyhede@info-architects.net>
2015-05-21 16:38:15 +1000

    [fix] Hande bad uris in localizer

--- app/services/localize_url_service.rb
+++ app/services/localize_url_service.rb
@@ -1,11 +1,15 @@
 class LocalizeUrlService
   class << self
     def localize_url(url, site_version)
-      locales_paths = SiteVersion.permalinks.map { |sv| "/#{sv}" }
-      locales_regex = Regexp.union(locales_paths)
-      url_without_locale = url.gsub(locales_regex, '')
-      uri = URI.parse(url_without_locale)
-      URI.join("#{uri.scheme}://#{uri.host}:#{uri.port}", "./#{site_version.to_param}/", "./#{uri.path}").to_s
+      begin
+        locales_paths = SiteVersion.permalinks.map { |sv| "/#{sv}" }
+        locales_regex = Regexp.union(locales_paths)
+        url_without_locale = url.gsub(locales_regex, '')
+        uri = URI.parse(url_without_locale)
+        URI.join("#{uri.scheme}://#{uri.host}:#{uri.port}", "./#{site_version.to_param}/", "./#{uri.path}").to_s
+      rescue URI::InvalidURIError => e
+        url
+      end
     end
   end
 end
