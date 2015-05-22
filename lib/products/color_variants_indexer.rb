c2356921c03106f4350655a0599250ec3af119a6
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-05-22 19:30:29 +1000

    [main][indexing] fallback for products without crop images

--- lib/products/color_variants_indexer.rb
+++ lib/products/color_variants_indexer.rb
@@ -92,10 +92,19 @@ module Products
     end
 
     def self.cropped_images_for(color_variant)
-      color_variant.images
-        .select { |i| i.attachment_file_name.downcase.include? 'crop' }
-        .sort_by(&:position)
-        .collect { |i| i.attachment.url(:large) }
+      find_images = ->(matcher, item) do
+        item.images
+          .select { |i| i.attachment_file_name.downcase.include? matcher }
+          .sort_by(&:position)
+          .collect { |i| i.attachment.url(:large) }
+      end
+
+      cropped_images = find_images.call('crop', color_variant)
+
+      if cropped_images.blank?
+        cropped_images = find_images.call('front', color_variant)
+      end
+      cropped_images
     end
   end
 end
