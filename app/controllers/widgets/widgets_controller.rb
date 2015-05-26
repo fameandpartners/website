b83ca2d65a0351e1ccfe5e93e4d1b8d1c1472c43
Ruslan <ruslan.gatiyatov@gmail.com>
2015-05-26 16:03:45 +0300

    [#284] set links

--- app/controllers/widgets/widgets_controller.rb
+++ app/controllers/widgets/widgets_controller.rb
@@ -1,3 +1,5 @@
 class Widgets::WidgetsController < ActionController::Base
   include Concerns::SiteVersion
+  include Spree::Core::ControllerHelpers::Order
+  include Spree::Core::ControllerHelpers::Auth
 end
