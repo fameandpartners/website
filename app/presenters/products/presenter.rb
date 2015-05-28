84e7b6ee8547024b7ce3b6bf01d8741317c6b854
Evgeniy <malleus.petrov@gmail.com>
2015-05-28 13:10:07 +0300

    [feat] make express option selected by default

--- app/presenters/products/presenter.rb
+++ app/presenters/products/presenter.rb
@@ -5,7 +5,7 @@ module Products
                   :permalink, :is_active, :images, :default_image, :price,
                   :discount, :recommended_products, :available_options, :preorder,
                   :moodboard, :fabric, :style_notes, :color_id, :color_name, :color,
-                  :size_chart
+                  :size_chart, :making_option_id
 
     def initialize(opts)
       opts.each do |k, v|
