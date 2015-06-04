acb342943c066ee4acacad385994ca6fc329ddf7
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-04 17:48:11 +1000

    [ref][admin] move variant generation into ColourVariantGenerator

--- app/controllers/spree/admin/product_colors_controller.rb
+++ app/controllers/spree/admin/product_colors_controller.rb
@@ -7,7 +7,7 @@ module Spree
       end
 
       def create
-        @variants = create_variants(params[:sizes], params[:colors])
+        @variants = ColourVariantGenerator.new(product: @product).create_variants(params[:sizes], params[:colors])
 
         flash.notice  = 'Variants successfully added'
         redirect_to admin_product_variants_path(@product)
@@ -24,59 +24,67 @@ module Spree
         @size_option  = Spree::OptionType.where(name: 'dress-size').first
       end
 
-      def create_variants(sizes_ids, colors_ids)
-        variants = []
-        size_option_values = @size_option.option_values.where(id: sizes_ids)
-        color_option_values = @color_option.option_values.where(id: colors_ids)
+      class ColourVariantGenerator
+        def initialize(product: product)
+          @product = product
+          @color_option = Spree::OptionType.where(name: 'dress-color').first
+          @size_option  = Spree::OptionType.where(name: 'dress-size').first
+        end
 
-        master_variant = @product.master
-        excluded_attributes = %w{id created_at deleted_at sku is_master count_on_hand}
+        def create_variants(sizes_ids, colors_ids)
+          variants = []
+          size_option_values = @size_option.option_values.where(id: sizes_ids)
+          color_option_values = @color_option.option_values.where(id: colors_ids)
 
-        size_option_values.each do |size_option_value|
-          color_option_values.each do |color_option_value|
-            unless product_have_size_and_color?(size_option_value, color_option_value)
-              variant = @product.variants.build(master_variant.attributes.except(*excluded_attributes))
+          master_variant = @product.master
+          excluded_attributes = %w{id created_at deleted_at sku is_master count_on_hand}
 
-              variant.default_price = Spree::Price.new(
-                amount: master_variant.default_price.amount,
-                currency: master_variant.default_price.currency
-              )
-              other_prices = master_variant.prices - Array(master_variant.default_price)
-              other_prices.each do |price|
-                variant.prices << Spree::Price.new(amount: price.amount, currency: price.currency).tap do |new_price|
-                  new_price.variant = variant
+          size_option_values.each do |size_option_value|
+            color_option_values.each do |color_option_value|
+              unless product_have_size_and_color?(size_option_value, color_option_value)
+                variant = @product.variants.build(master_variant.attributes.except(*excluded_attributes))
+
+                variant.default_price = Spree::Price.new(
+                  amount: master_variant.default_price.amount,
+                  currency: master_variant.default_price.currency
+                )
+                other_prices = master_variant.prices - Array(master_variant.default_price)
+                other_prices.each do |price|
+                  variant.prices << Spree::Price.new(amount: price.amount, currency: price.currency).tap do |new_price|
+                    new_price.variant = variant
+                  end
                 end
+                variant.save
+
+                variant.option_values = [size_option_value, color_option_value]
+
+                variants.push(variant)
               end
-              variant.save
-
-              variant.option_values = [size_option_value, color_option_value]
-
-              variants.push(variant)
             end
+          end
+
+          return variants
+        end
+
+        def get_product_default_price(product)
+          if product.price
+            product.prices.first
+          elsif product.master.default_price
+            product.master.default_price
+          else
+            product.variants.map(&:price).compact.first
           end
         end
 
-        return variants
-      end
-
-      def get_product_default_price(product)
-        if product.price
-          product.prices.first
-        elsif product.master.default_price
-          product.master.default_price
-        else
-          product.variants.map(&:price).compact.first
+        def product_options
+          @product_options ||= Products::VariantsReceiver.new(@product).available_options
         end
-      end
 
-      def product_options
-        @product_options ||= Products::VariantsReceiver.new(@product).available_options
-      end
-
-      def product_have_size_and_color?(size_option_value, color_option_value)
-        size  = size_option_value.name
-        color = color_option_value.name
-        product_options.any?{|option| option[:size] == size && option[:color] == color }
+        def product_have_size_and_color?(size_option_value, color_option_value)
+          size  = size_option_value.name
+          color = color_option_value.name
+          product_options.any?{|option| option[:size] == size && option[:color] == color }
+        end
       end
     end
   end
