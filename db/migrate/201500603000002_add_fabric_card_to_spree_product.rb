06c6e46602c0a89c676376d1e12fb5ae7bd928ed
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-04 10:24:03 +1000

    [fix][db] make fabric-product migration reversible

--- db/migrate/201500603000002_add_fabric_card_to_spree_product.rb
+++ db/migrate/201500603000002_add_fabric_card_to_spree_product.rb
@@ -1,7 +1,11 @@
 class AddFabricCardToSpreeProduct < ActiveRecord::Migration
-  def change
+  def up
     change_table :spree_products do |t|
       t.references :fabric_card
     end
+  end
+
+  def down
+    remove_column :spree_products, :fabric_card_id
   end
 end
