feea79743ecf2be280e0c4048798aca6cdf559c3
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-09 21:40:59 +1000

    [fix][fabrications] move guard check for duplicate state events into service

--- app/services/update_fabrication.rb
+++ app/services/update_fabrication.rb
@@ -1,11 +1,13 @@
 class UpdateFabrication
-  def self.state_change(line_item_id, user, state)
+  def self.state_change(line_item_id, user, new_state)
     fabrication = Fabrication.for(line_item(line_item_id))
+
+    return if fabrication.state == new_state
 
     fabrication.events.state_change.create!(
         user_id:   user.id,
         user_name: user.email,
-        state:     state
+        state:     new_state
     )
   end
 
