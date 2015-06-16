9f0f192b9f3abcda6c89e704cbe5f9e59cc3f555
Ruslan <ruslan.gatiyatov@gmail.com>
2015-06-16 15:47:02 +0300

    add utm campaign and medium to email capturing

--- app/controllers/campaigns/email_capture_controller.rb
+++ app/controllers/campaigns/email_capture_controller.rb
@@ -1,11 +1,12 @@
 class Campaigns::EmailCaptureController < ApplicationController
   def create
     Marketing::Subscriber.new(
-      token: cookies['utm_guest_token'],
-      email: params[:email],
-      campaign: params[:content],
-      promocode: params[:promocode],
-      sign_up_reason: "email_capture_#{params[:content]}"
+      token:          cookies['utm_guest_token'],
+      email:          params[:email],
+      campaign:       params[:content],
+      promocode:      params[:promocode],
+      sign_up_reason: "email_capture_#{params[:content]}",
+      user:           current_spree_user
     ).create
 
     render :json => { status: 'ok' }, status: :ok
