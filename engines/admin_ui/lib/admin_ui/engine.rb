8fbf5a30e43e3e4211e08b989fff99548116938d
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-19 14:09:31 +1000

    [fix][assets] include admin_ui and inspinia assets in compilation.

--- engines/admin_ui/lib/admin_ui/engine.rb
+++ engines/admin_ui/lib/admin_ui/engine.rb
@@ -1,5 +1,10 @@
 module AdminUi
   class Engine < ::Rails::Engine
     isolate_namespace AdminUi
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w{ admin_ui.css }
+      Rails.application.config.assets.precompile += %w{ admin_ui.js }
+      Rails.application.config.assets.paths << root.join("app", "assets", "images")
+    end
   end
 end
