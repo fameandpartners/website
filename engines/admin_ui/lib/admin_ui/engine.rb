8bcddca17942cd53d334b12512b4f0228b5365cb
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-19 17:38:46 +1000

    [fix][assets] Put engine asset initialiser in all group to ensure loading

--- engines/admin_ui/lib/admin_ui/engine.rb
+++ engines/admin_ui/lib/admin_ui/engine.rb
@@ -1,10 +1,10 @@
 module AdminUi
   class Engine < ::Rails::Engine
     isolate_namespace AdminUi
-    initializer :assets do |config|
-      Rails.application.config.assets.precompile += %w{ admin_ui.css }
-      Rails.application.config.assets.precompile += %w{ admin_ui.js }
-      Rails.application.config.assets.paths << root.join("app", "assets", "images")
+    initializer :admin_ui_assets, :group => :all do |app|
+      app.config.assets.precompile += %w{ admin_ui.css }
+      app.config.assets.precompile += %w{ admin_ui.js }
+      app.config.assets.paths << root.join("app", "assets", "images")
     end
   end
 end
