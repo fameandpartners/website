492d96aabf7d6e090bdfdad85fba2aea3b3a5185
Tiago Amaro <tiagocis@gmail.com>
2015-06-21 21:26:30 -0300

    [fix] use `#node` and `#app` methods from the global config variable (ey hooks)

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,6 +1,5 @@
 on_app_master do
   sudo "monit restart sidekiq_#{config.app}_0"
 
-  run("cd #{config.release_path} && bundle exec newrelic deployment --license-key=#{node[:newrelic_key]} --appname='#{app}' ")
-
+  run("cd #{config.release_path} && bundle exec newrelic deployment --license-key=#{config.node[:newrelic_key]} --appname='#{config.app}' ")
 end
