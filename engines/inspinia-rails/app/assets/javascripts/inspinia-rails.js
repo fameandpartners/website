aabbb0b48e3b2b46f4de8cfcd91eb404ca253ff3
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-22 15:53:00 +1000

    re-enable jquery & bootstrap

--- engines/inspinia-rails/app/assets/javascripts/inspinia-rails.js
+++ engines/inspinia-rails/app/assets/javascripts/inspinia-rails.js
@@ -10,8 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-// require jquery/jquery-2.1.1.js
-// require bootstrap-sprockets.js
+//= require jquery/jquery-2.1.1.js
+//= require bootstrap-sprockets.js
 //= require metisMenu/jquery.metisMenu.js
 //= require pace/pace.min.js
 //= require slimscroll/jquery.slimscroll.min.js
