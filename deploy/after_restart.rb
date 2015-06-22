1bf9c24da1040006f79c6eb8fb3b85ba27e22b93
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-22 12:48:49 +1000

    read newrelic config out of yml

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,8 +2,15 @@ on_app_master do
   sudo "monit restart sidekiq_#{config.app}_0"
 
   begin
-    run("cd #{config.release_path} && bundle exec newrelic deployment --license-key=#{config.node[:newrelic_key]} --appname='#{config.app}' ")
+    nr_config = YAML.load(File.read(File.join(config.release_path, 'config', 'newrelic.yml' )))
 
+    if nr_config
+      newrelic_api_key = nr_config['common']['license_key']
+      app_name         = nr_config['common']['app_name']
+      user             = config.deployed_by
+      revision         = config.active_revision
+      run("cd #{config.release_path} && bundle exec newrelic deployment --license-key=#{newrelic_api_key} --appname='#{app_name}' --user=#{user}  --revision=#{revision} ")
+    end
   rescue StandardError => e
     puts e.message
   end
