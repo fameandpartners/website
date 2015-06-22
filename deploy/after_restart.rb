fd65ed84cf9245e874c0343f7d5948b5c21e87b2
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-22 12:24:29 +1000

    garrow be better at defensive programming

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,6 +1,11 @@
 on_app_master do
   sudo "monit restart sidekiq_#{config.app}_0"
 
-  run("cd #{config.release_path} && bundle exec newrelic deployment --license-key=#{node[:newrelic_key]} --appname='#{app}' ")
+  begin
+    run("cd #{config.release_path} && bundle exec newrelic deployment --license-key=#{config.node[:newrelic_key]} --appname='#{config.app}' ")
+
+  rescue StandardError => e
+    puts e.message
+  end
 
 end
