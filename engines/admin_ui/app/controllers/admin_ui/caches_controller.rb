a3607fe85280b0c491b09e523ea812d927bdce95
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-06-29 19:20:36 +1000

    Add admin UI for rails cache, links for sidekiq

---
+++ engines/admin_ui/app/controllers/admin_ui/caches_controller.rb
@@ -0,0 +1,51 @@
+module AdminUi
+  class CachesController < AdminUi::ApplicationController
+
+    include ActionView::Helpers::TextHelper
+    def index
+      @stats = Rails.cache.stats.to_h
+      @cache_keys = cache_client.keys.sort
+    end
+
+    def destroy
+      message = if params[:id].present?
+        key     = Base64.urlsafe_decode64(params[:id])
+        deleted = cache_client.del(key)
+
+        ::NewRelic::Agent.record_custom_event(
+          'cache.expire.item',
+          user:           current_admin_user.email,
+          item_key:       key,
+          deletion_count: deleted
+        )
+        "Deleted #{pluralize(deleted, 'item')}"
+      else
+        "Nothing Deleted"
+      end
+
+      redirect_to caches_path, flash: {notice: message}
+    end
+
+    def expire
+      message = if params[:really_expire] == 'EXPIRE'
+        deleted = Rails.cache.clear
+
+        ::NewRelic::Agent.record_custom_event(
+          'cache.expire.full',
+          user:           current_admin_user.email,
+          deletion_count: deleted
+        )
+
+        "Expired #{pluralize(deleted, 'item')}"
+      else
+        "Didn't Expire"
+      end
+      redirect_to caches_path, flash: {notice: message}
+    end
+
+    private
+    def cache_client
+      Rails.cache.instance_variable_get(:@data)
+    end
+  end
+end
