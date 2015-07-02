01a23b0951b9dbd001de28433449dcb8f0050b36
Toby Hede <tobyhede@info-architects.net>
2015-07-02 11:35:36 +1000

    [feat] Update es config for bonsai

--- config/initializers/0_config.rb
+++ config/initializers/0_config.rb
@@ -15,6 +15,8 @@ configatron.asset_host = "assets.fameandpartners.com"
 
 configatron.typekit_id = 'kur6crm'
 
+configatron.es.url = 'http://localhost:9200'
+
 configatron.links do |links|
   links.twitter   = 'https://twitter.com/fameandpartners'
   links.facebook  = 'http://www.facebook.com/FameandPartners'
@@ -130,7 +132,7 @@ when :preproduction
   redis_host = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env][:hosts]
   configatron.redis_options = { namespace: 'fame_and_partners', url: "redis://#{redis_host}/0" }
 
-  configatron.es_url YAML::load(File.open("#{Rails.root}/config/elasticsearch.yml"))[Rails.env][:hosts]
+  configatron.es_url 'https://z9h24eavpg:6cygbrjpmh@preproduction-1404693529.us-east-1.bonsai.io'
 
   configatron.asset_host = "assets.fameandpartners.com/preprod"
 
@@ -150,7 +152,8 @@ when :production
   redis_host = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env][:hosts]
   configatron.redis_options = { namespace: 'fame_and_partners', url: "redis://#{redis_host}/0" }
 
-  configatron.es_url YAML::load(File.open("#{Rails.root}/config/elasticsearch.yml"))[Rails.env][:hosts]
+  # configatron.es_url YAML::load(File.open("#{Rails.root}/config/elasticsearch.yml"))[Rails.env][:hosts]
+  configatron.es.url = 'https://b13gy7hlm3:brc6ozc6oi@production-4224690387.us-east-1.bonsai.io'
 
   configatron.customerio.site_id = 'a416731201185e0c6f5f'
   configatron.typekit_id = 'day0prb'
