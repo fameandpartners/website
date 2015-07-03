a9e2797b6ac312fec903e13606fa9c28dde5b648
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-07-03 11:58:13 +1000

    remove flaky spec

--- spec/controllers/mysterious_route_controller_spec.rb
+++ spec/controllers/mysterious_route_controller_spec.rb
@@ -20,17 +20,3 @@ RSpec.describe MysteriousRouteController, type: :routing do
     expect(get: "/au/undefined").to route_to(controller)
   end
 end
-
-RSpec.describe 'Product "undefined" Redirection', type: :request do
-
-  before do
-    seed_site_zone
-    Revolution::Page.create!(:path => '/dresses/*', :template_path => '/products/collections/show.html.slim').publish!
-  end
-
-  context 'undefined urls on product collections' do
-    it_will :redirect,
-            "/dresses/plus-size/undefined",
-            "/undefined"
-  end
-end
