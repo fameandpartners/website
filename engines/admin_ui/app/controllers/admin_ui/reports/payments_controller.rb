061dc316b9285abd140701ddd426b4431cfad4ef
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-07-07 12:09:28 +1000

    unify reports controllers internal API

--- engines/admin_ui/app/controllers/admin_ui/reports/payments_controller.rb
+++ engines/admin_ui/app/controllers/admin_ui/reports/payments_controller.rb
@@ -3,27 +3,31 @@ module AdminUi
     class PaymentsController < AdminUi::ApplicationController
 
       def show
-        @rows = model_class.new.take(10).map &:to_h
+        @rows = report.take(10).map &:to_h
       end
 
       # Streaming CSV download.
       # See: http://smsohan.com/blog/2013/05/09/genereating-and-streaming-potentially-large-csv-files-using-ruby-on-rails/
       def create
-        file_name                      = "payments_up_to_#{DateTime.now.to_s(:filename)}.csv"
         headers["Content-Type"]        = "text/csv"
-        headers["Content-disposition"] = "attachment; filename=\"#{file_name}\""
+        headers["Content-disposition"] = "attachment; filename=\"#{filename}\""
         headers['X-Accel-Buffering']   = 'no'
         headers["Cache-Control"]       ||= "no-cache"
         headers.delete("Content-Length")
 
         response.status    = 200
-        self.response_body = model_class.new.to_csv_rows
+        self.response_body = report.to_csv_rows
       end
 
-      def model_class
-        ::Reports::Payments
+      private
+
+      def filename
+        "payments_up_to_#{DateTime.now.to_s(:filename)}.csv"
       end
 
+      def report
+        ::Reports::Payments.new
+      end
     end
   end
 end
