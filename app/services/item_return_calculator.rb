9ff416ba3b24b10d550452959a620f9eab468b84
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-07-14 12:09:09 +1000

    receive item events

--- app/services/item_return_calculator.rb
+++ app/services/item_return_calculator.rb
@@ -2,25 +2,18 @@ class ItemReturnCalculator < EventSourcedRecord::Calculator
   events :item_return_events
 
   def advance_creation(event)
-
+    @item_return.line_item_id = event.line_item_id
   end
 
   def advance_return_requested(event)
-    @item_return
-
-    # binding.pry
-    # @item_return
-
     event.data.map do |k,v|
       @item_return.send("#{k}=", v)
     end
-
-    # @item_return.order_number = event.order_number
   end
 
   def advance_receive_item(event)
-
-    binding.pry
-    @item_return
+    # @item_return.status            = :received
+    @item_return.received_location = event.location
+    @item_return.received_on       = event.received_on
   end
 end
--- spec/models/item_return_event_spec.rb
+++ spec/models/item_return_event_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ItemReturnEvent, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'creation' do
+    subject(:event) { ItemReturnEvent.creation.new }
+
+    it { is_expected.to validate_presence_of :line_item_id }
+  end
+
+  xdescribe 'return_requested'
+
+  describe 'receive_item' do
+    subject(:event) { ItemReturnEvent.receive_item.new }
+
+    it { is_expected.to validate_presence_of :user }
+    it { is_expected.to validate_presence_of :location }
+    it { is_expected.to validate_inclusion_of(:location).in_array(%w[AU US]) }
+    it { is_expected.to validate_presence_of :received_on }
+  end
 end
