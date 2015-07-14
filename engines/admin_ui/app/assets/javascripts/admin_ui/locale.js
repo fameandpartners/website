472ee420001d24a40009dadc0f52ea8e1e0b34d3
Nam Dang <namproctin@gmail.com>
2015-07-14 14:26:05 +0700

    add validation and fields for both us and au

--- engines/admin_ui/app/assets/javascripts/admin_ui/locale.js
+++ engines/admin_ui/app/assets/javascripts/admin_ui/locale.js
@@ -1,4 +1,44 @@
 $(document).ready(function () {
-  $('#locale').val($('.nav-tabs li[class=\"active\"] a').text());
-  $('#locale').closest(".form-group").hide();
+  // initialize locale fields based on tab's name
+  $('.locale').each(function() {
+    tabName = $(this).closest(".tab-pane").attr("id");
+    if (tabName == "tab-en-us") {
+      $(this).val("en-US");
+    } else if (tabName == "tab-en-au") {
+      $(this).val("en-AU");
+    }
+  });
+
+  // Hide locale fields
+  $('.locale').closest(".form-group").hide();
+
+  // Hide validation
+  $('.validation-error').hide();
+
+  // validate title and meta description before submit
+  $('.save').click(function(e){
+    e.preventDefault();
+
+    validated = true;
+    $('.translation-title').each(function(){
+      if ($(this).val() == "")
+      {
+        validated = false;
+      }
+    });
+
+    $('.translation-meta-description').each(function(){
+      if ($(this).val() == "")
+      {
+        validated = false;
+      }
+    });
+
+    if (!validated) {
+      $('.validation-error').show();
+    } else {
+      $('.validation-error').hide();
+      $('form').submit();
+    }
+  });
 });
