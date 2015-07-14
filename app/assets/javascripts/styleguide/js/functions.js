e0eaf18f03173d77316bcd4d8fdbf2c75fc3582d
Tiago Amaro <tiagocis@gmail.com>
2015-07-14 11:24:48 -0300

    [fix] remove unused alert JS

--- app/assets/javascripts/styleguide/js/functions.js
+++ app/assets/javascripts/styleguide/js/functions.js
@@ -28,53 +28,6 @@ jQuery(document).ready(function($){
 // ALERTS
 ////////////
 
-navAlertClass = $('.nav-alert-top');
-alert = $('.alert');
-pageTitle = $('.page-title');
-slider = $('#theTarget');
-
-
-function addAlertHeightToNav() {
-	var alertHeight = alert.outerHeight();
-	// Changing top margin of fixed nav based on alert size
-	navAlertClass.css("top", alertHeight);
-	pageTitle.css("margin-top", 70+alertHeight);
-	slider.css("margin-top", 70+alertHeight);
-}
-
-jQuery(document).ready(function($){
-
-	alertFixed = $('.alert-fixed-top');
-	navMenu = $('.nav-menu');
-	pageTitle = $('.page-title');
-	slider2 = $('#theTarget');
-	alertFixed.removeClass('isClosed');
-	alertFixed.addClass('isOpen');
-
-	// Alert functionality
-	alertFixed.on('closed.bs.alert', function () {
-
-  		alertFixed.addClass('display-none');
-  		navMenu.removeClass('nav-alert-top');
-  		navMenu.css('top', '0');
-  		pageTitle.css('margin-top', '70');
-  		slider2.css('margin-top', '70');
-  		alertFixed.removeClass('isOpen');
-  		alertFixed.addClass('isClosed');
-	})
-
-	addAlertHeightToNav();
-
-});
-
-$(window).resize(function() {
-	// If alert is displayed
-	if (alert.hasClass('isOpen')) {
-		addAlertHeightToNav();
-	}
-
-});
-
 jQuery(document).ready(function($){
 	var style = $('.style');
 	var overlay = $('.mobile-menu-overlay');
@@ -82,5 +35,4 @@ jQuery(document).ready(function($){
 	style.click(function() {
 		overlay.toggleClass('mobile-show');
 	});
-
 });
