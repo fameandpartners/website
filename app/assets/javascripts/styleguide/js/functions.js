905e6adcf24a976b5628ffcf352077921cc7d42d
Tiago Amaro <tiagocis@gmail.com>
2015-07-14 11:06:50 -0300

    [fix] remove unused “go to top” mobile button

--- app/assets/javascripts/styleguide/js/functions.js
+++ app/assets/javascripts/styleguide/js/functions.js
@@ -1,20 +1,4 @@
 jQuery(document).ready(function($){
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 500,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 700,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
-
-	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-	});
-
-});
-
-jQuery(document).ready(function($){
-
 	var searchBtn = $('.search');
 	var searchBox = $('.search-bar');
 	var close = $('.close-search');
