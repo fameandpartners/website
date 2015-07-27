31039487ff693a32b9c49e3081f4f4e6ecdea08b
Toby Hede <tobyhede@info-architects.net>
2015-07-27 16:08:23 +1000

    fix lp page title

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -251,6 +251,7 @@ class StaticsController < ApplicationController
   end
 
   def landing_page_mobile
+    @title = 'A mid-summer night out'
     @disable_notices = true
     @page_variant = params[:variant].to_s || "1"
     case @page_variant
