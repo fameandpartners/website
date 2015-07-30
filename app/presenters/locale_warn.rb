5d20f85c6bd4d9ae2ed88532fa5de8678fa03078
Tiago Amaro <tiagocis@gmail.com>
2015-07-30 15:04:35 -0300

    first version of warning presenter

--- app/presenters/locale_warn.rb
+++ app/presenters/locale_warn.rb
@@ -1,8 +1,11 @@
 class LocaleWarn
-  attr_reader :site_version
+  attr_reader :ip, :current_site_version, :session_site_version_code
 
-  def initialize(site_version)
-    @site_version = site_version
+  def initialize(opts = {})
+    @ip                        = opts[:ip]
+    @current_site_version      = opts[:current_site_version]
+    @session_site_version_code = opts[:session_site_version_code]
+
     create_preferences
   end
 
@@ -22,13 +25,25 @@ class LocaleWarn
     Spree::AppConfiguration.new[long_text_key]
   end
 
+  # - Warning will show when:
+  # -> User is from country that differs from current site version
+  # -> User did not choose any the site version
+  # -> User hasn't closed the alert
+  def show?
+    session_site_version_code.nil? && current_site_version != positional_site_version
+  end
+
   private
 
-  def create_preferences
-    Spree::AppConfiguration.preference "#{version_code}_locale_warn_text" , :string, default: "Looks like you're in the :country. Visit our local store for :country_code sizes and :country_currency pricing."
+  def positional_site_version
+    FindUsersSiteVersion.new(request_ip: ip).sv_chosen_by_ip || SiteVersion.default
   end
 
   def version_code
-    site_version.code
+    positional_site_version.code
+  end
+
+  def create_preferences
+    Spree::AppConfiguration.preference "#{version_code}_locale_warn_text" , :string, default: "Looks like you're in the :country. Visit our local store for :country_code sizes and :country_currency pricing."
   end
 end
