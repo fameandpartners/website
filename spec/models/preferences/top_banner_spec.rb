4daa5e3d174f7f3b2ce9db40644c9b477ff8acdb
Tiago Amaro <tiagocis@gmail.com>
2015-07-30 16:44:57 -0300

    readd preferences module on top banner spec

--- spec/models/preferences/top_banner_spec.rb
+++ spec/models/preferences/top_banner_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe TopBanner, type: :presenter, spree_config_support: true do
+describe Preferences::TopBanner, type: :presenter, spree_config_support: true do
   let(:spree_config) { Spree::AppConfiguration.new }
   let(:site_version) { build_stubbed(:site_version, permalink: 'au') }
   let(:banner)       { described_class.new(site_version) }
