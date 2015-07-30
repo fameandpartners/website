97abe544f7a88f12cc4a5235d61a510e36e77ad9
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-07-30 12:24:58 +1000

    DRY up cache keys spec setup

---
+++ candidates_for_deletion.md
@@ -0,0 +1,2 @@
+app/views/layouts/_header_regular_navigation.html.slim
+app/views/celebrities/old_show.html
--- spec/helpers/cache_keys_helper_spec.rb
+++ spec/helpers/cache_keys_helper_spec.rb
@@ -1,33 +1,32 @@
 require 'spec_helper'
 
 RSpec.describe CacheKeysHelper, :type => :helper do
+
+  before do
+    class << helper
+      attr_reader :current_site_version, :current_promotion
+    end
+
+    helper.instance_variable_set(:@current_site_version, current_site_version)
+    helper.instance_variable_set(:@current_promotion,    current_promotion)
+  end
+
   describe "#price_sensitive_cache_key" do
+
+    let(:current_site_version) { SiteVersion.new permalink: 'mars' }
+    let(:current_promotion)    { Spree::Promotion.new code: 'freestuff' }
+
     context 'with promo code' do
-      before do
-        def helper.current_promotion
-          Spree::Promotion.new code: 'freestuff'
-        end
-
-        def helper.current_site_version
-          SiteVersion.new permalink: 'mars'
-        end
-      end
-
       it "uses the site and current promotion" do
         expect(helper.price_sensitive_cache_key).to eql("site=mars,promo=freestuff")
       end
     end
 
     context 'no promo code' do
-      before do
-        def helper.current_promotion
-          nil
-        end
 
-        def helper.current_site_version
-          SiteVersion.new permalink: 'venus'
-        end
-      end
+      let(:current_site_version) { SiteVersion.new permalink: 'venus' }
+      let(:current_promotion)    { nil }
+
       it "ignores missing promotions" do
         expect(helper.price_sensitive_cache_key).to eql("site=venus,promo=")
       end
