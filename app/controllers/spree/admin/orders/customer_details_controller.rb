0b0d11c0900f95f016cfe69739e711f3dc42e93b
Evgeniy <malleus.petrov@gmail.com>
2015-07-31 16:08:18 +0300

    [update][order customer details]

--- app/controllers/spree/admin/orders/customer_details_controller.rb
+++ app/controllers/spree/admin/orders/customer_details_controller.rb
@@ -16,16 +16,18 @@ module Spree
         end
 
         def update
+          new_user_id = params[:order].delete(:user_id)
+
           if @order.update_attributes(params[:order])
             #shipping_method = @order.available_shipping_methods(:front_end).first
             #if @order.shipping_method
               #@order.shipping_method = shipping_method
 
             if @order.available_shipping_methods(:back_end).present?
-              if params[:user_id].present?
-                @order.user_id = params[:user_id]
-                @order.user true
+              if new_user_id && new_user_id != @order.user_id
+                update_order_owner(@order, new_user_id)
               end
+
               while @order.next; end
 
               flash[:success] = t('customer_details_updated')
@@ -53,6 +55,32 @@ module Spree
             @order = Order.find_by_number!(params[:order_id], :include => :adjustments)
           end
 
+          # only update, will not delete owner
+          def update_order_owner(order, owner_id)
+            previous_user = order.user
+            owner = Spree::User.find_by_id(owner_id)
+            return false if owner.blank?
+
+            order.user_id = owner.id
+            order.user
+
+            # update user-related details
+            order.user_first_name = owner.first_name
+            order.user_last_name  = owner.first_name
+            order.email           = owner.email
+
+            if order.save
+              tracker = Marketing::CustomerIOEventTracker.new
+              tracker.identify_user(owner, current_site_version)
+              tracker.track(owner, 'order:new_owner', {
+                order_number: order.number,
+                original_customer_email: previous_user.try(:email),
+                updated_customer_email: owner.try(:email)
+              })
+            end
+
+            owner
+          end
       end
     end
   end
