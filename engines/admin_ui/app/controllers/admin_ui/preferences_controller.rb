9ac09fcf6d01b24974409c28bdf9de450c848f36
Tiago Amaro <tiagocis@gmail.com>
2015-08-10 14:15:07 -0300

    [feat] wrap preferences parameters

--- engines/admin_ui/app/controllers/admin_ui/preferences_controller.rb
+++ engines/admin_ui/app/controllers/admin_ui/preferences_controller.rb
@@ -1,16 +1,22 @@
 module AdminUi
-  class PreferencesController < ApplicationController
+  class PreferencesController < AdminUi::ApplicationController
     def index
-      @site_versions = SiteVersion.all
+      @site_versions = ::SiteVersion.all
     end
 
     def update
-      params.each do |name, value|
+      preferences_params.each do |name, value|
         next unless Spree::Config.has_preference? name
         Spree::Config[name] = value
       end
 
-      redirect_to admin_ui.preferences_path, flash: { success: t(:successfully_updated, resource: 'Preferences') }
+      redirect_to admin_ui.preferences_path, flash: { success: 'Preferences have been successfully updated!' }
+    end
+
+    private
+
+    def preferences_params
+      params[:preferences] || []
     end
   end
 end
