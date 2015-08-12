2cda26c9883a2066ead9e8e3d3e9d497ac0c75fd
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-08-12 10:45:45 +1000

    Paypal helper methods out of global helpers
    Just moving code around, nothing to see here.

---
+++ app/helpers/paypal_helper.rb
@@ -0,0 +1,34 @@
+module PaypalHelper
+  def paypal_payment_method
+    @paypal_payment_method ||= Spree::PaymentMethod.where(
+      type: "Spree::Gateway::PayPalExpress",
+      environment: Rails.env,
+      active: true,
+      deleted_at: nil
+    ).first
+  end
+
+  def paypal_available?
+    paypal_payment_method.present?
+  end
+
+  def paypal_express_button
+    return if paypal_payment_method.blank?
+
+    url = paypal_express_url(payment_method_id: paypal_payment_method.id, protocol: request.protocol)
+    link_to(
+      image_tag('checkout/shopping_bag_paypal.png'),
+      url, method: :post, id: "paypal_button"
+    )
+  end
+
+  def guest_paypal_express_button
+    return if paypal_payment_method.blank?
+
+    url = guest_paypal_express_url(payment_method_id: paypal_payment_method.id, protocol: request.protocol)
+    link_to(
+      image_tag("https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif"),
+      url, method: :post, id: "paypal_button"
+    )
+  end
+end
