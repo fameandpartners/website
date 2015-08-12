4fa4eab31e7a75dd822bfe4ca623289f2c25b2fa
Tiago Amaro <tiagocis@gmail.com>
2015-08-12 17:59:28 -0300

    create jackets taxonomy/root and revolution page

---
+++ db/migrate/20150811143830_create_jackets_taxonomy.rb
@@ -0,0 +1,28 @@
+class CreateJacketsTaxonomy < ActiveRecord::Migration
+  class Spree::Taxonomy < ActiveRecord::Base
+  end
+
+  class Revolution::Page < ActiveRecord::Base
+  end
+
+  def up
+    # Taxonomy and Taxon
+    jackets_taxonomy = Spree::Taxonomy.create(name: 'Jackets')
+    jackets_taxon = jackets_taxonomy.root
+    jackets_taxon.publish!
+
+    # Pages
+    jackets_page = Revolution::Page.new(path: '/jackets', template_path: '/products/collections/show.html.slim')
+    jackets_page.save
+  end
+
+  def down
+    if jackets = Spree::Taxonomy.where(name: 'Jackets').first
+      jackets.destroy
+    end
+
+    if jackets_page = Revolution::Page.where(path: '/jackets', template_path: '/products/collections/show.html.slim').first
+      jackets_page.destroy
+    end
+  end
+end
