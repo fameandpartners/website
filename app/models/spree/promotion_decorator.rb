a6a6766a5bbe22bec6c41b5ddab4e96965cbc832
Evgeniy <malleus.petrov@gmail.com>
2015-08-22 18:34:01 +0300

    [feat][promotions] moved hardcoded or hacked settings to db   - eligible_to_sale_order: instead of hardcoded list of codes   - eligible_to_custom_order: don't hack, we have to specify manually, but this option becomes for flexible   - require_shipping_charge: allows to disable free shipping

--- app/models/spree/promotion_decorator.rb
+++ app/models/spree/promotion_decorator.rb
@@ -1,9 +1,5 @@
 Spree::Promotion.class_eval do
-  class << self
-    def find_by_code(code)
-      self.where("lower(code) = ?", code.to_s.downcase).first
-    end
-  end
+  attr_accessible :eligible_to_sale_order, :eligible_to_custom_order, :require_shipping_charge
 
   def eligible?(order)
     return false if expired? || usage_limit_exceeded?(order)
@@ -15,62 +11,22 @@ Spree::Promotion.class_eval do
     rules_are_eligible?(order, {})
   end
 
-  def discount
-    @discount ||= begin
-      action = self.actions.find{|a| a.calculator_type == 'Spree::Calculator::FlatPercentItemTotal' }
-      amount = action.present?  ? action.calculator.preferred_flat_percent : BigDecimal.new(0)
+  class << self
 
-      OpenStruct.new(amount: amount, size: amount)
+    def find_by_code(code)
+      self.where("lower(code) = ?", code.to_s.downcase).first
     end
+
   end
 
-  # use it to raw
-  def calculate_price_with_discount(price)
-    discount = 0.0
-    self.actions.each do |action|
-      case action.calculator_type
-      when "Spree::Calculator::FlatRate"
-        discount += action.calculator.compute(nil)
-      when "Spree::Calculator::PerItem"
-        discount += action.calculator.preferred_amount
-      when "Spree::Calculator::FlatPercentItemTotal"
-        # this calcultor requires an order
-        value = price.amount * BigDecimal(action.calculator.preferred_flat_percent.to_s) / 100.0
-        discount += (value * 100).round.to_f / 100
-      else
-        # do nothing
-      end
-    end
-    price.amount -= discount
-    return price
-  rescue
-    price
-  end
+#  # is dead code?
+#  def discount
+#    @discount ||= begin
+#      action = self.actions.find{|a| a.calculator_type == 'Spree::Calculator::FlatPercentItemTotal' }
+#      amount = action.present?  ? action.calculator.preferred_flat_percent : BigDecimal.new(0)
+#
+#      OpenStruct.new(amount: amount, size: amount)
+#    end
+#  end
 
-  protected
-
-    # rude method. possible, it should be thrown away
-    def can_apply_to_any_order?
-      applicable_to_any_order_promocodes = %w(xtra10 swm30 is20 who20 fam20 btb20p btb20d gf20 theparcel25 frenzy5p crafted4u vosn2015 famer35p zouponsfame famexinstyle5 ibotta5 famexAU5 famexUS5 bonus5p birthday5p famexsale5 famexhuffington5 fashionitgirlsale5)
-
-      applicable_to_any_order_promocodes.any?{|code| code.to_s.downcase == self.code.downcase }
-    end
-
-  private
-
-    # note - this methods should be set in db or somewhere else.
-    #
-    # Spree::Calculator::PersonalizationDiscount designed to hack orders with personalizations,
-    # so it can pass
-    def eligible_to_custom_order?
-      calculators = self.actions.map{|action| action.calculator.try(:type) }.compact.uniq
-
-      return true if calculators.include?('Spree::Calculator::PersonalizationDiscount')
-
-      self.can_apply_to_any_order?
-    end
-
-    def eligible_to_sale_order?
-      self.can_apply_to_any_order?
-    end
 end
---
+++ db/migrate/20150822135103_promotions_update.rb
@@ -0,0 +1,44 @@
+class PromotionsUpdate < ActiveRecord::Migration
+  def up
+    add_column Spree::Promotion.table_name, :eligible_to_custom_order,  :boolean, default: false
+    add_column Spree::Promotion.table_name, :eligible_to_sale_order,    :boolean, default: false
+    add_column Spree::Promotion.table_name, :require_shipping_charge,   :boolean, default: false
+
+    set_eligible_to_sale_order_value
+    set_eligible_to_custom_order_value
+    set_shipping_charge
+  end
+
+  def down
+    remove_column Spree::Promotion.table_name, :eligible_to_custom_order
+    remove_column Spree::Promotion.table_name, :eligible_to_sale_order
+    remove_column Spree::Promotion.table_name, :require_shipping_charge
+  end
+
+  private
+
+  def eligible_to_sale_order_promos
+    %w(xtra10 swm30 is20 who20 fam20 btb20p btb20d gf20 theparcel25 frenzy5p crafted4u vosn2015 famer35p zouponsfame famexinstyle5 ibotta5 famexAU5 famexUS5 bonus5p birthday5p famexsale5 famexhuffington5 fashionitgirlsale5)
+  end
+
+  def set_eligible_to_sale_order_value
+     Spree::Promotion.where(
+       "code ilike any(array[?])", eligible_to_sale_order_promos
+     ).update_all(eligible_to_sale_order: true)
+  end
+
+  def set_eligible_to_custom_order_value
+    Spree::Promotion.update_all(eligible_to_custom_order: false)
+
+    Spree::Promotion.find_each(batch_size: 100) do |promotion|
+      calculators = promotion.actions.map{|action| action.calculator.try(:type) }.compact.uniq
+      if calculators.include?('Spree::Calculator::PersonalizationDiscount')
+        promotion.update_column(:eligible_to_custom_order, true)
+      end
+    end
+  end
+
+  def set_shipping_charge
+    Spree::Promotion.update_all(require_shipping_charge: false)
+  end
+end
