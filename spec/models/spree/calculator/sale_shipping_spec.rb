14eb4cc6a7e7349befe1978e8955c8fbd0ea9bf0
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-08-24 17:00:37 +1000

    Custom shipping amount matchers, rspec style tweaks

--- spec/models/spree/calculator/sale_shipping_spec.rb
+++ spec/models/spree/calculator/sale_shipping_spec.rb
@@ -3,39 +3,57 @@ require 'spec_helper'
 describe Spree::Calculator::SaleShipping, type: :model do
   let(:order) { build(:spree_order) }
 
-  let(:subject) { Spree::Calculator::SaleShipping.new(
+  subject(:calculator) { Spree::Calculator::SaleShipping.new(
     preferred_sale_products_shipping_amount:    15.5,
     preferred_normal_products_shipping_amount:  9.9
   )}
 
+  matcher :be_default_charge do
+    match { |actual| actual == BigDecimal.new('9.9') }
+    failure_message do |actual|
+      "expected that #{actual.to_f} would be the default shipping charge (9.9)"
+    end
+  end
+
+  matcher :be_extra_charge do
+    match { |actual| actual == BigDecimal.new('15.5') }
+    failure_message do |actual|
+      "expected that #{actual.to_f} would be the extra shipping charge (15.5)"
+    end
+  end
+
   context "#compute" do
+    subject(:shipping_amount) { calculator.compute(order) }
+
     context 'orders' do
       it "returns default shipping for standard orders" do
-        expect(order).to receive(:in_sale?).and_return(false)
-        expect(subject.compute(order)).to eql(BigDecimal.new('9.9'))
+        allow(order).to receive(:in_sale?).and_return(false)
+
+        expect(shipping_amount).to be_default_charge
       end
 
       it "returns sale amount for orders with sale items" do
-        expect(order).to receive(:in_sale?).and_return(true)
-        expect(subject.compute(order)).to eql(BigDecimal.new('15.5'))
+        allow(order).to receive(:in_sale?).and_return(true)
+
+        expect(shipping_amount).to be_extra_charge
       end
     end
 
     context 'promotions' do
       it "returns sale amount if applied promotion requires charge" do
-        expect(order).to receive(:coupon_code_added_promotion).and_return(
+        allow(order).to receive(:coupon_code_added_promotion).and_return(
           double('promo', require_shipping_charge?: true)
         )
 
-        expect(subject.compute(order)).to eql(BigDecimal.new('15.5'))
+        expect(shipping_amount).to be_extra_charge
       end
 
       it "returns default amount if applied promotion requires charge" do
-        expect(order).to receive(:coupon_code_added_promotion).and_return(
+        allow(order).to receive(:coupon_code_added_promotion).and_return(
           double('promo', require_shipping_charge?: false)
         )
 
-        expect(subject.compute(order)).to eql(BigDecimal.new('9.9'))
+        expect(shipping_amount).to be_default_charge
       end
     end
   end
