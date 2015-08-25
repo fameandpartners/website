8432af300f66fa69f97799798d173ff2bb2b69dc
Tiago Amaro <tiagocis@gmail.com>
2015-08-25 20:40:49 -0300

    mobile friendly implementation of related jackets + recommended products

--- app/presenters/products/related_presenter.rb
+++ app/presenters/products/related_presenter.rb
@@ -8,21 +8,11 @@ module Products
     end
 
     def recommended
-      recommended_products.first(recommended_products_count)
+      recommended_products.first(Products::DetailsResource::RECOMMENDED_PRODUCTS_LIMIT)
     end
 
     def jackets
-      related_jackets.first(related_jackets_count)
-    end
-
-    private
-
-    def related_jackets_count
-      [related_jackets.count, Products::DetailsResource::RELATED_JACKETS_LIMIT].min
-    end
-
-    def recommended_products_count
-      recommended_products.count - related_jackets_count
+      related_jackets.first(Products::DetailsResource::RELATED_JACKETS_LIMIT)
     end
   end
 end
