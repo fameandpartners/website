42e9ef20186b6ca98213a7c3fb8d0a0cd87847e1
Tiago Amaro <tiagocis@gmail.com>
2015-08-26 19:10:36 -0300

    fix up controller specs

--- spec/controllers/spree/admin/products_controller_spec.rb
+++ spec/controllers/spree/admin/products_controller_spec.rb
@@ -10,23 +10,24 @@ describe Spree::Admin::ProductsController, type: :controller do
     )
   end
 
-  describe 'GET /admin/search/jackets' do
-    let!(:jackets_taxon)  { create(:taxon, :jackets) }
-    let!(:denim_jacket)   { create(:jacket, name: 'Denim Jacket', id: 101, taxons: [jackets_taxon]) }
-    let!(:leather_jacket) { create(:jacket, id: 102, taxons: [jackets_taxon]) }
-    let!(:dress)          { create(:dress, id: 103) }
+  describe 'GET /admin/search/outerwear' do
+    let!(:outerwear_taxonomy) { create(:taxonomy, :outerwear) }
+    let!(:jackets_taxon)      { create(:taxon, taxonomy: outerwear_taxonomy) }
+    let!(:denim_jacket)       { create(:spree_product, name: 'Denim Jacket', id: 101, taxons: [jackets_taxon]) }
+    let!(:leather_jacket)     { create(:spree_product, id: 102, taxons: [jackets_taxon]) }
+    let!(:dress)              { create(:dress, id: 103) }
 
     context "given 'ids' param" do
-      it 'returns jackets with the ids' do
-        get :search_jackets, { ids: '101,102,103' }
-        expect(assigns[:products]).to match([denim_jacket, leather_jacket])
+      it 'returns outerwear with the ids' do
+        get :search_outerwear, { ids: '101,102,103' }
+        expect(assigns[:products]).to match_array([denim_jacket, leather_jacket])
       end
     end
 
     context "given 'q' param" do
-      it 'returns jackets with queried name' do
-        get :search_jackets, { q: 'Denim' }
-        expect(assigns[:products]).to match([denim_jacket])
+      it 'returns outerwear with queried name' do
+        get :search_outerwear, { q: 'Denim' }
+        expect(assigns[:products]).to match_array([denim_jacket])
       end
     end
   end
