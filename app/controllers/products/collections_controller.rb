be2ba534062c98f3af18bdedd8b75e6c29f77b13
Evgeniy <malleus.petrov@gmail.com>
2015-09-03 18:01:23 +0300

    [chore] added default r-page for collections

--- app/controllers/products/collections_controller.rb
+++ app/controllers/products/collections_controller.rb
@@ -72,9 +72,17 @@ class Products::CollectionsController < Products::BaseController
 
     def load_page
       current_path = LocalizeUrlService.remove_version_from_url(request.path)
-      @page = Revolution::Page.find_for(current_path, '/dresses/*')
+      @page = Revolution::Page.find_for(current_path, '/dresses/*') || default_page
       page.locale = current_site_version.locale
       @banner = Revolution::PageBannerDecorator.new(page, params)
+    end
+
+    def default_page
+      Revolution::Page.new(
+        path: '/dresses/*',
+        template_path: '/products/collections/show',
+        variables: { 'limit' => 21 }
+      )
     end
 
     def punch_products
