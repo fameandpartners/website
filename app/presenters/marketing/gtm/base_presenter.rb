02fa2a104ea14b76c6108a66116c6c21d618c482
Tiago Amaro <tiagocis@gmail.com>
2015-09-07 09:54:31 -0300

    create base parent Gtm::BasePresenter

---
+++ app/presenters/marketing/gtm/base_presenter.rb
@@ -0,0 +1,15 @@
+module Marketing
+  module Gtm
+    class BasePresenter
+      UNKNOWN_STRING = 'unknown'
+
+      def key
+        raise NotImplementedError, '#key is not yet implemented'
+      end
+
+      def body
+        raise NotImplementedError, '#body is not yet implemented'
+      end
+    end
+  end
+end
