c149181705de7e74fc2b34bc19ce3037acd9ff29
Tiago Amaro <tiagocis@gmail.com>
2015-09-07 18:12:10 -0300

    gtm product presenter (yet, without many attributes)

--- app/presenters/marketing/gtm/product_presenter.rb
+++ app/presenters/marketing/gtm/product_presenter.rb
@@ -1,6 +1,53 @@
 module Marketing
   module Gtm
     class ProductPresenter
+      attr_reader :product
+
+      def initialize(product_presenter:)
+        @product = product_presenter
+      end
+
+      def key
+        'product'.freeze
+      end
+
+      def price
+        product.price.amount
+      end
+
+      def price_with_discount
+        if (discount = product.discount)
+          product.price.apply(discount).amount
+        else
+          price
+        end
+      end
+
+      def discount_percent
+        discount = product.discount ? product.discount.amount : 0
+        discount.to_s
+      end
+
+      def body
+        {
+            name: product.name,
+            brand: 'Fame & Partners', # Hardcoded for the moment
+            sku: product.sku,
+            price: price,
+            priceWithDiscount: price_with_discount,
+            discountPercent: discount_percent,
+            # type: 'dresses', # Hardcoded for the moment
+            # currency: currency,
+            # colors: colors,
+            # selectedColor: selected_color,
+            # categories: categories,
+            # image: categories,
+            # images: categories,
+            # description: product.description,
+            # expressMaking: product.fast_making,
+            # sizes: sizes
+        }
+      end
     end
   end
 end
