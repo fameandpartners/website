acfac8a1cd0ec0de4bd07b90e704ebc4abaa12c0
Tiago Amaro <tiagocis@gmail.com>
2015-09-07 10:53:53 -0300

    create site gtm presenter

--- app/presenters/marketing/gtm/site_presenter.rb
+++ app/presenters/marketing/gtm/site_presenter.rb
@@ -1,6 +1,25 @@
 module Marketing
   module Gtm
     class SitePresenter
+      attr_reader :current_site_version
+
+      def initialize(current_site_version: SiteVersion.default)
+        @current_site_version = current_site_version
+      end
+
+      def code
+        current_site_version.code
+      end
+
+      def key
+        'site'.freeze
+      end
+
+      def body
+        {
+            version: code
+        }
+      end
     end
   end
 end
