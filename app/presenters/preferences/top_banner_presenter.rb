d87d3c9d73f667a04d76e393b64c46ca2d9f4807
Nam Dang <namproctin@gmail.com>
2015-09-07 18:08:22 +0700

    fix Garrow comments

--- app/presenters/preferences/top_banner_presenter.rb
+++ app/presenters/preferences/top_banner_presenter.rb
@@ -13,5 +13,13 @@ module Preferences
     def center_text
       preference.center_text
     end
+
+    def customer_service_contact
+      "24/7 CUSTOMER SERVICE 1300 222 222"
+    end
+
+    def free_shipping_in_AU_and_NZ
+      "FREE SHIPPING WITHIN AUSTRALIA AND NEW ZEALAND"
+    end
   end
 end
