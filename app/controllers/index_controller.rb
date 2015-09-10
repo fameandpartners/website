e3a9d9b744e65808a8164a106cccf89e571c6e04
Tiago Amaro <tiagocis@gmail.com>
2015-09-09 23:02:38 -0300

    add gtm page type to the homepage

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,9 +2,10 @@ class IndexController < ApplicationController
   layout 'redesign/application'
 
   def show
-    @banner      = Spree::BannerBox.big_banner.for_site_version(current_site_version).limit(10)
-    @title       = [homepage_title, default_seo_title].join(' ')
-    @description = default_meta_description
+    @banner        = Spree::BannerBox.big_banner.for_site_version(current_site_version).limit(10)
+    @title         = [homepage_title, default_seo_title].join(' ')
+    @description   = default_meta_description
+    @gtm_page_type = 'homepage'
     if Features.active?(:maintenance)
       render :action => 'maintenance', :layout => 'redesign/maintenance'
     end
