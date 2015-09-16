faffc8f924145c62bd9bbcc4ffbf1ef05513cfd5
Tiago Amaro <tiagocis@gmail.com>
2015-09-16 17:59:21 -0300

    use gtm_page_type setter method on homepage

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,10 +2,10 @@ class IndexController < ApplicationController
   layout 'redesign/application'
 
   def show
-    @banner        = Spree::BannerBox.big_banner.for_site_version(current_site_version).limit(10)
-    @title         = [homepage_title, default_seo_title].join(' ')
-    @description   = default_meta_description
-    @gtm_page_type = 'homepage'
+    @banner            = Spree::BannerBox.big_banner.for_site_version(current_site_version).limit(10)
+    @title             = [homepage_title, default_seo_title].join(' ')
+    @description       = default_meta_description
+    self.gtm_page_type = 'homepage'.freeze
     if Features.active?(:maintenance)
       render :action => 'maintenance', :layout => 'redesign/maintenance'
     end
