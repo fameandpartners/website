e5f85c1f6d3c5437a0e4aed9f69d454b1166f9fa
Nam Dang <namproctin@gmail.com>
2015-09-16 14:24:10 +0700

    [feat] add new lookbook route and page
    I add new lookbook route and create new page migration

--- config/routes.rb
+++ config/routes.rb
@@ -98,6 +98,7 @@ FameAndPartners::Application.routes.draw do
     # # Widgets are iframes, and every href have no follows.
     get '/lookbook' => redirect('/lookbook/the-luxe-collection', :status => 302), as: :lookbook
 
+    get '/lookbook/make-a-statement' => 'products/collections#show', :permalink => 'make-a-statement', :as => :make_a_statement
     get '/lookbook/the-luxe-collection' => 'products/collections#show', :permalink => 'luxe', :as => :luxe_collection
 
     get '/lookbook/garden-weeding' => redirect('/lookbook/garden-wedding')
