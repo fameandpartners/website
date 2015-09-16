1497db4a61a8d6d4d4c3336d715f78458482c6e1
Tiago Amaro <tiagocis@gmail.com>
2015-09-16 18:44:50 -0300

    wrap presenters specs around their modules

--- spec/models/marketing/gtm/presenter/page_spec.rb
+++ spec/models/marketing/gtm/presenter/page_spec.rb
@@ -1,24 +1,30 @@
 require 'spec_helper'
 
-describe Marketing::Gtm::Presenter::Page, type: :presenter do
-  let(:type) { 'product' }
-  let(:meta_description) { 'Super Dress is awesome' }
-  let(:title) { 'Super Dress' }
-  let(:url) { 'http://lvh.me/super-dress' }
+module Marketing
+  module Gtm
+    module Presenter
+      describe Page, type: :presenter do
+        let(:type) { 'product' }
+        let(:meta_description) { 'Super Dress is awesome' }
+        let(:title) { 'Super Dress' }
+        let(:url) { 'http://lvh.me/super-dress' }
 
-  subject(:presenter) { described_class.new(type: type, meta_description: meta_description, title: title, url: url) }
+        subject(:presenter) { described_class.new(type: type, meta_description: meta_description, title: title, url: url) }
 
-  it_behaves_like 'a Marketing::Gtm::Presenter::Base'
+        it_behaves_like 'a Marketing::Gtm::Presenter::Base'
 
-  describe '#body' do
-    context 'given a type, meta description, title and url' do
-      it 'returns hash given data' do
-        expect(subject.body).to eq({
-                                       type:            'product',
-                                       metaDescription: meta_description,
-                                       title:           title,
-                                       url:             url
-                                   })
+        describe '#body' do
+          context 'given a type, meta description, title and url' do
+            it 'returns hash given data' do
+              expect(subject.body).to eq({
+                                             type:            'product',
+                                             metaDescription: meta_description,
+                                             title:           title,
+                                             url:             url
+                                         })
+            end
+          end
+        end
       end
     end
   end
--- spec/models/marketing/gtm/presenter/user_spec.rb
+++ spec/models/marketing/gtm/presenter/user_spec.rb
@@ -1,36 +1,42 @@
 require 'spec_helper'
 
-describe Marketing::Gtm::Presenter::User, type: :presenter do
-  let(:user) { build_stubbed(:spree_user, first_name: 'Loroteiro', last_name: 'Silvestre', email: 'loroteiro@silvestre.com') }
-  let(:request_ip) { '179.218.87.233' }
+module Marketing
+  module Gtm
+    module Presenter
+      describe User, type: :presenter do
+        let(:user) { build_stubbed(:spree_user, first_name: 'Loroteiro', last_name: 'Silvestre', email: 'loroteiro@silvestre.com') }
+        let(:request_ip) { '179.218.87.233' }
 
-  subject(:presenter) { described_class.new(spree_user: user, request_ip: request_ip) }
+        subject(:presenter) { described_class.new(spree_user: user, request_ip: request_ip) }
 
-  it_behaves_like 'a Marketing::Gtm::Presenter::Base'
+        it_behaves_like 'a Marketing::Gtm::Presenter::Base'
 
-  describe '#body' do
-    context 'given a spree user and his/her IP' do
-      context 'user is not logged in' do
-        subject(:presenter) { described_class.new(spree_user: nil, request_ip: request_ip) }
+        describe '#body' do
+          context 'given a spree user and his/her IP' do
+            context 'user is not logged in' do
+              subject(:presenter) { described_class.new(spree_user: nil, request_ip: request_ip) }
 
-        it 'returns hash with unknown user info' do
-          expect(subject.body).to eq({
-                                         name:     'unknown',
-                                         email:    'unknown',
-                                         loggedIn: false,
-                                         country:  'Brazil'
-                                     })
-        end
-      end
+              it 'returns hash with unknown user info' do
+                expect(subject.body).to eq({
+                                               name:     'unknown',
+                                               email:    'unknown',
+                                               loggedIn: false,
+                                               country:  'Brazil'
+                                           })
+              end
+            end
 
-      context 'user is logged in' do
-        it 'returns hash with user info' do
-          expect(subject.body).to eq({
-                                         name:     'Loroteiro Silvestre',
-                                         email:    'loroteiro@silvestre.com',
-                                         loggedIn: true,
-                                         country:  'Brazil'
-                                     })
+            context 'user is logged in' do
+              it 'returns hash with user info' do
+                expect(subject.body).to eq({
+                                               name:     'Loroteiro Silvestre',
+                                               email:    'loroteiro@silvestre.com',
+                                               loggedIn: true,
+                                               country:  'Brazil'
+                                           })
+              end
+            end
+          end
         end
       end
     end
