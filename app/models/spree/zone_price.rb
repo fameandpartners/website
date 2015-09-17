a04e0af98c286903602835f276dbb9575daef755
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-09-18 09:34:37 +1000

    Use currency based prices instead of zone prices in ES index.

--- app/models/spree/zone_price.rb
+++ app/models/spree/zone_price.rb
@@ -1,3 +1,7 @@
+# GB NOTES - This class has been causing issues, where variants already have the ability to have prices in
+# multiple currencies, this class and it's usages seem to define prices for site_versions.
+# TODO - Deprecate use of this, or at the very least, move it outside of the spree namespace.
+
 class Spree::ZonePrice < ActiveRecord::Base
   belongs_to :zone
   belongs_to :variant
