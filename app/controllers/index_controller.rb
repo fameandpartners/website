3aabb6d27de8fb055d6e0cb7382ae11a213b05c6
Tiago Amaro <tiagocis@gmail.com>
2015-09-16 23:17:23 -0300

    actually test Base#rescuable_body method

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,9 +2,9 @@ class IndexController < ApplicationController
   layout 'redesign/application'
 
   def show
-    @banner            = Spree::BannerBox.big_banner.for_site_version(current_site_version).limit(10)
-    @title             = [homepage_title, default_seo_title].join(' ')
-    @description       = default_meta_description
+    @banner      = Spree::BannerBox.big_banner.for_site_version(current_site_version).limit(10)
+    @title       = [homepage_title, default_seo_title].join(' ')
+    @description = default_meta_description
     if Features.active?(:maintenance)
       render :action => 'maintenance', :layout => 'redesign/maintenance'
     end
