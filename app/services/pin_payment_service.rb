545d5e5436e5d10d7d50f363ce3cafa1d6fa575d
Toby Hede <tobyhede@info-architects.net>
2015-09-19 11:58:19 +1000

    Add extra card validations to pin service

--- app/services/pin_payment_service.rb
+++ app/services/pin_payment_service.rb
@@ -2,25 +2,6 @@ class PinPaymentService
 
   attr_reader :opts, :charge, :error
 
-  # {
-  # email:       customer.email,
-  # amount:      1000,
-  # currency:    'USD', # only AUD and USD are supported by pin.net.au
-  # description: 'Widgets',
-  # ip_address:  request.ip,
-  # card:        {
-  #   number:           5520000000000000,
-  #   expiry_month:     5,
-  #   expiry_year:      2014,
-  #   cvc:              123,
-  #   name:             'Roland Robot',
-  #   address_line1:    '42 Sevenoaks St',
-  #   address_city:     'Lathlain',
-  #   address_postcode: 6454,
-  #   address_state:    'WA',
-  #   address_country:  'Australia'
-  # }
-
   def initialize(pin_api_key, opts)
     PinPayment.secret_key = pin_api_key
     @opts = opts
@@ -28,8 +9,8 @@ class PinPaymentService
 
   def create_charge
     begin
-      @charge = PinPayment::Charge.create(opts)
-    rescue PinPayment::Error::InvalidResource => e
+      @charge = PinPayment::Charge.create(opts)      
+    rescue PinPayment::Error => e
       handle_error(e)
       false
     end
