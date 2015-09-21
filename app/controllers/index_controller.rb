c48244931e464f3835f5c83ccc1bf6561c9ee21d
Nam Dang <namproctin@gmail.com>
2015-09-21 17:56:26 +0700

    remove no need code

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,3 @@
-require 'rest-client'
 class IndexController < ApplicationController
   layout 'redesign/application'
 
@@ -6,7 +5,6 @@ class IndexController < ApplicationController
     @banner      = Spree::BannerBox.big_banner.for_site_version(current_site_version).limit(10)
     @title       = [homepage_title, default_seo_title].join(' ')
     @description = default_meta_description
-    @blog_posts  = blog_posts
     if Features.active?(:maintenance)
       render :action => 'maintenance', :layout => 'redesign/maintenance'
     end
@@ -22,18 +20,4 @@ class IndexController < ApplicationController
     Preferences::Titles.new(current_site_version).homepage_title
   end
 
-  def blog_posts
-    xml = RestClient.get('http://blog.fameandpartners.com/rss')
-    doc = Nokogiri::XML(xml)
-
-    blogs = []
-    doc.search('//item').each do |item|
-      blog = {}
-      blog[:title]       = item.search('title').text
-      blog[:description] = item.search('description').text
-      blog[:link]        = item.search('link').text
-      blogs << blog
-    end
-    blogs
-  end
 end
