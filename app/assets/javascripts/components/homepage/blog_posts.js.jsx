e09627010de7b3fd26adf6f829a3233e5b816699
Nam Dang <namproctin@gmail.com>
2015-09-23 11:41:08 +0700

    get rid of data set up

--- app/assets/javascripts/components/homepage/blog_posts.js.jsx
+++ app/assets/javascripts/components/homepage/blog_posts.js.jsx
@@ -1,22 +1,18 @@
 var Post = React.createClass({
   render: function(){
-    function fixQuote(text){
-      return text.replace(/&#8217;/g,"'");
-    };
-
     return(
-      <a className={this.props.compact === "true" ? "post compact" : "post"} href={this.props.post.url}>
+      <a className={this.props.post.custom_fields.home_page_image_compact == "true" ? "post compact" : "post"} href={this.props.post.url}>
         <div className="media-wrap is-loaded">
           <div className="inner-wrap">
-            <img alt="Blog image" src={this.props.img_url}></img>
+            <img alt="Blog image" src={this.props.post.custom_fields.home_page_image_url}></img>
           </div>
         </div>
         <div className="content-wrap">
           <h5 className="title txt-truncate-1">
-            {this.props.title != null ? this.props.title : fixQuote(this.props.post.title)}
+            {this.props.post.custom_fields.home_page_image_title}
           </h5>
           <p className="txt-truncate-block">
-            {this.props.des}
+            {this.props.post.custom_fields.home_page_image_text}
           </p>
         </div>
       </a>
@@ -40,14 +36,14 @@ var BlogPosts = React.createClass({
       );
     } else {
 
-      post_0 = (<Post post={this.state.blogs[0]} des="We chat style with the NY fashionista" img_url={this.state.blogs[0].attachments[1].url}/>)
-      post_1 = (<Post post={this.state.blogs[5]} compact="true" title="Follow Friday @flura_" des="We're obsessed with this florist's Instagram feed" img_url={this.state.blogs[5].attachments[1].url} />)
-      post_2 = (<Post post={this.state.blogs[8]} des="Fashion's new favourite shade" img_url={this.state.blogs[8].attachments[1].url}/>)
-      post_3 = (<Post post={this.state.blogs[7]} des="This weeks old school style muse" img_url={this.state.blogs[7].attachments[0].url}/>)
-      post_4 = (<Post post={this.state.blogs[6]} des="Our campaign star spills on how she does formal chic" img_url="/assets/homepage/blog/blog-4.jpg"/>)
-      post_5 = (<Post post={this.state.blogs[1]} des="That’s a Maxi Black Dress, people" img_url={this.state.blogs[1].attachments[14].url}/>)
-      post_6 = (<Post post={this.state.blogs[3]} compact="true" des="It's her job to know what you want before you do." img_url={this.state.blogs[3].attachments[0].url}/>)
-      post_7 = (<Post post={this.state.blogs[4]} des="Because sometimes it's okay to outshine the bride" img_url={this.state.blogs[4].attachments[0].url}/>)
+      post_0 = (<Post post={this.state.blogs[0]} />)
+      post_1 = (<Post post={this.state.blogs[5]} />)
+      post_2 = (<Post post={this.state.blogs[8]} />)
+      post_3 = (<Post post={this.state.blogs[7]} />)
+      post_4 = (<Post post={this.state.blogs[6]} />)
+      post_5 = (<Post post={this.state.blogs[1]} />)
+      post_6 = (<Post post={this.state.blogs[3]} />)
+      post_7 = (<Post post={this.state.blogs[4]} />)
       return(
         <div className="blog-posts">
 
