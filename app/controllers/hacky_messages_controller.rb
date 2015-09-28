9832ff3e8018dc4960497693f98b772ad2473304
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-09-28 14:56:10 +1000

    Add expiry alert for getitquick messaging

--- app/controllers/hacky_messages_controller.rb
+++ app/controllers/hacky_messages_controller.rb
@@ -5,6 +5,9 @@ class HackyMessagesController < ApplicationController
   # Uses some totes hacky Feature flag to control routes
   # :getitquick_unavailable
   def getitquick_unavailable
+    if Date.current > Date.parse('2015-10-07')
+      NewRelic::Agent.notice_error('getitquick_unavailable is still active after expected re-enablement date (2015-10-07)')
+    end
     @collection = Products::CollectionResource.new(fast_making: true).read
   end
 end
