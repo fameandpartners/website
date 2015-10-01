672ec149a05bad1a92effff46363e30bbd75ab94
Nam Dang <namproctin@gmail.com>
2015-10-01 14:21:36 +0700

    fixing things

--- app/controllers/widgets/widgets_controller.rb
+++ app/controllers/widgets/widgets_controller.rb
@@ -1,5 +1,18 @@
 class Widgets::WidgetsController < ActionController::Base
-  include Concerns::SiteVersion
+  #include Concerns::SiteVersion
+  #include Spree::Core::ControllerHelpers::Order
+  #include Spree::Core::ControllerHelpers::Auth
+
+  layout 'redesign/application'
+  include Spree::AuthenticationHelpers
   include Spree::Core::ControllerHelpers::Order
   include Spree::Core::ControllerHelpers::Auth
+  include Spree::Core::ControllerHelpers::Common
+  include ApplicationHelper
+  include PathBuildersHelper
+  include Concerns::SiteVersion
+  include Concerns::UserCampaignable
+  include Concerns::AutomaticDiscount
+  include Marketing::Gtm::Controller::Container
+  include Preferences
 end
