9714e059d2cf164bb40ef8c6d75984dfa881aa2a
Nam Dang <namproctin@gmail.com>
2015-10-05 12:09:11 +0700

    remove old spec

--- spec/presenters/marketing/order_presenter_spec.rb
+++ spec/presenters/marketing/order_presenter_spec.rb
@@ -14,18 +14,6 @@ describe Marketing::OrderPresenter, type: :presenter do
     let(:dress_item) { build(:dress_item, price: 9.99, quantity: 1) }
     let(:adjustment) { build(:adjustment, adjustable_type: 'Spree::Order') }
     let(:order) { build_stubbed(:spree_order, number: 'R123', currency: 'BRL',line_items: [dress_item], adjustments: [adjustment], projected_delivery_date: Date.new) }
-
-    it 'build_line_items' do
-      result = described_class.build_line_items(order).first
-      expect(result[:sku]).to eq(dress_item.variant.sku)
-      expect(result[:name]).to eq(dress_item.variant.product.name)
-      expect(result[:making_options_text]).to eq(dress_item.making_options_text)
-      expect(result[:options_text]).to eq(dress_item.options_text)
-      expect(result[:quantity]).to eq(dress_item.quantity)
-      expect(result[:variant_display_amount]).to eq(dress_item.variant.display_amount)
-      expect(result[:display_amount]).to eq(dress_item.display_amount)
-    end
-
   end
 
   describe 'instance methods' do
