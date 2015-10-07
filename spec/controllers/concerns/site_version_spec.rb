f80624923a1e235569832572bd127577468e411f
Tiago Amaro <tiagocis@gmail.com>
2015-10-07 15:55:11 -0300

    spec #set_site_version

--- spec/controllers/concerns/site_version_spec.rb
+++ spec/controllers/concerns/site_version_spec.rb
@@ -91,13 +91,37 @@ describe Concerns::SiteVersion, type: :controller do
     end
 
     context 'params site version is not set' do
-      let(:australian_site_version) { create(:site_version, permalink: 'au', default: true) }
+      let(:australian_site_version) { build_stubbed(:site_version, permalink: 'au', default: true) }
 
       before(:each) { expect(SiteVersion).to receive(:default).and_return(australian_site_version) }
 
       it 'returns the default site version code' do
         expect(controller.site_version_param).to eq('au')
       end
+    end
+  end
+
+  describe '#set_site_version' do
+    it 'raises ArgumentError if no param is passed' do
+      expect { controller.set_site_version }.to raise_error(ArgumentError)
+    end
+
+    context 'given a new site version' do
+      let(:user)            { create(:spree_user) }
+      let(:order)           { build(:spree_order) }
+      let(:au_site_version) { build(:site_version, id: 101, permalink: 'au') }
+
+      subject(:subject_method) { controller.set_site_version(au_site_version) }
+
+      before(:each) do
+        expect(order).to receive(:use_prices_from).with(au_site_version)
+        allow(controller).to receive_messages(current_spree_user: user, session_order: order)
+        subject_method
+      end
+
+      it { expect(controller.instance_variable_get(:@current_site_version)).to eq au_site_version }
+      it { expect(session[:site_version]).to eq 'au' }
+      it { expect(user.site_version_id).to eq 101 }
     end
   end
 end
