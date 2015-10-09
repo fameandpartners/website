d2413c3bb41dafc2c79bebb13198f5c070efebaf
Tiago Amaro <tiagocis@gmail.com>
2015-10-08 21:06:33 -0300

    remove data migrate and its data migrations (old)

--- db/data/20140314070528_destroy_old_customization_values.rb
+++
@@ -1,9 +0,0 @@
-class DestroyOldCustomizationValues < ActiveRecord::Migration
-  def self.up
-    CustomisationValue.where('customisation_type_id IS NOT NULL').destroy_all
-  end
-
-  def self.down
-    raise IrreversibleMigration
-  end
-end
