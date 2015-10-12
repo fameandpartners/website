4d83224db40e6c30198fd0d2e0aaf4d588ee340e
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-10-12 17:57:55 +1100

    Always push prices onto non master variants

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -14,7 +14,7 @@ Spree::Variant.class_eval do
 
   before_validation :set_default_sku
 
-  before_save :push_changed_prices_to_variants
+  before_save :push_prices_to_variants
 
   after_save do
     product.update_index
@@ -212,13 +212,11 @@ Spree::Variant.class_eval do
   end
 
   # Used as a callback, so must return a truthy value to avoid breaking the save
-  def push_changed_prices_to_variants
-    return true unless is_master
-    changed_prices = prices.select(&:changed?)
-    return true if changed_prices.empty?
+  def push_prices_to_variants
+    return :not_master unless is_master
 
     product.variants.each do |v|
-      changed_prices.each do |master_price|
+      prices.each do |master_price|
         variant_price = v.prices.where(currency: master_price.currency).first_or_initialize
 
         # TODO - If we ever move to variants with different prices to the master
@@ -230,6 +228,6 @@ Spree::Variant.class_eval do
       end
     end
 
-    true
+    :completed
   end
 end
