ef33e1f1abeee5753c1946ecdee19b53382a911e
Wayne Harlech-Jones <waynehj@famesandpartners.com>
2015-10-21 08:09:51 +1100

    Fix page features
    Make some changes to spec.

--- engines/admin_ui/spec/controllers/admin_ui/pages_controller_spec.rb
+++ engines/admin_ui/spec/controllers/admin_ui/pages_controller_spec.rb
@@ -1,34 +1,37 @@
 require 'rails_helper'
-require_relative '../../support/authorization_support'
 
-RSpec.describe AdminUi::Content::PagesController, :type => :controller do
+module AdminUi
+  module Content
+    RSpec.describe PagesController, :type => :controller do
 
-  routes { AdminUi::Engine.routes }
-  before(:each) { stub_admin_authorization! }
+      routes { AdminUi::Engine.routes }
 
-  describe "GET index" do
-    let!(:page) { Revolution::Page.create(path: "/dresses/Test") }
+      describe "GET index" do
+        let!(:page) { Revolution::Page.create(path: "/dresses/Test") }
 
-    it "finds a entry in pages when giving a search parameter" do
-      get :index, search: "Test"
-      expect(@controller.send(:collection)[0]).to eql(page)
-    end
+        it "finds a entry in pages when giving a search parameter" do
+          get :index, search: "Test"
+          expect(@controller.send(:collection)[0]).to eql(page)
+        end
 
-    it "finds a entry in pages when giving a search parameter in a different case" do
-      get :index, search: "test"
-      expect(@controller.send(:collection)[0]).to eql(page)
-    end
+        it "finds a entry in pages when giving a search parameter in a different case" do
+          get :index, search: "test"
+          expect(@controller.send(:collection)[0]).to eql(page)
+        end
 
-    it "does not find an entry in pages when giving a search parameter not pages" do
-      get :index, search: "un-test"
-      expect(@controller.send(:collection)).to be_empty
-    end
+        it "does not find an entry in pages when giving a search parameter not pages" do
+          get :index, search: "un-test"
+          expect(@controller.send(:collection)).to be_empty
+        end
 
-    it "finds all entries in pages when giving an empty search parameter" do
-      get :index, search: ""
-      all_count = Revolution::Page.count
-      expect(@controller.send(:collection).size).to eql(all_count)
+        it "finds all entries in pages when giving an empty search parameter" do
+          get :index, search: ""
+          all_count = Revolution::Page.count
+          expect(@controller.send(:collection).size).to eql(all_count)
+        end
+      end
+
     end
   end
-
 end
+
