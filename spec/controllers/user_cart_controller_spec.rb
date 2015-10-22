7d560dcd48388df86bfbcd6db0f340155f733f73
Nam Dang <namproctin@gmail.com>
2015-10-22 17:06:44 +0700

    add context

--- spec/controllers/user_cart_controller_spec.rb
+++ spec/controllers/user_cart_controller_spec.rb
@@ -8,26 +8,30 @@ RSpec.describe UserCart::ProductsController , :type => :controller do
     let!(:gift)                  { create(:spree_product, name:"Gift", price: 0) }
     let!(:gift_variant)          { create(:spree_variant, product_id: gift.id, sku:"gift-Color:Casablanca") }
 
-    it 'add gift to empty cart' do
-      allow(controller).to receive(:current_order).and_return(order)
-      expect(order.line_items.size).to eq(0)
-      xhr :get, :check_gift_in_cart
-      expect(response.body).to eq ("{\"has_gift\":false}")
-      xhr :post, :create, { gift_sku: "gift-Color:Casablanca" }
-      xhr :get, :check_gift_in_cart
-      expect(response.body).to eq ("{\"has_gift\":false}")
-      expect(order.line_items.size).to eq(0)
+    context 'user somehow adds a gift to an empty cart' do
+      it 'should not add a gift to an empty cart' do
+        allow(controller).to receive(:current_order).and_return(order)
+        expect(order.line_items.size).to eq(0)
+        xhr :get, :check_gift_in_cart
+        expect(response.body).to eq ("{\"has_gift\":false}")
+        xhr :post, :create, { gift_sku: "gift-Color:Casablanca" }
+        xhr :get, :check_gift_in_cart
+        expect(response.body).to eq ("{\"has_gift\":false}")
+        expect(order.line_items.size).to eq(0)
+      end
     end
 
-    it 'add gift to non empty cart' do
-      allow(controller).to receive(:current_order).and_return(order_with_line_items)
-      expect(order_with_line_items.line_items.size).to eq(1)
-      xhr :get, :check_gift_in_cart
-      expect(response.body).to eq ("{\"has_gift\":false}")
-      xhr :post, :create, { gift_sku: "gift-Color:Casablanca" }
-      xhr :get, :check_gift_in_cart
-      expect(response.body).to eq ("{\"has_gift\":true}")
-      expect(order_with_line_items.line_items.size).to eq(2)
+    context 'user adds a gift to a non empty cart' do
+      it 'add gift to non empty cart' do
+        allow(controller).to receive(:current_order).and_return(order_with_line_items)
+        expect(order_with_line_items.line_items.size).to eq(1)
+        xhr :get, :check_gift_in_cart
+        expect(response.body).to eq ("{\"has_gift\":false}")
+        xhr :post, :create, { gift_sku: "gift-Color:Casablanca" }
+        xhr :get, :check_gift_in_cart
+        expect(response.body).to eq ("{\"has_gift\":true}")
+        expect(order_with_line_items.line_items.size).to eq(2)
+      end
     end
   end
 end
