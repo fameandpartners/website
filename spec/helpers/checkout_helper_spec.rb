c04a8034259f4e377de79b0ed8090569679da238
Wayne Harlech-Jones <waynehj@famesandpartners.com>
2015-10-23 11:26:34 +1100

    USA zone default to United States
    Remove magic numbers from testing and build zone members.

--- spec/helpers/checkout_helper_spec.rb
+++ spec/helpers/checkout_helper_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe CheckoutHelper, :type => :helper do
 
   let!(:user) { create(:spree_user) }
   let!(:address) { create(:address, email: user.email) }
-  let(:zone) {  Spree::Zone.find(4) }
+  let(:zone) {  Spree::Zone.create(name: 'usa', description: 'united states of america', default_tax: false) }
+  let(:usa) { Spree::Country.create(iso_name: "UNITED STATES",  name: "United States", states_required: false) }
+  let(:canada) { Spree::Country.create(iso_name: "CANADA",  name: "Canada", states_required: false) }
   let(:current_site_version) { create(:site_version) }
   let(:usa_id) { Spree::Country.where(name: "United States").first.id }
   let(:address1) { set_us_default(address) }
@@ -14,6 +16,13 @@ RSpec.describe CheckoutHelper, :type => :helper do
 
     context 'given the user is in the usa zone' do
       before do
+        Spree::Country.create(iso_name: "AUSTRALIA",  name: "Australia", states_required: false)
+        zm = Spree::ZoneMember.create(zoneable_id: usa.id, zoneable_type: 'Spree::Country')
+        zone.zone_members << zm
+        zm = Spree::ZoneMember.create(zoneable_id: canada.id, zoneable_type: 'Spree::Country')
+        zone.zone_members << zm
+        zone.zone_members_count += 2
+        zone.save
         current_site_version.permalink = 'us'
         current_site_version.zone = zone
       end
