08dd0f6a596036e242a86a55e644d4a82b1136d2
Toby Hede <tobyhede@info-architects.net>
2015-10-25 11:01:26 +1100

    YOLO THE TYPO I JUST YOLO'D

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -29,8 +29,8 @@ class StaticsController < ApplicationController
   end
 
   def yelp
-    @title = "Our Customer Service Improvments " + default_seo_title
-    @description = "Our Customer Service Improvments"
+    @title = "Our Customer Service Improvements " + default_seo_title
+    @description = "Our Customer Service Improvements"
   end
 
   def why_us
