9ff5b26760e7e6ba7f66a88401667d9eb4c2e3e7
Toby Hede <tobyhede@info-architects.net>
2015-10-27 10:38:35 +1100

    Update to reduce feed freqruency

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,19 +2,13 @@ set :environment, ENV['RAILS_ENV']
 
 every 4.hours do
   runner 'EmailMarketing.send_emails'
+  # runner 'StockInvent::Runner.run'
 end
 
-every 1.hour do
-  rake   'feed:export:all'
-
-  runner 'StockInvent::Runner.run'
+every 1.day, at: '5:00 am' do
+  rake 'sitemap:create', output: { error: 'log/sitemap_error.log', standard: 'log/sitemap.log' }
 end
 
-every 1.day, :at => '5:00 am' do
-  rake "sitemap:create", :output => { :error => 'log/sitemap_error.log', :standard => 'log/sitemap.log' }
+every 1.day, at: '3:00 am' do
+  rake 'feed:export:all'
 end
-
-# TODO - 2015.05.22 - Enable this once we have run it a few times manually in production. SOYOLO
-# every 1.day, :at => '4:00 am' do
-#   rake "data:clean", :output => { :error => 'log/data_clean_error.log', :standard => 'log/data_clean.log' }
-# end
