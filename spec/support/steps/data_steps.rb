ccf33cda53bbeec2fec1653006d0e0e5187d61b9
Tiago Amaro <tiagoa@fameandpartners.com>
2015-10-26 20:57:27 -0200

    sample database with data steps (acceptance)
    Sample database has 2.4 MB

---
+++ spec/support/steps/data_steps.rb
@@ -0,0 +1,20 @@
+module Acceptance
+  module DataSteps
+    DUMP_FILE     = Rails.root.join('spec', 'fixtures', 'database_snapshots', 'minified_production_data.dump')
+    TEST_DATABASE = Rails.configuration.database_configuration['test']
+
+    # User           => email: spree@example.com, password: 123456
+    # Product        => name: Connie, id: 681
+    # Payment Method => PIN test
+    step 'A sample database with valid data' do
+      %x( pg_restore -d #{TEST_DATABASE['database']} --clean --if-exists --jobs 8 --no-acl -U #{TEST_DATABASE['username']} #{DUMP_FILE} )
+    end
+
+    step 'Data is setup correctly' do
+      Utility::Reindexer.reindex
+      ClearCacheWorker.new.perform
+    end
+  end
+end
+
+RSpec.configure { |c| c.include Acceptance::DataSteps, type: :feature }
