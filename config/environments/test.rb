33e529e1e9ce80a2eda7723ea51fe2f39b6ccda5
Tiago Amaro <tiagoa@fameandpartners.com>
2015-10-27 18:00:21 -0200

    test env should use s3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,6 +11,9 @@ FameAndPartners::Application.configure do
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
 
+  # Use S3 for storing attachments
+  config.use_s3 = true
+
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
 
