ab7a38937d21655335e93537e850be9d4ccfde4d
Tiago Amaro <tiagoa@fameandpartners.com>
2015-10-27 17:59:33 -0200

    wrap modals in feature flag

--- config/initializers/features.rb
+++ config/initializers/features.rb
@@ -14,11 +14,16 @@ if Rails.env.development?
   Features.deactivate(:test_analytics)
 end
 
+if Rails.env.test?
+  Features.deactivate(:marketing_modals)
+end
+
 Features.deactivate(:checkout_fb_login)
 Features.deactivate(:maintenance)
 Features.deactivate(:shipping_message)
 Features.deactivate(:send_promotion_email_reminder)
 
+Features.activate(:marketing_modals)
 Features.activate(:express_making)
 Features.activate(:gift)
 Features.activate(:masterpass)
