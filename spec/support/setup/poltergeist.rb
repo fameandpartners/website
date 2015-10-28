477bf93ecf18e7383a7934b990047b3dc5fba345
Tiago Amaro <tiagoa@fameandpartners.com>
2015-10-27 22:52:11 -0200

    new average resolution for poltergeist

--- spec/support/setup/poltergeist.rb
+++ spec/support/setup/poltergeist.rb
@@ -2,8 +2,9 @@ require 'capybara/poltergeist'
 
 Capybara.register_driver :poltergeist do |app|
   poltergeist_options = {
-      debug:             false,
-      js_errors:         false
+      debug:       false,
+      js_errors:   false,
+      window_size: [1366, 768]
   }
 
   Capybara::Poltergeist::Driver.new(app, poltergeist_options)
