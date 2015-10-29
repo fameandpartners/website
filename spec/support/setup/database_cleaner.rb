d33167f1d3f46195fb039c01b69ee6b23e73b84e
Tiago Amaro <tiagoa@fameandpartners.com>
2015-10-28 22:40:44 -0200

    don’t seed site zone on spec setup and use deletion strategy

--- spec/support/setup/database_cleaner.rb
+++ spec/support/setup/database_cleaner.rb
@@ -10,7 +10,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :feature) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :deletion
   end
 
   config.before(:each) do
