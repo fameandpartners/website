be8c101889843d815c78d1a5a7687953bff5ec34
Nam Dang <namproctin@gmail.com>
2015-10-30 12:00:20 +0700

    remove things

--- spec/policies/product_delivery_date_policy_spec.rb
+++ spec/policies/product_delivery_date_policy_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Policies::ProjectDeliveryDatePolicy do
-  let(:completed_at)           { DateTime.parse('Wed April 1 2015') }
   let(:print_dress)            { create(:spree_product, name:"print", description:" test Print test", price: 0) }
   let(:beading_dress)          { create(:spree_product, name:"bead", description:" test Beading test", price: 0) }
   let(:embroid_dress)          { create(:spree_product, name:"embroid", description:" test Embroid test", price: 0) }
@@ -40,15 +39,6 @@ describe Policies::ProjectDeliveryDatePolicy do
       service = Policies::ProjectDeliveryDatePolicy.new(standard_dress)
       allow(service).to receive(:fast_making?).and_return(false)
       expect(service.delivery_date).to eq({:days_for_making => 7, :days_for_delivery => 4})
-    end
-
-    it "return correct delivery date for an order with all fast making dresses" do
-    end
-
-    it "return correct delivery date for an order with a fast making dress and a standard dress" do
-    end
-
-    it "return correct delivery date for an order with all standard dresses" do
     end
   end
 end
