63161bce420d26907632f411ac587674c975b5e1
Nam Dang <namproctin@gmail.com>
2015-10-30 10:34:39 +0700

    refactor and change ui

--- app/controllers/user_cart/details_controller.rb
+++ app/controllers/user_cart/details_controller.rb
@@ -3,37 +3,7 @@ class UserCart::DetailsController < UserCart::BaseController
   before_filter :set_user_cart
 
   def order_delivery_date
-    exist_express_making = @user_cart.products.any?{ |p| p.making_options.any?{|mo| mo.name=='Express Making'}}
-    all_express_making   = @user_cart.products.all?{ |p| p.making_options.any?{|mo| mo.name=='Express Making'}}
-
-    if all_express_making || !exist_express_making
-      max = 0
-      @user_cart.products.each do |p|
-        date_num = Policies::ProjectDeliveryDatePolicy.new(p).delivery_date
-        date_num = date_num[:days_for_making] + date_num[:days_for_delivery] + configatron.days_delivery_emergency
-        max = date_num if date_num > max
-      end
-      date = (Date.today + max).strftime("%d %B")
-      render json: {date: date}
-      return
-    end
-
-    if exist_express_making
-      max_express     = 0
-      max_non_express = 0
-      @user_cart.products.each do |p|
-        date_num = Policies::ProjectDeliveryDatePolicy.new(p).delivery_date
-        date_num = date_num[:days_for_making] + date_num[:days_for_delivery] + configatron.days_delivery_emergency
-        if p.making_options.any?{|mo| mo.name == 'Express Making'}
-          max_express = date_num if date_num > max_express
-        else
-          max_non_express = date_num if date_num > max_non_express
-        end
-      end
-      date_express     = (Date.today + max_express).strftime("%d %B")
-      date_non_express = (Date.today + max_non_express).strftime("%d %B")
-      render json: {date_express: date_express, date_non_express: date_non_express}
-    end
+    render json: Policies::ProjectDeliveryDatePolicy.order_delivery_date(@user_cart)
   end
 
   def show
