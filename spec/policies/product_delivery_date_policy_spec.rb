177b9eb86b26a77934b41f3c1478025efc601798
Nam Dang <namproctin@gmail.com>
2015-11-02 09:40:49 +0700

    refactor tests

--- spec/policies/product_delivery_date_policy_spec.rb
+++ spec/policies/product_delivery_date_policy_spec.rb
@@ -1,44 +1,67 @@
 require 'spec_helper'
 
 describe Policies::ProjectDeliveryDatePolicy do
-  let(:print_dress)            { create(:spree_product, name:"print", description:"Print", price: 0) }
-  let(:beading_dress)          { create(:spree_product, name:"bead", description:"Beading", price: 0) }
-  let(:embroid_dress)          { create(:spree_product, name:"embroid", description:"Embroid", price: 0) }
-  let(:fast_making_dress)      { create(:spree_product, name:"test fast making", price: 0) }
-  let(:standard_dress)         { create(:spree_product, name:"test standard dress", price: 0) }
 
-  context "get delivery date" do
+  context "get delivery date for printed dress" do
+    let(:print_dress)            { create(:spree_product, name:"print", description:"Print", price: 0) }
+    let(:service)                { described_class.new(print_dress) }
+
     it "return correct delivery date for a printed dress" do
-      service = Policies::ProjectDeliveryDatePolicy.new(print_dress)
       service.printed?.should_not be_nil
-      expect(service.special_order?).to eq(true)
       expect(service.delivery_date).to eq({:days_for_making => 9, :days_for_delivery => 4})
     end
+
+    it "is a special order" do
+      expect(service.special_order?).to eq(true)
+    end
+  end
+
+  context "get delivery date for beading dress" do
+    let(:beading_dress)          { create(:spree_product, name:"bead", description:"Beading", price: 0) }
+    let(:service)                { described_class.new(beading_dress) }
 
     it "return correct delivery date for a beading dress" do
-      service = Policies::ProjectDeliveryDatePolicy.new(beading_dress)
       service.beading?.should_not be_nil
-      expect(service.special_order?).to eq(true)
       expect(service.delivery_date).to eq({:days_for_making => 9, :days_for_delivery => 4})
     end
+
+    it "is a special order" do
+      expect(service.special_order?).to eq(true)
+    end
+  end
+
+  context "get delivery date for embroid dress" do
+    let(:embroid_dress)          { create(:spree_product, name:"embroid", description:"Embroid", price: 0) }
+    let(:service)                { described_class.new(embroid_dress) }
 
     it "return correct delivery date for a embroid dress" do
-      service = Policies::ProjectDeliveryDatePolicy.new(embroid_dress)
       service.embroidered?.should_not be_nil
-      expect(service.special_order?).to eq(true)
       expect(service.delivery_date).to eq({:days_for_making => 9, :days_for_delivery => 4})
     end
 
+    it "is a special order" do
+      expect(service.special_order?).to eq(true)
+    end
+  end
+
+  context "get delivery date for fast making dress" do
+    let(:fast_making_dress)      { create(:spree_product, name:"test fast making", price: 0) }
+    let(:service)                { described_class.new(fast_making_dress) }
+
     it "return correct delivery date for a fast making dress" do
-      service = Policies::ProjectDeliveryDatePolicy.new(fast_making_dress)
       allow(service).to receive(:fast_making?).and_return(true)
       expect(service.delivery_date).to eq({:days_for_making => 5, :days_for_delivery => 4})
     end
+  end
+
+  context "get delivery date for standard dress" do
+    let(:standard_dress)         { create(:spree_product, name:"test standard dress", price: 0) }
+    let(:service)                { described_class.new(standard_dress) }
 
     it "return correct delivery date for a standard dress" do
-      service = Policies::ProjectDeliveryDatePolicy.new(standard_dress)
       allow(service).to receive(:fast_making?).and_return(false)
       expect(service.delivery_date).to eq({:days_for_making => 7, :days_for_delivery => 4})
     end
   end
+
 end
