380442674da21297cb5df9f3d8e03889eb889c08
Radek Valenta <radekvalenta@Radeks-MBP.lan>
2015-11-05 21:19:51 +1100

    updated delivery message ui and styling

--- app/assets/javascripts/components/delivery.js.jsx
+++ app/assets/javascripts/components/delivery.js.jsx
@@ -2,16 +2,18 @@ var DeliveryTime = React.createClass({
   render: function(){
     if (this.props.start_date != null){
       return (
-        <span className="time-bold">
-          {this.props.start_date} - {this.props.end_date}
-        </span>
+        <ul>
+          <li>Between {this.props.start_date} and {this.props.end_date}</li>
+        </ul>
       )
     } else {
       return(
-        <span className="time-bold">
-          <div className="clearfix">{this.props.start_date_express} - {this.props.end_date_express} for express making dresses</div>
-          <div className="clearfix">{this.props.start_date_non_express} - {this.props.end_date_non_express} for express standard making dresses</div>
-        </span>
+        <ul>
+          <li>Between {this.props.start_date_express} and {this.props.end_date_express} 
+            <em> for express making dresses</em></li>
+          <li>Between {this.props.start_date_non_express} and {this.props.end_date_non_express} 
+            <em> for express standard making dresses</em></li>
+        </ul>
       )
     }
   }
@@ -36,37 +38,15 @@ var Delivery = React.createClass({
   },
   render: function() {
     return(
-      <div className="row delivered-row">
-        <div className={this.props.terms + " col-xs-6 col-sm-6 col-md-6 delivery-time"}>
-          <div className="col-xs-2 col-sm-2 col-md-2 icon-delivery-express">
+      <div className="row">
+        <div className="delivery-wrap">
+          <div className="inner-wrap">
+            <span className="icon icon-delivery-express"></span>
+            <div>
+              <div className="title">Delivery Guaranteed*</div>
+              <DeliveryTime start_date={this.state.start_date} end_date={this.state.end_date} start_date_express={this.state.start_date_express} end_date_express={this.state.end_date_express} start_date_non_express={this.state.start_date_non_express} end_date_non_express={this.state.end_date_non_express}/>
+            </div>
           </div>
-          <div className="truck"></div>
-          <div className="col-xs-10 col-sm-10 col-md-10 left-col">
-            <div className="head">
-              Delivery time
-            </div>
-            <div className="tail">
-              <span className="get-your">
-                Get your dress by the
-              </span>
-              <span className="time-bold">
-                <DeliveryTime start_date={this.state.start_date} end_date={this.state.end_date} start_date_express={this.state.start_date_express} end_date_express={this.state.end_date_express} start_date_non_express={this.state.start_date_non_express} end_date_non_express={this.state.end_date_non_express}/>
-              </span>
-            </div>
-
-          </div>
-        </div>
-        <div className={this.props.color +" "+ this.props.terms+ " col-xs-6 col-sm-6 col-md-6 delivery-time-text"}>
-          <span className="right-col">
-            <div className="clock"></div>
-            <div className="text guaranteed">
-              Delivery time guaranteed
-            </div>
-            <div className="text">
-              <div className="terms">Terms and conditions apply</div>
-            </div>
-          </span>
-          <span className="triangle"></span>
         </div>
       </div>
     )
