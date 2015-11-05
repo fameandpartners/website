37651146eaebef3f9f1f423c705d628bfa9c169d
Nam Dang <namproctin@gmail.com>
2015-11-05 15:55:54 +0700

    refactor

--- spec/controllers/user_cart/products_controller_spec.rb
+++ spec/controllers/user_cart/products_controller_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 module UserCart
   describe ProductsController , :type => :controller do
     describe 'add gift to cart via ajax' do
-      let!(:shipping_method)       { Spree::ShippingMethod.create(name: 'test') }
-      let!(:currency)              { SiteVersion.default.currency }
-      let!(:order)                 { create(:spree_order, currency: currency, shipping_method: shipping_method.id) }
-      let!(:order_with_line_items) { create(:complete_order_with_items) }
-      let!(:gift)                  { create(:spree_product, name:"Gift", price: 0) }
-      let!(:gift_variant)          { create(:spree_variant, product_id: gift.id, sku:"gift-Color:Casablanca") }
+      let!(:shipping_method)            { Spree::ShippingMethod.create(name: 'test') }
+      let!(:currency)                   { SiteVersion.default.currency }
+      let!(:order)                      { create(:spree_order, currency: currency, shipping_method: shipping_method.id) }
+      let!(:order_with_line_items)      { create(:complete_order_with_items) }
+      let!(:gift)                       { create(:spree_product, name:"Gift", price: 0) }
+      let!(:gift_variant)               { create(:spree_variant, product_id: gift.id, sku:"gift-Color:Casablanca") }
 
-      let(:product)                { create(:dress) }
-      let!(:variant)               { create(:spree_variant, product_id: product.id, sku: 'my-test-sku') }
-      let!(:variant2)              { create(:spree_variant, product_id: product.id, sku: 'my-test-sku2') }
-      let!(:line_item_personalization) { LineItemPersonalization.new(id: 1)}
+      let!(:product)                    { create(:dress) }
+      let!(:variant)                    { create(:spree_variant, product_id: product.id, sku: 'my-test-sku') }
+      let!(:variant2)                   { create(:spree_variant, product_id: product.id, sku: 'my-test-sku2') }
+      let!(:line_item_personalization)  { LineItemPersonalization.new }
 
       context 'user add a dress to cart ' do
         it 'add one line item to order per add to cart request' do
@@ -26,16 +26,14 @@ module UserCart
           allow_any_instance_of(UserCart::Populator).to receive(:personalized_product?).and_return(true)
           allow_any_instance_of(UserCart::Populator).to receive(:build_personalization).and_return(line_item_personalization)
 
-          line_item_personalization.id = 1
-          line_item_personalization.line_item_id= 1
-          line_item_personalization.product_id= product.id
-
-
           expect(order.line_items.size).to eq(0)
+
           xhr :post, :create, { variant_id: variant.id, quantity: 1 }
           expect(order.line_items.size).to eq(1)
+
           xhr :post, :create, { variant_id: variant.id, quantity: 1 }
           expect(order.line_items.size).to eq(2)
+
           xhr :post, :create, { variant_id: variant2.id, quantity: 1 }
           expect(order.line_items.size).to eq(3)
         end
