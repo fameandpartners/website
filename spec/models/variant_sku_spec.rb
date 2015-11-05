e96a86844e8e63ec66cb96c94ed84e6151951126
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-11-05 15:39:18 +1100

    Move characterisation tests back to Spree::Variant

--- spec/models/variant_sku_spec.rb
+++ spec/models/variant_sku_spec.rb
@@ -6,34 +6,38 @@ RSpec.describe VariantSku do
   let(:dress)         { create :dress, sku: style_number }
   let(:variant)       { dress.master }
   let(:sku_generator) { described_class.new(variant) }
-  subject(:sku)       { sku_generator.call }
 
-  context 'master variant' do
-    it "returns the master's sku" do
-      expect(sku).to eq style_number
-    end
-  end
 
-  context 'with variants' do
-    let(:dress)   { create :dress_with_variants, sku: style_number}
+  describe 'existing SKUs' do
+    subject(:sku)       { variant.generate_sku  }
 
-    subject(:variant_skus) {
-      dress.variants.map {|variant| described_class.new(variant).call }
-    }
-
-    it 'includes the style number' do
-      expect(variant_skus[0]).to include(style_number)
-      expect(variant_skus[1]).to include(style_number)
-      expect(variant_skus[2]).to include(style_number)
+    context 'master variant' do
+      it "returns the master's sku" do
+        expect(sku).to eq style_number
+      end
     end
 
-    it 'includes the colour' do
-      expect(variant_skus[0]).to include('Color:Red')
-      expect(variant_skus[1]).to include('Color:Red')
-      expect(variant_skus[2]).to include('Color:Red')
+    context 'with variants' do
+      let(:dress)   { create :dress_with_variants, sku: style_number}
+
+      subject(:variant_skus) {
+        dress.variants.map {|variant| variant.generate_sku }
+      }
+
+      it 'includes the style number' do
+        expect(variant_skus[0]).to include(style_number)
+        expect(variant_skus[1]).to include(style_number)
+        expect(variant_skus[2]).to include(style_number)
+      end
+
+      it 'includes the colour' do
+        expect(variant_skus[0]).to include('Color:Red')
+        expect(variant_skus[1]).to include('Color:Red')
+        expect(variant_skus[2]).to include('Color:Red')
+      end
+
+      xit "includes the size"
+
     end
-
-    xit "includes the size"
-
   end
 end
