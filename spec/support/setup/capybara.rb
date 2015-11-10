dea62e07c628da566971c5d5e2d02b1fc2063d70
Tiago Amaro <tiagoa@fameandpartners.com>
2015-11-10 20:28:29 -0200

    make Capybara’s max wait time configurable by ENV variable

--- spec/support/setup/capybara.rb
+++ spec/support/setup/capybara.rb
@@ -2,6 +2,6 @@ Capybara.configure do |config|
   config.match = :prefer_exact
 end
 
-Capybara.default_max_wait_time = 180
 # Capybara.javascript_driver     = :poltergeist
 Capybara.javascript_driver     = :selenium
+Capybara.default_max_wait_time = ENV.fetch('CAPYBARA_DEFAULT_MAX_WAIT_TIME', '180').to_i
