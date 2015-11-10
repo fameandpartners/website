630f4c67f082f8af5126d75e864b675cbe093e88
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-11-10 16:47:46 +1100

    Get rid of constant definition warnings
    Defining constants doesn't work inside namespace class references.

--- app/models/spree/taxonomy_decorator.rb
+++ app/models/spree/taxonomy_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Taxonomy.class_eval do
   # TODO: This shouldn't be hardcoded here. It would be interesting if taxonomies had the same
   # publishable concern as taxons, but it lacks an admin interface
-  CURRENT = %w(Event Range Style Edits)
-  OUTERWEAR_NAME = 'Outerwear'
+  ::Spree::Taxonomy::CURRENT = %w(Event Range Style Edits)
+  ::Spree::Taxonomy::OUTERWEAR_NAME = 'Outerwear'
 end
