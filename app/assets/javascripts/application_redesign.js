0692799401f7a586b15489952ea4c45c40938e84
Tiago Amaro <tiagoa@fameandpartners.com>
2015-11-11 14:52:25 -0200

    add es5-shim due to PhantomJS + React
    More details at https://github.com/reactjs/react-rails/issues/10#issuecomment-41210548

--- app/assets/javascripts/application_redesign.js
+++ app/assets/javascripts/application_redesign.js
@@ -13,6 +13,11 @@
 //= require libs/jquery.dotdotdot.min
 
 // React
+
+// Adding es5-shim due to PhantomJS
+// https://github.com/reactjs/react-rails/issues/10#issuecomment-41210548
+
+//= require es5-shim/es5-shim.js
 //= require react
 //= require react_ujs
 //= require components
