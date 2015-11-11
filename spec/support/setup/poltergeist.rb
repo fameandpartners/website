1572acd25a8f75fe53c85b34b5226f5799f71a8b
Tiago Amaro <tiagoa@fameandpartners.com>
2015-11-11 16:21:05 -0200

    poltergeist doesn’t obey capybara max default wait time

--- spec/support/setup/poltergeist.rb
+++ spec/support/setup/poltergeist.rb
@@ -4,6 +4,7 @@ Capybara.register_driver :poltergeist do |app|
   poltergeist_options = {
       debug:       false,
       js_errors:   true,
+      timeout:     ENV.fetch('CAPYBARA_DEFAULT_MAX_WAIT_TIME', '180').to_i,
       window_size: [1366, 768]
   }
 
