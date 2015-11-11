30cbd1692aab37bb7fa8c5b572fc7f34f8055e0e
Tiago Amaro <tiagoa@fameandpartners.com>
2015-11-11 14:53:11 -0200

    use poltergeist as JS driver

--- spec/support/setup/poltergeist.rb
+++ spec/support/setup/poltergeist.rb
@@ -1,13 +1,13 @@
-# Using selenium for the moment
+require 'capybara/poltergeist'
 
-# require 'capybara/poltergeist'
-#
-# Capybara.register_driver :poltergeist do |app|
-#   poltergeist_options = {
-#       debug:       false,
-#       js_errors:   false,
-#       window_size: [1366, 768]
-#   }
-#
-#   Capybara::Poltergeist::Driver.new(app, poltergeist_options)
-# end
+Capybara.register_driver :poltergeist do |app|
+  poltergeist_options = {
+      debug:       false,
+      js_errors:   true,
+      window_size: [1366, 768]
+  }
+
+  Capybara::Poltergeist::Driver.new(app, poltergeist_options)
+end
+
+Capybara.javascript_driver = :poltergeist
