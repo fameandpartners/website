254bc9590dc2082531c5bb66eccf1556ddc90cd4
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-11-12 11:20:23 +1100

    Partition feature specs last

---
+++ spec/support/setup/spec_partitioning.rb
@@ -0,0 +1,8 @@
+# Taken from https://gist.github.com/JonRowe/867423bab8201b1f852b
+
+RSpec.configure do |config|
+   config.register_ordering :global do |examples|
+     acceptance, unit = examples.partition { |ex| ex.metadata[:feature] }
+     unit.shuffle + acceptance.shuffle
+  end
+end
