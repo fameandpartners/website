40820d9aa88f0878a1ebf5a5029d4a71b9601369
Tiago Amaro <tiagoa@fameandpartners.com>
2015-11-11 22:17:23 -0200

    wait dress size to be visible and then click its DOM node

--- spec/support/steps/product_steps.rb
+++ spec/support/steps/product_steps.rb
@@ -2,7 +2,7 @@ module Acceptance
   module ProductSteps
     step 'I select :dress_size size' do |dress_size|
       find('#product-size-action', visible: true).click
-      click_link dress_size
+      find(:link, dress_size, visible: true).trigger('click')
     end
   end
 end
