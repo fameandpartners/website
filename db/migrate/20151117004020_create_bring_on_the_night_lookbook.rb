f57f3be3ea5c375d87884a4f8eaa7186890efce5
Toby Hede <tobyhede@info-architects.net>
2015-11-17 12:16:05 +1100

    Bring on the Night Landing Page

--- db/migrate/20151117004020_create_bring_on_the_night_lookbook.rb
+++ db/migrate/20151117004020_create_bring_on_the_night_lookbook.rb
@@ -3,7 +3,7 @@ class CreateBringOnTheNightLookbook < ActiveRecord::Migration
     page = Revolution::Page.create!(
       :path => '/lookbook/bring-on-the-night',
       :template_path => '/lookbook/show.html.slim',
-      :variables => {"image_count"=>6, "lookbook"=>true, "limit"=>24}
+      :variables => {:image_count=>6, :lookbook => true, :limit => 24}
     )
     page.translations.create!(:locale => 'en-US', :title => 'Bring on the Night', :meta_description => 'Bring on the Night', :heading => 'Bring on the Night')
     page.publish!
