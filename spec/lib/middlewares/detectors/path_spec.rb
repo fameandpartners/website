193aa9e7fc5bb2867e23b94d372c6e58f608ca83
Tiago Amaro <tiagoa@fameandpartners.com>
2015-11-18 12:54:25 -0200

    test more scenarios for path detector

--- spec/lib/middlewares/detectors/path_spec.rb
+++ spec/lib/middlewares/detectors/path_spec.rb
@@ -7,30 +7,32 @@ module Middleware
         describe '#detect_site_version' do
           subject { described_class.new }
 
+          shared_examples 'it will return code for path' do |sv_code, path|
+            let(:request) { double(Rack::Request, path: path) }
+
+            it do
+              result = subject.detect_site_version(request)
+              expect(result).to eq(sv_code)
+            end
+          end
+
           context 'given a rack request' do
             context 'with a path containing AU code' do
-              shared_examples 'it will return the au code' do |path|
-                let(:request)  { double(Rack::Request, path: path) }
-
-                it do
-                  result = subject.detect_site_version(request)
-                  expect(result).to eq('au')
-                end
-              end
-
-              it_behaves_like 'it will return the au code', '/au'
-              it_behaves_like 'it will return the au code', '/au/'
-              it_behaves_like 'it will return the au code', '/au/something'
-              it_behaves_like 'it will return the au code', '/au/something/'
+              it_behaves_like 'it will return code for path', 'au', '/au'
+              it_behaves_like 'it will return code for path', 'au', '/au/'
+              it_behaves_like 'it will return code for path', 'au', '/au/something'
+              it_behaves_like 'it will return code for path', 'au', '/au/something/'
             end
 
             context 'with an empty path' do
-              let(:request) { double(Rack::Request, path: '/something') }
+              it_behaves_like 'it will return code for path', 'us', '/'
+              it_behaves_like 'it will return code for path', 'us', '/something'
+            end
 
-              it 'returns the default code' do
-                result = subject.detect_site_version(request)
-                expect(result).to eq('us')
-              end
+            context 'with an invalid code' do
+              it_behaves_like 'it will return code for path', 'us', '/br'
+              it_behaves_like 'it will return code for path', 'us', '/br/'
+              it_behaves_like 'it will return code for path', 'us', '/br/something'
             end
           end
         end
