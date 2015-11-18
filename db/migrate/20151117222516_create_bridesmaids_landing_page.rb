21076d8729e7082fb0f759dd12eb1e772874b5ff
Radek Valenta <radekvalenta@Radeks-MacBook-Pro.local>
2015-11-18 13:09:39 +1100

    routing, hero section styling and images

--- db/migrate/20151117222516_create_bridesmaids_landing_page.rb
+++ db/migrate/20151117222516_create_bridesmaids_landing_page.rb
@@ -1,15 +1,15 @@
 class CreateBridesmaidsLandingPage < ActiveRecord::Migration
   def up
     page = Revolution::Page.create!(
-      :path => '/landing/bridesmaids',
-      :template_path => '/landing/bridesmaid.html.slim',
+      :path => '/famingtonway',
+      :template_path => '/landing_pages/famingtonway.html.slim',
       :variables => {"limit"=>99}
     )
     page.publish!
-    page.translations.create!(:locale => 'en-US', :title => 'Bridesmaids', :meta_description => 'Bridesmaids', :heading => 'Bridesmaids')
+    page.translations.create!(:locale => 'en-US', :title => 'Famingtonway', :meta_description => 'Famingtonway', :heading => 'Famingtonway')
   end
 
   def down
-    Revolution::Page.where(:path => '/landing/bridesmaids').delete_all
+    Revolution::Page.where(:path => '/famingtonway').delete_all
   end
 end
