4308a026e67d79e806068c79a38a341f32621bd7
Tiago Amaro <tiagoa@fameandpartners.com>
2015-11-19 20:14:45 -0200

    generate feeds at 1 AM

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,6 +9,6 @@ every 1.day, at: '5:00 am' do
   rake 'sitemap:create', output: { error: 'log/sitemap_error.log', standard: 'log/sitemap.log' }
 end
 
-every 1.day, at: '3:00 am' do
+every 1.day, at: '1:00 am' do
   rake 'feed:export:all'
 end
