66e47bf22cfb16327383d9408ca54630d9829476
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-11-23 10:38:35 +1100

    Add scope to Product for making options

--- app/models/spree/product/custom_scopes.rb
+++ app/models/spree/product/custom_scopes.rb
@@ -4,10 +4,19 @@ module Spree
       def hydrated_from_ids(ids)
         unscoped.hydrated.where(id: Array(ids))
       end
+
       def hydrated
         includes(:variants             => [:prices],
                  :master               => [:prices],
                  :product_color_values => {:images => []})
+      end
+
+      def with_making_options
+        joins(:making_options)
+      end
+
+      def with_fast_making
+        with_making_options.where("product_making_options.option_type = 'fast_making'")
       end
     end
   end
