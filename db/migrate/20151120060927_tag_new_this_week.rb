4b706fc943b36b273555f778705d8d03ce28907c
Nam Dang <namproctin@gmail.com>
2015-11-23 23:58:10 +0700

    fix code

--- db/migrate/20151120060927_tag_new_this_week.rb
+++ db/migrate/20151120060927_tag_new_this_week.rb
@@ -1,12 +1,19 @@
 class TagNewThisWeek < ActiveRecord::Migration
-  def change
-    new_this_week_taxon_id = Spree::Taxon.where(name: 'New This Week').first.id
-    dress_names = ["Paradiso","Preacher","Regalia","Spoken For Two Piece","Said It All Two Piece","Prism","Mystic Goddess","Eyes On You Two Piece","Dark Storm","Metallic Spirit","Jascinta","Midnight Song","Shimmer Vixen","Sorrento","Feathered Lover","One And Only Two Piece","Matrix","Patrice","Halle","Krissa Two Piece","Artful Mind Two Piece","Fire Eyes","Wisdom Two Piece","Neo Velvet Two Piece","Jupiter Two Piece","Midnight Eyes Two Piece","Art Soul","Retrogade","Velvet Valour","Black Widow","Mercury Rising","Belgravia Velvet Two Piece","Light Field","Velvet Rebel Three Piece","Beyond Desire","Piranha"]
-    dress_names.each do |n|
-      p = Spree::Product.where(name: n).first
-      sql = "INSERT INTO \"spree_products_taxons\" (\"product_id\", \"taxon_id\") VALUES (#{p.id}, #{new_this_week_taxon_id})"
-      ActiveRecord::Base.connection.execute(sql)
+  def up
+    if new_this_week_taxon
+      dresses.find_each { |product| product.taxons << new_this_week_taxon if !product.taxons.any?{|t| t.name == 'New This Week'}}
     end
+  end
+
+  private
+
+  def new_this_week_taxon
+    @taxon ||= Spree::Taxon.where(name: 'New This Week').first
+  end
+
+  def dresses
+    dress_names = ["Paradiso","Preacher","Regalia","Spoken For Two Piece","Said It All Two Piece","Prism","Mystic Goddess","Eyes On You Two Piece","Dark Storm","Metallic Spirit","Jascinta","Midnight Song","Shimmer Vixen","Sorrento","Feathered Lover","One And Only Two Piece","Matrix","Patrice","Halle","Krissa Two Piece","Artful Mind Two Piece","Fire Eyes","Wisdom Two Piece","Neo Velvet Two Piece","Jupiter Two Piece","Midnight Eyes Two Piece","Art Soul","Retrogade","Velvet Valour","Black Widow","Mercury Rising","Belgravia Velvet Two Piece","Light Field","Velvet Rebel Three Piece","Beyond Desire","Piranha"]
+    @dresses ||= Spree::Product.where(name: dress_names)
   end
 end
 
