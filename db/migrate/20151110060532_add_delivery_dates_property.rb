0b4b9fd424d5cd8e5d683be89ad8c7d0c422c35c
Nam Dang <namproctin@gmail.com>
2015-11-24 16:46:08 +0700

    fix migration

--- db/migrate/20151110060532_add_delivery_dates_property.rb
+++ db/migrate/20151110060532_add_delivery_dates_property.rb
@@ -1,6 +1,6 @@
 class AddDeliveryDatesProperty < ActiveRecord::Migration
-  def change
-    Spree::Property.create(name: "standard_days_for_making", presentation: "Standard days for making")
-    Spree::Property.create(name: "customised_days_for_making", presentation: "Customised days for making")
+  def up
+    Spree::Property.create(name: "standard_days_for_making", presentation: "Standard days for making") if Spree::Property.where(name: "standard_days_for_making").blank?
+    Spree::Property.create(name: "customised_days_for_making", presentation: "Customised days for making") if Spree::Property.where(name: "customised_days_for_making").blank?
   end
 end
