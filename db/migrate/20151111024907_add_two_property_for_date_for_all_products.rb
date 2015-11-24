02b789278cfa8edc8d0b54d7ee21c0d02436e936
Nam Dang <namproctin@gmail.com>
2015-11-24 16:46:08 +0700

    fix migration

--- db/migrate/20151111024907_add_two_property_for_date_for_all_products.rb
+++ db/migrate/20151111024907_add_two_property_for_date_for_all_products.rb
@@ -1,26 +1,27 @@
 class AddTwoPropertyForDateForAllProducts < ActiveRecord::Migration
-  def change
+  def up
     property1_id = Spree::Property.where(name: "standard_days_for_making").first.id
     property2_id = Spree::Property.where(name: "customised_days_for_making").first.id
 
     Spree::Product.all.each do |p|
+      if p.properties.none?{|pro| pro.id == property1_id || pro.id == property2_id}
+        policy = Policies::ProjectDeliveryDatePolicy.new(p)
+        pp = Spree::ProductProperty.new
+        if policy.special_order?
+          pp.value      = 11
+        else
+          pp.value      = 6
+        end
+        pp.product_id   = p.id
+        pp.property_id  = property1_id
+        pp.save
 
-      policy = Policies::ProjectDeliveryDatePolicy.new(p)
-      pp = Spree::ProductProperty.new
-      if policy.special_order?
-        pp.value      = 11
-      else
-        pp.value      = 6
+        pp = Spree::ProductProperty.new
+        pp.value        = 11
+        pp.product_id   = p.id
+        pp.property_id  = property2_id
+        pp.save
       end
-      pp.product_id   = p.id
-      pp.property_id  = property1_id
-      pp.save
-
-      pp = Spree::ProductProperty.new
-      pp.value        = 11
-      pp.product_id   = p.id
-      pp.property_id  = property2_id
-      pp.save
     end
   end
 end
