eb7af563ea1a61f9efceffe2461b21c780a43827
Tiago Amaro <tiagoa@fameandpartners.com>
2015-11-27 12:19:17 -0200

    sidekiq controller under backend module

--- engines/admin_ui/app/controllers/admin_ui/sidekiqs_controller.rb
+++ engines/admin_ui/app/controllers/admin_ui/sidekiqs_controller.rb
@@ -1,10 +1,13 @@
 module AdminUi
-  class SidekiqsController < AdminUi::ApplicationController
+  module Backend
+    class SidekiqsController < AdminUi::ApplicationController
 
-    layout 'admin_ui_nav_only'
-    def show
-      # NOOP
-      # We render the sidekiq views into an iframe
+      layout 'admin_ui_nav_only'
+
+      def show
+        # NOOP
+        # We render the sidekiq views into an iframe
+      end
     end
   end
 end
