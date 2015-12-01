05fad283d0488a891ff9195a87f21dcf6f59628f
Tiago Amaro <tiagoa@fameandpartners.com>
2015-12-01 19:28:48 -0200

    setup capybara’s default host

--- spec/support/setup/capybara.rb
+++ spec/support/setup/capybara.rb
@@ -1,5 +1,7 @@
 Capybara.configure do |config|
-  config.match = :prefer_exact
+  config.always_include_port = true
+  config.match               = :prefer_exact
 end
 
+Capybara.default_host          = ENV.fetch('CAPYBARA_DEFAULT_HOST', 'us.lvh.me')
 Capybara.default_max_wait_time = ENV.fetch('CAPYBARA_DEFAULT_MAX_WAIT_TIME', '180').to_i
