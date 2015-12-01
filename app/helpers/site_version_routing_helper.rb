3466c86becd37d7f444df252ad0c23bd59d28387
Tiago Amaro <tiagoa@fameandpartners.com>
2015-12-01 14:55:48 -0200

    make site version url helper use detector method

--- app/helpers/site_version_routing_helper.rb
+++ app/helpers/site_version_routing_helper.rb
@@ -1,5 +1,11 @@
 module SiteVersionRoutingHelper
   def site_version_url(current_url, site_version)
-    LocalizeUrlService.localize_url(current_url, site_version)
+    detector.site_version_url(current_url, site_version)
+  end
+
+  private
+
+  def detector
+    configatron.site_version_detector.new
   end
 end
