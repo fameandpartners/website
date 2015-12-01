23bd54e6855e3b7d1905de1641c62c4666a02dbd
Tiago Amaro <tiagoa@fameandpartners.com>
2015-12-01 19:29:33 -0200

    change support file to create switch subdomains methods along different types of tests

--- spec/support/capybara_host_support.rb
+++ spec/support/capybara_host_support.rb
@@ -1,15 +1,46 @@
+# Based on https://gist.github.com/turadg/5399790
+
+# Support for Rspec / Capybara subdomain integration testing
+# Make sure this file is required by spec_helper.rb
+# (e.g. save as spec/support/subdomains.rb)
+
 module CapybaraHostSupport
-  def with_capybara_host(host, &block)
-    capybara_default_host = Capybara.default_host
+  module Controllers
+    def switch_to_subdomain(subdomain)
+      hostname      = subdomain ? "#{subdomain}.lvh.me" : 'lvh.me'
+      @request.host = hostname
+    end
+  end
 
-    default_url_options[:host] = host
-    Capybara.app_host          = "http://#{host}"
-    yield
+  module Features
+    def switch_to_subdomain(subdomain)
+      hostname          = subdomain ? "#{subdomain}.lvh.me" : 'lvh.me'
+      Capybara.app_host = "http://#{hostname}"
+    end
+  end
 
-    Capybara.default_host = capybara_default_host
+  module Requests
+    def switch_to_subdomain(subdomain)
+      hostname = subdomain ? "#{subdomain}.lvh.me" : 'lvh.me'
+      host! hostname
+    end
   end
 end
 
 RSpec.configure do |config|
-  config.include CapybaraHostSupport, type: :feature
+  config.include CapybaraHostSupport::Controllers, type: :controller
+  config.include CapybaraHostSupport::Features, type: :feature
+  config.include CapybaraHostSupport::Requests, type: :request
+
+  config.before(:each, type: :request) do
+    switch_to_subdomain 'us'
+  end
+
+  config.before(:each, type: :feature) do
+    switch_to_subdomain 'us'
+  end
+
+  config.before(:each, type: :controller) do
+    switch_to_subdomain 'us'
+  end
 end
