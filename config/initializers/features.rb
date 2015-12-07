4687c99386d88158aa8d6e39ebf2e25bcb293c19
Toby Hede <tobyhede@info-architects.net>
2015-12-07 15:36:27 +1100

    Deactivate gift modal

--- config/initializers/features.rb
+++ config/initializers/features.rb
@@ -24,13 +24,14 @@ if Rails.env.test?
 end
 
 Features.deactivate(:checkout_fb_login)
+Features.deactivate(:gift)
 Features.deactivate(:maintenance)
 Features.deactivate(:shipping_message)
 Features.deactivate(:send_promotion_email_reminder)
 
 Features.activate(:marketing_modals)
 Features.activate(:express_making)
-Features.activate(:gift)
+
 Features.activate(:masterpass)
 Features.activate(:moodboard)
 Features.activate(:sales)
