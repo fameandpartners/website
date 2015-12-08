e94e97cbcb154aeeeb3f05713b2302deceeea57a
Nam Dang <namproctin@gmail.com>
2015-12-08 10:05:56 +0700

    [feat] remove moodboard icon and counter
    I remove moodboard icon and counter

--- app/assets/javascripts/components/user/moodboards.js.jsx
+++ app/assets/javascripts/components/user/moodboards.js.jsx
@@ -1,30 +1,5 @@
 // see helpers.UserMoodboard
 // app/assets/javascripts/partials/helpers/user_moodboard.js.coffee
-var MoodboardItemCount = React.createClass({
-  getInitialState: function(){
-    return { count: this.props.count || 0 };
-  },
-
-  componentDidMount: function(){
-    window.app.user_moodboard.on('change', this.handleChange);
-  },
-
-  handleChange: function(_ev){
-    this.setState({
-      // TODO - Remove the hard coded thingo & use event data.
-      count: window.app.user_moodboard.itemCount()
-    })
-  },
-
-  render: function(){
-    return(
-      <a href={this.props.moodboard_link}>
-        <span className="copy">{this.state.count}</span>
-        <span className="icon icon-heart"></span>
-      </a>
-    );
-  }
-});
 
 /**
  * Takes a moodboard, color_id, and product_id
