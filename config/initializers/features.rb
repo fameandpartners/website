0779927ff945debb98741d29fb2b3d7bd337fa22
Tiago Amaro <tiagoa@fameandpartners.com>
2015-12-08 16:36:24 -0200

    reorganize feature flags file

--- config/initializers/features.rb
+++ config/initializers/features.rb
@@ -1,39 +1,41 @@
+# Please, aggregate flags by deactivated/activated groups, and sorted by alphabetical order
+# This organization helps a lot on finding what's active or not on each env
+
+Features.deactivate(:checkout_fb_login)
+Features.deactivate(:content_revolution)
+Features.deactivate(:enhanced_moodboards)
+Features.deactivate(:maintenance)
+Features.deactivate(:send_promotion_email_reminder)
+Features.deactivate(:shipping_message)
+Features.deactivate(:test_analytics)
+
+Features.activate(:delivery_date_messaging)
+Features.activate(:express_making)
+Features.activate(:gift)
+Features.activate(:google_tag_manager)
+Features.activate(:marketing_modals)
+Features.activate(:masterpass)
+Features.activate(:moodboard)
+Features.activate(:style_quiz)
+
 if Rails.env.production?
-  Features.activate(:google_tag_manager)
-  Features.activate(:masterpass)
-  Features.deactivate(:enhanced_moodboards)
 end
 
 if Rails.env.preproduction?
-  Features.activate(:google_tag_manager)
-  Features.activate(:masterpass)
 end
 
 if Rails.env.development?
   Features.activate(:content_revolution)
   Features.activate(:enhanced_moodboards)
+
   Features.deactivate(:google_tag_manager)
   Features.deactivate(:test_analytics)
 end
 
 if Rails.env.test?
+  Features.activate(:enhanced_moodboards)
+
   Features.deactivate(:google_tag_manager)
   Features.deactivate(:marketing_modals)
   Features.deactivate(:test_analytics)
-  Features.activate(:enhanced_moodboards)
 end
-
-Features.deactivate(:checkout_fb_login)
-Features.deactivate(:maintenance)
-Features.deactivate(:shipping_message)
-Features.deactivate(:send_promotion_email_reminder)
-
-
-Features.activate(:express_making)
-Features.activate(:gift)
-Features.activate(:marketing_modals)
-Features.activate(:masterpass)
-Features.activate(:moodboard)
-Features.activate(:sales)
-Features.activate(:style_quiz)
-Features.activate(:delivery_date_messaging)
