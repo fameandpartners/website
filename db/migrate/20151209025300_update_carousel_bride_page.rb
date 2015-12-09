e25a882ed8cdf9edd3b860ba9db55907e120e197
Nam Dang <namproctin@gmail.com>
2015-12-09 09:56:55 +0700

    fix

--- db/migrate/20151209025300_update_carousel_bride_page.rb
+++ db/migrate/20151209025300_update_carousel_bride_page.rb
@@ -1,7 +1,9 @@
 class UpdateCarouselBridePage < ActiveRecord::Migration
   def up
-  end
-
-  def down
+    page = Revolution::Page.where(path: "/fameweddings/bride").first
+    if page.present?
+      page.variables = {"limit" => 16, "caroulsel_pids" => "345-white,262-white,829-white,824-white,822-white,805-white,185-white,474-white,814-white,823-white,830-white,821-white,826-white,517-white,828-white,827-white"}
+      page.save!
+    end
   end
 end
