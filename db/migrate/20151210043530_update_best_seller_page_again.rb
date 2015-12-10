5232f17e91cee548241a3a8a8be1f830da1671b2
Nam Dang <namproctin@gmail.com>
2015-12-10 11:41:54 +0700

    fix issues

---
+++ db/migrate/20151210043530_update_best_seller_page_again.rb
@@ -0,0 +1,11 @@
+class UpdateBestSellerPageAgain < ActiveRecord::Migration
+  def up
+    page = Revolution::Page.where(path: '/dresses/best-sellers').first
+    if page.present?
+      page.template_path = '/products/collections/show.html.slim'
+      page.variables["lookbook"] = false
+      page.variables["curated"]  = true
+      page.save!
+    end
+  end
+end
