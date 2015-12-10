072fedb0c8b3c5d98e7f1722802874c1aa2a4dd5
Tiago Amaro <tiagoa@fameandpartners.com>
2015-12-10 21:18:12 -0200

    check if bridal-dresses exists

--- db/migrate/20151210055035_create_bridal_landing_page.rb
+++ db/migrate/20151210055035_create_bridal_landing_page.rb
@@ -1,11 +1,13 @@
 class CreateBridalLandingPage < ActiveRecord::Migration
   def up
-    page = Revolution::Page.create!(
-      :path => '/bridal-dresses',
-      :template_path => '/landing_pages/bridal_dresses.html.slim'
-    )
-    page.publish!
-    page.translations.create!(:locale => 'en-US', :title => 'Bridal Dresses', :meta_description => 'Bridal Dresses', :heading => 'Bridal Dresses')
+    unless Revolution::Page.exists?(:path => '/bridal-dresses')
+      page = Revolution::Page.create(
+        :path => '/bridal-dresses',
+        :template_path => '/landing_pages/bridal_dresses.html.slim'
+      )
+      page.publish!
+      page.translations.create!(:locale => 'en-US', :title => 'Bridal Dresses', :meta_description => 'Bridal Dresses', :heading => 'Bridal Dresses')
+    end
   end
 
   def down
