d9d0e3460a28c3482cbf77e005bbdfbbb7afb156
Nam Dang <namproctin@gmail.com>
2015-12-10 12:24:09 +0700

    refactor

--- db/migrate/20151210043530_update_best_seller_page_again.rb
+++ db/migrate/20151210043530_update_best_seller_page_again.rb
@@ -2,7 +2,7 @@ class UpdateBestSellerPageAgain < ActiveRecord::Migration
   def up
     page = Revolution::Page.where(path: '/dresses/best-sellers').first
     if page.present?
-      page.template_path = '/products/collections/show.html.slim'
+      page.template_path = '/products/collections/show'
       page.variables["lookbook"] = false
       page.variables["curated"]  = true
       page.save!
