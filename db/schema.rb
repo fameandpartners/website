36aee61e82dc13339411ff6189554b54a150d274
Radek Valenta <valenta.radek@gmail.com>
2015-12-10 16:58:47 +1100

    CreateBridalLandingPage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151209035232) do
+ActiveRecord::Schema.define(:version => 20151210055035) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
