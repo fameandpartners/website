2fac837160a05f9173b0bb3f5360a018c31b95b6
Tiago Amaro <tiagoa@fameandpartners.com>
2015-12-16 20:39:16 -0200

    widgets controller with default url options and inside module

--- app/controllers/widgets/site_navigations_controller.rb
+++ app/controllers/widgets/site_navigations_controller.rb
@@ -1,8 +1,22 @@
-class Widgets::SiteNavigationsController < Widgets::WidgetsController
-  # widget for main nav of the site
-  # used in iframe
-  def main_nav
-    # default renderer
-    render :main_nav, layout: false
+module Widgets
+  class SiteNavigationsController < ActionController::Base
+    include Concerns::SiteVersion
+    include Spree::Core::ControllerHelpers::Order
+    include Spree::Core::ControllerHelpers::Auth
+
+    # widget for main nav of the site
+    # used in iframe
+    def main_nav
+      # default renderer
+      render :main_nav, layout: false
+    end
+
+    def footer
+      # NOOP
+    end
+
+    def default_url_options
+      { host: 'www.fameandpartners.com' }
+    end
   end
 end
