ba41e04721a5c68807fa4640639cba4e7b4a27f0
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2015-12-18 13:11:28 +1100

    Remove abortive attempts at feature spec for height selection

--- spec/support/setup/poltergeist.rb
+++ spec/support/setup/poltergeist.rb
@@ -3,7 +3,7 @@ require 'capybara/poltergeist'
 Capybara.register_driver :poltergeist do |app|
   poltergeist_options = {
       debug:       false,
-      js_errors:   false,
+      js_errors:   true,
       timeout:     ENV.fetch('CAPYBARA_DEFAULT_MAX_WAIT_TIME', '180').to_i,
       window_size: [1366, 768]
   }
--- spec/support/steps/product_steps.rb
+++ spec/support/steps/product_steps.rb
@@ -4,12 +4,6 @@ module Acceptance
       find('#product-size-action').click
       click_layered_element(:link, dress_size)
     end
-    step 'I select :my_height height' do |my_height|
-      find('#product-height-action').click
-      screenshot_and_open_image
-      click_layered_element(:link, my_height)
-      screenshot_and_open_image
-    end
   end
 end
 
