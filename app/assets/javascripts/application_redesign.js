fe2080aaa6618d85aa17bf21a58b6e86d9caba91
Radek Valenta <valenta.radek@gmail.com>
2015-12-21 08:37:30 +1100

    added rsslides back in

--- app/assets/javascripts/application_redesign.js
+++ app/assets/javascripts/application_redesign.js
@@ -114,4 +114,15 @@ jQuery(document).ready(function($){
     email = $('#spree_user_email').val()
     window.location = $(this).attr('href') + '?email=' + email;
   });
+
+	//home page slider		
+	$(function() {		
+		$(".rslides").responsiveSlides({		
+			auto: false,             // Boolean: Animate automatically, true or false		
+	 		pager: true,           // Boolean: Show pager, true or false		
+			speed: 3000,            // Integer: Speed of the transition, in milliseconds		
+	  		timeout: 6000,          // Integer: Time between slide transitions, in milliseconds		
+    	});		
+	});
+	
 })
