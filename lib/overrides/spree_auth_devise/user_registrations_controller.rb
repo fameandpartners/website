870ad18680362585dfab07443c4f8312315fe60f
Nam Dang <namproctin@gmail.com>
2016-01-06 01:53:05 +0700

    refactor js

--- lib/overrides/spree_auth_devise/user_registrations_controller.rb
+++ lib/overrides/spree_auth_devise/user_registrations_controller.rb
@@ -59,7 +59,7 @@ Spree::UserRegistrationsController.class_eval do
       # Marketing pixel
       flash[:signed_up_just_now] = true
 
-      if params[:page] == "bride"
+      if params[:ajax_signed_up] == "true"
         render :json => { status: 'ok' }
       else
         if session.delete(:personalization)
@@ -70,7 +70,7 @@ Spree::UserRegistrationsController.class_eval do
       end
     else
       clean_up_passwords(resource)
-      if params[:page] == "bride"
+      if params[:ajax_signed_up] == "true"
         render :json => { status: 'fail', messages: resource.errors.messages }
       else
         render :new
