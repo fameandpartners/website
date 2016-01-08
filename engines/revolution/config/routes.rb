3ecd26623c44040a5fdc1ecf68366264219d5bcd
Wayne Harlech-Jones <waynehj@famesandpartners.com>
2016-01-08 11:31:09 +1100

    Revolution Banners: add ability to delete
    Whoops!  Almost forgot this little detail.

--- engines/revolution/config/routes.rb
+++ engines/revolution/config/routes.rb
@@ -2,5 +2,8 @@ Revolution::Engine.routes.draw do
   scope "(:locale)" do
     # match '/*path' => 'pages#index', :via => :get
   end
-  
+
+  resources :banners, only: [] do
+    get :delete
+  end
 end
