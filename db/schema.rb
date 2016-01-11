eb43c356490fc80e03ad326eb8efbca08d413105
Radek Valenta <valenta.radek@gmail.com>
2016-01-11 15:55:50 +1100

    mobile fixes for invite form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160107044610) do
+ActiveRecord::Schema.define(:version => 20160105035758) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
@@ -485,16 +485,6 @@ ActiveRecord::Schema.define(:version => 20160107044610) do
   add_index "moodboard_collaborators", ["email"], :name => "index_moodboard_collaborators_on_email"
   add_index "moodboard_collaborators", ["moodboard_id"], :name => "index_moodboard_collaborators_on_moodboard_id"
   add_index "moodboard_collaborators", ["user_id"], :name => "index_moodboard_collaborators_on_user_id"
-
-  create_table "moodboard_item_comments", :force => true do |t|
-    t.integer  "moodboard_item_id"
-    t.integer  "user_id"
-    t.text     "comment"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-  end
-
-  add_index "moodboard_item_comments", ["moodboard_item_id"], :name => "index_moodboard_comments_on_moodboard_item_id"
 
   create_table "moodboard_item_events", :force => true do |t|
     t.string   "moodboard_item_uuid"
