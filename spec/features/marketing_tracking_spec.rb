444b896095a553a9775f9b3fa7145834a64678f8
Tiago Amaro <tiagoa@fameandpartners.com>
2016-01-14 20:26:34 -0200

    test cookie value using show_me_the_cookie

--- spec/features/marketing_tracking_spec.rb
+++ spec/features/marketing_tracking_spec.rb
@@ -6,6 +6,7 @@ describe 'track users for marketing purposes', type: :feature do
   it 'creates cookie with UUID when user visits the website' do
     visit '/'
 
-    expect(page.driver.request.cookies).to include(user_uuid: 'SUPER-SECURE-UUID-FTW')
+    cookie = get_me_the_cookie('user_uuid')
+    expect(cookie[:value]).to eq('SUPER-SECURE-UUID-FTW')
   end
 end
