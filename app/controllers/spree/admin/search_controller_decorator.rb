19e556b564915a8ff3a2d319113339587e87f5cd
Tiago Amaro <tiagoa@fameandpartners.com>
2016-01-18 15:46:38 -0200

    strip white spaces from user search form params

--- app/controllers/spree/admin/search_controller_decorator.rb
+++ app/controllers/spree/admin/search_controller_decorator.rb
@@ -1,4 +1,6 @@
 Spree::Admin::SearchController.class_eval do
+  before_filter :normalize_search_params
+
   def order_owners
     users = Spree::User.ransack(params[:q]).result.limit(10)
 
@@ -7,5 +9,11 @@ Spree::Admin::SearchController.class_eval do
 
   def model_class
     Spree::User
+  end
+
+  private def normalize_search_params
+    params[:q] = (params[:q] || {}).each_with_object({}) do |(key, value), hash|
+      hash[key] = value.to_s.strip
+    end
   end
 end
