d42450f4024cf0c4a8033e2ef689b05d79f72c89
Tiago Amaro <tiagoa@fameandpartners.com>
2016-01-21 21:54:07 -0200

    create and add docs to the CONTENT_DIRECTORY_TARGET env variable

--- script/import_products.sh
+++ script/import_products.sh
@@ -57,17 +57,8 @@ import_start_time=$(date '+%Y-%m-%d_%H.%M.%S')
 logfile="${import_base_directory}/log/${dryrun}product_import_${import_start_time}.log"
 log_date_format='%Y-%m-%d %H:%M:%S'
 
-content_directory="${import_base_directory}/content"
-
-# For easier local testing
-if [ $(whoami) = "garrow" ] ; then
-  content_directory='/home/deploy/product_upload_18_sep'
-fi
-
-# For easier local testing
-if [ $(whoami) = "tobyhede" ] ; then
-  content_directory='/Users/tobyhede/documents/fame/ProductUpload'
-fi
+CONTENT_DIRECTORY_TARGET="${CONTENT_DIRECTORY_TARGET:-$import_base_directory}"
+content_directory="${CONTENT_DIRECTORY_TARGET}/content"
 
 spreadsheets=$(find ${content_directory} -name '*.xls*' |grep -v "~")
 image_directories=$(find "${content_directory}"  -maxdepth 1  -mindepth 1 -type d | grep -vi spreadsheet)
