0de44eef26076e682f8e73098909c078ca510371
Nam Dang <namproctin@gmail.com>
2016-01-25 19:36:13 +0700

    remove site version from fb button

--- app/controllers/spree/omniauth_facebook_authorizations_controller.rb
+++ app/controllers/spree/omniauth_facebook_authorizations_controller.rb
@@ -34,7 +34,6 @@ class Spree::OmniauthFacebookAuthorizationsController < Spree::StoreController
     # Capture PLEASE REMIND ME ABOUT MY SALE events to push onto customer.io later.
     session[:email_reminder_promo] = params[:email_reminder_promo] if params.key?(:email_reminder_promo)
 
-
     redirect_to spree.spree_user_omniauth_authorize_url(provider: :facebook, scope: 'email,public_profile,user_friends')
   end
 
