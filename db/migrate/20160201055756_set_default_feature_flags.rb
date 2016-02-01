61d0ab477b297fdd9f73f8bc778bb30b7bd88170
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-02-01 21:47:36 +1100

    Remove migration class method, use local variable

--- db/migrate/20160201055756_set_default_feature_flags.rb
+++ db/migrate/20160201055756_set_default_feature_flags.rb
@@ -1,27 +1,27 @@
 class SetDefaultFeatureFlags < ActiveRecord::Migration
+
   class FeatureFlag < ActiveRecord::Base
     attr_accessible :key, :data
-    def self.default_flags
-      {
-          "feature:checkout_fb_login"         => "100|",
-          "feature:content_revolution"        => "100|",
-          "feature:enhanced_moodboards"       => "100|",
-          "feature:fameweddings"              => "100|",
-          "feature:height_customisation"      => "100|",
-          "feature:marketing_modals"          => "100|",
-          "feature:masterpass"                => "100|",
-          "feature:moodboard"                 => "100|",
-          "feature:redirect_to_com_au_domain" => "100|",
-          "feature:google_tag_manager"        => "100|",
-          "feature:getitquick_unavailable"    => "100|",
-          "feature:test_analytics"            => "0|",
-          "feature:__features__"              => "checkout_fb_login,content_revolution,enhanced_moodboards,fameweddings,google_tag_manager,height_customisation,marketing_modals,masterpass,test_analytics,moodboard,redirect_to_com_au_domain,getitquick_unavailable"
-        }
-    end
   end
 
   def up
-    FeatureFlag.default_flags.each do |key, data|
+    default_feature_flags = {
+      "feature:checkout_fb_login"         => "100|",
+      "feature:content_revolution"        => "100|",
+      "feature:enhanced_moodboards"       => "100|",
+      "feature:fameweddings"              => "100|",
+      "feature:height_customisation"      => "100|",
+      "feature:marketing_modals"          => "100|",
+      "feature:masterpass"                => "100|",
+      "feature:moodboard"                 => "100|",
+      "feature:redirect_to_com_au_domain" => "100|",
+      "feature:google_tag_manager"        => "100|",
+      "feature:getitquick_unavailable"    => "100|",
+      "feature:test_analytics"            => "0|",
+      "feature:__features__"              => "checkout_fb_login,content_revolution,enhanced_moodboards,fameweddings,google_tag_manager,height_customisation,marketing_modals,masterpass,test_analytics,moodboard,redirect_to_com_au_domain,getitquick_unavailable"
+    }
+
+    default_feature_flags.each do |key, data|
       say "Feature #{key}"
       FeatureFlag.create(key: key, data: data)
     end
