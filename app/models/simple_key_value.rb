1ce271b17827a5efe878263085ddd8f3dd8fd5fe
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-02-03 14:12:42 +1100

    Add functionality to remove Feature flags from DB

--- app/models/simple_key_value.rb
+++ app/models/simple_key_value.rb
@@ -14,4 +14,24 @@ class SimpleKeyValue < ActiveRecord::Base
   def self.get(key)
     where(key: key).first.try(:data)
   end
+
+  # Rollout does not define the ability to remove a feature, only turn it off
+  # Note that there is no `.add_feature()` method, as Features/Rollout will
+  # automatically add the feature as soon as any code checks for the feature name.
+  class FeatureMigration
+    def self.remove_feature(feature_name)
+      feature_key = "feature:#{feature_name}"
+
+      SimpleKeyValue.delete_all(key: feature_key)
+
+      if (features_list = SimpleKeyValue.where(key: 'feature:__features__').first)
+        features_list.data = drop_item_from_list(features_list.data, feature_name)
+        features_list.save!
+      end
+    end
+
+    def self.drop_item_from_list(list, item)
+      list.split(',').reject { |x| x == item }.join(',')
+    end
+  end
 end
