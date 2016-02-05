5aba00022a70cb4ef950ece8a6f4d890eca530f7
Nam Dang <namproctin@gmail.com>
2016-02-05 22:53:43 +0700

    wrap nil

--- db/migrate/20160205153608_add_great_mind_lookbook.rb
+++ db/migrate/20160205153608_add_great_mind_lookbook.rb
@@ -1,12 +1,14 @@
 class AddGreatMindLookbook < ActiveRecord::Migration
   def up
-    page = Revolution::Page.create!(
-      :path => '/lookbook/great-minds-think-alike',
-      :template_path => '/lookbook/show.html.slim',
-      :variables => {:image_count=>6, :lookbook => true, :limit => 24}
-    )
-    page.translations.create!(:locale => 'en-US', :title => 'Great Minds Think Alike', :meta_description => 'Great Minds Think Alike', :heading => 'Great Minds Think Alike')
-    page.publish!
+    if Revolution::Page.where(path: "/lookbook/great-minds-think-alike").first.nil?
+      page = Revolution::Page.create!(
+        :path => '/lookbook/great-minds-think-alike',
+        :template_path => '/lookbook/show.html.slim',
+        :variables => {:image_count=>6, :lookbook => true, :limit => 24}
+      )
+      page.translations.create!(:locale => 'en-US', :title => 'Great Minds Think Alike', :meta_description => 'Great Minds Think Alike', :heading => 'Great Minds Think Alike')
+      page.publish!
+    end
   end
 
   def down
