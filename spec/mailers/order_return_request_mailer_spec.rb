e8ddd4ce8668509e751a26b96200e28f7a49fe04
Tiago Amaro <tiagoa@fameandpartners.com>
2016-02-11 23:29:58 -0200

    use user email for reply_to on return request mailer

--- spec/mailers/order_return_request_mailer_spec.rb
+++ spec/mailers/order_return_request_mailer_spec.rb
@@ -1,22 +1,16 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe OrderReturnRequestMailer, :type => :mailer do
+describe OrderReturnRequestMailer, type: :mailer do
+  let(:order_return_request) { instance_double(OrderReturnRequest, name: 'blah', phone_number: '1234', shipping_address: '', number: 'R1234', completed_at: 1.days.ago, return_request_items: []) }
+  let(:user)                 { instance_double(Spree::User, email: 'blah@vtha.com') }
 
-  let(:order_return_request)  { double(OrderReturnRequest, :name => 'blah', :phone_number => '1234', :shipping_address => '', :number => '1234', :completed_at => 1.days.ago, :return_request_items => [])}
-  let(:user)                  { double(Spree::User, :email => 'blah@vtha.com') }
+  subject(:email)            { OrderReturnRequestMailer.email(order_return_request, user).deliver }
 
-  let(:email)                 { OrderReturnRequestMailer.email(order_return_request, user).deliver }
-
-  it 'delivers' do
-    ActionMailer::Base.deliveries.empty?
-  end
-
-  it 'sends to desk' do
-    expect(email.to).to include 'team@fameandpartners.com'
-  end
+  it { expect(email.to).to contain_exactly('team@fameandpartners.com') }
+  it { expect(email.reply_to).to contain_exactly('blah@vtha.com') }
+  it { expect(email.subject).to eq('[Order Return Request] R1234') }
 
   it 'has some data' do
-    expect(email.to_s).to include order_return_request.number
+    expect(email).to have_content('R1234')
   end
-
 end
