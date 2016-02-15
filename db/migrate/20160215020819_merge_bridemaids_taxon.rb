f04c251476c0d25aba330987fa6dcf470b927446
Nam Dang <namproctin@gmail.com>
2016-02-15 09:15:54 +0700

    [feat] merge taxon
    I merge taxon

---
+++ db/migrate/20160215020819_merge_bridemaids_taxon.rb
@@ -0,0 +1,12 @@
+class MergeBridemaidsTaxon < ActiveRecord::Migration
+  def up
+    dressses = Spree::Taxon.find(217).products
+    event_bridesmaid_taxon = Spree::Taxon.find(221)
+    dressses.each do |d|
+      if d.taxons.none?{|t| t.id == 221 }
+        d.taxons << event_bridesmaid_taxon
+        d.save!
+      end
+    end
+  end
+end
