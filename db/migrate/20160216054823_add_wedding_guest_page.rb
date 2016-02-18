8ac7575b569b3ac96ae73987a8b0efb2a128af4c
Nam Dang <namproctin@gmail.com>
2016-02-18 10:50:16 +0700

    add css

--- db/migrate/20160216054823_add_wedding_guest_page.rb
+++ db/migrate/20160216054823_add_wedding_guest_page.rb
@@ -3,7 +3,7 @@ class AddWeddingGuestPage < ActiveRecord::Migration
     page = Revolution::Page.create!(
          :path => '/wedding-guest',
          :template_path => '/landing_pages/wedding-guest.html.slim',
-         :variables => {:image_count=>6, :lookbook => false, :limit => 60, :pids => ["791-indigo","789-nautical-stripe","798-ice-blue","793-ice-grey","792-aqua","801-sage-fallen-leaves","806-teal","797-mint","810-lilac","572-surreal-white-floral","697-blush","631-burgundy","547-peach","724-navy","481-nude","494-brick","660-cherry-red","667-rosebud","515-light-pink","666-bright-lavender","537-ice-blue","670-apricot","635-peach","694-navy","794-ice-grey","717-chartreuse"] }
+         :variables => {:image_count=>6, :lookbook => false, :limit => 60, :pids => ["791-indigo","789-nautical-stripe","798-ice-blue","793-ice-grey","792-aqua","801-sage-fallen-leaves","806-teal","797-mint","810-lilac","572-surreal-floral-white","697-blush","631-burgundy","547-peach","724-navy","481-nude","494-brick","660-cherry-red","667-rosebud","515-light-pink","666-bright-lavender","537-ice-blue","670-apricot","635-peach","694-navy","794-ice-grey","717-chartreuse"] }
        )
     page.translations.create!(:locale => 'en-US', :title => 'Wedding Guest Dresses', :meta_description => 'Wedding Guest Dresses', :heading => 'Wedding Guest Dresses')
     page.publish!
