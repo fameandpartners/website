d4878a5cc47101038837184151dd2a8cac95aea1
Nam Dang <namproctin@gmail.com>
2016-02-26 12:49:38 +0700

    add pids

--- db/migrate/20160223040544_create_prom_landing_page_for_adquadrant.rb
+++ db/migrate/20160223040544_create_prom_landing_page_for_adquadrant.rb
@@ -3,7 +3,7 @@ class CreatePromLandingPageForAdquadrant < ActiveRecord::Migration
     page = Revolution::Page.create!(
          :path => '/prom-ad',
          :template_path => '/landing_pages/prom-ad.html.slim',
-         :variables => {:image_count=>6, :lookbook => false, :limit => 60, :pids => [] }
+         :variables => {:image_count=>6, :lookbook => false, :limit => 60, :pids => ["902-black","915-pale-grey","931-black","918-ivory","934-ivory","922-navy","903-peach","897-dark-burgundy"] }
        )
     page.translations.create!(:locale => 'en-US', :title => 'PROM DRESSES', :meta_description => 'PROM DRESSES', :heading => 'PROM DRESSES')
     page.publish!
