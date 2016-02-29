179c81229f4c00b9e14145b6c65024a609f4ebf8
Radek Valenta <valenta.radek@gmail.com>
2016-03-01 10:19:27 +1100

    cleanup stage 2

--- app/assets/javascripts/styleguide/js/functions.js
+++ app/assets/javascripts/styleguide/js/functions.js
@@ -1,25 +1,10 @@
 jQuery(document).ready(function($){
 	var searchInput = $('#input-search');
 	var searchBtn   = $('.search');
-	var close       = $('.close-search');
 	var shopBtn     = $('.shop');
 
 	searchBtn.click(function() {
 		searchInput.focus();
 	});
 
-});
-
-
-////////////
-// ALERTS
-////////////
-
-jQuery(document).ready(function($){
-	var style = $('.style');
-	var overlay = $('.mobile-menu-overlay');
-
-	style.click(function() {
-		overlay.toggleClass('mobile-show');
-	});
 });
