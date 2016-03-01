3c131a6c0d8a3df08486288b1e08649cbacc474f
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-03-01 14:19:52 +1100

    Remove false positive Ordering spec

--- spec/services/search/color_variants_query_spec.rb
+++ spec/services/search/color_variants_query_spec.rb
@@ -65,7 +65,6 @@ module Search
           ['best_sellers',  {'product.total_sales' => 'desc'}],
           ['alpha_asc',     {'product.name' => 'asc'}],
           ['alpha_desc',    {'product.name' => 'desc'}],
-          ['created',       {'product.created_at' => 'desc'}],
           ['most_views',    {'product.statistics.total_views' => 'desc'}],
           ['most_carts',    {'product.statistics.total_carts' => 'desc'}],
           ['most_wishlists',{'product.statistics.total_wishlists' => 'desc'}],
