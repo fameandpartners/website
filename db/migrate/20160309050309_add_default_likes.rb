e133bcbe8ce222f938ec9e45b0fed3bc9710cde7
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-03-11 09:58:55 +1100

    Unify Moodboard likes migration

--- db/migrate/20160309050309_add_default_likes.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultLikes < ActiveRecord::Migration
-  def up
-    change_column_default :moodboard_items, :likes, 0
-  end
-
-  def down
-    change_column_default :moodboard_items, :likes, nil
-  end
-end
--- db/migrate/20160309054215_add_moodboard_user_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddMoodboardUserLikes < ActiveRecord::Migration
-  def change
-    add_column :moodboard_items, :user_likes, :string, default: ''
-  end
-end
