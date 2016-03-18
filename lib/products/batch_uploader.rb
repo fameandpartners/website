2239d4c31f319bcd82ed67e08e55003d56a01178
Nam Dang <namproctin@gmail.com>
2016-03-18 09:29:08 +0700

    fix

--- lib/products/batch_uploader.rb
+++ lib/products/batch_uploader.rb
@@ -564,7 +564,7 @@ module Products
       end
 
       product.product_color_values.where(custom: false).each do |p|
-        p 'haha1',recommended_colors.pluck(:id), 'haha2', p.option_value_id
+        p 'haha1',recommended_colors, 'haha2', p.option_value_id
         if !recommended_colors.include?(p.option_value_id)
           p.destroy!
         end
