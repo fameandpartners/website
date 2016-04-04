6e4aa09ea39f0a331c498a8c5f3216b410dfc2af
Shaun MacDougall <me@shaunmacdougall.com>
2016-04-04 12:13:12 +1000

    merge master with this branch

--- spec/features/marketing/body_shape_calculator_request_spec.rb
+++ spec/features/marketing/body_shape_calculator_request_spec.rb
@@ -1,4 +1,4 @@
-require  'rails_helper'
+require 'rails_helper'
 
 describe 'Body Shape Calculator', type: :request do
   describe 'store measures' do
@@ -7,20 +7,21 @@ describe 'Body Shape Calculator', type: :request do
         post '/body-shape-calculator/store-measures.json'
 
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.body).to eq("{\"errors\":{\"email\":[\"is invalid\",\"can't be blank\"],\"shape\":[\"can't be blank\"],\"size\":[\"can't be blank\"]}}")
+        expect(response.body).to eq("{\"errors\":{\"email\":[\"is invalid\",\"can't be blank\"],\"shape\":[\"can't be blank\"],\"size\":[\"can't be blank\"],\"unit\":[\"can't be blank\"]}}")
       end
     end
 
     context 'valid attributes' do
       let(:valid_measure) do
         {
-          bust_circumference: '90.0',
-          email: 'something@example.com',
-          hip_circumference: '92.0',
-          shape: 'apple',
-          size: 'US 2/AU 6',
+          bust_circumference:       '90.0',
+          email:                    'something@example.com',
+          hip_circumference:        '92.0',
+          shape:                    'apple',
+          size:                     'US 2/AU 6',
           under_bust_circumference: '92.0',
-          waist_circumference: '92.0'
+          unit:                     'cm',
+          waist_circumference:      '92.0'
         }
       end
 
