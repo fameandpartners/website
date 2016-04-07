b6699dcd1aaa668cca77f2cc862d85cb3e92a8ee
Tiago Amaro <tiagoa@fameandpartners.com>
2016-04-07 22:23:36 +0200

    trying to make REX11 API accept receiving ticket add. FAIL

--- lib/bergen/soap_methods/receiving_ticket_add.rb
+++ lib/bergen/soap_methods/receiving_ticket_add.rb
@@ -18,26 +18,52 @@ module Bergen
 
       def required_fields_hash
         {
-          'AuthenticationString'  => client.auth_token,
-          'SupplierDetails'       => {
-            'CompanyName' => 'Fame & Partners',
-            'Country'     => 'Australia'
-          },
-          'ReceivingTicketObject' => {
-            'Carrier'          => 'Truck', # Carrier should be the method being used to ship the product to us. If UPS is shipping the product, then this would be UPS. If you don't know what the carrier is, you can default to "Truck".
-            'Warehouse'        => 'Bergen Logistics NJ2',
-            'ShipmentTypelist' => 'Open To Hang'
-          },
-          'Shipmentitemslist'     => {
-            'SKU'              => global_sku.sku,
-            'Style'            => global_sku.style_number,
-            'Color'            => global_sku.color_name,
-            'Size'             => global_sku.size,
-            'ExpectedQuantity' => '0', # Required, but field is ignored
-            'ActualQuantity'   => '0', # Required, but field is ignored
-            'DamagedQuantity'  => '0' # Required, but field is ignored
+          'AuthenticationString' => client.auth_token,
+          'receivingTicket'      => {
+            'ReceivingStatus'     => 'Draft',
+            'ReceivingStatusCode' => 0,
+            'CreatedDate'         => '07/04/2016',
+            'Shipmentitemslist'   => {
+              'Sku'              => global_sku.sku,
+              'Color'            => global_sku.color_name,
+              'UPC'              => global_sku.id,
+              'Size'             => global_sku.size,
+              'Style'            => global_sku.style_number,
+              'ExpectedQuantity' => '0', # Required, but field is ignored
+              'ActualQuantity'   => '0', # Required, but field is ignored
+              'DamagedQuantity'  => '0' # Required, but field is ignored
+            },
+            'ShipmentTypelist'    => 'OPENTOHANG',
+            'Warehouse'           => 'Bergen Logistics NJ2',
+            'SupplierDetails'     => {
+              'CompanyName' => 'Fame & Partners',
+              'Country'     => 'Australia'
+            },
+            'Carrier'             => 'Truck',
           }
         }
+
+        # {
+        #   'AuthenticationString' => client.auth_token,
+        #   'receivingTicket'      => {
+        #     'Carrier'           => 'Truck', # Carrier should be the method being used to ship the product to us. If UPS is shipping the product, then this would be UPS. If you don't know what the carrier is, you can default to "Truck".
+        #     'Warehouse'         => 'Bergen Logistics NJ2',
+        #     'ShipmentTypelist'  => 'Open To Hang',
+        #     'Shipmentitemslist' => {
+        #       'SKU'              => global_sku.sku,
+        #       'Style'            => global_sku.style_number,
+        #       'Color'            => global_sku.color_name,
+        #       'Size'             => global_sku.size,
+        #       'ExpectedQuantity' => '0', # Required, but field is ignored
+        #       'ActualQuantity'   => '0', # Required, but field is ignored
+        #       'DamagedQuantity'  => '0' # Required, but field is ignored
+        #     },
+        #     'SupplierDetails'   => {
+        #       'CompanyName' => 'Fame & Partners',
+        #       'Country'     => 'Australia'
+        #     }
+        #   }
+        # }
       end
 
       def line_item_presenter
