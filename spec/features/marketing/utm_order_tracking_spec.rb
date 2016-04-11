dfb84e278d711de64656888b80d418bd865cabea
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-04-11 15:16:21 +0300

    utm params order spec updated

--- spec/features/marketing/utm_order_tracking_spec.rb
+++ spec/features/marketing/utm_order_tracking_spec.rb
@@ -9,6 +9,14 @@ describe 'I’m capturing order UTM stuff now', type: :feature do
     expect(utm.utm_campaign).to eq('utm_campaign')
     expect(utm.utm_source).to eq('utm_source')
     expect(utm.utm_medium).to eq('utm_medium')
+
+    expect { visit('/?utm_campaign=utm_campaign_new&utm_source=utm_source_new&utm_medium=utm_medium_new') }
+      .to_not change { Marketing::OrderTrafficParameters.count }
+
+    utm_updated = Marketing::OrderTrafficParameters.last
+    expect(utm_updated.utm_campaign).to eq('utm_campaign_new')
+    expect(utm_updated.utm_source).to eq('utm_source_new')
+    expect(utm_updated.utm_medium).to eq('utm_medium_new')
   end
 
   it "doesn't capture UTM params" do
