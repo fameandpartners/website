982d1f0edc402ed8d60dc5c14a9f9921d24dae10
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-04-13 15:27:51 +0300

    mailchimp spec refactored

--- spec/services/email_capture_spec.rb
+++ spec/services/email_capture_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe EmailCapture do
   let(:user) { build(:spree_user_real) }
 
-  let(:mailchimp) { EmailCapture.new({ service: :mailchimp },
+  let(:email_capture) { EmailCapture.new({ service: :mailchimp },
                                        email:              user.email,
                                        first_name:         user.first_name,
                                        last_name:          user.last_name,
@@ -13,7 +13,7 @@ describe EmailCapture do
                                        site_version:       'US',
                                        form_name:          'contact') }
 
-  it { expect(mailchimp.service).to eq :mailchimp }
+  it { expect(email_capture.service).to eq :mailchimp }
   it { expect(EmailCapture.new({service: 'Mailchimp'},
                                email:              user.email,
                                first_name:         user.first_name,
@@ -25,9 +25,9 @@ describe EmailCapture do
                                form_name:          'contact').service).to_not eq :mailchimp }
 
   describe '#email_changed?' do
-    it { expect(mailchimp.email_changed?).to be_falsey }
+    it { expect(email_capture.email_changed?).to be_falsey }
     it 'returns true when email was changed' do
-      mailchimp = EmailCapture.new({ service: :mailchimp },
+      email_capture = EmailCapture.new({ service: :mailchimp },
                                          email:              user.email,
                                          previous_email: 'email@changes.com',
                                          first_name:         user.first_name,
@@ -37,14 +37,14 @@ describe EmailCapture do
                                          utm_params:         '',
                                          site_version:       'US',
                                          form_name:          'contact')
-      expect(mailchimp.email_changed?).to be_truthy
+      expect(email_capture.email_changed?).to be_truthy
     end
   end
 
   describe '#retrieve_first_name' do
-    it { expect(mailchimp.retrieve_first_name).to eq user.first_name }
+    it { expect(email_capture.retrieve_first_name).to eq user.first_name }
     it 'returns nil when an empty first name is passed' do
-      mailchimp = EmailCapture.new({ service: :mailchimp },
+      email_capture = EmailCapture.new({ service: :mailchimp },
                                    email:              user.email,
                                    previous_email: 'email@changes.com',
                                    last_name:          user.last_name,
@@ -53,14 +53,14 @@ describe EmailCapture do
                                    utm_params:         '',
                                    site_version:       'US',
                                    form_name:          'contact')
-      expect(mailchimp.retrieve_first_name).to be_nil
+      expect(email_capture.retrieve_first_name).to be_nil
     end
   end
 
   describe '#retrieve_last_name' do
-    it { expect(mailchimp.retrieve_last_name).to eq user.last_name }
+    it { expect(email_capture.retrieve_last_name).to eq user.last_name }
     it 'returns nil when an empty last name is passed' do
-      mailchimp = EmailCapture.new({ service: :mailchimp },
+      email_capture = EmailCapture.new({ service: :mailchimp },
                        email:              user.email,
                        first_name:         user.first_name,
                        current_sign_in_ip: "101.0.79.50",
@@ -68,13 +68,13 @@ describe EmailCapture do
                        utm_params:         '',
                        site_version:       'US',
                        form_name:          'contact')
-      expect(mailchimp.retrieve_last_name).to be_nil
+      expect(email_capture.retrieve_last_name).to be_nil
     end
   end
 
   describe '#set_newsletter' do
     it 'returns no when newsletter is set false' do
-      mailchimp = EmailCapture.new({ service: :mailchimp },
+      email_capture = EmailCapture.new({ service: :mailchimp },
                        email:              user.email,
                        newsletter: false,
                        first_name:         user.first_name,
@@ -84,10 +84,10 @@ describe EmailCapture do
                        utm_params:         '',
                        site_version:       'US',
                        form_name:          'contact')
-      expect(mailchimp.set_newsletter).to eq 'no'
+      expect(email_capture.set_newsletter).to eq 'no'
     end
     it 'returns yes when newsletter is set true' do
-      mailchimp = EmailCapture.new({ service: :mailchimp },
+      email_capture = EmailCapture.new({ service: :mailchimp },
                        email:              user.email,
                        first_name:         user.first_name,
                        newsletter: true,
@@ -97,47 +97,33 @@ describe EmailCapture do
                        utm_params:         '',
                        site_version:       'US',
                        form_name:          'contact')
-      expect(mailchimp.set_newsletter).to eq 'yes'
+      expect(email_capture.set_newsletter).to eq 'yes'
     end
     it 'returns nil when no newsletter is set' do
-      expect(mailchimp.set_newsletter).to be_nil
+      expect(email_capture.set_newsletter).to be_nil
     end
   end
 
   describe '#set_merge' do
-    it { expect(mailchimp.set_merge.class.to_s).to eq 'Hash' }
-    it { expect(mailchimp.set_merge[:fname]).to eq user.first_name }
-    it { expect(mailchimp.set_merge[:lname]).to eq user.last_name }
-    it { expect(mailchimp.set_merge[:ip_address]).to eq "101.0.79.50" }
-    it { expect(mailchimp.set_merge[:country]).to be }
-    it { expect(mailchimp.set_merge[:n_letter]).to be_nil }
+    it { expect(email_capture.set_merge.class.to_s).to eq 'Hash' }
+    it { expect(email_capture.set_merge[:fname]).to eq user.first_name }
+    it { expect(email_capture.set_merge[:lname]).to eq user.last_name }
+    it { expect(email_capture.set_merge[:ip_address]).to eq "101.0.79.50" }
+    it { expect(email_capture.set_merge[:country]).to be }
+    it { expect(email_capture.set_merge[:n_letter]).to be_nil }
   end
 
   describe '#unsubscribe' do
-    it 'unsubscribe user and check it' do
-      email_capture = EmailCapture.new({ service: :mailchimp },
-                   email:              user.email,
-                   first_name:         user.first_name,
-                   current_sign_in_ip: "101.0.79.50",
-                   landing_page:       "/dresses/formal",
-                   utm_params:         '',
-                   site_version:       'US',
-                   form_name:          'contact')
+    before(:each) do
       email_capture.capture
+    end
+
+    it 'unsubscribe user and check it' do
       email_capture.mailchimp.lists.unsubscribe(email_capture.set_list_id, {email: user.email}, true, false, false)
       expect(email_capture.unsubscribed?(user.email)).to be_truthy
     end
 
     it "check that we can't subscribe user which unsubscribed" do
-      email_capture = EmailCapture.new({ service: :mailchimp },
-                   email:              user.email,
-                   first_name:         user.first_name,
-                   current_sign_in_ip: "101.0.79.50",
-                   landing_page:       "/dresses/formal",
-                   utm_params:         '',
-                   site_version:       'US',
-                   form_name:          'contact')
-      email_capture.capture
       expect(email_capture.unsubscribed?(user.email)).to be_truthy
     end
   end
