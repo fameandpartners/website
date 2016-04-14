0c7e2dab09ceec5eb86e1ee7e271d2ed9b01921d
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-04-14 14:38:21 +0300

    contact model upgraged

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -27,9 +27,15 @@ class Contact
   ]
 
   attr_accessor :first_name, :last_name, :email, :subject, :site_version, :message, :phone, :order_number
+  attr_accessor :name, :linkedin, :interests, :join_team
 
+  validates :site_version, presence: true
   validates :email, format: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, presence: true
-  validates :first_name, :last_name, :subject, :site_version, :message, presence: true
+  validates :linkedin, format: { with: /\A((http|https):\/\/)?(www.)?linkedin.com\//, message: 'use a valid LinkedIn URL' },
+    if: Proc.new { |c| c.linkedin.present? && join_us_form? }
+
+  validates :first_name, :last_name, :subject, :message, presence: true, if: :contact_form?
+  validates :name, :interests, presence: true, if: :join_us_form?
 
   def initialize(attributes = {})
     attributes.each do |name, value|
@@ -44,4 +50,13 @@ class Contact
   def to_key
     nil
   end
+
+  def contact_form?
+    join_team.nil?
+  end
+
+  def join_us_form?
+    join_team.present?
+  end
+
 end
