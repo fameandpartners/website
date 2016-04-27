463a35bf5e01f65fdb7a1276e3db0e494a1a33c8
Nam Dang <namproctin@gmail.com>
2016-04-27 10:33:30 +0700

    add pids

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160413092439) do
+ActiveRecord::Schema.define(:version => 20160427030510) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
