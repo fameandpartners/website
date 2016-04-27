f332fe05b98c053d19f117079db1681129cdc5eb
Nam Dang <namproctin@gmail.com>
2016-04-27 10:56:41 +0700

    change lookbook name

--- config/routes.rb
+++ config/routes.rb
@@ -155,7 +155,7 @@ FameAndPartners::Application.routes.draw do
     get '/lookbook/love-lace-collection' => 'products/collections#show', :permalink => 'love-lace', :as => :love_lace_collection
     get '/lookbook/just-the-girls'       => 'products/collections#show', :permalink => 'just-the-girls', :as => :just_the_girls_collection
     get '/lookbook/partners-in-crime'    => 'products/collections#show', :permalink => 'partners-in-crime', :as => :partners_in_crime_collection
-    get '/lookbook/pass-the-mimosa'      => 'products/collections#show', :permalink => 'pass-the-mimosa', :as => :pass_the_mimosa_collection
+    get '/lookbook/la-belle-epoque'      => 'products/collections#show', :permalink => 'pass-the-mimosa', :as => :la_belle_epoque_collection
 
     get '/all-size' => redirect('/lookbook/all-size')
     get '/lookbook/all-size' => 'products/collections#show', :permalink => 'all-size', :as => :all_size_collection
