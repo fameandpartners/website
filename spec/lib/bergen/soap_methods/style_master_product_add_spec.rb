7a22b7141db92ea42ef4a710e3356ba6b89f3de4
Tiago Amaro <tiagoa@fameandpartners.com>
2016-05-02 12:16:18 +0200

    response + result + multiple variants upload to WMS

--- spec/lib/bergen/soap_methods/style_master_product_add_spec.rb
+++ spec/lib/bergen/soap_methods/style_master_product_add_spec.rb
@@ -5,22 +5,10 @@ module Bergen
     RSpec.describe StyleMasterProductAdd, :vcr do
       let(:savon_client) { SavonClient.new }
       let(:spree_variant) { build_stubbed(:dress_variant) }
-      let(:soap_method) { described_class.new(savon_client: savon_client, spree_variant: spree_variant) }
+      let(:soap_method) { described_class.new(savon_client: savon_client, spree_variants: [spree_variant]) }
 
-      it 'adds a variant to WMS queue' do
-        expect(soap_method.request.body).to eq({
-                                                 :style_master_product_add_response =>
-                                                   { :style_master_product_add_result =>
-                                                       { :notifications =>
-                                                           { :notification =>
-                                                               { :error_code => '0',
-                                                                 :severity   => 'Success',
-                                                                 :message    => 'Data saved successfully' }
-                                                           }
-                                                       },
-                                                     :@xmlns                          => 'http://rex11.com/webmethods/'
-                                                   }
-                                               })
+      it 'adds some variants to WMS queue' do
+        expect(soap_method.result).to eq({ error_code: '0', severity: 'Success', message: 'Data saved successfully' })
       end
     end
   end
