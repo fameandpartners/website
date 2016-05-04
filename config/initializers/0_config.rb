e2799f711d13926b93a50d12394bd740379da099
Tim Hemi <tim@reinteractive.net>
2016-05-04 15:36:13 +1000

    using envvars for sentry creds

--- config/initializers/0_config.rb
+++ config/initializers/0_config.rb
@@ -8,6 +8,11 @@ configatron.blog_host               = 'fameandpartners.tumblr.com'
 configatron.days_delivery_emergency = 0
 configatron.secret_token            = ENV['SECRET_TOKEN']
 
+configatron.sentry do |sentry|
+  sentry.public = ENV['SENTRY_PUBLIC']
+  sentry.secret = ENV['SENTRY_SECRET']
+end
+
 # assets
 configatron.aws.enabled    = false
 configatron.aws.bucket     = ""
