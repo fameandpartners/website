44e903dcecfc3b40d2520f4226e3753e1c0f294f
Tim Hemi <tim@reinteractive.net>
2016-05-10 11:22:01 +1000

    configure paperclip for iam role auth

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,15 +3,15 @@ Paperclip::Attachment.send :include, Paperclip::ProtocolRelativeURLSupport
 
 if Rails.application.config.use_s3
   Paperclip::Attachment.default_options.merge!(
-    :storage => :s3,
-    :path => "/system/:attachment/:id/:style/:basename.:extension",
-    :url => ":s3_alias_url",
-    :s3_credentials => {
-      :bucket => configatron.aws.s3.bucket,
-      :access_key_id => configatron.aws.s3.access_key_id,
-      :secret_access_key => configatron.aws.s3.secret_access_key
+    storage: :fog,
+    fog_credentials: {
+      use_iam_profile: true,
+      provider: 'AWS',
+      region: configatron.aws.s3.region
     },
-    s3_host_alias: configatron.aws.host
+    fog_directory: configatron.aws.s3.bucket,
+    path: "/system/:attachment/:id/:style/:basename.:extension",
+    fog_host: configatron.aws.host
   )
 else
   Paperclip::Attachment.default_options.merge!(
