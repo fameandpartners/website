128ff8894a80dbed584752d48986ea3324426610
Tiago Amaro <tiagoa@fameandpartners.com>
2016-05-11 14:09:41 -0700

    call operation style creation on order return creation

---
+++ engines/bergen/app/services/bergen/operations/create_style_master.rb
@@ -0,0 +1,15 @@
+module Bergen
+  module Operations
+    class CreateStyleMaster
+      class << self
+        def process(return_request_items:)
+          service.style_master_product_add_by_return_request_items(return_items: return_request_items)
+        end
+
+        private def service
+          Bergen::Service.new
+        end
+      end
+    end
+  end
+end
---
+++ engines/bergen/spec/services/operations/create_style_master_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+module Bergen
+  module Operations
+    RSpec.describe CreateStyleMaster do
+      let(:return_request_items) { double('Return Request Items') }
+      let(:bergen_service) { double('Bergen Service') }
+
+      describe '.process' do
+        before(:each) { allow(described_class).to receive(:service).and_return(bergen_service) }
+
+        it do
+          expect(bergen_service).to receive(:style_master_product_add_by_return_request_items).with(return_items: return_request_items)
+
+          described_class.process(return_request_items: return_request_items)
+        end
+      end
+    end
+  end
+end
