00048458b8ba6d8e05f4c09287e7080c7e57fd43
Tiago Amaro <tiagoa@fameandpartners.com>
2016-05-13 09:56:11 -0700

    guest checkout + user association: file renaming + fix + spec

--- spec/services/guest_checkout_association_spec.rb
+++ spec/services/guest_checkout_association_spec.rb
@@ -1,21 +1,30 @@
 require 'spec_helper'
-describe GuestCheckoutAssociation, type: :service do
-  subject(:order)     { Spree::Order.new }
 
-  describe 'associate user when guest checkout based on email, first name and last name' do
-    context 'guest checkout' do
-      let(:match_user) { create(:spree_user, email: 'something@music.com', first_name: 'Something', last_name: 'Music') }
-      let(:billing_address) { create(:address, firstname: match_user.first_name, lastname: match_user.last_name, email: match_user.email)}
-      it 'should associate user' do
-        order.billing_address = billing_address
+describe GuestCheckoutAssociation, type: :service do
+  let!(:order) { create(:spree_order) }
+  let!(:user) { create(:spree_user, email: 'something@music.com', first_name: 'Something', last_name: 'Music') }
+
+  describe 'associate user to a completed order' do
+    context 'bill address email, first and last name matches an existing user' do
+      let!(:bill_address) { create(:address, firstname: 'Something', lastname: 'Music', email: 'something@music.com') }
+
+      before(:each) do
+        order.bill_address = bill_address
+        order.save
+      end
+
+      it 'associates user with order' do
         result = GuestCheckoutAssociation.associate_user_for_guest_checkout(spree_order: order, spree_current_user: nil)
         expect(result).to be true
-        expect(order.user).to eq(match_user)
+        expect(order.reload.user).to eq(user)
       end
+    end
+
+    context 'bill address information does not match any user' do
       it 'should not associate user with correct email and incorrect first/last name' do
         result = GuestCheckoutAssociation.associate_user_for_guest_checkout(spree_order: order, spree_current_user: nil)
         expect(result).to be false
-        expect(order.user).to be_nil
+        expect(order.reload.user).to be_nil
       end
     end
   end
