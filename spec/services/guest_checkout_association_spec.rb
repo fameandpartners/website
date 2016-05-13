0dcdcd9906dcedcc6e1b0efe68c043058baad50c
Nam Dang <namproctin@gmail.com>
2016-05-13 22:49:04 +0700

    change logic

--- spec/services/guest_checkout_association_spec.rb
+++ spec/services/guest_checkout_association_spec.rb
@@ -5,17 +5,18 @@ describe GuestCheckoutAssociation, type: :service do
   describe 'associate user when guest checkout based on email, first name and last name' do
     context 'guest checkout' do
       let(:match_user) { create(:spree_user, email: 'something@music.com', first_name: 'Something', last_name: 'Music') }
+      let(:billing_address) { create(:address, firstname: match_user.first_name, lastname: match_user.last_name, email: match_user.email)}
       it 'should associate user' do
-        result = GuestCheckoutAssociation.associate_user_for_guest_checkout(spree_order: order, spree_current_user: nil, user_address:  {"bill_address_attributes" => {"email" => match_user.email, "firstname" => 'Something', "lastname" => 'Music'}})
+        order.billing_address = billing_address
+        result = GuestCheckoutAssociation.associate_user_for_guest_checkout(spree_order: order, spree_current_user: nil)
         expect(result).to be true
         expect(order.user).to eq(match_user)
       end
       it 'should not associate user with correct email and incorrect first/last name' do
-        result = GuestCheckoutAssociation.associate_user_for_guest_checkout(spree_order: order, spree_current_user: nil, user_address:  {"bill_address_attributes" => {"email" => match_user.email, "firstname" => "", "lastname" => ""}})
+        result = GuestCheckoutAssociation.associate_user_for_guest_checkout(spree_order: order, spree_current_user: nil)
         expect(result).to be false
         expect(order.user).to be_nil
       end
     end
   end
-
 end
