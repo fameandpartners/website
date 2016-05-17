305d6a81b4d121eeb54e806e0e052ea22c63bd57
Tiago Amaro <tiagoa@fameandpartners.com>
2016-05-17 11:43:59 -0700

    write stub of test that should actually go through the real process

---
+++ engines/bergen/spec/integration/item_process_integration_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'Process Return Request Item', type: :integration, vcr: true do
+  it 'Actually implements integration test with real bergen calls on Bergen::Operations::ReturnItemProcess'
+end
