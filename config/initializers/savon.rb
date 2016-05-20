dc611b602b2fad0f65a4cd373b0ea14c86d06cd8
Tiago Amaro <tiagoa@fameandpartners.com>
2016-05-20 11:41:00 -0700

    enable env variables for salon logs and log level

--- config/initializers/savon.rb
+++ config/initializers/savon.rb
@@ -5,3 +5,13 @@ if Rails.env.development?
     config.pretty_print_xml = true
   end
 end
+
+if ENV.fetch('SAVON_ENABLE_LOGS', false)
+  savon_log_level = ENV.fetch('SAVON_LOG_LEVEL', 'debug').to_s.to_sym
+
+  Savon.configure do |config|
+    config.log              = true
+    config.log_level        = savon_log_level
+    config.pretty_print_xml = true
+  end
+end
