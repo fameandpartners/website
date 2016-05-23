47a06fb8fd5348b51d0372e9ebcc7d9769d23b03
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-05-23 11:40:34 +0300

    line items images logic implementation started

--- engines/admin_ui/app/controllers/admin_ui/reports/daily_orders_controller.rb
+++ engines/admin_ui/app/controllers/admin_ui/reports/daily_orders_controller.rb
@@ -4,14 +4,49 @@ module AdminUi
   module Reports
     class DailyOrdersController < AdminUi::Reports::StreamingCsvController
 
-      # def show
-      #   @rows = report.take(10).map &:to_h
-      # end
+    def show
+      @rows = CSV.parse(report_csv).map {|a| Hash[ headers_en.zip(a) ] }[1..10]
+    end
 
     private
 
       def report
         ::Reports::DailyOrders.new(from: from_date, to: to_date)
+      end
+
+      def report_csv
+        CSV.generate(headers: true) do |csv|
+          csv << headers_en
+
+          report.each do |r|
+            csv << [
+              r['order_number'],
+              r['site_version'],
+              r['total_items'],
+              r['promo_codes'],
+              price(r),
+              r['currency'],
+              r['style'],
+              r['line_item_id'],
+              r['size'],
+              adjusted_size(r),
+              r['height'],
+              r['color'],
+              r['quantity'],
+              r['factory'],
+              delivery_date(r),
+              r['fast_making'].present? ? "TRUE" : '',
+              customization_values(r),
+              r['customer_notes'],
+              customer_name(r),
+              r['customer_phone_number'] || 'No Phone',
+              r['email'],
+              shipping_address(r).present? ? shipping_address(r) : 'No Shipping Address',
+              r['variant_image'],
+              r['custom_variant_image']
+            ]
+          end
+        end
       end
 
       def filename
@@ -27,7 +62,7 @@ module AdminUi
       end
 
       def from_date
-        date(params[:from], default: 14.weeks.ago)
+        date(params[:from], default: 7.weeks.ago)
       end
 
       def to_date
@@ -42,6 +77,67 @@ module AdminUi
         end
       rescue StandardError => e
         default
+      end
+
+      def headers_en
+          [
+            :order_number,
+            :site_version,
+            :total_items,
+            :promo_codes,
+            :payment_total,
+            :currency,
+            :style,
+            :line_item_id,
+            :size,
+            :adjusted_size,
+            :height,
+            :color,
+            :quantity,
+            :factory,
+            :delivery_date,
+            :making_options,
+            :customisations,
+            :customer_notes,
+            :customer_name,
+            :customer_phone_number,
+            :email,
+            :shipping_address,
+            :variant_image,
+            :custom_variant_image
+          ]
+        end
+
+      def price(r)
+        r['price'].to_f + r['personalization_price'].to_f + r['making_options_price'].to_f
+      end
+
+      def adjusted_size(r)
+        "#{r['size']} (#{r['site_version']})"
+      end
+
+      def delivery_date(r)
+        return unless r['completed_at_char'].present?
+        Policies::LineItemProjectedDeliveryDatePolicy.new(r['completed_at_char'].to_date, r['fast_making']).delivery_date
+      end
+
+      def customization_values(r)
+        if r['personalization'].present?
+          values = YAML.load(r['customization_value_ids'])
+          customs = values.present? ? CustomisationValue.where(id: values).pluck(:presentation) : []
+          customs << ["Custom Color: #{color_name(r)}"] unless r['custom_color']
+          customs.join('|')
+        else
+          'N/A'
+        end
+      end
+
+      def customer_name(r)
+        "#{r['user_first_name']} #{r['user_last_name']}"
+      end
+
+      def shipping_address(r)
+        "#{r['address1']} #{r['address2']} #{r['city']} #{r['state']} #{r['zipcode']} #{r['country']}"
       end
 
     end
--- engines/admin_ui/app/models/reports/daily_orders.rb
+++ engines/admin_ui/app/models/reports/daily_orders.rb
@@ -34,8 +34,9 @@ module Reports
           INNER JOIN spree_activators sa ON sa.id = spa.activator_id
           WHERE sadj.adjustable_id = o.id AND sadj."adjustable_type" = 'Spree::Order' AND sadj.originator_type = 'Spree::PromotionAction' AND sa.type IN ('Spree::Promotion')
           GROUP BY sadj.adjustable_id ) as promo_codes,
-        (li.price + lip.price +
-        ( SELECT case when SUM(price) > 0 then SUM(price) else 0 end FROM line_item_making_options WHERE line_item_id = li.id )) as price,
+        li.price,
+        lip.price as personalization_price,
+        ( SELECT SUM(price) FROM line_item_making_options WHERE line_item_id = li.id ) as making_options_price,
         li.currency,
         (SELECT "spree_variants".sku FROM "spree_variants" WHERE "spree_variants"."product_id" = sp.id AND "spree_variants"."is_master" = 't' LIMIT 1) as style,
         li.id as line_item_id,
@@ -47,7 +48,6 @@ module Reports
             INNER JOIN "spree_option_types" ON "spree_option_types".id = "spree_option_values"."option_type_id"
             WHERE "spree_option_types"."name" = 'dress-size' AND "spree_option_values_variants"."variant_id" = sv.id)
         END as size,
-        (size || ' ' || '(' || o.site_version || ')') as adjusted_size,
         lip.height as height,
         CASE WHEN lip.id > 0
           THEN (SELECT name FROM spree_option_values WHERE id = lip.color_id)
@@ -58,7 +58,7 @@ module Reports
         END as color,
         li.quantity as quantity,
         fa.name as factory,
-        to_char(o.completed_at, 'YYYY-MM-DD') as delivery_date,
+        to_char(o.completed_at, 'YYYY-MM-DD') as completed_at_char,
         ( SELECT pmo.id FROM line_item_making_options ilmo
           INNER JOIN product_making_options pmo ON ilmo.making_option_id = pmo.id
           WHERE ilmo.line_item_id = li.id AND pmo.option_type = 'fast_making' ) as fast_making,
@@ -89,20 +89,77 @@ module Reports
         ssa.city,
         ssa.zipcode,
         ssa_s.name as state,
-        ssa_c.name as country
+        ssa_c.name as country,
+
+        ( SELECT "spree_assets".id || '/large/' || "spree_assets".attachment_file_name
+          FROM "spree_assets"
+          WHERE
+            (
+              ("spree_assets".viewable_id IN
+                ( SELECT pcv.id
+                  FROM "product_color_values" pcv
+                  WHERE
+                  pcv.product_id = sp.id
+                  AND
+                  pcv.option_value_id IN (SELECT "spree_option_values".id FROM "spree_option_values"
+                    INNER JOIN "spree_option_values_variants" ON "spree_option_values"."id" = "spree_option_values_variants"."option_value_id"
+                          WHERE "spree_option_values_variants".variant_id = sv.id)) AND "spree_assets".viewable_type = 'ProductColorValue')
+            OR
+              ("spree_assets".viewable_id = sv.id AND "spree_assets".viewable_type = 'Spree::Variant')
+            )
+          AND
+            lower("spree_assets".attachment_file_name) LIKE '%front-crop%'
+          LIMIT 1) as variant_image,
+
+        CASE WHEN lip.id > 0
+          THEN
+            ( SELECT "spree_assets".id || '/large/' || "spree_assets".attachment_file_name
+              FROM "spree_assets"
+              WHERE
+                (
+                  ("spree_assets".viewable_id IN
+                    ( SELECT pcv.id
+                      FROM "product_color_values" pcv
+                      WHERE
+                      pcv.product_id = sp.id
+                      AND
+                      pcv.option_value_id IN (SELECT "spree_option_values".id FROM "spree_option_values"
+                        INNER JOIN "spree_option_values_variants" ON "spree_option_values"."id" = "spree_option_values_variants"."option_value_id"
+                              WHERE "spree_option_values_variants".variant_id = custom_variant_id)) AND "spree_assets".viewable_type = 'ProductColorValue')
+                OR
+                  ("spree_assets".viewable_id = custom_variant_id AND "spree_assets".viewable_type = 'Spree::Variant')
+                )
+              AND
+                lower("spree_assets".attachment_file_name) LIKE '%front-crop%'
+              LIMIT 1)
+          ELSE NULL
+          END as custom_variant_image
 
         FROM "spree_orders" o
-        LEFT OUTER JOIN "spree_addresses" sa ON sa."id" = o."bill_address_id"
-        LEFT OUTER JOIN "spree_addresses" ssa ON ssa."id" = o."ship_address_id"
-        LEFT OUTER JOIN "spree_states" ssa_s ON ssa_s."id" = ssa."state_id"
-        LEFT OUTER JOIN "spree_countries" ssa_c ON ssa_c."id" = ssa."country_id"
         LEFT OUTER JOIN "spree_line_items" li ON li."order_id" = o."id"
         LEFT OUTER JOIN "line_item_personalizations" lip ON lip."line_item_id" = li."id"
         LEFT OUTER JOIN "spree_variants" sv ON sv."id" = li."variant_id"
         LEFT OUTER JOIN "spree_products" sp ON sp."id" = sv."product_id"
-        LEFT OUTER JOIN "fabrications" f ON f."line_item_id" = li."id"
-        LEFT OUTER JOIN "factories" fa ON sp."factory_id" = fa."id"
-        LEFT OUTER JOIN "spree_shipments" ss ON ss."order_id" = o."id"
+        INNER JOIN "spree_addresses" sa ON sa."id" = o."bill_address_id"
+        INNER JOIN "spree_addresses" ssa ON ssa."id" = o."ship_address_id"
+        INNER JOIN "spree_states" ssa_s ON ssa_s."id" = ssa."state_id"
+        INNER JOIN "spree_countries" ssa_c ON ssa_c."id" = ssa."country_id"
+        INNER JOIN "fabrications" f ON f."line_item_id" = li."id"
+        INNER JOIN "factories" fa ON sp."factory_id" = fa."id"
+        INNER JOIN "spree_shipments" ss ON ss."order_id" = o."id",
+
+        LATERAL( SELECT
+          CASE WHEN lip.id > 0
+          THEN (SELECT sovv.variant_id FROM "spree_option_values" sov
+            INNER JOIN "spree_option_values_variants" sovv ON "sov"."id" = sovv."option_value_id"
+            WHERE
+              sovv.variant_id IN (SELECT "spree_variants".id FROM "spree_variants"
+                WHERE "spree_variants".product_id = sp.id AND "spree_variants"."is_master" = 'f' AND "spree_variants"."deleted_at" IS NULL)
+              AND sov.id = lip.color_id
+              LIMIT 1)
+          ELSE NULL
+          END as custom_variant_id) s1
+
         WHERE completed_at IS NOT NULL
           AND o.completed_at between '#{from}' and '#{to}'
         ORDER BY completed_at;
