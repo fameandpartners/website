2307b44b0ae4c9f77fa3a87fb7903d7f8e97865e
Tiago Amaro <tiagoa@fameandpartners.com>
2016-05-24 15:10:06 -0700

    guarantee that HTTPS redirection is a 301

--- lib/overrides/spree/core/ssl_requirement.rb
+++ lib/overrides/spree/core/ssl_requirement.rb
@@ -6,4 +6,15 @@ module SslRequirement
     return Spree::Config[:allow_ssl_in_staging] if Rails.env.staging?
     return Spree::Config[:allow_ssl_in_development_and_test] if (Rails.env.development? or Rails.env.test?)
   end
+
+  def ensure_proper_protocol
+    return true if ssl_allowed?
+    if ssl_required? && !request.ssl? && ssl_supported?
+      redirect_to 'https://' + request.host + request.fullpath, status: :moved_permanently
+      flash.keep
+    elsif request.ssl? && !ssl_required?
+      redirect_to 'http://' + request.host + request.fullpath, status: :moved_permanently
+      flash.keep
+    end
+  end
 end
