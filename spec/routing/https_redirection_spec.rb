7ae2564c2eee997ed647646eb3a886a32047bdb0
Tiago Amaro <tiagoa@fameandpartners.com>
2016-05-24 15:19:21 -0700

    add description to https://www spec case

--- spec/routing/https_redirection_spec.rb
+++ spec/routing/https_redirection_spec.rb
@@ -7,20 +7,22 @@ describe 'HTTPS redirection', type: :request do
   end
 
   context 'given any request to fame and partners domain (.com.au and .com)' do
-    context '.com domain' do
-      it_will :redirect, 'http://fameandpartners.com/anything?something=else', 'https://www.fameandpartners.com/anything?something=else'
-      it_will :redirect, 'http://fameandpartners.com', 'https://www.fameandpartners.com'
+    describe 'redirects to the www subdomain' do
+      context '.com domain' do
+        it_will :redirect, 'http://fameandpartners.com/anything?something=else', 'https://www.fameandpartners.com/anything?something=else'
+        it_will :redirect, 'http://fameandpartners.com', 'https://www.fameandpartners.com'
 
-      it_will :redirect, 'https://fameandpartners.com/anything?something=else', 'https://www.fameandpartners.com/anything?something=else'
-      it_will :redirect, 'https://fameandpartners.com', 'https://www.fameandpartners.com'
-    end
+        it_will :redirect, 'https://fameandpartners.com/anything?something=else', 'https://www.fameandpartners.com/anything?something=else'
+        it_will :redirect, 'https://fameandpartners.com', 'https://www.fameandpartners.com'
+      end
 
-    context '.com.au domain' do
-      it_will :redirect, 'http://fameandpartners.com.au/anything?something=else', 'https://www.fameandpartners.com.au/anything?something=else'
-      it_will :redirect, 'http://fameandpartners.com.au', 'https://www.fameandpartners.com.au'
+      context '.com.au domain' do
+        it_will :redirect, 'http://fameandpartners.com.au/anything?something=else', 'https://www.fameandpartners.com.au/anything?something=else'
+        it_will :redirect, 'http://fameandpartners.com.au', 'https://www.fameandpartners.com.au'
 
-      it_will :redirect, 'https://fameandpartners.com.au/anything?something=else', 'https://www.fameandpartners.com.au/anything?something=else'
-      it_will :redirect, 'https://fameandpartners.com.au', 'https://www.fameandpartners.com.au'
+        it_will :redirect, 'https://fameandpartners.com.au/anything?something=else', 'https://www.fameandpartners.com.au/anything?something=else'
+        it_will :redirect, 'https://fameandpartners.com.au', 'https://www.fameandpartners.com.au'
+      end
     end
   end
 end
