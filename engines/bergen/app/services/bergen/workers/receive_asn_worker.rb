72ed3b30623a666a3805632a1353bc7b1591715f
Tiago Amaro <tiagoa@fameandpartners.com>
2016-05-25 16:39:54 -0700

    checking if notifications from ASN received are null

--- engines/bergen/app/services/bergen/workers/receive_asn_worker.rb
+++ engines/bergen/app/services/bergen/workers/receive_asn_worker.rb
@@ -62,9 +62,8 @@ module Bergen
         }
       end
 
-      # TODO: checking the notification status is better here. This will raise errors if messages are InvalidAsnError or something similar!! # Aren't receving_status and notification messages the same? Check it
       def item_was_received?
-        bergen_ticket_response[:receiving_status] == ADDED_TO_INVENTORY
+        bergen_ticket_response[:notifications].nil? && bergen_ticket_response[:receiving_status] == ADDED_TO_INVENTORY
       end
 
       def bergen_ticket_response
