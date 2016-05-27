433e544dfee44949e4c8f257b64fcf86e02ad665
Mateus Moraes <mateusm@fameandpartners.com>
2016-05-27 00:01:12 -0300

    [feat] make additional fields mandatory
    Added phone, preferred time #1 and timezone as mandatory fields.

--- app/models/style_session.rb
+++ app/models/style_session.rb
@@ -14,7 +14,7 @@ class StyleSession
     :preference3,
     :timezone
 
-  validates_presence_of :email, :full_name
+  validates_presence_of :email, :full_name, :phone, :preference1, :timezone
   validates_format_of :email, with: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
 
   def initialize(attributes = {})
