3206d68159f203ff7a65a27bc549d4521d8ca30a
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-05-30 19:51:49 +0300

    china team and Ben emails assigned

--- app/mailers/daily_report.rb
+++ app/mailers/daily_report.rb
@@ -5,7 +5,7 @@ class DailyReport < ActionMailer::Base
   def email(report_full_name, report_filename, report_type)
     mail.attachments[report_filename] = File.read(report_full_name)
     mail(
-      to: 'dmitry.chekalin@gmail.com',
+      to: 'orders@fameandpartners.com.cn, bens@fameandpartners.com',
       subject: "Daily report #{Date.today.to_date} #{report_type}",
       layout: false
     )
