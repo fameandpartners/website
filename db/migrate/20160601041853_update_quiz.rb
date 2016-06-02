dfc18797c4f588f4527a8b0facb9c103d28b83e1
Nam Dang <namproctin@gmail.com>
2016-06-02 10:20:20 +0700

    fix stuffs

--- db/migrate/20160601041853_update_quiz.rb
+++ db/migrate/20160601041853_update_quiz.rb
@@ -12,28 +12,17 @@ class UpdateQuiz < ActiveRecord::Migration
     q10 = Quiz.active.questions.where(text: "What is your skin colour?").first
     q11 = Quiz.active.questions.where(text: "What is your body shape?").first
 
-    q1.update_column(:position, "1002")
-    q1.update_column(:step, 2)
-    q2.update_column(:position, "1001")
-    q2.update_column(:step, 1)
-    q3.update_column(:position, "1004")
-    q3.update_column(:step, 4)
-    q4.update_column(:position, "1005")
-    q4.update_column(:step, 5)
-    q5.update_column(:position, "1006")
-    q5.update_column(:step, 6)
-    q6.update_column(:position, "1007")
-    q6.update_column(:step, 7)
-    q7.update_column(:position, "1008")
-    q7.update_column(:step, 8)
-    q8.update_column(:position, "1009")
-    q8.update_column(:step, 9)
-    q9.update_column(:position, "1010")
-    q9.update_column(:step, 10)
-    q10.update_column(:position, "1011")
-    q10.update_column(:step, 10)
-    q11.update_column(:position, "1012")
-    q11.update_column(:step, 11)
+    q1.update_column(:position: "1002", step: 2)
+    q2.update_column(:position: "1001", step: 1)
+    q3.update_column(:position: "1004", step: 4)
+    q4.update_column(:position: "1005", step: 5)
+    q5.update_column(:position: "1006", step: 6)
+    q6.update_column(:position: "1007", step: 7)
+    q7.update_column(:position: "1008", step: 8)
+    q8.update_column(:position: "1009", step: 9)
+    q9.update_column(:position: "1010", step: 10)
+    q10.update_column(:position: "1011", step: 10)
+    q11.update_column(:position: "1012", step: 11)
 
     q1.answers.where('code NOT IN (?)', ["boho1","boho2","boho3","classic1","classic2","classic3","edgy1","edgy2","edgy3","girly1","girly2","girly3","glam1","glam2","glam3"]).delete_all
     q2.answers.where('code NOT IN (?)', ["boho1","boho2","boho3","classic1","classic2","classic3","edgy1","edgy2","edgy3","girly1","girly2","girly3","glam1","glam2","glam3"]).delete_all
@@ -42,9 +31,21 @@ class UpdateQuiz < ActiveRecord::Migration
     q6.answers.where('code NOT IN (?)', ["boho1","boho2","boho3","classic1","classic2","classic3","edgy1","edgy2","edgy3","girly1","girly2","girly3","glam1","glam2","glam3"]).delete_all
 
     q8.answers.where('sexiness > 5').delete_all
+
     new_q3 = Quiz.active.questions.create(text: "Which colors do you like to wear?", position: "1003", partial: "which_colors", multiple: true, step: 3)
+    ['black','blue','blush','green','nude','pink','print','purple','red','silver','white','yellow'].each do |c|
+      new_q3.answers.create(code: c)
+    end
+
     new_q12 = Quiz.active.questions.create(text: "What is your dress size?", position: "1013", partial: "dress_size", multiple: false, step: 11)
+    ['US 0','US 2','US 4','US 6','US 8','US 10','US 12','US 14','US 16','US 18','US 20','US 22'].each do |s|
+      new_q12.answers.create(code: s)
+    end
+
     new_q13 = Quiz.active.questions.create(text: "How tall are you?", position: "1014", partial: "how_tall", multiple: false, step: 11)
+    ['Petite','Standard','Tall'].each do |h|
+      new_q13.answers.create(code: h)
+    end
   end
 
   def down
