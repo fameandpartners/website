ce9d5d22fdf2254427190240f27693feba218ca4
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-06-09 19:47:21 +0300

    manual order new form ajax implemented

--- engines/manual_order/app/controllers/manual_order/manual_orders_controller.rb
+++ engines/manual_order/app/controllers/manual_order/manual_orders_controller.rb
@@ -1,12 +1,47 @@
 module ManualOrder
   class ManualOrdersController < ::AdminUi::ApplicationController
 
+    layout 'manual_order'
+
+    helper_method :length_options_json
+
     def index
 
     end
 
     def new
       @manual_order_form = Forms::ManualOrderForm.new(Spree::Product.new)
+    end
+
+    def sizes_options_json
+      render json: get_size_options(params[:product_id])
+    end
+
+    def colors_options_json
+      render json: get_color_options(params[:product_id])
+    end
+
+
+    private
+
+    def products
+      Spree::Product.active
+    end
+
+    def get_size_options(product_id)
+      products.find(product_id).variants.map {|v| { id: v.dress_size.id, name: v.dress_size.name} }.uniq
+    end
+
+    def get_color_options(product_id)
+      products.find(product_id).variants.map {|v| { id: v.dress_color.id, name: v.dress_color.name} }.uniq
+    end
+
+    def length_options_json
+      {
+        'petite' =>'Petite',
+        'standart' => 'Standart',
+        'tall' => 'Tall'
+      }
     end
 
   end
--- engines/manual_order/config/routes.rb
+++ engines/manual_order/config/routes.rb
@@ -1,5 +1,10 @@
 ManualOrder::Engine.routes.draw do
 
-  resources :manual_orders
+  resources :manual_orders do
+    collection do
+      get 'colors/:product_id' => 'manual_orders#colors_options_json'
+      get 'sizes/:product_id' => 'manual_orders#sizes_options_json'
+    end
+  end
 
 end
