0b757884d5f8a494c00fb0229858f69ea6c63242
Mateus Moraes <mateus@Mateuss-MacBook-Pro.local>
2016-06-11 02:00:21 -0300

    [feat] update model

--- app/models/wedding_consultation.rb
+++ app/models/wedding_consultation.rb
@@ -1,31 +1,25 @@
 class WeddingConsultation
   include ActiveModel::Validations
-  MAX_WORD_COUNT = 500
 
-  attr_accessor :full_name, :email, :phone, :dob, :location, :skype_id, :timezone, :info
+  SESSION_TYPES = %w{ default birthday prom }
 
-  validates :email, format: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, presence: true
+  attr_accessor :full_name,
+    :session_type,
+    :email,
+    :phone,
+    :birthday,
+    :preference1,
+    :preference2,
+    :preference3,
+    :timezone
 
-  validates :full_name, :email, :phone, :dob, :location, :timezone, :info, presence: true
-
-  validate do
-    if info.present?
-      if info.split(/\s+/).size > MAX_WORD_COUNT
-        errors.add(:info, "maximum #{MAX_WORD_COUNT} words allowed")
-      end
-    end
-  end
+  validates_presence_of :email, :full_name, :phone, :preference1, :timezone
+  validates_format_of :email, with: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
 
   def initialize(attributes = {})
     attributes.each do |name, value|
-      if respond_to?("#{name}=")
-        send("#{name}=", value)
-      end
+      public_send("#{name}=", value)
     end
-  end
-
-  def persisted?
-    false
   end
 
   def to_key
