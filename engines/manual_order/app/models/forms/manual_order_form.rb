35ab09d15c6c09577c85715478d72349754e3cfa
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-06-13 19:47:02 +0300

    starter customer fields added to the new manual order form

--- engines/manual_order/app/models/forms/manual_order_form.rb
+++ engines/manual_order/app/models/forms/manual_order_form.rb
@@ -10,6 +10,15 @@ module Forms
     property :customisations, virtual: true
     property :notes, virtual: true
     property :status, virtual: true
+    property :email, virtual: true
+    property :first_name, virtual: true
+    property :last_name, virtual: true
+    property :address, virtual: true
+    property :city, virtual: true
+    property :state, virtual: true
+    property :cntry, virtual: true
+    property :zip, virtual: true
+    property :phone, virtual: true
 
     def style_names
       Spree::Product.active
