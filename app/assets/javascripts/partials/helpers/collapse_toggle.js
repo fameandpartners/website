578251fe3c5affe567ab03de4783c4dcd9477e9a
Mateus Moraes <mateus@seomaster.com.br>
2016-06-14 14:42:59 -0300

    [fix] declare variable and wrap block in iife

--- app/assets/javascripts/partials/helpers/collapse_toggle.js
+++ app/assets/javascripts/partials/helpers/collapse_toggle.js
@@ -1,12 +1,13 @@
-$('body').on('click.collapse-next.data-api', '[data-toggle=collapse-next]', function() {
-  $this = $(this);
-  var $target = $this.parent().next();
-  $this.toggleClass('collapsed');
-  $target.data('bs.collapse') ? $target.collapse('toggle') : $target.collapse();
-});
 (function ($) {
+  $('body').on('click.collapse-next.data-api', '[data-toggle=collapse-next]', function () {
+    var $this   = $(this);
+    var $target = $this.parent().next();
+    $this.toggleClass('collapsed');
+    $target.data('bs.collapse') ? $target.collapse('toggle') : $target.collapse();
+  });
+
   var hash = document.location.hash;
   if (hash) {
     $(hash).addClass('in');
   }
-}(jQuery));
+})(jQuery);
