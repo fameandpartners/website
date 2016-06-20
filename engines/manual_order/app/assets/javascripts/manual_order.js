e314f58bd28adef91128c9eb1c76523aef9485d4
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-06-20 18:39:37 +0300

    small refactor

--- engines/manual_order/app/assets/javascripts/manual_order.js
+++ engines/manual_order/app/assets/javascripts/manual_order.js
@@ -10,4 +10,4 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require manual_order/manual_orders.js
+//= require manual_order/products
