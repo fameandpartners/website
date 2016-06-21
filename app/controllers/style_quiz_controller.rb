4253164bd7faae612f0b058f54a7d2117f91019a
Alex Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-06-21 17:01:14 +0300

    add capture email view

--- app/controllers/style_quiz_controller.rb
+++ app/controllers/style_quiz_controller.rb
@@ -100,7 +100,7 @@ class StyleQuizController < ActionController::Base
     end
 
     style_profile.save
-    track_event(style_profile.user)
+    style_profile.user ? track_user_email(style_profile.user.email) : track_user_email(params[:email])
 
     if taxons.present?
       taxons.group_by(&:id).each do |id, group|
@@ -147,8 +147,8 @@ class StyleQuizController < ActionController::Base
       end
     end
 
-    def track_event(user)
-      tracker = Marketing::CustomerIOEventTracker.new
-      tracker.track(user, :some_event, nil)
+    def track_user_email(email)
+      return unless email
+      Marketing::CustomerIOEventTracker.new.identify_user_by_email(email, current_site_version)
     end
 end
