270bbfee6c1a3dd24b66e55edad2c8ef34aa7a00
Tiago Amaro <tiagoa@fameandpartners.com>
2016-06-21 14:16:23 +0200

    resize firefox window to 1400x768 (on selenium)

--- spec/support/setup/capybara.rb
+++ spec/support/setup/capybara.rb
@@ -6,17 +6,25 @@ end
 Capybara.default_host          = ENV.fetch('CAPYBARA_DEFAULT_HOST', 'us.lvh.me')
 Capybara.default_max_wait_time = ENV.fetch('CAPYBARA_DEFAULT_MAX_WAIT_TIME', '30').to_i
 
-# On Mac, remember to point to `firefox-bin`. Example: "/Applications/Firefox.app/Contents/MacOS/firefox-bin"
-Selenium::WebDriver::Firefox::Binary.path = ENV.fetch('CAPYBARA_SELENIUM_FIREFOX_BINARY_PATH', Selenium::WebDriver::Firefox::Binary.path)
-
 # Allow using Chrome
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app,
-    browser: :chrome,
+  Capybara::Selenium::Driver.new(
+    app,
+    browser:              :chrome,
     desired_capabilities: {
       'chromeOptions' => {
         'args' => %w{ window-size=1400,768 }
       }
     }
   )
+end
+
+# Selenium custom configurations
+# On Mac, remember to point to `firefox-bin`. Example: "/Applications/Firefox.app/Contents/MacOS/firefox-bin"
+Selenium::WebDriver::Firefox::Binary.path = ENV.fetch('CAPYBARA_SELENIUM_FIREFOX_BINARY_PATH', Selenium::WebDriver::Firefox::Binary.path)
+
+RSpec.configure do |config|
+  config.before(:each, selenium: true) do
+    Capybara.current_session.current_window.resize_to(1400, 768)
+  end
 end
