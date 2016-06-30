7abbfacf19c59cd526c64c54d84fd81ffa41b1a4
Mateus Moraes <mateusm@fameandpartners.com>
2016-06-29 22:34:21 -0300

    [feat] replace js function with css for the fixed header

--- app/assets/javascripts/partials/header.js
+++ app/assets/javascripts/partials/header.js
@@ -64,16 +64,4 @@
     });
   }
 
-  // Fixed Navbar during scroll
-  $(window).scroll(function() {
-    var scrollTop = $(window).scrollTop();
-    var topMargin = 110;
-    $(".nav-menu.nav-alert-top").css("top", scrollTop + "px");
-      if (scrollTop > topMargin) {
-        $("#fixed-header").css("top", scrollTop - topMargin + "px");
-      } else {
-        $("#fixed-header").css("top", "0px");
-      }
-  });
-
 })(jQuery);
