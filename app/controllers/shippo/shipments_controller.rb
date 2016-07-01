ef419c95c37a8a52d3d3b200193f476c840b0ada
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-07-01 15:36:22 +0300

    small refactor

--- app/controllers/shippo/shipments_controller.rb
+++ app/controllers/shippo/shipments_controller.rb
@@ -2,7 +2,7 @@ module Shippo
   class ShipmentsController < ActionController::Base
 
     def update
-      order_number = params[:metadata].split(' ')[1]
+      order_number = params[:metadata].split(' ').second
       if (order = Spree::Order.where(number: order_number).first)
         if (shipment = order.shipments.first)
           shipment.update_attributes(tracking: params[:tracking_number])
