b3713e6cbfacf0843d34f7d8f7e7538a0b0231fd
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-07-04 18:20:55 +0300

    existing customer spec added

--- engines/admin_ui/spec/acceptance/manual_orders.feature
+++ engines/admin_ui/spec/acceptance/manual_orders.feature
@@ -1,8 +1,18 @@
-Feature: Assign colors to products
+Feature: Create manual orders
 
   Background:
     Given A sample database with valid data
     And The example admin is signed in
+    And The example user created with:
+      | Email                   | test@email.com |
+      | First Name              | Roger          |
+      | Last Name               | That           |
+      | Street Address          | Street X       |
+      | Street Address 2        | House Y        |
+      | City                    | Melbourne      |
+      | Phone Number            | 2255-4422      |
+      | Zipcode                 | 12345          |
+    And The example completed order exists with this user
 
   @javascript
   Scenario:
@@ -16,3 +26,10 @@ Feature: Assign colors to products
     Then I should have black dress image
     And I select "Australia" from chosen country select box
     Then I should see "$319.0 AUD"
+
+  @javascript
+  Scenario:
+    When I am on the manual orders page
+    And I click on "Create" link
+    And I select "Roger That" from chosen customers select box
+    Then I should see correct user data prefilled
--- engines/admin_ui/spec/support/steps/manual_orders_steps.rb
+++ engines/admin_ui/spec/support/steps/manual_orders_steps.rb
@@ -5,6 +5,35 @@ module Acceptance
       visit '/fame_admin/manual_orders'
     end
 
+    step 'The example user created with:' do |user_data|
+      user_data = user_data.to_h
+      address = create :address,
+                 firstname: user_data['First Name'],
+                 lastname: user_data['Last Name'],
+                 email: user_data['Email'],
+                 address1: user_data['Street Address'],
+                 address2: user_data['Street Address 2'],
+                 city: user_data['City'],
+                 zipcode: user_data['Zipcode'],
+                 phone: user_data['Phone Number']
+      user = create :spree_user
+      user.first_name = user_data['First Name']
+      user.last_name = user_data['Last Name']
+      user.email = user_data['Email']
+      user.bill_address = address
+      user.ship_address = address
+      user.save
+    end
+
+    step 'The example completed order exists with this user' do
+      order = create :complete_order
+      user = Spree::User.last
+      order.user = user
+      order.bill_address_id = user.bill_address_id
+      order.ship_address_id = user.ship_address_id
+      order.save
+    end
+
     step 'I select "Connie" product from chosen style name select box' do
       chosen_select('Connie (SKU: 4B453)', from: '#forms_manual_order_style_name')
     end
@@ -26,7 +55,15 @@ module Acceptance
     end
 
     step 'I should have black dress image' do
-      page.should have_selector('h4.product_image img[src*=black-front-crop]')
+      expect(page).to have_selector('h4.product_image img[src*=black-front-crop]')
+    end
+
+    step 'I select "Roger That" from chosen customers select box' do
+      chosen_select('Roger That', from: '#forms_manual_order_existing_customer')
+    end
+
+    step 'I should see correct user data prefilled' do
+      expect(page).to have_selector("//input[contains(@value,'Roger')]")
     end
 
   end
