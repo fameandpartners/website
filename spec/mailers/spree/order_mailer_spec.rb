de06b852ca69bb9175b7067ecb50b66ddcfeda40
Tiago Amaro <tiagoa@fameandpartners.com>
2016-07-05 18:38:14 +0200

    make test values explicit on order mailer test

--- spec/mailers/spree/order_mailer_spec.rb
+++ spec/mailers/spree/order_mailer_spec.rb
@@ -1,26 +1,42 @@
 require 'spec_helper'
 
-describe Spree::OrderMailer do
+module Spree
+  RSpec.describe OrderMailer, type: :mailer do
+    let(:today_date) { Date.parse('05/05/2016') }
+    let(:delivery_date) { Date.parse('10/10/2016') }
 
-  let(:order) { create(:complete_order_with_items, projected_delivery_date: Date.today) }
+    let(:user) { build(:spree_user, email: 'loroteiro@silvestre.com') }
+    let(:address) { build(:address, address1: 'Street Macarena', address2: 'Around the Corner', zipcode: '12321', city: 'Las Ketchup', phone: '1234-5678') }
+    let(:order) { create(:complete_order_with_items, number: 'R123123123', projected_delivery_date: delivery_date, user: user, bill_address: address, ship_address: address) }
 
-  it 'send data to customerio correctly' do
-    expected_attrs = { email_to: order.user.email,
-                       subject:  "#{Spree::Config[:site_name]} #{I18n.t('order_mailer.confirm_email.subject')} ##{order.number}",
-                       order_number: order.number,
-                       line_items: Marketing::OrderPresenter.build_line_items(order),
-                       display_item_total: order.display_item_total.to_s,
-                       adjustments: Marketing::OrderPresenter.build_adjustments(order),
-                       display_total: order.display_total.to_s,
-                       auto_account: order.user && order.user.automagically_registered?,
-                       today: Date.today.strftime("%d.%m.%y"),
-                       billing_address: order.try(:billing_address).to_s  || 'No Billing Address',
-                       shipping_address: order.try(:shipping_address).to_s || 'No Shipping Address',
-                       phone: order.try(:billing_address).try(:phone) || 'No Phone',
-                       delivery_date: order.projected_delivery_date.strftime("%a, %d %b %Y")
-                        }
-    expect_any_instance_of(Marketing::CustomerIOEventTracker).to receive(:track).with(order.user, 'order_confirmation_email', expected_attrs)
-    described_class.confirm_email(order)
+    before(:each) do
+      Spree::Config[:site_name] = 'My Super eCommerce'
+      allow(Date).to receive(:today).and_return(today_date)
+    end
+
+    let(:expected_attributes) {
+      {
+        # Calculated attributes. TODO on assertive values
+        adjustments:        Marketing::OrderPresenter.build_adjustments(order),
+        display_item_total: order.display_item_total.to_s,
+        display_total:      order.display_total.to_s,
+        line_items:         Marketing::OrderPresenter.build_line_items(order),
+        # Delegated attributes. Easy to process
+        auto_account:       false,
+        billing_address:    'Street Macarena Around the Corner, Las Ketchup, Alabama, 12321, United States of Foo',
+        delivery_date:      'Sun, 09 Oct 2016',
+        email_to:           'loroteiro@silvestre.com',
+        order_number:       'R123123123',
+        phone:              '1234-5678',
+        shipping_address:   'Street Macarena Around the Corner, Las Ketchup, Alabama, 12321, United States of Foo',
+        subject:            'My Super eCommerce Order Confirmation #R123123123',
+        today:              '05.05.16',
+      }
+    }
+
+    it 'sends data to customerio correctly' do
+      expect_any_instance_of(Marketing::CustomerIOEventTracker).to receive(:track).with(order.user, 'order_confirmation_email', expected_attributes)
+      described_class.confirm_email(order)
+    end
   end
-
 end
