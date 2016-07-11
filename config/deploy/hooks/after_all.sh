6c567c373080ac89de717900acf21d19510c1f3e
Raphael Campardou <ralovely@gmail.com>
2016-07-11 20:20:14 +1000

    change deploy hook to use git sha instead of git tag

--- config/deploy/hooks/after_all.sh
+++ config/deploy/hooks/after_all.sh
@@ -4,8 +4,6 @@
 # # Don't touch this line
 # . ${current_app_path}/config/deploy/hooks/services/load.sh
 
-git_revision_tag=`cd ${current_app_path} && git describe --tags`
-
 if [ "${SERVER_ROLE}" == "web" ] ; then
 
   # Alert Sentry about deploy
@@ -13,11 +11,11 @@ if [ "${SERVER_ROLE}" == "web" ] ; then
   curl ${sentry_endpoint}/release/builtin/${SENTRY_PUBLIC_KEY}/${SENTRY_PRIVATE_KEY}/ \
   -X POST \
   -H "Content-Type: application/json" \
-  -d "{'version': '${git_revision_tag}'}"
+  -d "{'version': '${remote_sha}'}"
 fi
 
 # Alert Slack about deploy
 slack_endpoint='https://hooks.slack.com/services'
 curl ${slack_endpoint}/T026PUF20/B046TP83D/${SLACK_API_KEY} \
 -X POST \
---data-urlencode "payload={'channel':'#development','username':'FameBot','icon_emoji':':dancer:','text':'*Sentinel* started deploying ${git_revision_tag} to *${SERVER_ROLE} ${FRAMEWORK_ENV}*'}"
+--data-urlencode "payload={'channel':'#development','username':'FameBot','icon_emoji':':dancer:','text':'*Sentinel* started deploying ${remote_sha} to *${SERVER_ROLE} ${FRAMEWORK_ENV}*'}"
