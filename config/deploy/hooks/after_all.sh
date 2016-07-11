2d7b3570118bdde0986adc750d96708fb93dcc17
Raphael Campardou <ralovely@gmail.com>
2016-07-11 11:07:26 +1000

    Add after_all deploy hook

---
+++ config/deploy/hooks/after_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# # Loading all the services helpers.
+# # Don't touch this line
+# . ${current_app_path}/config/deploy/hooks/services/load.sh
+
+if [ "${SERVER_ROLE}" == "web" ] ; then
+  git_revision_tag=`cd ${current_app_path} && git describe --tags`
+
+  # Clear cache
+  echo "Clearing Application Cache"
+  cd ${current_app_path} && bundle exec rake cache:clear &
+
+  # Alert Sentry about deploy
+  sentry_endpoint='https://app.getsentry.com/api/hooks'
+  curl ${sentry_endpoint}/release/builtin/${SENTRY_PUBLIC_KEY}/${SENTRY_PRIVATE_KEY}/ \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d "{'version': '${git_revision_tag}'}"
+fi
+
+# Alert Slack about deploy
+slack_endpoint='https://hooks.slack.com/services'
+curl ${slack_endpoint}/T026PUF20/B046TP83D/${SLACK_API_KEY} \
+-X POST \
+--data-urlencode "payload={'channel':'#development','username':'FameBot','icon_emoji':':dancer:','text':'*Sentinel* started deploying ${git_revision_tag} to *${SERVER_ROLE} ${FRAMEWORK_ENV}*'}"
