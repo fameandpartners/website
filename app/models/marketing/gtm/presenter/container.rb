3350662f6e18ab76475f57ff92efc2f1f9358bd4
Alex Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-07-12 19:00:01 +0300

    fix tests

--- app/models/marketing/gtm/presenter/container.rb
+++ app/models/marketing/gtm/presenter/container.rb
@@ -13,14 +13,6 @@ module Marketing
           base_hash[presenter.key] = presenter.rescuable_body
         end
 
-        def append_single_variable(key, value)
-          base_hash[key] = value
-        end
-
-        def append_variables(hash)
-          hash.each { |key, value| base_hash[key.to_s] = value }
-        end
-
         def to_json
           presenters.each { |presenter| append(presenter) }
           base_hash.to_json.html_safe
