ab8e29b57ef7cb7582435f1195e7d8cc639fad90
Mateus Moraes <mateusm@fameandpartners.com>
2016-07-15 13:18:32 -0300

    optimise hero carousel on mobile

--- app/assets/javascripts/partials/homepage.js
+++ app/assets/javascripts/partials/homepage.js
@@ -9,11 +9,12 @@
     fade: true,
     arrows: false,
     dots: true,
+    edgeFriction: 10,
     responsive: [
       {
         breakpoint: 768,
         settings: {
-          speed: 800,
+          speed: 400,
           fade: false,
           slidesToShow: 1,
           slidesToScroll: 1
