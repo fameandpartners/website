6757cc0c80f01858d8df4157f06a216c300070ba
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-07-27 16:30:09 +0300

    update tracking numbers implemented

--- engines/bergen/app/services/bergen/workers/create_asn_worker.rb
+++ engines/bergen/app/services/bergen/workers/create_asn_worker.rb
@@ -10,6 +10,7 @@ module Bergen
         @return_request_item = return_item_process.return_request_item
 
         label = Shippo::Label.new(@return_request_item).create      # Create Shippo return label
+        update_tracking_number(label[:tracking_number])             # Update tracking number in the line_item shipment
         asn_number = create_asn(label[:tracking_number])            # Create ASN
         create_asn_retrieval_event(asn_number, label[:label_url])   # Save Bergen Ticket Number on Item Request Return
         advance_in_return_item_process                              # Continue
@@ -40,6 +41,18 @@ module Bergen
           tracking_number: tracking_number
         )
         result[:receiving_ticket_id]
+      end
+
+      def update_tracking_number(tracking_number)
+        order = return_request_item.line_item.order
+        shipments = order.shipments.select do |shipment|
+          shipment.line_items.any? {|line_item| line_item.id == return_request_item.line_item.id}
+        end
+
+        shipments.each do |shipment|
+          shipment.tracking = tracking_number
+          shipment.save
+        end
       end
 
       def bergen
