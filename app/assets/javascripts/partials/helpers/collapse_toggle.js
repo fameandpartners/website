6c36374f15e2e836b1143a8ebb74e9ca3a58cd71
Mateus Moraes <mateusm@fameandpartners.com>
2016-07-30 02:08:46 -0300

    optimise collapsible panels toggle behaviour
    That was needed because some links point to specific questions from FAQs. Now when they’re clicked the page is loaded with that very answer already open.

--- app/assets/javascripts/partials/helpers/collapse_toggle.js
+++ app/assets/javascripts/partials/helpers/collapse_toggle.js
@@ -1,18 +1,30 @@
 (function ($) {
 
+  function expand_target_panel(){
+    var url = window.location.hash;
+    if ( url.match('#') ) {
+        var hash = url.split('#')[1];
+        // activate the requested panel
+        $('.active #' + hash + ' .panel-collapse').addClass('in');
+        $('.active #' + hash + ' .panel-title').removeClass('collapsed');
+    }
+  }
+
   $('body').on('click.collapse-next.data-api', '[data-toggle=collapse-next]', function () {
     var $this   = $(this);
     var $target = $this.parent().next();
-    $this.toggleClass('collapsed');
     $target.data('bs.collapse') ? $target.collapse('toggle') : $target.collapse();
+    $this.toggleClass('collapsed');
   });
 
-  var url = window.location.hash;
-  if ( url.match('#') ) {
-      var hash = url.split('#')[1];
-      // activate the requested panel
-      $('#' + hash + ' .panel-collapse').addClass('in');
-      $('#' + hash + ' .panel-title').removeClass('collapsed')
-  }
+  // Watch clicks on anchor links
+  $(document).has(".panel-collapse").on("click", "a[href*='#']:not([href='#'], [href*='#panel-'], [data-toggle=collapse-next])", function() {
+    // Expand/Collapse panels
+    $('.active #' + this.hash.slice(1) + ' .panel-collapse').addClass('in').css('height','');
+    $('.active #' + this.hash.slice(1) + ' .panel-title').removeClass('collapsed');
+  });
+
+  // On page load
+  expand_target_panel();
 
 })(jQuery);
