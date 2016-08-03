beed34840ad3b33610bae24972f72988e2d17caf
Mateus Moraes <mateusm@fameandpartners.com>
2016-08-03 00:02:11 -0300

    rename js variables (anchor navigation)

--- app/assets/javascripts/partials/helpers/anchor_navigation.js
+++ app/assets/javascripts/partials/helpers/anchor_navigation.js
@@ -2,54 +2,53 @@
 (function ($) {
 
   // Get useful data before any interaction
-  var $navHeightDesktop = 0,
-      $navHeightLocalMenu = 0,
-      $localNavOffsetTop = 0;
+  var navHeightDesktop = 0,
+      navHeightLocalMenu = 0,
+      localNavOffsetTop = 0;
 
   if ($("#fixed-header").length)
-    var $navHeightDesktop = $("#fixed-header").delay(300).outerHeight();
+    var navHeightDesktop = $("#fixed-header").delay(300).outerHeight();
 
   if ($(".js-float-menu-on-scroll").length)
-    var $navHeightLocalMenu = $(".js-float-menu-on-scroll").delay(300).outerHeight();
+    var navHeightLocalMenu = $(".js-float-menu-on-scroll").delay(300).outerHeight();
 
   if ($(".local-navigation-wrapper .js-float-menu-on-scroll").length)
-    var $localNavOffsetTop = $(".local-navigation-wrapper .js-float-menu-on-scroll").offset().top;
+    var localNavOffsetTop = $(".local-navigation-wrapper .js-float-menu-on-scroll").offset().top;
 
-    var $offsetHeight = $navHeightDesktop+$navHeightLocalMenu,
-        $offsetHeightExtra = 45, // The desired distance between the target and the fixed header + local navigation
-        $mdScreenWidth = 992;
+    var offsetHeight = navHeightDesktop+navHeightLocalMenu,
+        offsetHeightExtra = 45, // The desired distance between the target and the fixed header + local navigation
+        mdScreenWidth = 992;
 
   // Add DOM helper if we are loading this page directly from an URL containing an anchor (/something#foo=bar)
   // This is needed for our fixed header and floating menu
   if ( window.location.hash ) {
-    var $hash = window.location.hash,
-        $id   = $hash.slice(1),
-        $elem = document.getElementById($id),
-        $hashlink = '<div id='+$id+' class="hashlink js-hashlink"></div>';
+    var hash_var = window.location.hash,
+        id   = hash_var.slice(1),
+        elem = document.getElementById(id),
+        hashlink = '<div id='+id+' class="hashlink js-hashlink"></div>';
 
-    $elem.removeAttribute('id');
-    $elem.insertAdjacentHTML('beforebegin', $hashlink);
+    elem.removeAttribute('id');
+    elem.insertAdjacentHTML('beforebegin', hashlink);
     if ($(".local-navigation .nav").length) {
-      $('.js-hashlink').css({'height': ($offsetHeight-$offsetHeightExtra)+'px', 'margin-top': -($offsetHeight-$offsetHeightExtra)+'px'});
+      $('.js-hashlink').css({'height': (offsetHeight-offsetHeightExtra)+'px', 'margin-top': -(offsetHeight-offsetHeightExtra)+'px'});
     } else {
-      $('.js-hashlink').css({'height': $offsetHeight+'px', 'margin-top': -$offsetHeight+'px'});
+      $('.js-hashlink').css({'height': offsetHeight+'px', 'margin-top': -offsetHeight+'px'});
     }
-    window.location.hash = $hash;
+    window.location.hash = hash_var;
   }
 
   // Check if we have any floating menu in the page
   if ($('.js-float-menu-on-scroll').length) {
 
     // Add scrollspy trigger
-    $('body').scrollspy({ target: '.js-float-menu-on-scroll', offset: ($offsetHeight+$offsetHeightExtra) })
+    $('body').scrollspy({ target: '.js-float-menu-on-scroll', offset: (offsetHeight+offsetHeightExtra) })
 
     // Watch scrolling to show/hide floating menu
     $(document).delay(500).on("scroll", function() {
-      var $windowPosition = $(window).scrollTop();
+      var windowPosition = $(window).scrollTop();
 
       // Toggle floating menu if window position is below the target element
-      // if ($windowPosition < ($localNavOffsetTop+$navHeightLocalMenu)) {
-      if ($windowPosition+$navHeightDesktop >= $(".local-navigation-wrapper").offset().top+$navHeightLocalMenu){
+      if (windowPosition+navHeightDesktop >= $(".local-navigation-wrapper").offset().top+navHeightLocalMenu){
         if ($('.js-float-menu-on-scroll.fixed-nav').length) {
           $('.js-float-menu-on-scroll.fixed-nav').fadeIn(100);
           $('.local-navigation-wrapper .js-float-menu-on-scroll').addClass('fixed-nav-mobile').fadeIn(100);
@@ -67,14 +66,14 @@
 
     // Responsive floating menu as a Carousel on mobile
     if ($(".local-navigation .nav").length) {
-      var $window = $(window),
-          $toggleSlick,
-          $responsiveNavLocal = $('.local-navigation .nav')
+      var window_var = $(window),
+          toggleSlick,
+          responsiveNavLocal = $('.local-navigation .nav')
 
-      $toggleSlick = function () {
-        if ($window.width() < $mdScreenWidth) {
-          if(!$responsiveNavLocal.hasClass('slick-initialized')) {
-            $responsiveNavLocal.slick({
+      toggleSlick = function () {
+        if (window_var.width() < mdScreenWidth) {
+          if(!responsiveNavLocal.hasClass('slick-initialized')) {
+            responsiveNavLocal.slick({
               autoplay: false,
               fade: false,
               arrows: false,
@@ -89,14 +88,14 @@
             });
           }
         } else {
-          if($responsiveNavLocal.hasClass('slick-initialized')) {
-            $responsiveNavLocal.slick('unslick');
+          if(responsiveNavLocal.hasClass('slick-initialized')) {
+            responsiveNavLocal.slick('unslick');
           }
         }
       }
 
-      $window.delay(500).resize($toggleSlick);
-      $toggleSlick();
+      window_var.delay(500).resize(toggleSlick);
+      toggleSlick();
     }
 
   }
@@ -106,44 +105,44 @@
 
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
-      var $target = $(this.hash);
-      $target = $target.length ? $target : $('[name=' + this.hash.slice(1) +']');
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-      if ($target.length) {
+      if (target.length) {
         // Get some useful data after the click interaction
-        var $offsetClickFromLocalNav = 0,
-            $navHeightLocalMenu = 0,
-            $navHeightDesktop = 0,
-            $offsetHeightNav = 0;
+        var offsetClickFromLocalNav = 0,
+            navHeightLocalMenu = 0,
+            navHeightDesktop = 0,
+            offsetHeightNav = 0;
 
         if ($(".js-float-menu-on-scroll").length)
-          var $navHeightLocalMenu = $(".js-float-menu-on-scroll").delay(300).outerHeight();
+          var navHeightLocalMenu = $(".js-float-menu-on-scroll").delay(300).outerHeight();
 
         if ($("#fixed-header").length)
-          var $navHeightDesktop = $("#fixed-header").delay(300).outerHeight();
+          var navHeightDesktop = $("#fixed-header").delay(300).outerHeight();
 
         // Reset our on-load anchor target helper
         if ($('.js-hashlink').length)
           if ($(".local-navigation .nav").length) {
-            $('.js-hashlink').css({'height': '0px', 'margin-top': $offsetHeightExtra+'px'});
+            $('.js-hashlink').css({'height': '0px', 'margin-top': offsetHeightExtra+'px'});
           } else {
             $('.js-hashlink').css({'height': '0px', 'margin-top': '0px'});
           }
 
         // This prevents the anchor target to be overlayed by our floating header
         if ($(this).closest(".local-navigation-wrapper").length)
-          $offsetClickFromLocalNav = $navHeightLocalMenu;
+          offsetClickFromLocalNav = navHeightLocalMenu;
 
         // Define the top offset for our anchor navigation, based on screen size
-        if ($(window).width() < $mdScreenWidth) {
-          $offsetHeightNav = $navHeightLocalMenu;
+        if ($(window).width() < mdScreenWidth) {
+          offsetHeightNav = navHeightLocalMenu;
         } else {
-          $offsetHeightNav = $navHeightDesktop+$offsetClickFromLocalNav;
+          offsetHeightNav = navHeightDesktop+offsetClickFromLocalNav;
         }
 
         // Scroll smoothly to our target
         $('html, body').delay(300).animate({
-          scrollTop: ($target.offset().top-$offsetHeightNav)
+          scrollTop: (target.offset().top-offsetHeightNav)
         }, 500);
         // Keep local anchor navigation in browser history
         history.pushState({}, "", this.href);
