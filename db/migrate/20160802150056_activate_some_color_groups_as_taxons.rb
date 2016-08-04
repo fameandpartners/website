34776c1786aaf92655d70606949cd4a3a48c637b
Tiago Amaro <tiagoa@fameandpartners.com>
2016-08-04 15:17:16 +0200

    create revolution pages for new colors (if they don’t exist)

--- db/migrate/20160802150056_activate_some_color_groups_as_taxons.rb
+++ db/migrate/20160802150056_activate_some_color_groups_as_taxons.rb
@@ -12,6 +12,70 @@ class ActivateSomeColorGroupsAsTaxons < ActiveRecord::Migration
     'white-ivory'   => ['white', 'ivory'],
   }
 
+  REVOLUTION_PAGES_INFO = {
+    'black'         => {
+      meta_description: 'Find a black dress for any occasion and style from prom to maxi’s. Transform and customize your look from day-time office to night-time chic.',
+      meta_title:       'Black dresses and gowns',
+      subtitle:         'The black dress has taken a new turn. A timeless item in your wardrobe perfect for every occasion. Any style, we’ve got you covered.',
+      title:            'Black Dresses',
+    },
+    'blues-purples' => {
+      title:            'Blue Dresses',
+      subtitle:         'Feeling blue? Change it up in bold & daring shades of teal, turquoise or navy dresses and add an unexpected turn to your wardrobe.',
+      meta_title:       'Blue dresses | Shop our trending range',
+      meta_description: 'Fame & Partners offers a wide variety of blue dresses. From Icy to baby blue, we have it all.',
+    },
+    'greens'        => {
+      title:            'Green Dresses',
+      subtitle:         "A look that's on-the-money.",
+      meta_title:       'Green Dresses - Shop Online',
+      meta_description: "Make 'em green with envy in our collection of green gowns, cocktail dresses, two-piece sets and slips, all made-to-order and guaranteed to fit.",
+    },
+    'grey'          => {
+      title:            'Grey Dresses',
+      subtitle:         "It's good to go grey.",
+      meta_title:       'Grey Dresses - Shop Online',
+      meta_description: 'Ready to go grey? Search our shoppable collection of grey slip dresses, grey gowns, grey cocktail dresses and more, all customizable and made-to-order.',
+    },
+    'metallic'      => {
+      title:            'Metallic dresses',
+      subtitle:         'Heavy Metal',
+      meta_title:       'Metallic dress online',
+      meta_description: 'Shop gold, silver, rose gold and black metallic dresses in sequins and shiny silks in fully customizable styles.',
+
+    },
+    'nude-tan'      => {
+      title:            'Nude Dresses',
+      subtitle:         'Stripped-down style',
+      meta_title:       'Nude Dresses and Tan Dresses - Shop Online',
+      meta_description: 'Shop the new Naked Dress. Our collection of nude dresses and tan dresses is customizable, made-to-order and guaranteed to fit.',
+    },
+    'pastels'       => {
+      title:            'Pastel Dresses',
+      subtitle:         'Indulge in sweet treats, in the prettiest way possible. A Pastel dress will take you from girly to glam and across all seasons in a click of a button.',
+      meta_title:       'Beautiful pastel dresses',
+      meta_description: 'Change it up in pastel hues and take on a fresh look inspired straight from the latest runway trends. Find your own dress style & add  these treats to your wardrobe.',
+    },
+    'pinks'         => {
+      title:            'Pink Dresses',
+      subtitle:         'Candy and lolly pops? Only pink frocks! meh! Only pink dresses. Whether you’re tuning into your feminine or fierce side, pink will deliver.',
+      meta_title:       'Pink dresses',
+      meta_description: 'Embrace girl power in every shade of pink. We’ve got you covered girl. Customise these killer dresses into the style you always wished for!',
+    },
+    'reds'          => {
+      title:            'Red Dresses',
+      subtitle:         'Look red haute in statement-making shades.',
+      meta_title:       'Red Dresses',
+      meta_description: 'Look red haute in statement-making shades.',
+    },
+    'white-ivory'   => {
+      title:            'White/Ivory Dresses',
+      subtitle:         'Clean cuts and pristine whites make for a refreshing look. Achieve effortless style in sleek white dresses, with a modern twist.',
+      meta_title:       'White dresses online',
+      meta_description: 'Made to order white dresses. Perfect for every occasion .Whether a bride or a fashion diva, white is a class and a wardrobe must.',
+    },
+  }
+
   def up
     # Deactivate current groups available as taxons
     Spree::OptionValuesGroup.for_colors.available_as_taxon.update_all(available_as_taxon: false)
@@ -38,6 +102,26 @@ class ActivateSomeColorGroupsAsTaxons < ActiveRecord::Migration
 
     # Activate new available as taxons
     current_groups.update_all(available_as_taxon: true)
+
+    # Create Revolution Pages for meta information
+    REVOLUTION_PAGES_INFO.map do |color_name, page_info|
+      page = Revolution::Page.where(path: "/dresses/#{color_name}").first
+      if page.nil?
+        new_page               = Revolution::Page.new
+        new_page.path          = "/dresses/#{color_name}"
+        new_page.template_path = '/products/collections/show'
+        new_page.publish_from  = 3.days.ago
+        new_page.save!
+
+        new_page.translations.create!(
+          locale:           Revolution::Translation::DEFAULT_LOCALE,
+          heading:          page_info[:title],
+          meta_description: page_info[:meta_description],
+          sub_heading:      page_info[:subtitle],
+          title:            page_info[:meta_title]
+        )
+      end
+    end
   end
 
   def down
