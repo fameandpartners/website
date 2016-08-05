ccbe34b5076d23fd9309dbb50301e5a8cbe517cf
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-08-05 16:26:57 +0300

    redundant comments removed

--- app/presenters/marketing/line_item_presenter.rb
+++ app/presenters/marketing/line_item_presenter.rb
@@ -1,7 +1,5 @@
 require 'forwardable'
 
-# TODO This is being used by the /mailers/spree/order_mailer_decorator.rb, but its behaviour is almost the same as the GTM object.
-# TODO Replace it by the GTM object (gtm/line_item.rb)
 module Marketing
   class LineItemPresenter
     extend Forwardable
