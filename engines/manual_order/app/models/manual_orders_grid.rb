2b3968e999f009d44cd3660d88bf299433468f22
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-08-16 17:43:14 +0300

    manual order grid improved

--- engines/manual_order/app/models/manual_orders_grid.rb
+++ engines/manual_order/app/models/manual_orders_grid.rb
@@ -2,12 +2,28 @@ class ManualOrdersGrid
   include Datagrid
 
   scope do
-    Spree::Order.complete.where("number ILIKE 'M%' or number ILIKE 'E%'")
+    # Spree::Order.complete.where("number ILIKE 'M%' or number ILIKE 'E%'").includes(:line_items)
+    # Spree::Order.find_by_sql(
+    #   <<-SQL
+    #     SELECT
+    #       o.id as order_id,
+    #       o.number,
+    #       o.completed_at,
+    #       li.*
+    #     FROM "spree_line_items" li INNER JOIN "spree_orders" o ON o."id" = li."order_id"
+    #     WHERE (o.completed_at is NOT NULL and (o.number ILIKE 'M%' or o.number ILIKE 'E%'))
+    #   SQL
+    # )
+    Spree::LineItem.joins(:order).where("spree_orders.completed_at is NOT NULL and
+      (spree_orders.number ILIKE 'M%' or spree_orders.number ILIKE 'E%')").includes(:order)
   end
 
   filter :number
 
-  column :id
-  column :number
+  # decorate { |line_item| Orders::LineItemPresenter.new(line_item, Orders::OrderPresenter.new(line_item.order)) }
+
+  column(:completed_at, header: 'Order date') {|model| model.order.completed_at.strftime("%m/%d/%y")  }
+  column(:number, header: 'Order number') {|model| model.order.number}
+  column(:id, header: 'Line ID') {|model| model.id}
 
 end
