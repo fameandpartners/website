652dbc37d22d153e0e792d81ebf038541f9a19c1
Mateus Moraes <mateusm@fameandpartners.com>
2016-08-17 01:40:07 -0300

    add proper fade-in animations

--- app/assets/javascripts/partials/helpers/anchor_navigation.js
+++ app/assets/javascripts/partials/helpers/anchor_navigation.js
@@ -128,7 +128,7 @@
         // Mobile: attach the local menu to the bottom
         if( $(window).width() < mdScreenWidth ) {
 
-          $('.local-navigation-wrapper .js-float-menu-on-scroll').addClass('fixed-nav-mobile').fadeIn(100);
+          $('.local-navigation-wrapper .js-float-menu-on-scroll').addClass('fixed-nav-mobile');
           $('.js-footer').css({'padding-bottom': ''+navLocalMenuHeight*1.1+'px'}); //Add an extra bottom padding in footer (so the the mobile local menu doesn't cover any content)
 
         } else {
@@ -147,7 +147,7 @@
 
             // Attach the local navigation to the fixed header
             if (!$('.js-float-menu-on-scroll.fixed-nav').length) {
-              $('.js-float-menu-on-scroll').addClass('fixed-nav').css({'top': ''+sitewideHeaderHeight+'px'}).fadeIn(100);
+              $('.js-float-menu-on-scroll').addClass('fixed-nav').css({'top': ''+sitewideHeaderHeight+'px'});
             }
 
           } else {
@@ -181,7 +181,7 @@
         // If we're on mobile add proper styling to the local navigation
         if ( $(window).width() < mdScreenWidth ) {
           $('.js-float-menu-on-scroll').removeClass('fixed-nav').css({'top': ''});
-          $('.local-navigation-wrapper .js-float-menu-on-scroll').addClass('fixed-nav-mobile').fadeIn(100);
+          $('.local-navigation-wrapper .js-float-menu-on-scroll').addClass('fixed-nav-mobile');
           $('.js-footer').css({'padding-bottom': ''+navLocalMenuHeight*1.1+'px'}); //Add extra bottom padding in footer (so the the mobile local menu doesn't cover any content)
         }
 
