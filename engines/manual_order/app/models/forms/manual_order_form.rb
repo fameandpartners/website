f9c1d07e3f8c5a2674e41f48a77450f01bc2af04
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-08-23 12:54:00 +0300

    height def refactored

--- engines/manual_order/app/models/forms/manual_order_form.rb
+++ engines/manual_order/app/models/forms/manual_order_form.rb
@@ -128,11 +128,7 @@ module Forms
     end
 
     def skirt_length_options
-      {
-        'petite' =>'Petite',
-        'standard' => 'Standard',
-        'tall' => 'Tall'
-      }
+      Hash[LineItemPersonalization::HEIGHTS.map{|h| [h, h.humanize]}]
     end
 
     def save_order(params)
