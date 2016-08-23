c3bf42bdd5b0f8eb2f27e16e74077f79567813b1
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-08-23 18:00:25 +0300

    datagrid decorate method applied

--- engines/manual_order/app/models/manual_orders_grid.rb
+++ engines/manual_order/app/models/manual_orders_grid.rb
@@ -18,33 +18,26 @@ class ManualOrdersGrid
     where(Spree::Order.arel_table[:number].matches("#{value}%"))
   end
 
-  # TODO: replace decorate class def with gem's def after datagrid gem update to 1.4.4 version
-  # decorate { |line_item| Orders::LineItemPresenter.new(line_item, Orders::OrderPresenter.new(line_item.order)) }
+  decorate { |line_item| Orders::LineItemPresenter.new(line_item, Orders::OrderPresenter.new(line_item.order)) }
 
   column(:order_id, header: 'Order ID', order: 'spree_orders.id') {|model| model.order.id}
   column(:order_date, header: 'Order date', order: 'spree_orders.completed_at') do |model|
     model.order.completed_at.strftime("%m/%d/%y")
   end
   column(:delivery_due, header: 'Delivery due') do |model|
-    ManualOrdersGrid.decorate(model).projected_delivery_date.strftime("%m/%d/%y")
+    model.projected_delivery_date.strftime("%m/%d/%y")
   end
   column(:order_number, header: 'Order number', order: 'spree_orders.number', :html => true) do |model|
     link_to model.order.number, spree.admin_order_path(id: model.order.number)
   end
   column(:id, header: 'Line ID', order: 'spree_line_items.id') {|model| model.id}
-  column(:style, header: 'Style') {|model| ManualOrdersGrid.decorate(model).style_number}
-  column(:style_name, header: 'Style name') {|model| ManualOrdersGrid.decorate(model).style_name}
-  column(:sku, header: 'SKU') {|model| ManualOrdersGrid.decorate(model).sku}
-  column(:size, header: 'Size') {|model| ManualOrdersGrid.decorate(model).size}
-  column(:height, header: 'Height') {|model| ManualOrdersGrid.decorate(model).height}
-  column(:color, header: 'Color') {|model| ManualOrdersGrid.decorate(model).colour_name}
-  column(:customisations, header: 'Customisations') {|model| ManualOrdersGrid.decorate(model).customisation_text}
-  column(:factory, header: 'Factory') {|model| ManualOrdersGrid.decorate(model).factory.name}
-
-  private
-
-  def self.decorate(line_item)
-    Orders::LineItemPresenter.new(line_item, Orders::OrderPresenter.new(line_item.order))
-  end
+  column(:style, header: 'Style') {|model| model.style_number}
+  column(:style_name, header: 'Style name') {|model| model.style_name}
+  column(:sku, header: 'SKU') {|model| model.sku}
+  column(:size, header: 'Size') {|model| model.size}
+  column(:height, header: 'Height') {|model| model.height}
+  column(:color, header: 'Color') {|model| model.colour_name}
+  column(:customisations, header: 'Customisations') {|model| model.customisation_text}
+  column(:factory, header: 'Factory') {|model| model.factory.name}
 
 end
