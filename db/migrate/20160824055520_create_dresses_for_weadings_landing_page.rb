3308261b24e661f5ea65124e553c96e3a3f4f990
Tiago Amaro <tiagoa@fameandpartners.com>
2016-08-24 10:29:41 +0200

    LP PIDs must be a string separated by commas

--- db/migrate/20160824055520_create_dresses_for_weadings_landing_page.rb
+++ db/migrate/20160824055520_create_dresses_for_weadings_landing_page.rb
@@ -1,13 +1,14 @@
 class CreateDressesForWeadingsLandingPage < ActiveRecord::Migration
   DRESS_FOR_WEDDING_PATH = '/dress-for-wedding'.freeze
+  DRESS_FOR_WEDDING_PIDS = %w(1054-ivory 283-white 262-white 345-white 1060-ivory 1049-ivory 1008-white 1009-ivory 1064-blush 1096-light-nude 1106-mushroom 635-peach 861-pale-grey 997-pale-blue 840-navy 582-navy
+1037-champagne 1084-indigo-cotton-stripe 1087-navy 1067-pale-blue 987-grey 1018-black 1071-pale-blue 1056-looking-glass 980-burgundy 1092-black 643-black 1028-light-khaki 1095-black 485-black 640-forest-green 1046-black).join(',').freeze
 
   def up
     unless Revolution::Page.where(path: DRESS_FOR_WEDDING_PATH).exists?
       page = Revolution::Page.create!(
         path:          DRESS_FOR_WEDDING_PATH,
         template_path: '/landing_pages/dress_for_wedding',
-        variables:     { lookbook: true, pids: %w(1054-ivory 283-white 262-white 345-white 1060-ivory 1049-ivory 1008-white 1009-ivory 1064-blush 1096-light-nude 1106-mushroom 635-peach 861-pale-grey 997-pale-blue 840-navy 582-navy
-1037-champagne 1084-indigo-cotton-stripe 1087-navy 1067-pale-blue 987-grey 1018-black 1071-pale-blue 1056-looking-glass 980-burgundy 1092-black 643-black 1028-light-khaki 1095-black 485-black 640-forest-green 1046-black) },
+        variables:     { lookbook: true, pids: DRESS_FOR_WEDDING_PIDS },
         publish_from:  Date.current - 1.day
       )
       page.translations.create!(locale: 'en-US', title: "Dress for weddings", heading: "Dress for weddings", meta_description: "Discover beautiful wedding dresses here at Fame & Partners")
--- db/migrate/20160824055611_create_dresses_for_parties_landing_page.rb
+++ db/migrate/20160824055611_create_dresses_for_parties_landing_page.rb
@@ -1,13 +1,14 @@
 class CreateDressesForPartiesLandingPage < ActiveRecord::Migration
   DRESS_FOR_PARTIES_PATH = '/dress-for-parties'.freeze
+  DRESS_FOR_PARTIES_PIDS = %w(1054-ivory 283-white 262-white 345-white 1060-ivory 1049-ivory 1008-white 1009-ivory 1064-blush 1096-light-nude 1106-mushroom 635-peach 861-pale-grey 997-pale-blue 840-navy 582-navy
+1037-champagne 1084-indigo-cotton-stripe 1087-navy 1067-pale-blue 987-grey 1018-black 1071-pale-blue 1056-looking-glass 980-burgundy 1092-black 643-black 1028-light-khaki 1095-black 485-black 640-forest-green 1046-black).join(',').freeze
 
   def up
     unless Revolution::Page.where(path: DRESS_FOR_PARTIES_PATH).exists?
       page = Revolution::Page.create!(
         path:          DRESS_FOR_PARTIES_PATH,
         template_path: '/landing_pages/dress_for_parties',
-        variables:     { lookbook: true, pids: %w(1054-ivory 283-white 262-white 345-white 1060-ivory 1049-ivory 1008-white 1009-ivory 1064-blush 1096-light-nude 1106-mushroom 635-peach 861-pale-grey 997-pale-blue 840-navy 582-navy
-1037-champagne 1084-indigo-cotton-stripe 1087-navy 1067-pale-blue 987-grey 1018-black 1071-pale-blue 1056-looking-glass 980-burgundy 1092-black 643-black 1028-light-khaki 1095-black 485-black 640-forest-green 1046-black) },
+        variables:     { lookbook: true, pids: DRESS_FOR_PARTIES_PIDS },
         publish_from:  Date.current - 1.day
       )
       page.translations.create!(locale: 'en-US', title: "Dress for parties", heading: "Dress for parties", meta_description: "Discover beautiful party dresses here at Fame & Partners")
