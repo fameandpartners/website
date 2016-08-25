9213d09268309e15ecb72c8176d690e586515938
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-08-25 10:51:47 +0300

    redundant code removed

--- engines/manual_order/app/models/manual_orders_grid.rb
+++ engines/manual_order/app/models/manual_orders_grid.rb
@@ -6,7 +6,7 @@ class ManualOrdersGrid
   scope do
     Spree::LineItem.joins(:order)
       .where("spree_orders.completed_at is NOT NULL and (spree_orders.number ILIKE 'M%' or spree_orders.number ILIKE 'E%')")
-      .order('spree_orders.completed_at DESC').includes(:order)
+      .order('spree_orders.completed_at DESC')
   end
 
   decorate { |line_item| Orders::LineItemPresenter.new(line_item, Orders::OrderPresenter.new(line_item.order)) }
