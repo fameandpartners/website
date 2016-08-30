b4213b0521c3bada50882e3daedb4bf2534392c3
Mateus Moraes <mateusm@fameandpartners.com>
2016-08-30 17:57:00 -0300

    add “limit” parameter in db migration files
    Without this, only the first 20 ~ 25 pids are shown (leading to blank areas in the page).

--- db/migrate/20160824055520_create_dresses_for_weadings_landing_page.rb
+++ db/migrate/20160824055520_create_dresses_for_weadings_landing_page.rb
@@ -7,6 +7,7 @@ class CreateDressesForWeadingsLandingPage < ActiveRecord::Migration
       pids:             %w(1127-ivory 1054-ivory 1136-ivory 283-white 11060-ivory 1049-ivory 1008-white 1072-white 1064-blush 1096-light-nude 1106-mushroom 635-peach 861-pale-grey 997-pale-blue 840-navy 582-navy 1037-champagne 1084-indigo-cotton-stripe 1087-navy 1067-pale-blue 987-grey 1018-black 1071-pale-blue 1056-looking-glass).join(','),
       heading:          'Dress for weddings',
       title:            'Dress for weddings',
+      limit:            '99',
       meta_description: 'Discover beautiful wedding dresses here at Fame & Partners',
     }
   end
@@ -16,7 +17,7 @@ class CreateDressesForWeadingsLandingPage < ActiveRecord::Migration
       page = Revolution::Page.create!(
         path:          landing_page_properties[:path],
         template_path: landing_page_properties[:template_path],
-        variables:     { lookbook: true, pids: landing_page_properties[:pids] },
+        variables:     { lookbook: true, limit: landing_page_properties[:limit], pids: landing_page_properties[:pids] },
         publish_from:  1.day.ago
       )
       page.translations.create!(locale: 'en-US', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
--- db/migrate/20160824055611_create_dresses_for_parties_landing_page.rb
+++ db/migrate/20160824055611_create_dresses_for_parties_landing_page.rb
@@ -7,6 +7,7 @@ class CreateDressesForPartiesLandingPage < ActiveRecord::Migration
       pids:             %w(1119-navy 1128-black 1136-navy 1127-black 1067-pale-blue 1077-navy 1061-looking-through-glass 1069-white 1138-navy 1112-rosewater-floral 1091-pale-pink 1030-dark-burgundy 1065-black 1123-champagne 1094-ornate-midnight-floral 1080-navy 1031-black 1042-pale-pink 1139-silver 1052-black 1124-black 1047-ivory 1043-navy 1048-ivory 980-burgundy 1092-black 643-black 1028-light-khaki 1053-black 485-black 640-forest-green 1046-black).join(','),
       heading:          'Dress for parties',
       title:            'Dress for parties',
+      limit:            '99',
       meta_description: 'Discover beautiful party dresses here at Fame & Partners',
     }
   end
@@ -16,7 +17,7 @@ class CreateDressesForPartiesLandingPage < ActiveRecord::Migration
       page = Revolution::Page.create!(
         path:          landing_page_properties[:path],
         template_path: landing_page_properties[:template_path],
-        variables:     { lookbook: true, pids: landing_page_properties[:pids] },
+        variables:     { lookbook: true, limit: landing_page_properties[:limit], pids: landing_page_properties[:pids] },
         publish_from:  1.day.ago
       )
       page.translations.create!(locale: 'en-US', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
