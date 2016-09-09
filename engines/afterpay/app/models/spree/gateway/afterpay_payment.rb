afb3ce7a4b160f0b3fa2a1b1035f3275a1f525be
Tiago Amaro <tiagoa@fameandpartners.com>
2016-09-09 16:30:57 +0200

    use Spree::Bogus method and allow ActivMerchant to respond OK

--- engines/afterpay/app/models/spree/gateway/afterpay_payment.rb
+++ engines/afterpay/app/models/spree/gateway/afterpay_payment.rb
@@ -22,10 +22,11 @@ module Spree
       def provider_class
         # TODO: Actual API wrapper here
         # More information: https://github.com/spree/spree/blob/1-3-stable/core/app/models/spree/gateway.rb#L27
+        Spree::Gateway::Bogus
       end
 
-      def payment_source_class
-        Spree::CreditCard
+      def currency
+        'AUD'
       end
 
       # Payment Actions
@@ -33,6 +34,8 @@ module Spree
       def purchase(amount, transaction_details, options = {})
         # ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
         # TODO: here is where the magic happens
+
+        ActiveMerchant::Billing::Response.new(true, 'Bogus Gateway: Forced success', {}, :test => true, :authorization => '12345', :avs_result => { :code => 'A' })
       end
 
       def refund(payment, amount)
