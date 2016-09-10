7894ef0aa0804fc8a5ff3bf2bdafd6a55c6480a0
Mateus Moraes <mateusm@fameandpartners.com>
2016-09-09 22:12:41 -0300

    [wip] create route and migration file
    Note: PIDs are pending.

---
+++ db/migrate/20160910010242_create_spring_racing_collection_landing_page.rb
@@ -0,0 +1,31 @@
+class CreateSpringRacingCollectionLandingPage < ActiveRecord::Migration
+  # All values MUST be Strings!
+  private def landing_page_properties
+    {
+      path:             '/spring-racing-collection',
+      template_path:    '/landing_pages/spring_racing_collection',
+      # pids:             %w([TODO] Awaiting real pids).join(','),
+      limit:            '99',
+      heading:          'Spring Racing Collection',
+      title:            'Dresses for Spring Racing Events',
+      meta_description: 'Happy Spring Racing season! Get dressed up in customizable, made-to-order day dresses, cocktail dresses, jumpsuits, and matching sets for Derby Day, Girls\' Day Out, The Melbourne Cup, and beyond.',
+    }
+  end
+
+  def up
+    unless Revolution::Page.where(path: landing_page_properties[:path]).exists?
+      page = Revolution::Page.create!(
+        path:          landing_page_properties[:path],
+        template_path: landing_page_properties[:template_path],
+        variables:     { lookbook: true, limit: landing_page_properties[:limit], pids: landing_page_properties[:pids] },
+        publish_from:  1.day.ago
+      )
+      page.translations.create!(locale: 'en-US', title: "#{landing_page_properties[:title]} | fameandpartners.com", heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+      page.translations.create!(locale: 'en-AU', title: "#{landing_page_properties[:title]} | fameandpartners.com.au", heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+    end
+  end
+
+  def down
+    Revolution::Page.where(path: landing_page_properties[:path]).delete_all
+  end
+end
