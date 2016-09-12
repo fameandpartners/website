313c3f81773ecc5c161f2a9c058ad49dcc82d916
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-09-12 16:35:22 +0300

    price and image now could be selected without size

--- engines/manual_order/config/routes.rb
+++ engines/manual_order/config/routes.rb
@@ -5,8 +5,8 @@ ManualOrder::Engine.routes.draw do
       get 'colors/:product_id' => 'manual_orders#colors_options'
       get 'sizes/:product_id' => 'manual_orders#sizes_options'
       get 'customisations/:product_id' => 'manual_orders#customisations_options'
-      get 'images/:product_id/:size_id/:color_id' => 'manual_orders#image'
-      get 'prices/:product_id/:size_id/:color_id/:currency' => 'manual_orders#price'
+      get 'images/:product_id/:color_id' => 'manual_orders#image'
+      get 'prices/:product_id/:color_id/:currency' => 'manual_orders#price'
       get 'autocomplete_customers' => 'manual_orders#autocomplete_customers'
       get 'user/:user_id' => 'manual_orders#user_data'
     end
