52dd37f78ddc4d1d8893c08d1ba5ef6d4ba32c0b
Mateus Moraes <mateusm@fameandpartners.com>
2016-09-12 21:39:31 -0300

    [temp] add “spring racing collection” pids (migration file)

--- db/migrate/20160910010242_create_spring_racing_collection_landing_page.rb
+++ db/migrate/20160910010242_create_spring_racing_collection_landing_page.rb
@@ -4,7 +4,7 @@ class CreateSpringRacingCollectionLandingPage < ActiveRecord::Migration
     {
       path:             '/spring-racing-collection',
       template_path:    '/landing_pages/spring_racing_collection',
-      # pids:             %w([TODO] Awaiting real pids).join(','),
+      pids:             %w(1043-navy 997-pale-blue 1037-taupe 1139-silver 1104-pale-pink 671-white 1056-looking-glass 1111-black 1054-black 1021-white 1053-black 1069-white 1110-rosewater-floral 1106-mushroom 1133-navy 1120-dusk 1091-pale-pink 1046-black 1078-indigo-cotton-stripe 1123-navy 1103-ornate-midnight-floral 1061-looking-glass 963-palermo-floral 1114-rosewater-floral 987-grey 640-black 1087-navy 1018-black).join(','),
       limit:            '99',
       heading:          'Spring Racing Collection',
       title:            'Dresses for Spring Racing Events',
