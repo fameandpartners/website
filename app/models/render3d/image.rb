1baa028abd2b6d2940b2b760d5334d882bc15eb3
Tiago Amaro <tiagoa@fameandpartners.com>
2016-09-16 10:33:55 +0200

    3d render image associations are required

--- app/models/render3d/image.rb
+++ app/models/render3d/image.rb
@@ -3,5 +3,10 @@ module Render3d
     belongs_to :spree_product, class_name: 'Spree::Product'
     belongs_to :customisation_value
     belongs_to :product_color_value
+
+    validates :customisation_value,
+              :product_color_value,
+              :spree_product,
+              presence: true
   end
 end
