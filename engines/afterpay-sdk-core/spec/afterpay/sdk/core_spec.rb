d9de907d5ef612f09fd6608eb6c3f861a9893eb8
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-09-16 14:12:31 +0300

    namespace fix

--- engines/afterpay-sdk-core/spec/afterpay/sdk/core_spec.rb
+++ engines/afterpay-sdk-core/spec/afterpay/sdk/core_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Afterpay::Sdk::Core do
+describe Afterpay::SDK::Core do
   it 'has a version number' do
-    expect(Afterpay::Sdk::Core::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Afterpay::SDK::Core::VERSION).not_to be nil
   end
 end
--- engines/afterpay-sdk-merchant/spec/afterpay/sdk/merchant_spec.rb
+++ engines/afterpay-sdk-merchant/spec/afterpay/sdk/merchant_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Afterpay::Sdk::Merchant do
+describe Afterpay::SDK::Merchant do
   it 'has a version number' do
-    expect(Afterpay::Sdk::Merchant::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Afterpay::SDK::Merchant::VERSION).not_to be nil
   end
 end
