9860778b0570e88c6cb60f0fef391038451adcbe
Tiago Amaro <tiagoa@fameandpartners.com>
2016-09-22 12:02:10 +0200

    small typo on merchant services

--- engines/afterpay-sdk-merchant/lib/afterpay/sdk/merchant/services.rb
+++ engines/afterpay-sdk-merchant/lib/afterpay/sdk/merchant/services.rb
@@ -51,11 +51,10 @@ module Afterpay::SDK::Merchant
       get('payments', options, http_header)
     end
 
-    def creat_refund(options = {}, http_header = {})
+    def create_refund(options = {}, http_header = {})
       amount = options.fetch(:amount) { raise ArgumentError, 'Refund amount (:amount) is required' }
       payment_id = options.fetch(:payment_id) { raise ArgumentError, 'Payment ID (:payment_id) is required' }
       post("payments/#{payment_id}/refund", options, http_header)
     end
-
   end
 end
