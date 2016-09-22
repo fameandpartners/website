babf066bc52643d83aae13934501bd7f4abf80e1
Tiago Amaro <tiagoa@fameandpartners.com>
2016-09-22 16:38:43 +0200

    real controller action to process Afterpay

--- engines/afterpay/app/controllers/afterpay/payments_controller.rb
+++ engines/afterpay/app/controllers/afterpay/payments_controller.rb
@@ -1,49 +1,61 @@
 module Afterpay
   class PaymentsController < ActionController::Base
+    AFTERPAY_APPROVED_PAYMENT = 'APPROVED'.freeze
+
     # This method is called with pre approval (more info at http://docs.afterpay.com.au/merchant-api-v1.html#direct-payment-flow)
     # => We now have to call `/v1/payments/capture`
     # => Sample parameters {"status"=>"SUCCESS", "orderToken"=>"664ku47hgk8nv5cplhd1k52noce2pjs9asjkq5cnr4hnfd18qeko", "order_number"=>"R884754500"}
+    # Note: Afterpay payment ID is used to refund, so it's necessary saving it on the payment transaction as a response_code
+    # TODO: handle failure case and notifications
     def new
-      binding.pry
+      payment_status, payment_id = begin
+        payment_details = provider.direct_capture_payment(token: params[:orderToken])
+        [payment_details['status'], payment_details['id']]
+      rescue Afterpay::SDK::Core::Exceptions::ClientError => e
+        Raven.capture_exception(e)
+        nil
+      end
 
+      if payment_status == AFTERPAY_APPROVED_PAYMENT
+        afterpay_source = Spree::CreditCard.create(
+          number:                     '1234 1234 1234 1234',
+          verification_value:         '123',
+          cc_type:                    'master',
+          gateway_payment_profile_id: payment_id
+        )
 
-      # order.payments.create!({
-      #                          :source => Spree::PaypalExpressCheckout.create({
-      #                                                                           :token => params[:token],
-      #                                                                           :payer_id => params[:PayerID]
-      #                                                                         }, :without_protection => true),
-      #                          :amount => order.total,
-      #                          :payment_method => payment_method
-      #                        }, :without_protection => true)
-      # order.next
+        current_order.payments.create!({
+                                         source:         afterpay_source,
+                                         amount:         current_order.total,
+                                         payment_method: payment_method
+                                       },
+                                       without_protection: true)
 
-      # if order.complete?
-      #   flash.notice = t(:order_processed_successfully)
-      #   flash[:commerce_tracking] = "nothing special"
-      #   session[:order_id] = nil
-      #   redirect_to order_path(order, :token => order.token)
-      # else
-      #   redirect_to checkout_state_path(order.state)
-      # end
+        current_order.next
+      end
 
-      # If fails, redirect to checkout path
-      payment_failed = true
-      if payment_failed
-        redirect_to spree.checkout_state_path(state: :payment), notice: 'Payment Failed'
+      if current_order.complete?
+        flash.notice              = t(:order_processed_successfully)
+        flash[:commerce_tracking] = 'nothing special'
+        session[:order_id]        = nil
+        redirect_to spree.order_path(current_order, token: current_order.token)
       else
-        redirect_to spree.checkout_state_path(state: :success)
+        redirect_to spree.checkout_state_path(current_order.state)
       end
     end
 
     private
 
+    def payment_method
+      @payment_method ||= Spree::PaymentMethod.where(type: 'Spree::Gateway::AfterpayPayment').available.first
+    end
+
     def provider
-      payment_method = Spree::PaymentMethod.where(type: 'Spree::Gateway::AfterpayPayment').available.first
-      payment_method.provider
+      @provider ||= payment_method.provider
     end
 
     def current_order
-      Spree::Order.where(number: params[:order_number]).first
+      @current_order ||= Spree::Order.where(number: params[:order_number]).first
     end
   end
 end
