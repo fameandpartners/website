2ba731602053a9f4dfba06c493ee96ca5d420805
Tiago Amaro <tiagoa@fameandpartners.com>
2016-09-27 15:24:29 +0200

    use Afterpay sandbox URL outside production

--- engines/afterpay/config/routes.rb
+++ engines/afterpay/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :afterpay do
     get '/confirm/:order_number', to: 'payments#new', as: :confirm
+
+    namespace :merchant_panel do
+      get '/refund/:payment_id', to: redirect("#{Afterpay::AFTERPAY_MERCHANT_URL}/merchant/order/%{payment_id}"), as: :refund
+    end
   end
 end
