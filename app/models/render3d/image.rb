44f324f8657493523cee0338bd59d52b8c07f0f9
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-10-05 21:30:48 +0300

    Image attachment

--- app/models/render3d/image.rb
+++ app/models/render3d/image.rb
@@ -8,5 +8,20 @@ module Render3d
               :product_color_value,
               :spree_product,
               presence: true
+
+    validates_attachment_presence :attachment
+    validate :no_attachment_errors
+
+    attr_accessible :alt, :attachment, :position, :viewable_type, :viewable_id
+
+    has_attached_file :attachment,
+                      :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' },
+                      :default_style => :product,
+                      :url => '/spree/products/:id/render3d/:style/:basename.:extension',
+                      :path => ':rails_root/public/spree/products/render3d/:id/:style/:basename.:extension',
+                      :convert_options => { :all => '-strip -auto-orient' }
+
+    include Spree::Core::S3Support
+    supports_s3 :attachment
   end
 end
