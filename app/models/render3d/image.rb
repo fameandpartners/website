555eaf78094922ab8da37132e64da53d2ee9c138
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-10-13 00:29:25 +0300

    Set attachment attributes per Image model class instead of Paperclip::Attachment class. Ref 908c0c36a1b99ac5bbdcc5d2b7d64ca42fe81663

--- app/models/render3d/image.rb
+++ app/models/render3d/image.rb
@@ -11,14 +11,13 @@ module Render3d
 
     validates_attachment_presence :attachment
     attr_accessible :attachment
-    has_attached_file :attachment
+
+    has_attached_file :attachment,
+      path: 'spree/products/render3d/:id/:style/:basename.:extension',
+      styles: { product: '240x240>', large: '600x600>' },
+      default_style: :product,
+      convert_options: { :all => '-strip -auto-orient' }
 
     self.attachment_definitions[:attachment] = Paperclip::Attachment.default_options
-
-    # TODO: Do NOT use this. This overrides `Paperclip::Attachment.default_options` (!!!). Check other places that have this as well!
-    # self.attachment_definitions[:attachment][:path]   = 'spree/products/render3d/:id/:style/:basename.:extension'
-    # self.attachment_definitions[:attachment][:styles] = { product: '240x240>', large: '600x600>' }
-    # self.attachment_definitions[:attachment][:default_style] = :product
-    # self.attachment_definitions[:attachment][:convert_options] = { :all => '-strip -auto-orient' }
   end
 end
