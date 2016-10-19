b410bf082d11a7f830e0b4112ac7913e55c9565d
Mateus Moraes <mateusm@fameandpartners.com>
2016-10-19 11:38:33 -0200

    [temporary] use empty value for pids in migration file
    This is temporary, just to avoid errors since we don’t have the real PIDs yet.

--- db/migrate/20161018071817_create_holiday_collection_landing_page.rb
+++ db/migrate/20161018071817_create_holiday_collection_landing_page.rb
@@ -4,7 +4,7 @@ class CreateHolidayCollectionLandingPage < ActiveRecord::Migration
     {
       path:             '/holiday-collection',
       template_path:    '/landing_pages/holiday_collection',
-      pids:             %w(1162-black 1181-dark-tan 1158-black 1164-olive 1161-black 1156-black 1176-hot-pink 1182-black 1143-white 1146-pale-grey 1178-dark-nude 1174-black 1144-navy 1148-black 1167-black 1173-ivory 1175-chambray 1155-white 1169-black 1165-black 740-ivory 1154-olive 1173-ivory 1159-black 1142-art-soul 1166-navy 1171-spot 1179-black 1152-navy 1168-white 1150-pale-blue-cotton-stripe 1149-white 1149-white 1145-ochre 1163-chambray 1151-navy 1177-pale-pink 1153-watercolour-camo 1147-red 1172-olive).join(','),
+      pids:             %w('').join(','),
       limit:            '99',
       heading:          'Holiday Collection',
       title:            'Holiday Collection',
