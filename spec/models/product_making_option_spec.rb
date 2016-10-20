5afd1de7fc8347993e468fa9d60531cc15c2b313
Tiago Amaro <tiagoa@fameandpartners.com>
2016-10-20 17:54:44 +0200

    validate making option price value

--- spec/models/product_making_option_spec.rb
+++ spec/models/product_making_option_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe ProductMakingOption, type: :model do
+  it { is_expected.to validate_presence_of(:option_type) }
+  it { is_expected.to validate_inclusion_of(:option_type).in_array(described_class::OPTION_TYPES) }
+  it { is_expected.to validate_uniqueness_of(:option_type).scoped_to(:product_id) }
+
+  it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+
   context '#assign_default_attributes' do
     let(:subject) { described_class.new }
 
