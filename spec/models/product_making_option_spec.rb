760f54620b16a32f28a692163af3814faa637197
Tiago Amaro <tiagoa@fameandpartners.com>
2016-10-20 19:15:07 +0200

    set default currency a constant of the making options model

--- spec/models/product_making_option_spec.rb
+++ spec/models/product_making_option_spec.rb
@@ -12,15 +12,12 @@ describe ProductMakingOption, type: :model do
   context '#assign_default_attributes' do
     let(:subject) { described_class.new }
 
-    it 'set price' do
+    it do
       subject.assign_default_attributes
-      expect(subject.price).to eq(described_class::DEFAULT_OPTION_PRICE)
-    end
 
-    it 'sets currency' do
-      expect(SiteVersion).to receive(:default).and_return(instance_double('sv', currency: 'USD'))
-      subject.assign_default_attributes
-      expect(subject.currency).to eq('USD')
+      expect(subject.active).to eq(true)
+      expect(subject.price).to eq(described_class::DEFAULT_OPTION_PRICE)
+      expect(subject.currency).to eq(described_class::DEFAULT_CURRENCY)
     end
   end
 end
