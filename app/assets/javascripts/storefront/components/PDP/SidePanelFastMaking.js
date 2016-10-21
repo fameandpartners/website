c023940b30778b3ec8b5371cdf1ad96bc5278988
Mateus Moraes <mateusm@fameandpartners.com>
2016-10-21 19:55:03 -0200

    [wip] add markup for “express making“ in pdp

--- app/assets/javascripts/storefront/components/PDP/SidePanelFastMaking.js
+++ app/assets/javascripts/storefront/components/PDP/SidePanelFastMaking.js
@@ -1,9 +1,13 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {Scrollbars} from 'react-custom-scrollbars';
 import * as pdpActions from '../../actions/PdpActions';
+import SidePanel from './SidePanel';
 
-class SidePanelFastMaking extends React.Component {
+// TODO: [WIP] This is just a placeholder. Valid logic for displaying the "express making" row properly is needed here.
+
+class SidePanelFastMaking extends SidePanel {
   constructor(props, context) {
     super(props, context);
 
@@ -11,50 +15,32 @@ class SidePanelFastMaking extends React.Component {
   }
 
   onChange(event) {
-    let customize = {};
-    let makingOption = { price: 0 };
-    let makingOptions = this.props.product.available_options.table.making_options;
 
-    if (event.target.checked && makingOptions.length) {
-      makingOption = makingOptions[0].product_making_option;
-    }
-
-    customize.makingOption = makingOption;
-    this.props.actions.customizeDress(customize);
   }
 
   render() {
-    if (this.props.flags.fastMaking && this.props.product.fast_making) {
-      return (
-        <div className="pdp-side-container pdp-side-container-fast-making checkbox-inline custom-form-element-thin form-small">
-          <a href="javascript:;">
-            <input type="checkbox" id="fast-making" onChange={this.onChange} />
-            <label htmlFor="fast-making">
-            <div className="c-card-customize__content__left">
-              EXPRESS MAKING (6-9 days)
-              <div className="pdp-side-note">Only available for Recommended Colors</div>
-            </div>
-            <div className="c-card-customize__content__right">$30</div>
-            </label>
-          </a>
-        </div>
-      );
-    } else {
-      return (
-        <span></span>
-      );
-    }
+
+    return (
+      <div className="pdp-side-container pdp-side-container-fast-making checkbox-inline custom-form-element-thin form-small">
+        <a href="javascript:;">
+          <input type="checkbox" id="fast-making" />
+          <label htmlFor="fast-making">
+          <div className="c-card-customize__content__left">EXPRESS MAKING (6-9 days)</div>
+          <div className="c-card-customize__content__right">$30</div>
+          </label>
+        </a>
+      </div>
+    );
   }
 }
 
 SidePanelFastMaking.propTypes = {
-  product: PropTypes.object.isRequired
+  customize: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
   return {
-    product: state.product,
-    flags: state.flags
+
   };
 }
 
