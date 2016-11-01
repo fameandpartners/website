7a87ec5fbb4a7cc8a7a8d860186c8f76544b7f78
Mateus Moraes <mateusm@fameandpartners.com>
2016-11-01 19:33:20 -0200

    add pids to migration file

--- db/migrate/20161101111038_create_evening_hours_collection_landing_page.rb
+++ db/migrate/20161101111038_create_evening_hours_collection_landing_page.rb
@@ -4,7 +4,8 @@ class CreateEveningHoursCollectionLandingPage < ActiveRecord::Migration
     {
       path:             '/the-evening-hours-collection',
       template_path:    '/landing_pages/evening_hours_collection',
-      pids:             %w().join(','),
+      pids:             %w('1202-cherry-red 1210-black 1216-black 1204-evening-bloom 1217-silver 1194-micro-star
+1191-white 1197-indigo-cotton-stripe 1225-dark-forest 1201-black 1221-black 1222-silver 1190-ivory 1223-dark-forest 1098-black 1196-navy 1207-black 1214-navy 1225-black 1198-cherry-red 1218-black 1226-micro-spot 1228-dusk 1209-dark-forest 1189-cobalt-blue 1190-black 1187-evening-bloom 1203-dark-tan 1200-dark-teal 1206-black 1188-dark-tan 1224-black 1220-navy 1186-cobalt-blue 1212-black 1192-micro-star 1215-dark-tan 1199-burgundy 1227-dark-burgundy 1193-micro-spot 1228-black 1195-micro-star 1205-black 1219-dark-tan 1189-cherry-red 1213-black').join(','),
       limit:            '99',
       heading:          'The Evening Hours Collection',
       title:            'Easy, Effortless Cocktail Dresses',
