e171781601a7c9b236f23a0493f7143bd0182bcc
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-11-09 15:00:40 +0200

    Backup props instead of duplication of product

--- db/migrate/20161101153915_copy_products_properties_with_new_values.rb
+++ db/migrate/20161101153915_copy_products_properties_with_new_values.rb
@@ -8,11 +8,7 @@ class CopyProductsPropertiesWithNewValues < ActiveRecord::Migration
     patterns_collections.each do |hash|
       Spree::Property.find_by_name(hash[:name]).product_properties.where('value LIKE ?', "%#{hash[:pattern]}%").find_each do |property|
         product = property.product
-        property.product.duplicate.tap do |duplicate|
-          duplicate.hidden = true
-          duplicate.save!
-        end
-
+        product.set_property("backup-#{hash[:name]}", property.value)
         property.update_attribute(value: property.value.gsub(/(\n?)(#{hash[:pattern]}.+)/, ''))
         product.touch
       end
@@ -21,19 +17,15 @@ class CopyProductsPropertiesWithNewValues < ActiveRecord::Migration
 
   def down
     properties = ['fabric', 'fit']
-    pattern = /COPY OF/
 
-    Spree::Product.where('name LIKE ?', "#{pattern}%").where(hidden: true).find_each do |duplicate|
-      if product_name = duplicate.name[/#{'COPY OF '}(.+)/,1]
-        product = Spree::Product.where(name: product_name)
-
-        properties.each do |name|
-          product.set_property(name, duplicate.property(name))
-          product.touch
-        end
-
-        duplicate.destroy
+    properties.each do |name|
+      Spree::Property.find_by_name("backup-#{name}").product_properties.find_each do |property|
+        product = property.product
+        product.set_property(name, property.value)
+        product.touch
+        property.destroy
       end
     end
   end
+  
 end
