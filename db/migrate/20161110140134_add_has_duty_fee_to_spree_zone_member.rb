027b7b1eafbb76480324f1d2d392d780ef4cce22
Nickolay Kondratenko <devmarkup@gmail.com>
2016-11-11 11:03:16 +0200

    set default has_duty_fee as false

--- db/migrate/20161110140134_add_has_duty_fee_to_spree_zone_member.rb
+++ db/migrate/20161110140134_add_has_duty_fee_to_spree_zone_member.rb
@@ -1,5 +1,5 @@
 class AddHasDutyFeeToSpreeZoneMember < ActiveRecord::Migration
   def change
-    add_column :spree_zone_members, :has_duty_fee, :boolean
+    add_column :spree_zone_members, :has_duty_fee, :boolean, default: false
   end
 end
