7d76f4275e5a5084eba3cc3b8d3feec998d2a490
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-11-11 15:22:03 +0200

    [Fix] Extend regex pattern to match color w/ name at image's names (#2132)
    * Extend regex pattern to match color w/ name at image's names
    
    * Avoid filename w/ cyrillic letters to be processed
    
    * Update render3d images if they are present

--- lib/batch_upload/render3d_images_uploader.rb
+++ lib/batch_upload/render3d_images_uploader.rb
@@ -15,24 +15,18 @@ module BatchUpload
               errors = []
               file_name = File.basename(file_path)
 
-              # 4B141-PLUM-C1.jpg
-              # 4B141_PLUM-C3.jpg
-              # 4B141-RED-D.jpg
-              # 4B141-SAGE-GREEN-C2.jpg
+              # NOTE: Alexey Bobyrev 08/11/16
+              # For name examples - take a look on
+              # "spec/lib/batch_upload/render3d_images_uploader_spec.rb"
 
-              match_data = \
-                file_name.match(/(?<sku>\w+)[-,_](?<color>\S+)[-,_](?<customisation>\w+).\w+$/)
+              match_data, parse_error = self.class::FileParser.new(file_name: file_name).values
 
-              if match_data.nil?
-                errors.push({
-                  kind: :error,
-                  message: "File name is invalid: #{file_name}"
-                })
-                match_data = {}
+              if parse_error.present?
+                errors.push(parse_error)
               end
 
-              color_name = match_data[:color].to_s.downcase
-              customisation_name = match_data[:customisation].to_s.downcase
+              color_name = match_data[:color]
+              customisation_name = match_data[:customisation]
 
               if color_name.present?
                 debug "Search color by name"
@@ -42,7 +36,7 @@ module BatchUpload
                 if color_value.blank?
                   errors.push({
                     kind: :error,
-                    message: "Color not found (#{color_name}) #{file_name}"
+                    message: "Color not found (#{color_name}) '#{file_name}'"
                   })
                 end
               end
@@ -65,7 +59,7 @@ module BatchUpload
                 if customisation_value_id.nil?
                   errors.push({
                     kind: :warn,
-                    message: "Customisation not found (#{customisation_name}) #{file_name}"
+                    message: "Customisation not found (#{customisation_name}) '#{file_name}'"
                   })
                 end
               end
@@ -82,8 +76,10 @@ module BatchUpload
                 next
               end
 
-              image = Render3d::Image.new.tap do |img|
+              options = { product_id: product.id, color_value_id: color_value.id, customisation_value_id: customisation_value_id }
+              image = find_or_initialize_render3d_image(options) do |img|
                 img.product = product
+
                 img.color_value = color_value
                 img.customisation_value_id = customisation_value_id
 
@@ -107,6 +103,66 @@ module BatchUpload
           end
         end
 
+      end
+    end
+
+    class FileParser
+      attr_reader :file_name, :data, :error
+
+      def initialize(file_name: file_name)
+        @file_name = file_name
+        parse
+      end
+
+      def values
+        [data, error]
+      end
+
+      private def parse
+        cyrillic_index = check_for_cyrillic_symbols
+
+        if cyrillic_index.present?
+          @error = {
+            kind: :error,
+            message: "File cannot be parsed due to cyrrilic symbols on it - '#{file_name}':#{cyrillic_index}"
+          }
+
+          @data = {}
+        else
+          pattern = /(?<sku>\w+)[-,_](?<color>[\S\s]+)[-,_](?<customisation>\S+)\.\w+$/
+          data = file_name.match(pattern)
+
+          if data.nil?
+            @error = {
+              kind: :error,
+              message: "File name is invalid and can't be parsed: '#{file_name}'"
+            }
+
+            @data = {}
+          else
+            @data = {
+              sku: data[:sku].to_s.parameterize,
+              color: data[:color].to_s.parameterize,
+              customisation: data[:customisation].to_s.parameterize
+            }
+          end
+        end
+      end
+
+      private def check_for_cyrillic_symbols
+        file_name.index(/\p{Cyrillic}+/)
+      end
+
+    end
+
+    def find_or_initialize_render3d_image(options, &block)
+      image_class = Render3d::Image
+      image = image_class.where(options).first || image_class.new
+
+      if block_given?
+        image.tap(&block)
+      else
+        image
       end
     end
 
