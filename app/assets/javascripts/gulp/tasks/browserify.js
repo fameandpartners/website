e4d38b056bdcd89c4afbc497ec28d523e2d625a0
elgrecode <elgrecode@gmail.com>
2016-11-10 17:11:11 -0800

    breaks apart browserify into managable functions

--- app/assets/javascripts/gulp/tasks/browserify.js
+++ app/assets/javascripts/gulp/tasks/browserify.js
@@ -6,112 +6,116 @@ const clean = require('gulp-clean');
 const config = require('../config').browserify;
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
+const gulpIf = require('gulp-if');
 const gutil = require('gulp-util');
-const vinyl = require('vinyl-source-stream');
+const source = require('vinyl-source-stream');
 const watchify = require('watchify');
 
 // Initializations
 const _true = chalk.green('true');
 const _false = chalk.red('false');
-const prodBuild = argv.prod;
+const isProd = argv.prod; // Will be useful for separation from pipeline
 const isDevelopment = process.env.NODE_ENV !== 'production';
+const isWatch = argv.watch;
 
 // Status Check
-gutil.log('Is build prod?', prodBuild ? _true : _false);
+gutil.log('Is build using watchify?', isWatch ? _true : _false );
+gutil.log('Is build prod?', isProd ? _true : _false);
 gutil.log('Is this dev?', isDevelopment ? _true : _false);
 
-gulp.task('apply-node-env', () => {
-  process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-});
+
+/**
+ * Checks if a file's eslint flag is fixed
+ * @param  {Object} file
+ * @return {Boolean}
+ */
+function isFixed(file) {
+    return file.eslint !== null && file.eslint.fixed;
+}
+
+/**
+ * Exits from bundle build
+ * @param  {Object} err
+ */
+function crashProcess (err) {
+    gutil.log(err);
+    gutil.log(chalk.red(err.message));
+    gutil.log(err.stack);
+    process.exit(1);
+}
+
+function bundle(bundler) {
+    const startTime = new Date().getTime();
+
+    if (isProd){
+        // TODO: buffer, uglify, minify
+    } else { // development build should not be minified and compressed
+        return bundler.bundle()
+            .on('error', function (err) {
+                crashProcess(err);
+            })
+            .pipe(source('application_bundle.js'))
+            .pipe(gulp.dest(config.paths.dist))
+            .on('end', function () {
+                const time = (new Date().getTime() - startTime) / 1000;
+                gutil.log('Finished. Took:', chalk.magenta(time, 's') );
+            });
+    }
+}
+
+function attachBundleUpdate(bundler){
+  bundler = watchify(bundler);
+  bundler.on('update', function (updatedFile) {
+      const lint = gulp.src(updatedFile)
+          .pipe(eslint({
+              fix: true
+          }))
+          .pipe(eslint.format())
+          .pipe(gulpIf(
+              isFixed,
+              gulp.dest(config.paths.dist)
+          ));
+
+      bundle(bundler);
+  });
+}
+
+function createBundle() {
+  const bundler = browserify({
+    entries: [config.paths.mainJS],
+    cache: {},
+    packageCache: {},
+    transform: config.settings.transform
+  });
+  if (isWatch) { attachBundleUpdate(bundler); }
+  return bundle(bundler);
+}
+
 
 gulp.task('clean-scripts', () => {
   return gulp.src(config.paths.dist + 'application_bundle.js', {read: false})
     .pipe(clean());
 });
 
-gulp.task('js', ['apply-node-env', 'clean-scripts'], () => {
-	browserify(config.paths.mainJS)
-		.transform(babelify, {presets: ['es2015', 'react']})
-		.bundle()
-		.on('error', (err) => gutil.log(chalk.red(err)))
-		.pipe(vinyl('application_bundle.js'))
-		.pipe(gulp.dest(config.paths.dist));
-});
+// gulp.task('js', ['apply-node-env', 'clean-scripts'], () => {
+// 	browserify(config.paths.mainJS)
+// 		.transform(babelify, {presets: ['es2015', 'react']})
+// 		.bundle()
+// 		.on('error', (err) => gutil.log(chalk.red(err)))
+// 		.pipe(vinyl('application_bundle.js'))
+// 		.pipe(gulp.dest(config.paths.dist));
+// });
 
-gulp.task('lint', () => {
-  return gulp.src(config.paths.js)
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+// gulp.task('lint', () => {
+//   return gulp.src(config.paths.js)
+//     .pipe(eslint())
+//     .pipe(eslint.format())
+//     .pipe(eslint.failAfterError());
+// });
 
-gulp.task('watch', ['apply-node-env'], () => {
-  const b = browserify({
-    entries:      [config.paths.mainJS],
-    cache:        {},
-    packageCache: {},
-    plugin:       [watchify]
-  });
-
-  // var bundler = browserify({
-  //       cache: {},
-  //       packageCache: {},
-  //       fullPaths: false,
-  //       transform: config.settings.transform
-  //   });
-
-  function bundle() {
-    const startTime = new Date().getTime();
-    // if (prodBuild){
-    //     return bundler.bundle()
-    //         .on('error', crashProcess)
-    //         .pipe(source(options.output))
-    //         .pipe(buffer())
-    //         .pipe(uglify())
-    //         .pipe(gulp.dest(options.destination))
-    //         .on('end', function () {
-    //             const time = (new Date().getTime() - startTime) / 1000;
-    //             gutil.log(options.output, 'took', `${time}s` );
-    //         });
-    // } else { // development build should not be minified and compressed
-    //     return bundler.bundle()
-    //         .on('error', function (err) {
-    //             if (isDevelopment) {
-    //                 beeper('*');
-    //                 return console.log(chalk.bgBlack.red(err.message));
-    //             }
-    //             crashProcess(err);
-    //         })
-    //         .pipe(source(options.output))
-    //         .pipe(gulp.dest(options.destination))
-    //         .on('end', function () {
-    //             const time = (new Date().getTime() - startTime) / 1000;
-    //             gutil.log('Finished', `'${chalk.green(options.output)}'`, 'took', chalk.magenta(time, 's') );
-    //         });
-    // }
-
-    b.transform(babelify, {presets: ['es2015', 'react']})
-      .bundle()
-      .on('error', (err) => gutil.log(chalk.red(err)))
-      .pipe(vinyl('application_bundle.js'))
-      .pipe(gulp.dest(config.paths.dist))
-      .on('end', function () {
-        const time = (new Date().getTime() - startTime) / 1000;
-        gutil.log('Finished. Took', chalk.magenta(time, 's') );
-      });
-  }
-
-  b.on("update", bundle);
-  b.on("log", function (msg) {
-    gutil.log(msg);
-  });
-  b.on('end', function () {
-    const time = (new Date().getTime() - startTime) / 1000;
-    gutil.log(options.output, 'took', `${time}s` );
-  });
-
-
-  bundle();
+gulp.task('browserify', () => {
+  const startTime = new Date().getTime();
+  const bundle = createBundle();
 });
 
 gulp.task('default', ['watch']);
--- app/assets/javascripts/gulp/tasks/build.js
+++ app/assets/javascripts/gulp/tasks/build.js
@@ -1,3 +1,7 @@
 const gulp = require('gulp');
 
-gulp.task('build', ['watch']);
+gulp.task('apply-node-env', () => {
+  process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+});
+
+gulp.task('build', ['apply-node-env', 'browserify']);
