8a3026bc4f190d778a1c3f4836df4be092e4e061
elgrecode <elgrecode@gmail.com>
2016-11-16 19:40:00 -0800

    :performing_arts: #2163 sets up store/reducers for Slay

--- app/assets/javascripts/storefront/components/MarketingPage.jsx
+++ app/assets/javascripts/storefront/components/MarketingPage.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
-import createStore from '../store/templateStore';
-import Template from '../containers/Template';
+class MarketingPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
 
-// See documentation for https://github.com/reactjs/react-redux.
-// This is how you get props from the Rails view into the redux store.
-// This code here binds your smart component to the redux store.
-// railsContext provides contextual information especially useful for server rendering, such as
-// knowing the locale. See the React on Rails documentation for more info on the railsContext
-const SlayItForward = (props) => {
-  const store = createStore(props);
-  const reactComponent = (
-    <Provider store={store}>
-      <Template />
-    </Provider>
-  );
-  return reactComponent;
-};
+  render(){
+    <div>
+      Marketing Page
+      {this.props.children}
+    </div>
+  }
+
+}
+
+export default MarketingPage;
