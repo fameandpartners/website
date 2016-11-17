68486a935e03c7c032b2b527db6bf6e1d3fd1bff
Tiago Amaro <tiagoa@fameandpartners.com>
2016-11-17 16:38:51 +0100

    display making option price on PDP

--- app/assets/javascripts/storefront/components/PDP/SidePanelFastMaking.js
+++ app/assets/javascripts/storefront/components/PDP/SidePanelFastMaking.js
@@ -3,29 +3,51 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as pdpActions from '../../actions/PdpActions';
 
+class MakingOption {
+  constructor(id = null, price = 0) {
+    this.price = price;
+    this.id    = id;
+  }
+
+  get displayPrice() {
+    let price = parseFloat(this.price) || 0;
+    return price.toFixed(0);
+  }
+}
+
 class SidePanelFastMaking extends React.Component {
   constructor(props, context) {
     super(props, context);
 
+    this.defaultMakingOption = new MakingOption();
+
     this.onChange = this.onChange.bind(this);
+  }
+
+  get fastMakingOption() {
+    let makingOptions = this.props.product.available_options.table.making_options;
+    if (makingOptions.length) {
+      let fastMakingOption = makingOptions[0].product_making_option;
+      return new MakingOption(fastMakingOption.id, fastMakingOption.price);
+    } else {
+      return this.defaultMakingOption;
+    }
   }
 
   onChange(event) {
     let customize = {};
-    let makingOption = { price: 0 };
-    let makingOptions = this.props.product.available_options.table.making_options;
 
-    if (event.target.checked && makingOptions.length) {
-      makingOption = makingOptions[0].product_making_option;
+    if (event.target.checked) {
+      customize.makingOption = this.fastMakingOption;
+    } else {
+      customize.makingOption = this.defaultMakingOption;
     }
 
-    customize.makingOption = makingOption;
     this.props.actions.customizeDress(customize);
   }
 
   render() {
-    if (this.props.flags.fastMaking
-        && this.props.product.fast_making) {
+    if (this.props.flags.fastMaking && this.props.product.fast_making) {
       return (
         <div className="pdp-side-container pdp-side-container-fast-making checkbox-inline custom-form-element-thin form-small">
           <a href="javascript:;">
@@ -35,7 +57,7 @@ class SidePanelFastMaking extends React.Component {
               EXPRESS MAKING (6-9 days)
               <div className="pdp-side-note">Only available for Recommended Colors</div>
             </div>
-            <div className="c-card-customize__content__right">$30</div>
+            <div className="c-card-customize__content__right">${this.fastMakingOption.displayPrice}</div>
             </label>
           </a>
         </div>
@@ -49,6 +71,8 @@ class SidePanelFastMaking extends React.Component {
 }
 
 SidePanelFastMaking.propTypes = {
+  actions: PropTypes.object.isRequired,
+  flags: PropTypes.object.isRequired,
   product: PropTypes.object.isRequired
 };
 
