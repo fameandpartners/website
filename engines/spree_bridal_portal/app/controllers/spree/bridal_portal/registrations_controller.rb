6a4f5b1f587debf749d2f5e44b94cb3399e82f62
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-11-17 18:50:12 -0600

    [feature][WED-3] Add size signup step

--- engines/spree_bridal_portal/app/controllers/spree/bridal_portal/registrations_controller.rb
+++ engines/spree_bridal_portal/app/controllers/spree/bridal_portal/registrations_controller.rb
@@ -4,6 +4,23 @@ module Spree
       layout 'bridal_portal'
       def signup
       end
+
+      def size
+        @heights = [
+          [ 1, "5'19 / 177cm "],
+          [ 2, "5'19 / 177cm "],
+          [ 3, "5'19 / 177cm "],
+          [ 4, "5'19 / 177cm "]
+        ]
+
+        @dress_sizes = [0,2,4,5,8,10,12,14,16,18,20,22,24,26]
+      end
+
+      def details
+      end
+
+      def invite
+      end
     end
   end
 end
