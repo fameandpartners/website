17110d31a6babca73256e8bac7785e753cceb6af
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-11-17 18:49:07 -0600

    [feature][WED-3] add routes for next signup steps

--- engines/spree_bridal_portal/config/routes.rb
+++ engines/spree_bridal_portal/config/routes.rb
@@ -1,6 +1,11 @@
 Spree::Core::Engine.routes.draw do
   namespace :bridal_portal, path: 'bridal-portal' do
     root to: 'home#index'
-    get 'signup', to: 'registrations#signup'
+    scope '/signup', as: 'signup' do
+      get '/', to: 'registrations#signup'
+      get '/size', to: 'registrations#size'
+      get '/details', to: 'registrations#details'
+      get '/invite', to: 'registrations#invite'
+    end
   end
 end
