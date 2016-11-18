18d0fff730f5eba7909b2810047ae01801d6e8f6
Mateus Moraes <mateusm@fameandpartners.com>
2016-11-18 11:39:28 -0600

    update db/schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20161118172622) do
+ActiveRecord::Schema.define(:version => 20161118173743) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
