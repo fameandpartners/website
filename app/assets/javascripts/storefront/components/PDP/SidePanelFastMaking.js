1940dd2663683e0acd4470817ce5ced54427d3c5
Tiago Amaro <tiagoa@fameandpartners.com>
2016-11-21 16:29:46 +0100

    attach fast making checkbox rendering

--- app/assets/javascripts/storefront/components/PDP/SidePanelFastMaking.js
+++ app/assets/javascripts/storefront/components/PDP/SidePanelFastMaking.js
@@ -2,24 +2,13 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as pdpActions from '../../actions/PdpActions';
-
-class MakingOption {
-  constructor(id = null, price = 0) {
-    this.price = price;
-    this.id    = id;
-  }
-
-  get displayPrice() {
-    let price = parseFloat(this.price) || 0;
-    return price.toFixed(0);
-  }
-}
+import {MakingOption, defaultMakingOption} from './models/MakingOption';
 
 class SidePanelFastMaking extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.defaultMakingOption = new MakingOption();
+    this.state = { checked: false };
 
     this.onChange = this.onChange.bind(this);
   }
@@ -28,36 +17,37 @@ class SidePanelFastMaking extends React.Component {
     let makingOptions = this.props.product.available_options.table.making_options;
     if (makingOptions.length) {
       let fastMakingOption = makingOptions[0].product_making_option;
-      return new MakingOption(fastMakingOption.id, fastMakingOption.price);
+      return new MakingOption({id: fastMakingOption.id, price: fastMakingOption.price});
     } else {
-      return this.defaultMakingOption;
+      return defaultMakingOption;
     }
+  }
+
+  // Custom colors always have prices bigger than 0
+  get canFastMake() {
+    let isCustomColorSelected = this.props.customize.color.price > 0;
+    return !isCustomColorSelected;
+  }
+
+  get isFastMaking() {
+    return this.state.checked && this.canFastMake;
   }
 
   onChange(event) {
-    // TODO: potentially the default, let customize = {color: {price: 0}}
-    let customize = {};
+    let enableFastMaking = event.target.checked && this.canFastMake;
 
-    if (event.target.checked) {
-      customize.makingOption = this.fastMakingOption;
-    } else {
-      // else, unchecking fast making
-      // TODO: We need to maintain current color price because of above default
-      // customize.color.price = this.props.customize.color.price
-      customize.makingOption = this.defaultMakingOption;
-    }
+    let makingOption = enableFastMaking ? this.fastMakingOption : defaultMakingOption;
+    this.props.actions.customizeMakingOption({makingOption: makingOption});
 
-    this.props.actions.customizeDress(customize);
+    this.setState({checked: enableFastMaking});
   }
 
   render() {
-    // TODO: Check color price here and thus infer if fast making is possible
-    // console.log('customize color props', this.props.customize.color);
     if (this.props.flags.fastMaking && this.props.product.fast_making) {
       return (
         <div className="pdp-side-container pdp-side-container-fast-making checkbox-inline custom-form-element-thin form-small">
           <a href="javascript:;">
-            <input type="checkbox" id="fast-making" onChange={this.onChange} />
+            <input type="checkbox" id="fast-making" checked={this.isFastMaking} onChange={this.onChange} />
             <label htmlFor="fast-making">
             <div className="c-card-customize__content__left">
               EXPRESS MAKING (6-9 days)
@@ -78,6 +68,7 @@ class SidePanelFastMaking extends React.Component {
 
 SidePanelFastMaking.propTypes = {
   actions: PropTypes.object.isRequired,
+  customize: PropTypes.object.isRequired,
   flags: PropTypes.object.isRequired,
   product: PropTypes.object.isRequired
 };
