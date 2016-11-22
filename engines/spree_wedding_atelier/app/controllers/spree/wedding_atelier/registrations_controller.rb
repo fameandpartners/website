377d6bb42c63c1de03a8bfc5fe1a819b7d29b30b
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-11-22 12:10:12 -0600

    [feature][wed-9] Update registrations controller to handle new routes

--- engines/spree_wedding_atelier/app/controllers/spree/wedding_atelier/registrations_controller.rb
+++ engines/spree_wedding_atelier/app/controllers/spree/wedding_atelier/registrations_controller.rb
@@ -1,33 +1,71 @@
 module Spree
   module WeddingAtelier
-    class RegistrationsController < ApplicationController
+    class RegistrationsController < Spree::UserRegistrationsController
       layout 'wedding_atelier'
-      def signup
+      def new
+        @spree_user = Spree::User.new
+        @signup_params = {
+          site_version: current_site_version.code,
+          portal: 'wedding-atelier'
+         }
+      end
+
+      def create
+        @user = build_resource(params[:spree_user])
+        if resource.new_record?
+          resource.sign_up_via    = Spree::User::SIGN_UP_VIA.index('Email')
+          resource.sign_up_reason = session[:sign_up_reason]
+        end
+
+        if resource.save
+          EmailCaptureWorker.perform_async(resource.id, remote_ip:    request.remote_ip,
+                                                        landing_page: session[:landing_page],
+                                                        utm_params:   session[:utm_params],
+                                                        site_version: current_site_version.name,
+                                                        form_name:    'Register')
+
+          session.delete(:sign_up_reason)
+
+          sign_in :spree_user, resource
+
+          set_flash_message(:notice, :signed_up)
+          session[:spree_user_signup] = true
+          associate_user
+          resource.create_wedding
+
+          # Marketing pixel
+          flash[:signed_up_just_now] = true
+          redirect_to size_wedding_atelier_signup_path
+        else
+          render :new
+        end
+      end
+
+      def update
+        if current_spree_user.update_attributes(params[:spree_user])
+          redirect_to action: current_spree_user.wedding_atelier_signup_step
+        else
+          render current_spree_user.wedding_atelier_signup_step
+        end
       end
 
       def size
+        @dress_sizes = Spree::OptionType.find_by_name('dress-size').option_values
         @heights = [
-            [1, "5'19 / 177cm "],
-            [2, "5'19 / 177cm "],
-            [3, "5'19 / 177cm "],
-            [4, "5'19 / 177cm "]
+            "5'19 / 177cm ",
+            "5'19 / 180cm ",
+            "5'19 / 190cm ",
+            "5'19 / 200cm "
         ]
-
-        @dress_sizes = [0, 2, 4, 5, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26]
       end
 
       def details
-        @roles = [
-            [1, "Bride"],
-            [2, "Bride"],
-            [3, "Bride"],
-            [4, "Bride"]
-        ]
-
+        @roles = Spree::Role.where(name: ['bride', 'bridesmaid'])
       end
 
       def invite
       end
+
     end
   end
 end
