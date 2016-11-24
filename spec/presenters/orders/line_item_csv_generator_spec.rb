b88742bb382ab1e182197f4d846d335df4d60962
Nickolay Kondratenko <devmarkup@gmail.com>
2016-11-24 11:17:28 +0200

    update specs

--- spec/presenters/orders/line_item_csv_generator_spec.rb
+++ spec/presenters/orders/line_item_csv_generator_spec.rb
@@ -5,7 +5,38 @@ module Orders
     let(:orders) { FactoryGirl.create_list(:complete_order_with_items, 3) }
 
     let(:csv_header) do
-      ["order_state ", "order_number (订单号码)", "line_item ", "total_items ", "completed_at (订单日期)", "express_making (快速决策)", "projected_delivery_date (要求出厂日期)", "tracking_number (速递单号)", "shipment_date ", "fabrication_state ", "style (款号)", "style_name ", "factory (工厂)", "color (颜色)", "size (尺寸)", "height ", "customisations (特殊要求)", "custom_color ", "promo_codes ", "email ", "customer_notes ", "customer_name (客人名字)", "customer_phone_number (客人电话)", "shipping_address (客人地址)", "return_request ", "return_action ", "return_details ", "price ", "currency ", "upc "]
+      [
+        "order_state ",
+        "order_number (订单号码)",
+        "line_item ",
+        "total_items ",
+        "completed_at (订单日期)",
+        "express_making (快速决策)",
+        "projected_delivery_date (要求出厂日期)",
+        "tracking_number (速递单号)",
+        "shipment_date ",
+        "fabrication_state ",
+        "style (款号)",
+        "style_name ",
+        "factory (工厂)",
+        "color (颜色)",
+        "size (尺寸)",
+        "height ",
+        "customisations (特殊要求)",
+        "custom_color ",
+        "promo_codes ",
+        "email ",
+        "customer_notes ",
+        "customer_name (客人名字)",
+        "customer_phone_number (客人电话)",
+        "shipping_address (客人地址)",
+        "return_request ",
+        "return_action ",
+        "return_details ",
+        "price ",
+        "currency ",
+        "upc "
+      ]
     end
 
     subject { described_class.new(orders) }
--- spec/presenters/orders/line_item_csv_presenter_spec.rb
+++ spec/presenters/orders/line_item_csv_presenter_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 module Orders
   RSpec.describe LineItemCSVPresenter do
+    let(:order) { FactoryGirl.create(:complete_order_with_items) }
+    let(:line) { order.attributes }
+
     # the same as UPC
     describe '::product_number' do
+      # TODO: we should define which GlobalSku is used
       it "gets id from global_sku" do
-        expect(described_class).to receive(:global_sku).and_return(double(:global_sku, id: 'test_upc'))
+        described_class.set_line(line)
 
-        expect(described_class.product_number).to eq('test_upc')
+        expect(described_class.product_number).to eq(GlobalSku.last.id)
       end
     end
   end
