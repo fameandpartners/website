285c02ffa4f586162540b90c0fa0430348fd84e0
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-11-25 15:43:51 -0600

    [feature][wed-3] Add devise custom failure to redirect to weddint atelier app when necessary

--- engines/spree_wedding_atelier/config/routes.rb
+++ engines/spree_wedding_atelier/config/routes.rb
@@ -3,12 +3,16 @@ Spree::Core::Engine.routes.draw do
     root to: 'home#index'
 
     devise_scope :spree_user do
-      resource :signup, controller: :registrations, except: [:destroy, :edit, :show] do
+      resource :signup, controller: :registrations, only: [:update] do
+        get '/', to: 'registrations#new'
         get '/size', to: 'registrations#size'
         get '/details', to: 'registrations#details'
         get '/invite', to: 'registrations#invite'
         post '/send_invites', to: 'registrations#send_invites'
       end
+
+      get '/sign_in', to: 'sessions#new'
+      post '/sign_in', to: 'sessions#create'
     end
 
     resources :events
