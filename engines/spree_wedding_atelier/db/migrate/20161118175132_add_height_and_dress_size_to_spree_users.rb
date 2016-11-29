32dd0814be32fff3edbcf4b61fe00bae8231273e
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-11-29 10:39:48 -0600

    [feature][wed-3] Save trend updates checkbox to user attributes

--- engines/spree_wedding_atelier/db/migrate/20161118175132_add_height_and_dress_size_to_spree_users.rb
+++
@@ -1,6 +0,0 @@
-class AddHeightAndDressSizeToSpreeUsers < ActiveRecord::Migration
-  def change
-    add_column :spree_users, :height, :string
-    add_column :spree_users, :dress_size, :string
-  end
-end
