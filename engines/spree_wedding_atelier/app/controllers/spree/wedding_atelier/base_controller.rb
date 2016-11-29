222400406ea50b699a90f931e5cc1e6821fdca8d
Mario Gomez <mariogomezmtz@gmail.com>
2016-11-28 18:38:53 -0600

    [WED-3] Create helper for facebook button

--- engines/spree_wedding_atelier/app/controllers/spree/wedding_atelier/base_controller.rb
+++ engines/spree_wedding_atelier/app/controllers/spree/wedding_atelier/base_controller.rb
@@ -4,6 +4,7 @@ module Spree
       layout 'wedding_atelier'
       before_filter :authenticate_spree_user!
       before_filter :check_signup_completeness
+      helper SpreeWeddingAtelier::Engine.helpers
 
       def check_signup_completeness
         if current_spree_user && !current_spree_user.wedding_atelier_signup_complete?
