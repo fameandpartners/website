58af2e8366257f7e4bdabe0d565c9e8237c63c89
Mario Gomez <mariogomezmtz@gmail.com>
2016-11-28 19:22:12 -0600

    [WED-3] Complete fancy facebook button helper

--- engines/spree_wedding_atelier/app/helpers/spree/wedding_atelier/application_helper.rb
+++ engines/spree_wedding_atelier/app/helpers/spree/wedding_atelier/application_helper.rb
@@ -1,15 +1,19 @@
 module Spree
   module WeddingAtelier
     module ApplicationHelper
+
       def registration?
         controller_name == 'registrations'
       end
 
       def fancy_facebook_button
-        content_tag :div, class: 'text-center hidden-xs' do
+        content_tag :div, class: ['text-center','hidden-xs'] do
           link_to fb_auth_path, class: 'btn-facebook-fancy' do
-            content_tag :span, 'Sign in with', class: 'pre-text'
-            content_tag :span, 'Facebook', class: 'facebook-text'
+            [
+              image_tag('facebook-letter-logo.svg'),
+              content_tag(:span, 'Sign up with ', class: 'pre-text'),
+              content_tag(:span, 'Facebook', class: 'facebook-text')
+            ].join("\n").html_safe
           end
         end
       end
