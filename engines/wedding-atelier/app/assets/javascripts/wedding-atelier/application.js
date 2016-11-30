f6b8a3eb19944b7ee0e627ec961eb70069b9485a
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-11-30 13:53:02 -0600

    [feature][wed-3] fix fonts and js errors after engine rename

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/application.js
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/application.js
@@ -4,4 +4,5 @@
 //= require react_ujs
 //= require components
 //= require select2-4.0.3
-//= require store/registrations/details
+//= require bootstrap-datepicker-1.6.4
+//= require_tree ./registrations
