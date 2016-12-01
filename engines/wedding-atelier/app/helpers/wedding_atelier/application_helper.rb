da3411f50ff36028c3e6f20d722a6ada7066fa4e
Victor De la Rocha <vyk2rr@gmail.com>
2016-12-01 17:59:28 -0600

    Adds the moodboard header
    Adds the moodboard header, and svg logo black version and some styling for the header
    algo added a helper to get the profile image from the spree user.

--- engines/wedding-atelier/app/helpers/wedding_atelier/application_helper.rb
+++ engines/wedding-atelier/app/helpers/wedding_atelier/application_helper.rb
@@ -1,7 +1,13 @@
 module WeddingAtelier
   module ApplicationHelper
+
+    def registration?
+      controller_name == 'registrations'
+    end
+
     def registration_background_image_class
       current_spree_user.try(:wedding_atelier_signup_step) || 'signup'
     end
+
   end
 end
