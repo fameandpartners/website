0f4cdf50418f5984e554e937548d25627b8ddec9
Victor De la Rocha <vyk2rr@gmail.com>
2016-12-01 17:59:28 -0600

    Adds the moodboard header
    Adds the moodboard header, and svg logo black version and some styling for the header
    algo added a helper to get the profile image from the spree user.

--- engines/wedding-atelier/app/helpers/wedding_atelier/application_helper.rb
+++ engines/wedding-atelier/app/helpers/wedding_atelier/application_helper.rb
@@ -34,5 +34,13 @@ module WeddingAtelier
       end
     end
 
+    def profile_image_url
+      if current_spree_user.profile_image.present?
+        image_tag current_spree_user.profile_image.attachment.url(:mini), class: 'profile-image'
+      else
+        image_tag 'profile-placeholder.jpg', class: 'profile-image'
+      end
+    end
+
   end
 end
