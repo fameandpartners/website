af90c6f4fb1d4f4b1425eb8d41e4665e8d7356c5
Victor De la Rocha <vyk2rr@gmail.com>
2016-12-01 17:59:28 -0600

    Adds the moodboard header
    Adds the moodboard header, and svg logo black version and some styling for the header
    algo added a helper to get the profile image from the spree user.

--- engines/wedding-atelier/app/helpers/wedding_atelier/application_helper.rb
+++ engines/wedding-atelier/app/helpers/wedding_atelier/application_helper.rb
@@ -9,6 +9,35 @@ module WeddingAtelier
       current_spree_user.try(:wedding_atelier_signup_step) || 'signup'
     end
 
+    def fancy_facebook_button(pre_text = 'Log in')
+      content_tag :div, class: ['text-center','hidden-xs'] do
+        link_to fb_auth_path({ return_to: wedding_atelier.events_path}), class: 'btn-facebook-fancy' do
+          [
+            image_tag('facebook-letter-logo.svg'),
+            content_tag(:span, pre_text, class: 'pre-text'),
+            ' ',
+            content_tag(:span, 'Facebook', class: 'facebook-text')
+          ].join("\n").html_safe
+        end
+      end
+    end
+
+    def dress_size_grid(form, dress_sizes)
+      content_tag :div, class: 'dress-sizes' do
+        content_tag :ul do
+          items = dress_sizes.map do |size|
+            content_tag :li do
+              [
+                form.radio_button(:dress_size, "#{@site_version}/#{size}"),
+                form.label(:dress_size, size, value: "#{@site_version}/#{size}")
+              ].join("\n").html_safe
+            end
+          end
+          items.join("\n").html_safe
+        end
+      end
+    end
+
     def profile_image_url
       if current_spree_user.profile_image.present?
         image_tag current_spree_user.profile_image.attachment.url(:mini), class: 'profile-image'
@@ -16,5 +45,6 @@ module WeddingAtelier
         image_tag 'profile-placeholder.jpg', class: 'profile-image'
       end
     end
+
   end
 end
