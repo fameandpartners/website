12bf056e6b6b98bb13de5efc30de4971efc13405
Victor De la Rocha <vyk2rr@gmail.com>
2016-12-05 12:57:06 -0600

    [FEATURE][WED-25] Add validation to event.name existing in the template

--- engines/wedding-atelier/app/helpers/wedding_atelier/application_helper.rb
+++ engines/wedding-atelier/app/helpers/wedding_atelier/application_helper.rb
@@ -1,4 +1,11 @@
 module WeddingAtelier
   module ApplicationHelper
+    def profile_image_url
+      if current_spree_user.profile_image.present?
+        image_tag current_spree_user.profile_image.attachment.url(:mini), class: 'profile-image'
+      else
+        image_tag 'profile-placeholder.jpg', class: 'profile-image'
+      end
+    end
   end
 end
