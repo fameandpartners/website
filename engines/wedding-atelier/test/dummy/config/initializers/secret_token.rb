d8ffdc9c21e96fb424c2da20070905bba898c09b
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-12-06 15:08:44 -0600

    [FEATURE][WED-18] remove test folder

--- engines/wedding-atelier/test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '77934763085e572fe5c67d70aa3aae0edc98b0a86dc9fb4d14ba2b9244cb0538c008dd6161073e5b67bfd6021dfb88dc7ff8b60a8dc23bc21c944c18faf90e2a'
