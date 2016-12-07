27d9cbd0658efc5607f532613470237ba2024245
Tiago Amaro <tiagoa@fameandpartners.com>
2016-12-07 16:02:22 +0100

    create the currency method for PayPal express gateway

---
+++ spec/spree/gateway/pay_pal_express_decorator_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Spree::Gateway::PayPalExpress do
+  subject(:gateway) { described_class.new }
+
+  describe '#currency' do
+    context 'currency preference is setup' do
+      before(:each) { gateway.preferred_currency = 'BRL' }
+
+      it 'returns setup currency' do
+        expect(gateway.currency).to eq('BRL')
+      end
+    end
+
+    context 'currency preference is not setup' do
+      before(:each) { gateway.preferred_currency = nil }
+
+      it 'returns default USD value' do
+        expect(gateway.currency).to eq('USD')
+      end
+    end
+  end
+end
