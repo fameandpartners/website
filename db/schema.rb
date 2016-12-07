37b153bd5190db7e629e7416f57d470a59fb561b
Alberto Renteria Correa <sejeb2001@hotmail.com>
2016-12-07 22:40:41 +1100

    Add new Migrate file wedding sweepstakes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20161123132241) do
+ActiveRecord::Schema.define(:version => 20161207100752) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
