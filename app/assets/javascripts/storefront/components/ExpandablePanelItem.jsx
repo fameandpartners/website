88f0df056cd16693ad620eea48b4f63eede78be7
elgrecode <elgrecode@gmail.com>
2016-12-07 17:52:05 -0800

    :peforming_arts: replaces conventional component naming structure, builds first clickable ExpandablePanelItem

--- app/assets/javascripts/storefront/components/ExpandablePanelItem.jsx
+++ app/assets/javascripts/storefront/components/ExpandablePanelItem.jsx
@@ -1,21 +1,38 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 
 class ExpandablePanel extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
 
-  render(){
-    return (
-      <div className="ExpandablePanelItem">
+        this.openPanel = this.openPanel.bind(this);
+    }
 
-      </div>
-    );
-  }
+    openPanel() {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    render() {
+      const { itemGroup, revealedContent } = this.props;
+      const { isOpen } = this.state;
+      return (
+          <div className={`ExpandablePanelItem ${isOpen ? 'isOpen' : ''}`}>
+              <div className="ExpandablePanelItem__item-bar" onClick={this.openPanel}>
+                  {itemGroup}
+              </div>
+              <div className="ExpandablePanelItem__revealed-content">
+                  {revealedContent}
+              </div>
+          </div>
+      );
+    }
 }
 
 ExpandablePanel.propTypes = {
-  children: PropTypes.object
+    itemGroup: PropTypes.node,
+    revealedContent: PropTypes.node
 };
 
 export default ExpandablePanel;
