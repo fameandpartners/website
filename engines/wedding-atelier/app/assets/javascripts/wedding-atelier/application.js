0ac00d1bd2edb0712551fd2b1fb6dd8224c61233
Mario Gomez <mariogomezmtz@gmail.com>
2016-12-09 09:40:17 -0600

    [FEATURE][WED-27] Create assign dress modal

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/application.js
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/application.js
@@ -6,3 +6,16 @@
 //= require select2-4.0.3
 //= require bootstrap-datepicker-1.6.4
 //= require_tree
+
+$(document).ready(function () {
+
+  $('#test').bind('click', function () {
+    $('#modal-assign-dress').modal('show');
+    $('.test-modal').addClass('after_append');
+    $('.modal-backdrop').appendTo('.test-modal');
+
+    $('body').removeClass('modal-open');
+    $('body').css('padding-right', '');
+  });
+
+});
