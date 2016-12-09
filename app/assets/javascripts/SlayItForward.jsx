7024519060816b7ad76d2c2fb0c9b58f4648539a
Mateus Moraes <mateusm@fameandpartners.com>
2016-12-08 22:57:01 -0200

    [fix] remove invalid character

--- app/assets/javascripts/SlayItForward.jsx
+++ app/assets/javascripts/SlayItForward.jsx
@@ -60,13 +60,13 @@ class SlayItForward extends Component {
     const {breakpoint} = this.props;
     if (typeof Modernizr === 'object' && Modernizr.adownload){
       return (
-        <a className='link' href={path} download='slayitforward.jpg'>
+        <a className="link" href={path} download="slayitforward.jpg">
           <img className="share-tiles" src={path} />
         </a>
       );
     } else {
         return (
-          <a className='link' href={path} target='_blank'>
+          <a className="link" href={path} target="_blank">
             <img className="share-tiles" src={path} />
           </a>
         );
@@ -99,8 +99,8 @@ class SlayItForward extends Component {
 
           <MarketingSection className="MarketingSection-slay slay-reasoning inner-buffer">
             <div>
-              { this.props.breakpoint === 'mobile' ? null :
-                <h3 className='main-slay-text text-center font--secondary'>#SLAYITFORWARD</h3>
+              {this.props.breakpoint === 'mobile' ? null :
+                <h3 className="main-slay-text text-center font--secondary">#SLAYITFORWARD</h3>
               }
               <p className="text-center">
                 It’s a movement. <br/>
@@ -172,12 +172,12 @@ class SlayItForward extends Component {
 
           <MarketingSection className="MarketingSection-slay slay-carousel clearfix">
             <div>
-              <p className='text-center font-large'>Post, empower and <em>give back</em>.</p>
+              <p className="text-center font-large">Post, empower and <em>give back</em>.</p>
               <div className="insta-carousel clearfix">
-                { this.props.breakpoint === 'mobile' ?
+                {this.props.breakpoint === 'mobile' ?
                   this.renderShareImgs(shareTileImages[0]) :
                   shareTileImages.map((imgPath)=>{
-                    return this.renderShareImgs(imgPath)
+                    return this.renderShareImgs(imgPath);
                   })
                 }
               </div>
@@ -187,9 +187,9 @@ class SlayItForward extends Component {
           <MarketingSection className="MarketingSection-slay slay-ceo-letter inner-buffer">
             <div className="text-center">
               <p className="font-large">From our CEO.</p>
-              <img className='nyree-img' src={getImages('nyree')}></img>
+              <img className="nyree-img" src={getImages('nyree')}></img>
               <p>A message from Nyree Corby, a female founder and CEO, to women everywhere.</p>
-              <p className="font--secondary"><a href="/from-our-ceo"><span className='read-ceo-link u-textDecoration--underline'><b>READ NOW</b></span></a>&nbsp;&nbsp;<b>></b></p>
+              <p className="font--secondary"><a href="/from-our-ceo"><span className="read-ceo-link u-textDecoration--underline"><b>READ NOW</b></span></a>&nbsp;&nbsp;<b>></b></p>
             </div>
           </MarketingSection>
 
@@ -198,7 +198,7 @@ class SlayItForward extends Component {
   );
 
   }
-};
+}
 
 SlayItForward.propTypes = {
   breakpoint: PropTypes.string,
