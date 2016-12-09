b510d5948abb833c7023ee2f99ccd4950476b14c
Nickolay Kondratenko <devmarkup@gmail.com>
2016-12-09 12:00:39 +0200

    rollback order factory

--- spec/factories/spree/orders.rb
+++ spec/factories/spree/orders.rb
@@ -3,15 +3,17 @@ FactoryGirl.define do
     association(:shipping_address, factory: :address)
 
     factory :complete_order do
+      completed_at { DateTime.now }
+    end
+
+    factory :complete_order_with_items do
       association(:user,         factory: :spree_user)
       association(:bill_address, factory: :address)
 
       completed_at { DateTime.now }
       state        'complete'
 
-      factory :complete_order_with_items do
-        line_items   { build_list :dress_item, 1 }
-      end
+      line_items   { build_list :dress_item, 1 }
     end
   end
 end
