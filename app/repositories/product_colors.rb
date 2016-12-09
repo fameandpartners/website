0fdfee2e77dae5456c2bf3a05f377dac86b768e6
elgrecode <elgrecode@gmail.com>
2016-12-09 14:57:28 -0800

    :performing_arts: removes OpenStruct for colors replacing with hash, updates specs

--- app/repositories/product_colors.rb
+++ app/repositories/product_colors.rb
@@ -14,14 +14,14 @@ module Repositories
         @colors_map ||= begin
           result = {}
           Spree::OptionType.color.option_values.each do |option_value|
-            result[option_value.id] = OpenStruct.new(
+            result[option_value.id] = {
               id: option_value.id,
               name: option_value.name,
               presentation: option_value.presentation,
               value: option_value.value,
               use_in_customisation: option_value.use_in_customisation,
               image: option_value.image? ? option_value.image.url(:small_square) : nil
-            )
+            }
           end
           result
         end
@@ -34,23 +34,23 @@ module Repositories
             if color_ids.size == 1
               representative = Repositories::ProductColors.read(color_ids.first)
             else
-              representative = OpenStruct.new(name: group.name, presentation: group.name)
+              representative = {name: group.name, presentation: group.name}
             end
 
-            OpenStruct.new(
+            {
               id: group.id,
               name: group.name.to_s.downcase,
               presentation: group.presentation,
               color_ids: color_ids,
               representative: representative
-            )
+            }
           end
         end
       end
 
       # colors guarantee will be reloaded after restart... we can live with that
       def read_all
-        colors_map.values.clone.sort_by{ |s| s.name }
+        colors_map.values.clone.sort_by{ |s| s[:name] }
       end
 
       def read(id)
@@ -61,7 +61,7 @@ module Repositories
       def get_by_name(color_name = nil)
         result = Array.wrap(color_name).compact.map do |c|
           color_name = c.to_s.downcase
-          read_all.find{|color| color.name.downcase == color_name || color.presentation.downcase == color_name}
+          read_all.find{|color| color[:name].downcase == color_name || color[:presentation].downcase == color_name}
         end
         result.size < 2 ? result.first : result
       end
@@ -75,7 +75,7 @@ module Repositories
       def get_group_by_name(name)
         nil if name.blank?
         group_name = name.to_s.downcase
-        color_groups.find{|group| group.name == group_name }.try(:clone)
+        color_groups.find{|group| group[:name] == group_name }.try(:clone)
       end
     end
   end
--- spec/repositories/product_colors_spec.rb
+++ spec/repositories/product_colors_spec.rb
@@ -8,7 +8,7 @@ describe Repositories::ProductColors do
 
     it 'finds color group by name' do
       result = described_class.get_group_by_name('ReD')
-      expect(result.id).to eq(color_group.id)
+      expect(result[:id]).to eq(color_group[:id])
     end
 
     it 'does not find inexistent color group' do
@@ -21,6 +21,15 @@ describe Repositories::ProductColors do
         result = described_class.get_group_by_name('')
         expect(result).to be_nil
       end
+    end
+  end
+
+  describe '.get_color_by_name' do
+    let!(:color_group) { create(:option_values_group, option_type: color_option_type, name: 'blue') }
+    it 'finds a color by name' do
+      name = 'blue'
+      result = described_class.get_by_name(name)
+      expect(result[:name]).to eq(name)
     end
   end
 end
