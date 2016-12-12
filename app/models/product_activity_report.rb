5c90edbd2ede6dec06a6af2b85a755104a542def
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-12-12 11:33:43 +0200

    Remove product activity report code as it's now excess

--- app/models/product_activity_report.rb
+++ app/models/product_activity_report.rb
@@ -1,24 +1,5 @@
 module ProductActivityReport
-  module_function
-
-  def total_product_viewed(product_id)
-    action_count :viewed, product_id
-  end
-
-  def total_product_added_to_cart(product_id)
-    action_count :added_to_cart, product_id
-  end
-
-  def total_product_added_to_wishlist(product_id)
-    action_count :added_to_wishlist, product_id
-  end
-
-  def total_product_purchased(product_id)
-    action_count :purchased, product_id
-  end
-
-  def action_count(action, product_id)
-    0
-  end
-
+  # NOTE: Alexey Bobyrev 12/12/16
+  # This model is DEPRECATED - do NOT use it.
+  # After ensuring that we do not need old activities records - also remove this model!
 end
