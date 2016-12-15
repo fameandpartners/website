ee06fccfd8dca6a342333d246e2dc89836f68c26
elgrecode <elgrecode@gmail.com>
2016-12-14 16:02:35 -0800

    :performing_arts: adds redux devtools

--- app/assets/javascripts/storefront/store/AppStore.js
+++ app/assets/javascripts/storefront/store/AppStore.js
@@ -1,21 +1,22 @@
-import { compose, createStore, applyMiddleware, combineReducers } from 'redux';
+import { compose, createStore, applyMiddleware, combineReducers, } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import reducers, { initialStates } from '../reducers_immutable';
+import reducers, { initialStates, } from '../reducers_immutable';
+import { composeWithDevTools, } from 'redux-devtools-extension';
 
 export default (props) => {
   const initialComments = props.comments;
-  const { $$collectionFilterSortState } = initialStates;
+  const { $$collectionFilterSortState, } = initialStates;
   const initialState = {
     $$collectionFilterSortStore: $$collectionFilterSortState.merge({
       $$bodyShapes: props.bodyShapes,
       $$colors: props.colors,
-      $$secondaryColors: props.secondaryColors
-    })
+      $$secondaryColors: props.secondaryColors,
+    }),
   };
 
   const reducer = combineReducers(reducers);
   const composedStore = compose(
-    applyMiddleware(thunkMiddleware)
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
   );
 
   return composedStore(createStore)(reducer, initialState);
