e19b53f8141aa96b9f761c9bcfc1d19e4e304a68
Victor De la Rocha <vyk2rr@gmail.com>
2016-12-15 10:37:10 -0600

    [FEATURE][WED-25] Added chat into moodboard component
    Moved chat component into moodboard event

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/events/moodboard.js.jsx
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/events/moodboard.js.jsx
@@ -48,68 +48,83 @@ var MoodBoardEvent = React.createClass({
 
   render: function () {
     return (
-        <div className='right-container'>
-          <h1 className="moodboard-title text-center">
-            {this.state.event.name} - in {this.state.event.remaining_days} days
-          </h1>
+      <div id="events__moodboard">
+        <div className="chat left-content col-sm-6">
+          <Chat twilio_token_path={this.props.twilio_token_path}
+                event_id={this.props.event_id}
+                wedding_name={this.props.wedding_name}
+                profile_photo={this.props.profile_photo}
+                username={this.props.username} />
+        </div>
+        <div className="right-content col-sm-6" id="atelier">
+          <div className='right-container'>
+            <h1 className="moodboard-title text-center">
+              {this.state.event.name} - in {this.state.event.remaining_days} days
+            </h1>
 
-          <div className="moodboard-tabs center-block">
-            <ul className="nav nav-tabs center-block" role="tablist">
-              <li role="presentation" className="hidden-md hidden-lg">
-                <a aria-controls="chat-mobile" data-toggle="tab" href="#chat-mobile" role="tab">
-                  Chat  <span className="badge">42</span></a>
-              </li>
-              <li className="active" role="presentation">
-                <a aria-controls="bridesmaid-dresses" data-toggle="tab" href="#bridesmaid-dresses" role="tab">
-                  Bridesmaid dresses</a>
-              </li>
-              <li role="presentation">
-                <a aria-controls="wedding-details" data-toggle="tab" href="#wedding-details" role="tab"> Wedding
-                  details</a>
-              </li>
-              <li role="presentation">
-                <a aria-controls="manage-bridal-party" data-toggle="tab" href="#manage-bridal-party" role="tab"> Manage
-                  bridal party</a>
-              </li>
-              <li role="presentation">
-                <a aria-controls="bridal-gowns" data-toggle="tab" href="#bridal-gowns" role="tab"> Bridal Gowns</a>
-              </li>
-            </ul>
-            <div className="tab-content">
-              <div id="chat-mobile" className="tab-pane" role="tabpanel">
-                <Chat />
-              </div>
-              <div id="bridesmaid-dresses" className="tab-pane active" role="tabpanel">
-                <div className="add-dress-box hidden">
-                  <button className="add">Add your first dress</button>
+            <div className="moodboard-tabs center-block">
+              <ul className="nav nav-tabs center-block" role="tablist">
+                <li role="presentation" className="hidden-md hidden-lg">
+                  <a aria-controls="chat-mobile" data-toggle="tab" href="#chat-mobile" role="tab">
+                    Chat  <span className="badge">42</span></a>
+                </li>
+                <li className="active" role="presentation">
+                  <a aria-controls="bridesmaid-dresses" data-toggle="tab" href="#bridesmaid-dresses" role="tab">
+                    Bridesmaid dresses</a>
+                </li>
+                <li role="presentation">
+                  <a aria-controls="wedding-details" data-toggle="tab" href="#wedding-details" role="tab"> Wedding
+                    details</a>
+                </li>
+                <li role="presentation">
+                  <a aria-controls="manage-bridal-party" data-toggle="tab" href="#manage-bridal-party" role="tab"> Manage
+                    bridal party</a>
+                </li>
+                <li role="presentation">
+                  <a aria-controls="bridal-gowns" data-toggle="tab" href="#bridal-gowns" role="tab"> Bridal Gowns</a>
+                </li>
+              </ul>
+              <div className="tab-content">
+                <div id="chat-mobile" className="tab-pane" role="tabpanel">
+                  <Chat twilio_token_path={this.props.twilio_token_path}
+                    event_id={this.props.event_id}
+                    wedding_name={this.props.wedding_name}
+                    profile_photo={this.props.profile_photo}
+                    username={this.props.username} />
                 </div>
-                <div className="dresses-actions text-center"><a href="#" className="btn-transparent btn-create-a-dress">
-                  <em>Create</em> a dress</a>
+                <div id="bridesmaid-dresses" className="tab-pane active" role="tabpanel">
+                  <div className="add-dress-box hidden">
+                    <button className="add">Add your first dress</button>
+                  </div>
+                  <div className="dresses-actions text-center"><a href="#" className="btn-transparent btn-create-a-dress">
+                    <em>Create</em> a dress</a>
+                  </div>
+                  <div className="dresses-list">
+                    <DressTiles dresses={this.state.event.dresses} />
+                  </div>
                 </div>
-                <div className="dresses-list">
-                  <DressTiles dresses={this.state.event.dresses} />
+                <div id="wedding-details" className="tab-pane" role="tabpanel">
+                  <EventDetails event={this.state.event}
+                                updater={this.handleEventDetailUpdate} />
                 </div>
-              </div>
-              <div id="wedding-details" className="tab-pane" role="tabpanel">
-                <EventDetails event={this.state.event}
-                              updater={this.handleEventDetailUpdate} />
-              </div>
-              <div id="manage-bridal-party" className="tab-pane center-block" role="tabpanel">
-                <h1 className="text-center">
-                  <em>Janine</em>, intive the bridal party.
-                </h1>
-                <EventInvitations invitations={this.state.event.invitations}
-                                  assistants={this.state.event.assistants}
-                                  handleRemoveAssistant={this.handleRemoveAssistant}
-                                  send_invite_path={this.props.send_invite_path}
-                                  curret_user_id={this.props.current_user_id} />
-              </div>
-              <div id="bridal-gowns" className="tab-pane" role="tabpanel">
+                <div id="manage-bridal-party" className="tab-pane center-block" role="tabpanel">
+                  <h1 className="text-center">
+                    <em>Janine</em>, intive the bridal party.
+                  </h1>
+                  <EventInvitations invitations={this.state.event.invitations}
+                                    assistants={this.state.event.assistants}
+                                    handleRemoveAssistant={this.handleRemoveAssistant}
+                                    send_invite_path={this.props.send_invite_path}
+                                    curret_user_id={this.props.current_user_id} />
+                </div>
+                <div id="bridal-gowns" className="tab-pane" role="tabpanel">
 
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
     )
   }
 });
