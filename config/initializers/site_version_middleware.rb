5ee36c93d8252834d1a1deaa327ab9bc0706708d
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-12-15 14:46:04 +0200

    Simplify middleware insert

--- config/initializers/site_version_middleware.rb
+++ config/initializers/site_version_middleware.rb
@@ -1,9 +1,5 @@
 require 'middleware/site_version/detector'
 
 FameAndPartners::Application.configure do |config|
-  if defined? ActionDispatch::DebugExceptions
-    config.middleware.insert_after ActionDispatch::DebugExceptions, Middleware::SiteVersion::Detector
-  else
-    config.middleware.use Middleware::SiteVersion::Detector
-  end
+  config.middleware.insert_after ActionDispatch::DebugExceptions, Middleware::SiteVersion::Detector
 end
