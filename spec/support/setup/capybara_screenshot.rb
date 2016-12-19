6b3fc1c538929f766e63fb75b7666b00520cdaf2
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-12-19 15:59:49 +0200

    Set path for capybara to store tmp files

--- spec/support/setup/capybara_screenshot.rb
+++ spec/support/setup/capybara_screenshot.rb
@@ -2,12 +2,5 @@ require 'capybara-screenshot/rspec'
 
 Capybara::Screenshot.autosave_on_failure = true
 Capybara::Screenshot.prune_strategy      = :keep_last_run
+Capybara.save_and_open_page_path = ::Rails.root.join('tmp', 'capybara')
 
-# Uncomment this for quick binding.pry in acceptance specs.
-# RSpec.configure do |config|
-#   config.after(:each, :type => :feature) do |example|
-#     if example.exception
-#       binding.pry
-#     end
-#   end
-# end
