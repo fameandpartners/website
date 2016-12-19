396779f58d8c9a6e5b876a1a59a83adefacfed29
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-12-19 14:00:27 -0600

    [FEATURE][WED-23] Add backend needed for customizations set up

---
+++ engines/wedding-atelier/app/serializers/wedding_atelier/customization_serializer.rb
@@ -0,0 +1,11 @@
+module WeddingAtelier
+  class CustomizationSerializer < ActiveModel::Serializer
+    has_many :silhouettes, serializer: WeddingAtelier::ProductSerializer
+    has_many :fabrics, serializer: WeddingAtelier::OptionValueSerializer
+    has_many :colours, serializer: WeddingAtelier::OptionValueSerializer
+    has_many :sizes, serializer: WeddingAtelier::OptionValueSerializer
+    has_many :lengths, serializer: WeddingAtelier::OptionValueSerializer
+    has_many :assistants, serializer: WeddingAtelier::UserSerializer
+    has_many :heights
+  end
+end
