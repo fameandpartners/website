422bc57ad6fed2adab6e5043007153ad9df1d238
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2016-12-19 16:30:36 +0200

    Update capybara-screenshot gem in order to latest fixes

--- spec/support/setup/capybara_screenshot.rb
+++ spec/support/setup/capybara_screenshot.rb
@@ -2,5 +2,4 @@ require 'capybara-screenshot/rspec'
 
 Capybara::Screenshot.autosave_on_failure = true
 Capybara::Screenshot.prune_strategy      = :keep_last_run
-Capybara.save_and_open_page_path = ::Rails.root.join('tmp', 'capybara')
 
