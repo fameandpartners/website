6e32c358aab0db183b594cf854487bcfc1ff66c0
Mario Gomez <mariogomezmtz@gmail.com>
2016-12-18 21:26:26 -0600

    [FEATURE][WED-27] Add styles for customization preview

---
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/customizations/mobile/customizations-menu-mobile.js.jsx
@@ -0,0 +1,12 @@
+var CustomizationsMenuMobile = React.createClass({
+  propTypes: {
+
+  },
+
+  render: function() {
+    return (
+      <div className="customizations-menu-mobile">
+      </div>
+    );
+  }
+});
