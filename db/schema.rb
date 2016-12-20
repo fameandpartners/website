865637b8b2123393bf812b22c64e93fb9bb016cf
Nickolay Kondratenko <devmarkup@gmail.com>
2016-12-20 13:24:03 +0200

    add delivery period to taxon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20161213021906) do
+ActiveRecord::Schema.define(:version => 20161220101024) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
@@ -1546,7 +1546,7 @@ ActiveRecord::Schema.define(:version => 20161213021906) do
   create_table "spree_taxons", :force => true do |t|
     t.integer  "parent_id"
     t.integer  "position",          :default => 0
-    t.string   "name",                             :null => false
+    t.string   "name",                                  :null => false
     t.string   "permalink"
     t.integer  "taxonomy_id"
     t.integer  "lft"
@@ -1556,13 +1556,14 @@ ActiveRecord::Schema.define(:version => 20161213021906) do
     t.integer  "icon_file_size"
     t.datetime "icon_updated_at"
     t.text     "description"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.string   "meta_title"
     t.string   "meta_description"
     t.string   "meta_keywords"
     t.string   "title"
     t.datetime "published_at"
+    t.string   "delivery_period",   :default => "7-10"
   end
 
   add_index "spree_taxons", ["parent_id"], :name => "index_taxons_on_parent_id"
