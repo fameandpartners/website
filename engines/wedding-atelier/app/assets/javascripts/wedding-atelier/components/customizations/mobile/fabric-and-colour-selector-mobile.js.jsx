8c1a8e80442970de85dc408bcc1bc99b2c5e2715
Mario Gomez <mariogomezmtz@gmail.com>
2016-12-19 20:24:16 -0600

    [FEATURE][WED-27] Finish fabric and color callbacks

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/customizations/mobile/fabric-and-colour-selector-mobile.js.jsx
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/customizations/mobile/fabric-and-colour-selector-mobile.js.jsx
@@ -1,21 +1,18 @@
 var FabricAndColourSelectorMobile = React.createClass({
   propTypes: {
-    colours:          React.PropTypes.array,
-    fabrics:          React.PropTypes.array,
-    selectedOption:   React.PropTypes.object,
-    selectCallback:   React.PropTypes.func.isRequired
-  },
-
-  selectFabric: function(fabric) {
-    this.props.selectCallback('fabric', fabric);
+    colours:                React.PropTypes.array,
+    fabrics:                React.PropTypes.array,
+    selectedOption:         React.PropTypes.object,
+    selectCallback:         React.PropTypes.func.isRequired,
+    selectOptionCallback:   React.PropTypes.func.isRequired
   },
 
   render: function() {
     var fabrics = this.props.fabrics.map(function(fabric, index) {
-      var inputId = fabric.id + "-" + 'desktop';
+      var inputId = fabric.id + "-" + 'mobile';
 
       return (
-        <div key={index} onClick={this.selectFabric.bind(this, fabric)} className="fabric-radio-container">
+        <div key={index} onClick={this.props.selectOptionCallback.bind(null, 'fabric', fabric)} className="fabric-radio-container">
           <input id={inputId} type="radio" value={fabric} name="fabric" className="customization-radio"/>
           <label htmlFor={inputId} className="customization-radio-label">
             <span className="box"></span>
@@ -27,29 +24,30 @@ var FabricAndColourSelectorMobile = React.createClass({
 
     var colours = this.props.colours.map(function(colour, index) {
       var classes = classNames({
-        'customization-options-item-small': true,
+        'customizations-selector-mobile-options-item-small': true,
         'active': this.props.selectedOption && this.props.selectedOption.id == colour.id
       });
       return (
-        <div key={index} onClick={this.props.selectCallback.bind(null, 'colour', colour)} className="col-sm-4 col-md-3">
-          <div className={classes} style={{backgroundColor: colour.value}}></div>
-          <p className="customization-options-item-label">{colour.presentation}</p>
+        <div key={index} onClick={this.props.selectOptionCallback.bind(null, 'colour', colour)} className="col-xs-6">
+          <div className={classes} style={{backgroundColor: colour.value}}>
+          </div>
+          <p className="customizations-selector-mobile-options-item-label">{colour.presentation}</p>
         </div>
       );
     }.bind(this));
 
     return (
-      <div ref="container" className="customizations-selector-mobile fabric animated slideInLeft">
-        <div className="customization">
-          <div className="customization-title">
-            <h1><em>Create</em> the look and feel</h1>
-            <div className="row">{fabrics}</div>
-            <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <div ref="container" className="customizations-selector-mobile">
+        <div className="customizations-selector-mobile-title">
+          <h1><em>Create</em> it how you want</h1>
+          <div className="row">
+            {fabrics}
           </div>
-          <div className="customization-options-grid row">
-            {colours}
-          </div>
-         </div>
+          <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div className="customizations-selector-mobile-grid row">
+          {colours}
+        </div>
       </div>
     );
   }
