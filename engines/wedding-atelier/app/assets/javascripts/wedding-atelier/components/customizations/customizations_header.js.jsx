efb6e3ef15eb1ce54a863273793f45c258bc8beb
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-12-19 18:27:38 -0600

    [FEATURE][WED-24] Customization experience header bar

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/customizations/customizations_header.js.jsx
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/customizations/customizations_header.js.jsx
@@ -1,7 +1,24 @@
 var CustomizationsHeader = React.createClass({
+  propTypes: {
+    silhouette: React.PropTypes.object
+  },
+
   render: function(){
+    var designName = ''
+    if(this.props.silhouette){
+      designName = 'You are designing the ' + this.props.silhouette.name;
+    }
     return(
-      <div></div>
+      <div className="customization-experience-header">
+        <div className="col-sm-6 arrow">
+          <a href="#" className="back-to-moodboard">
+            Back to wedding moodboard
+          </a>
+        </div>
+        <div className="col-sm-6">
+          <span className="design-name">{designName}</span>
+        </div>
+      </div>
     )
   }
 })
