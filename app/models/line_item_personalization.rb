05b75e05a309b9cc8360a3fc5e7d7557ad608929
Gustavo Robles <gustavo.robles@magmalabs.io>
2016-12-21 10:28:08 -0600

    [FEATURE][WED-23] Add length id to line item personalizations

--- app/models/line_item_personalization.rb
+++ app/models/line_item_personalization.rb
@@ -13,12 +13,15 @@ class LineItemPersonalization < ActiveRecord::Base
              class_name: 'Spree::OptionValue'
   belongs_to :fabric,
              class_name: 'Spree::OptionValue'
+  belongs_to :length,
+             class_name: 'Spree::OptionValue'
 
   attr_accessible :customization_value_ids,
                   :height,
                   :size_id,
                   :color_id,
-                  :fabric_id
+                  :fabric_id,
+                  :length_id
 
   validates :size, :color,  presence: true
 
