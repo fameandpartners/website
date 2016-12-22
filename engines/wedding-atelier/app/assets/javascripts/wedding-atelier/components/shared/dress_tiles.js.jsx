74ac1e81fd4fa4e23b9030ebc121279a9aa769ea
Victor De la Rocha <vyk2rr@gmail.com>
2016-12-22 11:36:13 -0600

    [FEATURE][WED-31] Adds a fix for the dress-tiles, debug dada was added

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/shared/dress_tiles.js.jsx
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/shared/dress_tiles.js.jsx
@@ -9,43 +9,7 @@ var DressTiles = React.createClass({
   },
 
   componentDidMount: function(){
-
-    this.setState({dresses: [
-
-      {
-        title: 'test',
-        image: 'https://s-media-cache-ak0.pinimg.com/736x/5e/f3/f1/5ef3f10a3d464c28fc2a3711195b700d.jpg',
-        author: 'Trino',
-        price: '3.45'
-      },
-      {
-        title: 'test',
-        image: 'https://s-media-cache-ak0.pinimg.com/564x/43/17/15/4317155ad1cce7d6b36e42ed53a1413b.jpg',
-        author: 'Trino',
-        price: '3.45'
-      },
-      {
-        title: 'test',
-        image: 'https://s-media-cache-ak0.pinimg.com/originals/67/e6/ea/67e6ea8103b45da806865143d55ddad3.jpg',
-        author: 'Trino',
-        price: '3.45'
-      },
-      {
-        title: 'test',
-        image: 'https://s-media-cache-ak0.pinimg.com/736x/24/c2/70/24c270867a71b90b76500397ce5fbfb3.jpg',
-        author: 'Trino',
-        price: '3.45'
-      },
-      {
-        title: 'test',
-        image: 'http://www.bookeveningdress.com/wp-content/uploads/2016/05/sexy-wedding-dresses-for-sale-with-keyword.jpg',
-        author: 'Trino',
-        price: '3.45'
-      }
-
-    ]})
-
-    // this.setState({dresses: this.props.dresses})
+    this.setState({dresses: this.props.dresses})
   },
 
   render: function() {
