05f640a5a1b92ddf0c82a7cec8d98a69793ac840
Mario Gomez <mariogomezmtz@gmail.com>
2016-12-22 11:48:36 -0600

    [FEATURE][WED-23] Mobile header logo

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/customizations/customizations_header.js.jsx
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/customizations/customizations_header.js.jsx
@@ -4,21 +4,22 @@ var CustomizationsHeader = React.createClass({
   },
 
   render: function(){
-    var designName = ''
+    var designName = '';
     if(this.props.silhouette){
       designName = 'You are designing the ' + this.props.silhouette.name;
     }
     return(
       <div className="customization-experience-header">
-        <div className="col-sm-6 arrow">
-          <a href="#" className="back-to-moodboard">
+        <div className="col-sm-6 col-xs-12 arrow">
+          <a href="#" className="back-to-moodboard hidden-xs">
             Back to wedding moodboard
           </a>
+          <img src="/assets/fp-logo.svg" className="logo hidden-sm hidden-md hidden-lg"/>
         </div>
-        <div className="col-sm-6">
+        <div className="col-sm-6" hidden-xs>
           <span className="design-name">{designName}</span>
         </div>
       </div>
-    )
+    );
   }
-})
+});
