951902463e3701cc048ea31832e52c9a2290c366
Nickolay Kondratenko <devmarkup@gmail.com>
2016-12-26 17:14:13 +0200

    fix PDP spec

--- db/migrate/20161220101024_add_delivery_period_to_spree_taxons.rb
+++ db/migrate/20161220101024_add_delivery_period_to_spree_taxons.rb
@@ -1,5 +1,5 @@
 class AddDeliveryPeriodToSpreeTaxons < ActiveRecord::Migration
   def change
-    add_column :spree_taxons, :delivery_period, :string, default: '7-10'
+    add_column :spree_taxons, :delivery_period, :string, default: '7 - 10'
   end
 end
