1dfa8511782d01f19c5670e4e758a3966928fdbf
Mateus Moraes <mateusm@fameandpartners.com>
2017-01-06 21:15:04 -0200

    add migration files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20161213021906) do
+ActiveRecord::Schema.define(:version => 20170106163718) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
