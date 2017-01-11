5f7baa65ffc81a5a5098cf05b2e676ae22395d4b
Mateus Moraes <mateusm@fameandpartners.com>
2017-01-11 09:16:38 -0200

    add route to /bespoke-bridal-collection

--- config/routes.rb
+++ config/routes.rb
@@ -210,6 +210,9 @@ FameAndPartners::Application.routes.draw do
     # Pre-Prom/Pre-Season Evening Collection - Landing page
     get '/pre-season-evening-collection' => 'products/collections#show', :permalink => 'pre-season-evening-collection', :as => :pre_season_evening_collection_landing_page
 
+    # Bespoke Bridal Collection - Landing page
+    get '/bespoke-bridal-collection' => 'products/collections#show', :permalink => 'bespoke-bridal-collection', :as => :bespoke_bridal_collection_landing_page
+
     # Landing pages
     get '/fameweddings/bridesmaid' => 'products/collections#show', :permalink => 'bridesmaid14', :as => :bridesmaid_landing_page
     get '/fameweddings/bride' => 'products/collections#show', :permalink => 'bridesmaid14', :as => :brides_landing_page
