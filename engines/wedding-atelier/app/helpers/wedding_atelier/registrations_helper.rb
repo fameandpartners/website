29d61f6397bc8b3d50e062babba87fd4ee221fde
Tiago Amaro <tiagoa@fameandpartners.com>
2017-01-12 16:10:29 +0100

    delete unused methods/commented out code

--- engines/wedding-atelier/app/helpers/wedding_atelier/registrations_helper.rb
+++ engines/wedding-atelier/app/helpers/wedding_atelier/registrations_helper.rb
@@ -54,22 +54,6 @@ module WeddingAtelier
         end
       end
     end
-    #
-    # def dress_size_grid(form, dress_sizes)
-    #   content_tag :div, class: 'dress-sizes' do
-    #     content_tag :ul do
-    #       items = dress_sizes.map.with_index do |size, index|
-    #         parsedSize = size.name.match(/#{@site_version}(\d+)/i)[1]
-    #         content_tag :li do
-    #           [
-    #             form.radio_button(:dress_size_id, size.id),
-    #             form.label(:dress_size_id, parsedSize, value: size.id)
-    #           ].join("\n").html_safe
-    #         end
-    #       end
-    #       items.join("\n").html_safe
-    #     end
-    #   end
-    # end
+
   end
 end
