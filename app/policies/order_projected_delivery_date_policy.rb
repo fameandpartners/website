0b34c4d8eb1132175eda8f9ba2644f931b7bf3fc
Nickolay Kondratenko <devmarkup@gmail.com>
2017-01-12 14:23:47 +0100

    update order projected delivery date policy

--- app/policies/order_projected_delivery_date_policy.rb
+++ app/policies/order_projected_delivery_date_policy.rb
@@ -6,6 +6,7 @@ module Policies
 
     DELIVERY_DAYS = 10
     FAST_DELIVERY_DAYS = 4
+    DELAY_DELIVERY_DAYS = 28
 
     attr_reader :order
 
@@ -15,11 +16,15 @@ module Policies
 
     # note - possible, this should be order.line_items.map(&:projected_delivery_date).min
     def delivery_date
-      if order.has_fast_making_items?
-        FAST_DELIVERY_DAYS.business_days.after(order.completed_at)
-      else
-        DELIVERY_DAYS.business_days.after(order.completed_at)
-      end
+      delivery_days = if Features.active?(:cny_delivery_delays)
+                        DELAY_DELIVERY_DAYS
+                      elsif order.has_fast_making_items?
+                        FAST_DELIVERY_DAYS
+                      else
+                        DELIVERY_DAYS
+                      end
+
+      delivery_days.business_days.after(order.completed_at)
     end
   end
 end
