626530619c3d44051fde30b2fa32fc8e65dea486
elgrecode <elgrecode@gmail.com>
2017-01-17 16:13:02 -0800

    :performing_arts: [WEBSITE-1311] migration for bridal guide landing page

---
+++ db/migrate/20170118000752_create_bridal_guide_landing_page.rb
@@ -0,0 +1,28 @@
+class CreateBridalGuideLandingPage < ActiveRecord::Migration
+  # All values MUST be Strings!
+  private def landing_page_properties
+    {
+      path:             '/bridal-guide',
+      template_path:    '/statics/bridal_guide',
+      heading:          'Download Your Bridal Guides',
+      title:            'Bridal Guides',
+      meta_description: 'Download comprehensive bridal guides for finding the perfect dress, hair, makeup, accessories and arrangements',
+    }
+  end
+
+  def up
+    unless Revolution::Page.where(path: landing_page_properties[:path]).exists?
+      page = Revolution::Page.create!(
+        path:          landing_page_properties[:path],
+        template_path: landing_page_properties[:template_path],
+        publish_from:  1.day.ago
+      )
+      page.translations.create!(locale: 'en-US', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+      page.translations.create!(locale: 'en-AU', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+    end
+  end
+
+  def down
+    Revolution::Page.where(path: landing_page_properties[:path]).delete_all
+  end
+end
