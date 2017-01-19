739e0ffaf8ccef1af30428368a5b3b9423e51ff1
Mario Gomez <mariogomezmtz@gmail.com>
2017-01-18 19:41:25 -0600

    [FEATURE][WED-62] Add layout for mobile an desktop with header

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/events/shopping-bag.js.jsx
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/events/shopping-bag.js.jsx
@@ -1,8 +1,52 @@
 var ShoppingBag = React.createClass({
+  propTypes: {
+    checkoutUrl: React.PropTypes.string
+  },
+
+  getInitialState: function () {
+    return {
+      show: false
+    };
+  },
+
+  bagOpenHandle: function () {
+    this.setState({show: true});
+  },
+
+  bagClosedHandle: function () {
+    this.setState({show: false});
+  },
+
   render: function () {
+    var windowClasses = classNames({
+      'shopping-bag-window': true,
+      'animate': this.state.show
+    });
+
     return (
       <div className="shopping-bag-container">
-        <div className="commands-shopping-bag"></div>
+        <div className="commands-shopping-bag" onClick={this.bagOpenHandle}></div>
+        <div className={windowClasses}>
+          <div className="shopping-bag-header">
+            <div className="shopping-bag-header-close">
+              <img src="/assets/wedding-atelier/close.svg" onClick={this.bagClosedHandle}></img>
+            </div>
+            <div className="shopping-bag-header-title">
+              <em>Your</em> cart
+            </div>
+            <div className="shopping-bag-header-checkout-link">
+              <a href={this.props.checkoutUrl || '#'}>check out</a>
+            </div>
+          </div>
+          <div className="shopping-bag-content">
+            <p className="shopping-bag-content-statement">
+              <span className="free-shipping">Free shipping</span> to the US, Canada, and the UK within 3-4 weeks. Easy exchanges within 30 days.
+            </p>
+          </div>
+          <button className="shopping-bag-continue-payment btn-black">
+            continue to payment
+          </button>
+        </div>
       </div>
     );
   }
