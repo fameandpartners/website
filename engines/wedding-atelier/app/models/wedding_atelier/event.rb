978abdcc212e733dcc717221ecf0e872e6ee868c
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-01-26 21:40:36 -0600

    [WED-51] Aadd relation between invitations and events

--- engines/wedding-atelier/app/models/wedding_atelier/event.rb
+++ engines/wedding-atelier/app/models/wedding_atelier/event.rb
@@ -5,6 +5,7 @@ module WeddingAtelier
     has_many :event_assistants, class_name: 'WeddingAtelier::EventAssistant'
     has_many :assistants, through: :event_assistants, source: :user
     has_many :dresses, class_name: 'WeddingAtelier::EventDress'
+    has_many :invitations
 
     resourcify :event_roles, role_cname: 'WeddingAtelier::EventRole'
 
