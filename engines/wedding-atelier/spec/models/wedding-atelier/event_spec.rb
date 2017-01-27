9a75d1273dedd3195d6b8723599a54182d732683
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-01-27 13:54:21 -0600

    Add case insetiveness to event name validation

--- engines/wedding-atelier/spec/models/wedding-atelier/event_spec.rb
+++ engines/wedding-atelier/spec/models/wedding-atelier/event_spec.rb
@@ -35,6 +35,35 @@ describe WeddingAtelier::Event do
         expect(event.date.year).to eq 1989
       end
     end
+  end
 
+  describe 'validates uniqueness of name' do
+    context 'with different names' do
+      it 'saves the event' do
+        create(:wedding_atelier_event, name: 'test')
+        event = build(:wedding_atelier_event, name: 'test1')
+        expect(event.save).to be_truthy
+      end
+    end
+
+    context 'with same names' do
+      context 'when case sensitive' do
+        it 'should not allow events with the same name' do
+          create(:wedding_atelier_event, name: 'test')
+          event = build(:wedding_atelier_event, name: 'TeSt')
+          expect(event.save).to be_falsey
+          expect(event.errors.messages[:name][0]).to eq 'has already been taken'
+        end
+      end
+
+      context 'when all downcase letters' do
+        it 'should not allow events with the same name' do
+          create(:wedding_atelier_event, name: 'test')
+          event = build(:wedding_atelier_event, name: 'test')
+          expect(event.save).to be_falsey
+          expect(event.errors.messages[:name][0]).to eq 'has already been taken'
+        end
+      end
+    end
   end
 end
