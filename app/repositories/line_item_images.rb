9263c47f95f4afb177bafd1d5898152d1f489c92
Tiago Amaro <tiagoa@fameandpartners.com>
2017-01-29 16:45:13 +0100

    specs (kind of) for line item repository

--- app/repositories/line_item_images.rb
+++ app/repositories/line_item_images.rb
@@ -11,7 +11,8 @@ module Repositories
     # @return [Repositories::Images::Template]
     def read(color_id: nil, cropped: true)
       if product_from_wedding_atelier?
-        image_for_wedding_atelier
+        wedding_attrs = image_for_wedding_atelier
+        Images::Template.new(wedding_attrs)
       else
         image_ostruct = Repositories::ProductImages.new(product: product).read(color_id: color_id, cropped: cropped)
         Images::Template.new(image_ostruct)
@@ -20,6 +21,7 @@ module Repositories
 
     private
 
+    # TODO: related to "primitive obsession" reported at the `WeddingAtelier::EventDress` model. This conversion shouldn't be here
     def image_for_wedding_atelier
       customization_values = line_item.personalization.customization_values
       images = WeddingAtelier::EventDress.new({
@@ -31,12 +33,12 @@ module Repositories
         length_id:  customization_values.by_type(:length).first&.id
       }).images
 
-      Images::Template.new(
+      {
         original: images.dig(:real, :large)&.first,
         large:    images.dig(:front, :normal),
         xlarge:   images.dig(:front, :large),
         small:    images.dig(:front, :moodboard)
-      )
+      }
     end
 
     def product
