93e8cb15c819d7c050ffefe4281ae88a020696c8
Tiago Amaro <tiagoa@fameandpartners.com>
2017-01-30 16:28:26 +0100

    sign in process do not require authentication

--- engines/wedding-atelier/app/controllers/wedding_atelier/sessions_controller.rb
+++ engines/wedding-atelier/app/controllers/wedding_atelier/sessions_controller.rb
@@ -5,14 +5,20 @@ module WeddingAtelier
     layout 'wedding_atelier/application'
     helper WeddingAtelier::Engine.helpers
 
+    skip_before_filter :require_no_authentication
+
     def new
       @user = Spree::User.new
+
       if params[:invitation_id]
         invitation = Invitation.find(params[:invitation_id])
         @signup_params = { invitation_id: params[:invitation_id] }
         @user.email = invitation.user_email if invitation
       end
-      super
+
+      if current_spree_user
+        redirect_to wedding_atelier.events_path
+      end
     end
 
     def create
@@ -22,7 +28,8 @@ module WeddingAtelier
           invitation.accept
         end
       end
-      super
+
+      redirect_to wedding_atelier.events_path
     end
   end
 end
