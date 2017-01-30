d7b3bf9eec123265e02a3063a8f0b81e2bff5c5d
Nickolay Kondratenko <devmarkup@gmail.com>
2017-01-30 14:21:50 +0200

    fix item return model specs

--- engines/admin_ui/spec/models/item_return_spec.rb
+++ engines/admin_ui/spec/models/item_return_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe ItemReturn do
-  it { is_expected.to validate_presence_of(:order_paid_currency) }
+  describe 'validation' do
+    describe 'order_paid_currency' do
+      before { allow(subject).to receive(:set_currency_from_line_item) }
+
+      it { is_expected.to validate_presence_of :order_paid_currency }
+    end
+  end
 end
