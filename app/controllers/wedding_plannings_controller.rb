ba2209183afa151afa58817f3c74812d4a3bc093
Tiago Amaro <tiagoa@fameandpartners.com>
2017-02-07 15:23:47 -0800

    pass non-nil values + http status response for errors

--- app/controllers/wedding_plannings_controller.rb
+++ app/controllers/wedding_plannings_controller.rb
@@ -3,11 +3,11 @@ class WeddingPlanningsController < ApplicationController
 
   def create
     @wedding_planning = Forms::WeddingPlanning.new(WeddingConsultation.new)
-    if @wedding_planning.validate(params[:forms_wedding_planning])
+    if @wedding_planning.validate(params)
       WeddingConsultationMailer.email(@wedding_planning).deliver
       render json: { success: true }
     else
-      render json: { errors: @wedding_planning.errors }
+      render json: { errors: @wedding_planning.errors }, status: :unprocessable_entity
     end
   end
 end
