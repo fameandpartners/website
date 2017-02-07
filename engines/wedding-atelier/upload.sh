9391c194b98fdc578b6ab6cd798562b9e524ecaa
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-02-07 16:16:50 -0600

    change set-x for set -v

--- engines/wedding-atelier/upload.sh
+++ engines/wedding-atelier/upload.sh
@@ -10,39 +10,30 @@ for (( i=0;i<$ELEMENTS;i++)); do
     small="280(x|X)404$"
     medium="900(x|X)1300$"
     large="1800(x|X)2600$"
+    set -v
     if [[ "$name" =~ $grey ]]; then
       echo "Starting upload for $name"
-      set -x
       aws s3 sync $name/. s3://fandp-web-qa1/wedding-atelier/dresses/180x260/grey/ --acl=public-read
-      set +x
       echo "Upload done for $name"
     fi
     if [[ "$name" =~ $white ]]; then
       echo "Starting upload for $name"
-      set -x
       aws s3 sync $name/. s3://fandp-web-qa1/wedding-atelier/dresses/180x260/white/ --acl=public-read
-      set +x
       echo "Upload done for $name"
     fi
     if [[ "$name" =~ $small ]]; then
       echo "Starting upload for $name"
-      set -x
       aws s3 sync $name/. s3://fandp-web-qa1/wedding-atelier/dresses/280x404/ --acl=public-read
-      set +x
       echo "Upload done for $name"
     fi
     if [[ "$name" =~ $medium ]]; then
       echo "Starting upload for $name"
-      set -x
       aws s3 sync $name/. s3://fandp-web-qa1/wedding-atelier/dresses/900x1300/ --acl=public-read
-      set +x
       echo "Upload done for $name"
     fi
     if [[ "$name" =~ $large ]]; then
       echo "Starting upload for $name"
-      set -x
       aws s3 sync $name/. s3://fandp-web-qa1/wedding-atelier/dresses/1800x2600/ --acl=public-read
-      set +x
       echo "Upload done for $name"
     fi
   done
