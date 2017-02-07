8d2c4ce2163f99f08a7807080aafec488ceef2ae
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-02-07 10:10:58 -0600

    [WED-256] Simplify if logic

--- engines/wedding-atelier/app/controllers/wedding_atelier/assistants_controller.rb
+++ engines/wedding-atelier/app/controllers/wedding_atelier/assistants_controller.rb
@@ -4,8 +4,7 @@ module WeddingAtelier
     def destroy
       event = WeddingAtelier::Event.find(params[:event_id])
       event_assistant = event.event_assistants.where(user_id: params[:id]).first
-      if event_assistant
-        event_assistant.destroy
+      if event_assistant.try(:destroy)
         head :ok
       else
         render json: { errors: "Couldn't find board member" }, status: :not_found
