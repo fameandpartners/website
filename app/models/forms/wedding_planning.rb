e123f97646c38e436717085b846d7b1ba85c884f
elgrecode <elgrecode@gmail.com>
2017-02-08 14:50:17 -0800

    [WEBSITE-1311] sends correct values

--- app/models/forms/wedding_planning.rb
+++ app/models/forms/wedding_planning.rb
@@ -27,7 +27,6 @@ module Forms
     validates :email,
               format: { with: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i }
 
-
     def wedding_date_cannot_be_in_the_past
       if wedding_date.present? && Date.strptime(wedding_date, '%m/%d/%Y') < Date.today
         errors.add(:wedding_date, "Wedding date can't be in the past")
