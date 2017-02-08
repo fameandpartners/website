a1c6bd38cfcd599cdc175b6a7c0e4a554d509a20
Mario Gomez <mariogomezmtz@gmail.com>
2017-02-08 16:28:34 -0600

    [FIX][WED-267] Restrict past dates from registrations date-picker

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/registrations/details.js
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/registrations/details.js
@@ -53,7 +53,8 @@ $(document).ready(function() {
   $('.registrations__details-form .input-group').datepicker({
     format: "mm/dd/yyyy",
     autoclose: true,
-    showOnFocus: true
+    showOnFocus: true,
+    startDate: moment().format('mm/dd/YYYY')
   }).on('show', function(e){
     $(this).addClass('active');
   }).on('hide', function(e){
