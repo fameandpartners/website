7576bb2ce3c3b143101b0c269721597c9b693c1d
elgrecode <elgrecode@gmail.com>
2017-02-13 17:39:54 -0800

    [WEBSITE-1311] sends mailer if "should_contact"

--- app/controllers/wedding_plannings_controller.rb
+++ app/controllers/wedding_plannings_controller.rb
@@ -5,7 +5,7 @@ class WeddingPlanningsController < ApplicationController
     @wedding_planning = Forms::WeddingPlanning.new(WeddingPlanning.new)
     if @wedding_planning.validate(params)
       @wedding_planning.save
-      # WeddingConsultationMailer.email(@wedding_planning).deliver
+      WeddingConsultationMailer.email(@wedding_planning).deliver if params[:should_contact]
       render json: { success: true }
     else
       render json: { errors: @wedding_planning.errors }, status: :unprocessable_entity
