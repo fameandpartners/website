38931d238a101a1a6fad7556ebadff86aceeabf9
Nickolay Kondratenko <devmarkup@gmail.com>
2017-02-14 15:25:03 +0200

    use max taxon period for product delivery period

--- app/policies/product_delivery_period_policy.rb
+++ app/policies/product_delivery_period_policy.rb
@@ -13,19 +13,19 @@ module Policies
       elsif @product.fast_making?
         FAST_MAKING_DELIVERY_PERIOD
       else
-        minimum_delivery_period
+        maximum_delivery_period
       end
     end
 
-    # Min delivery period got from taxons
-    def minimum_delivery_period
+    # Max delivery period got from taxons
+    def maximum_delivery_period
       return Spree::Taxon::DELIVERY_PERIODS.first unless @product.taxons.any?
 
-      @product.taxons.inject(@product.taxons.first.delivery_period) do |min_period, taxon|
+      @product.taxons.inject(@product.taxons.first.delivery_period) do |max_period, taxon|
         current_major_value = major_value_from_period(taxon.delivery_period)
-        min_major_value = major_value_from_period(min_period)
+        max_major_value = major_value_from_period(max_period)
 
-        current_major_value < min_major_value ? taxon.delivery_period : min_period
+        current_major_value > max_major_value ? taxon.delivery_period : max_period
       end
     end
 
