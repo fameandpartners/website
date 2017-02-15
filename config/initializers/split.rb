86552b711fb137e1a82121374c0ef0be22dd9240
elgrecode <elgrecode@gmail.com>
2017-02-15 15:05:26 -0800

    [WEBSITE-1387] password protect split dashboard page

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -7,3 +7,12 @@ Split.configure do |config|
     }
   }
 end
+
+# Apps without activesupport
+Split::Dashboard.use Rack::Auth::Basic do |username, password|
+  # Protect against timing attacks:
+  # - Use & (do not use &&) so that it doesn't short circuit.
+  # - Use digests to stop length information leaking
+  Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SPLIT_USERNAME"])) &
+    Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SPLIT_PASSWORD"]))
+end
