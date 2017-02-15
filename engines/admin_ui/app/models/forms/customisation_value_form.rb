4ab919dc88e61331525f4705cd27c7fbfdef44b5
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-02-15 17:22:13 -0600

    [WED-258] Change POV to point of view

--- engines/admin_ui/app/models/forms/customisation_value_form.rb
+++ engines/admin_ui/app/models/forms/customisation_value_form.rb
@@ -6,13 +6,13 @@ module Forms
     property :presentation
     property :price
     property :customisation_type
-    property :pov
+    property :point_of_view
     property :product_id
     property :image
 
     validates :presentation, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 0 }
-    validates :pov, inclusion: %w(front back), allow_nil: true, allow_blank: true
+    validates :point_of_view, inclusion: %w(front back), allow_nil: true, allow_blank: true
     validates_uniqueness_of :presentation
     validates_uniqueness_of :name
   end
