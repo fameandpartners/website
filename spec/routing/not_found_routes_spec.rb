62843b23da4bf39cb2e8ca1f5fd0042a618d3743
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2017-02-16 18:11:21 -0800

    Fix specs

--- spec/routing/not_found_routes_spec.rb
+++ spec/routing/not_found_routes_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 describe 'Not found requests', type: :request do
 
-  describe 'with *.php' do
-    it 'routes to application#non_matching_request' do
+  describe 'routes to application#non_matching_request' do
+    before(:each) do
+      allow(Rails.application.config).to receive(:consider_all_requests_local).and_return(false)
+    end
+
+    it 'with *.php' do
       get '/abc.php'
 
       expect(response).to render_template(nil)
       expect(response).to have_http_status(406)
     end
-  end
 
-  describe 'with any non-existent page' do
-    it 'rotes to application#non_matching_request' do
+    it 'with any non-existent page' do
       get '/abc'
 
       expect(response.body).to match("this page doesn't exist")
@@ -20,10 +22,8 @@ describe 'Not found requests', type: :request do
       expect(response.body).to match('Fame &amp; Partners. All rights reserved.')
       expect(response).to have_http_status(404)
     end
-  end
 
-  describe 'with an undefined page' do
-    it 'rotes to application#non_matching_request' do
+    it 'with an undefined page' do
       get '/undefined'
 
       expect(response.body).to match('Not Found')
