7082c1b51e625b249593af053d2a480c2046a9f1
Alberto Renteria Correa <sejeb2001@hotmail.com>
2017-02-17 14:16:28 +1100

    Add DB Migrate file

---
+++ db/migrate/20170217005814_create_thanks_bride_landing_page.rb
@@ -0,0 +1,28 @@
+class CreateThanksBrideLandingPage < ActiveRecord::Migration
+  # All values MUST be Strings!
+  private def landing_page_properties
+    {
+      path:             '/thanks-bride',
+      template_path:    '/statics/landing_page_thanks_bride',
+      heading:          'Thanks for shopping with Fame',
+      title:            'Thanks for shopping with Fame',
+      meta_description: 'Now that you have your women-empowering, planet-saving, custom-made garment, what\'s next?',
+    }
+  end
+
+  def up
+    unless Revolution::Page.where(path: landing_page_properties[:path]).exists?
+      page = Revolution::Page.create!(
+        path:          landing_page_properties[:path],
+        template_path: landing_page_properties[:template_path],
+        publish_from:  1.day.ago
+      )
+      page.translations.create!(locale: 'en-US', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+      page.translations.create!(locale: 'en-AU', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+    end
+  end
+
+  def down
+    Revolution::Page.where(path: landing_page_properties[:path]).delete_all
+  end
+end
