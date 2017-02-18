d8680a2f6e6b66ea2d5a2d17ae342cb281e5304a
Tiago Amaro <tiagoa@fameandpartners.com>
2017-02-17 17:22:21 -0800

    add image and product URL to line item GTM object

--- app/models/marketing/gtm/presenter/line_item.rb
+++ app/models/marketing/gtm/presenter/line_item.rb
@@ -4,6 +4,9 @@ module Marketing
       class LineItem < Base
         extend Forwardable
 
+        include Rails.application.routes.url_helpers
+        include PathBuildersHelper
+
         attr_reader :line_item
         def_delegators :@line_item, :quantity
 
@@ -31,6 +34,10 @@ module Marketing
           product.name
         end
 
+        def product_description
+          product.description
+        end
+
         def category
           product.taxons.first.try(:name)
         end
@@ -39,15 +46,27 @@ module Marketing
           line_item.total.to_f
         end
 
+        def image_url
+          repository_image_template = Repositories::LineItemImages.new(line_item: line_item).read
+          repository_image_template.original
+        end
+
+        def product_url
+          collection_product_path(product)
+        end
+
         def body
           {
-              category:     category,
-              name:         product_name,
-              quantity:     quantity,
-              total_amount: total_amount,
-              sku:          sku,
-              variant_sku:  variant_sku,
-              product_sku:  product_sku
+            category:     category,
+            name:         product_name,
+            quantity:     quantity,
+            total_amount: total_amount,
+            sku:          sku,
+            variant_sku:  variant_sku,
+            product_sku:  product_sku,
+            description:  product_description,
+            image_url:    image_url,
+            product_url:  product_url,
           }
         end
 
