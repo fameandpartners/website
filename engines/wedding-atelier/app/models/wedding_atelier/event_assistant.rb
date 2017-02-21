1e80b68e1083adc852adb5498e89501d2c5c7c6c
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-02-20 18:19:20 -0600

    [WED-292] DEstroy previously accepted invitation when removing users from an event

--- engines/wedding-atelier/app/models/wedding_atelier/event_assistant.rb
+++ engines/wedding-atelier/app/models/wedding_atelier/event_assistant.rb
@@ -2,5 +2,14 @@ module WeddingAtelier
   class EventAssistant < ActiveRecord::Base
     belongs_to :user, class_name: Spree.user_class.name
     belongs_to :event, class_name: 'WeddingAtelier::Event'
+
+    after_destroy :destroy_invitation
+
+    private
+
+    def destroy_invitation
+      invitation = event.invitations.where(user_email: user.email).first
+      invitation.destroy if invitation
+    end
   end
 end
