c9c3999989c95d398550f66e703939aca359b35d
Alberto Renteria Correa <sejeb2001@hotmail.com>
2017-02-22 18:25:13 +1100

    Add Migrate file Regular Thank you page

---
+++ db/migrate/20170222071204_create_regular_thank_you_landing_page.rb
@@ -0,0 +1,28 @@
+class CreateRegularThankYouLandingPage < ActiveRecord::Migration
+  # All values MUST be Strings!
+  private def landing_page_properties
+    {
+      path:             '/thank-you',
+      template_path:    '/statics/landing_page_regular_thank_you',
+      heading:          '[TODO]',
+      title:            '[TODO]',
+      meta_description: '[TODO]',
+    }
+  end
+
+  def up
+    unless Revolution::Page.where(path: landing_page_properties[:path]).exists?
+      page = Revolution::Page.create!(
+        path:          landing_page_properties[:path],
+        template_path: landing_page_properties[:template_path],
+        publish_from:  1.day.ago
+      )
+      page.translations.create!(locale: 'en-US', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+      page.translations.create!(locale: 'en-AU', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+    end
+  end
+
+  def down
+    Revolution::Page.where(path: landing_page_properties[:path]).delete_all
+  end
+end
