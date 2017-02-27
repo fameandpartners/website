d97b3db1b830864935e241e5a7887f7dc4068abd
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-02-27 13:09:03 -0600

    Add test for saving dresses

--- engines/wedding-atelier/spec/features/customization_experience/create_dress_spec.rb
+++ engines/wedding-atelier/spec/features/customization_experience/create_dress_spec.rb
@@ -15,8 +15,31 @@ describe 'Create new custom dress', type: :feature, js: true do
   context 'before leaving screen' do
     it 'saves the dress' do
       sign_in_with(user.email, user.password)
+      dress_count = WeddingAtelier::EventDress.count
       find(:css, '.add-dress-box .add').click
       expect(page).to have_content 'Customize and make it yours.'
+      find(:css, '.customizations #style').click
+      find(:css, '#S1').click
+      find(:css, '.customization-experience--desktop .save-dress-button').click
+      expect(page).to have_content 'Your dress has been saved!'
+      expect(WeddingAtelier::EventDress.count).to be > dress_count
+    end
+  end
+
+  context 'trying to leave screen' do
+    it 'saves the dress' do
+      sign_in_with(user.email, user.password)
+      dress_count = WeddingAtelier::EventDress.count
+      find(:css, '.add-dress-box .add').click
+      expect(page).to have_content 'Customize and make it yours.'
+      find(:css, '.customizations #style').click
+      find(:css, '#S1').click
+      find(:css, '.js-customizations-container .selector-close').click
+      find(:css, '.customization-experience-header-back-arrow').click
+      expect(page).to have_content 'like to save this dress customization to the wedding moodboard?'
+      find(:css, '.js-save-dress-before-leave-modal .save-dress-button').click
+      sleep 5
+      expect(WeddingAtelier::EventDress.count).to be > dress_count
     end
   end
 end
