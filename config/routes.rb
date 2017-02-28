f0a788db6fcbcde6d4ccae7c3ecb750c4976f785
Alberto Renteria Correa <sejeb2001@hotmail.com>
2017-02-28 22:15:12 +1100

    Add route new form validator

--- config/routes.rb
+++ config/routes.rb
@@ -397,6 +397,9 @@ FameAndPartners::Application.routes.draw do
     resource 'wedding-consultation', as: 'wedding_consultation', only: [:create]
     resource 'wedding-planning', as: 'wedding_planning', only: [:create]
 
+    get '/micro-influencer' => 'micro_influencer#new', as: :micro_influencer
+    resource 'micro-influencer', as: 'micro_influencer', only: [:create]
+
     get '/contact/new', to: redirect('/contact'), as: :old_contact_page
     resource 'contact', as: 'contact', only: [:new, :create], path_names: { new: '/' } do
       get 'success'
