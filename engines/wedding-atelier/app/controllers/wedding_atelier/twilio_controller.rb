32939af7822db5bad30c0d0a1385b0bac6af18be
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-03-16 16:19:47 -0600

    Make identity the same for fame and partners users

--- engines/wedding-atelier/app/controllers/wedding_atelier/twilio_controller.rb
+++ engines/wedding-atelier/app/controllers/wedding_atelier/twilio_controller.rb
@@ -10,7 +10,7 @@ module WeddingAtelier
       token = get_token
       grant = get_grant
       token.add_grant(grant)
-      render json: {username: spree_current_user.full_name, token: token.to_jwt}
+      render json: { username: identity, token: token.to_jwt }
     end
 
     private
@@ -20,15 +20,20 @@ module WeddingAtelier
         ENV['TWILIO_API_KEY'],
         ENV['TWILIO_API_SECRET'],
         3600,
-        spree_current_user.full_name
+        identity
       )
     end
 
     def get_grant
       grant = Twilio::Util::AccessToken::IpMessagingGrant.new
-      grant.endpoint_id = "Chatty:#{spree_current_user.full_name.gsub(" ", "_")}:browser"
+      grant.endpoint_id = "Chatty:#{identity.gsub(" ", "_")}:browser"
       grant.service_sid = ENV['TWILIO_IPM_SERVICE_SID']
       grant
+    end
+
+    private
+    def identity
+      current_spree_user.email =~ /^.*@fameandpartners.com$/ ? 'Amber (Fame Stylist)' : current_spree_user.full_name
     end
   end
 end
