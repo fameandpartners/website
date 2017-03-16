0c1965313370c34f74dba223f69abc56b272d805
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-03-16 10:59:26 -0600

    Remove non sense code :derp:

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/registrations/invitations.js
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/registrations/invitations.js
@@ -1,17 +1,13 @@
 (function () {
 
   $(document).ready(function() {
-    var $invitation = $('.js-invitation:last').clone();
-    $invitation.find('label').remove();
-    $invitation.find('input').val('');
-
     function addAnother(e) {
       var invitationsCount = $('.js-invitations .js-invitation').length,
-          invitation = $invitation.clone();
+          $invitation = $('.js-invitation:last').clone();
 
-      invitation.find('input').attr('id', 'email_address_' + invitationsCount);
-      $('.js-invitations>.js-invitation:last')
-        .after(invitation);
+      $invitation.find('input').attr('id', 'email_address_' + invitationsCount);
+      $('.js-invitations .js-invitation:last')
+        .after($invitation);
     }
 
     $('.js-add-another a').bind('click', addAnother);
