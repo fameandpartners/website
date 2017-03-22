6bfe339dad8e94bd1822094d0192d67592e44983
Douglas Sellers <doug@fitorb.com>
2017-03-22 10:50:18 -0700

    Added in the spec for the redirected search term model

--- app/models/redirected_search_term.rb
+++ app/models/redirected_search_term.rb
@@ -2,7 +2,6 @@ class RedirectedSearchTerm < ActiveRecord::Base
   attr_accessible :redirect_to, :term
   validates_presence_of :term
   validates_uniqueness_of :term
-  
   validates_presence_of :redirect_to
 
   before_validation :downcase_term
--- spec/models/redirected_search_term_spec.rb
+++ spec/models/redirected_search_term_spec.rb
@@ -1,5 +1,32 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe RedirectedSearchTerm, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe RedirectedSearchTerm do
+  it "should not allow the same term to be saved twice" do
+    first =  RedirectedSearchTerm.new( {term: 'prom', redirect_to: '/dresses' })
+    expect( first.save ).to be_truthy
+
+    second =  RedirectedSearchTerm.new( {term: 'prom', redirect_to: '/dresses' })
+    expect( second.save ).to be_falsey
+    expect( second.errors.empty? ).to be_falsey
+  end
+
+  it "should always save the lowercase version of the term" do
+    first =  RedirectedSearchTerm.new( {term: 'Prom', redirect_to: '/dresses' })
+    expect( first.save ).to be_truthy
+
+    expect(first.term).to eq('prom')
+  end
+
+  it "should always require a redirect_to" do
+    first =  RedirectedSearchTerm.new( {term: 'Prom', redirect_to: '' })
+    expect( first.save ).to be_falsey
+  end
+
+  it "should always require a term" do
+    first =  RedirectedSearchTerm.new( {term: '', redirect_to: '/dresses' })
+    expect( first.save ).to be_falsey
+  end
+  
+  
+  
 end
