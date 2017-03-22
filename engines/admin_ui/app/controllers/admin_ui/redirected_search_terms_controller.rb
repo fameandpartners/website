5c4ac2afe6f308bdc69d0590c92e4355e36a6152
Douglas Sellers <doug@fitorb.com>
2017-03-22 11:07:14 -0700

    Got delete working

--- engines/admin_ui/app/controllers/admin_ui/redirected_search_terms_controller.rb
+++ engines/admin_ui/app/controllers/admin_ui/redirected_search_terms_controller.rb
@@ -14,6 +14,20 @@ module AdminUi
       end
       index
     end
+
+    def destroy
+      search_term = RedirectedSearchTerm.find( params[:id] )
+      if( search_term )
+        if( search_term.destroy )
+          flash[:success] = 'Redirect Removed'
+        else
+          flash[:error] = search_term.errors.full_messages.to_sentence
+        end
+      else
+        flash[:error] = "Unknown Redirect to Delete"
+      end
+      redirect_to action: :index
+    end
     
   end
 end
