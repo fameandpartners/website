09e67a710025b8c12691c53ed5b807d0f1d60a93
Douglas Sellers <doug@fitorb.com>
2017-03-22 09:32:40 -0700

    Made it so that now you can add a search term redirect

--- engines/admin_ui/app/controllers/admin_ui/redirected_search_terms_controller.rb
+++ engines/admin_ui/app/controllers/admin_ui/redirected_search_terms_controller.rb
@@ -2,6 +2,14 @@ module AdminUi
   class RedirectedSearchTermsController < AdminUi::ApplicationController
     def index
       @search_terms = RedirectedSearchTerm.all
+      render :index      
     end
+
+    def create
+      search_term = RedirectedSearchTerm.new( params[:redirected_search_term] )
+      search_term.save
+      index
+    end
+    
   end
 end
