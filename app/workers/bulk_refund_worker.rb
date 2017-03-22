b7922ed8fb412c19a28467ca73add9e43f38901b
Nickolay Kondratenko <devmarkup@gmail.com>
2017-03-22 12:54:35 +0200

    add bulk refund worker

---
+++ app/workers/bulk_refund_worker.rb
@@ -0,0 +1,32 @@
+class BulkRefundWorker
+  include Sidekiq::Worker
+
+  def perform(user)
+    @user = user
+
+    events = item_returns.map do |item_return|
+      refund(item_return)
+    end
+
+    binding.pry
+  end
+
+  private
+
+  # Need to memoize this list to avoid unprocessed items at email report.
+  def item_returns
+    @item_returns ||= ItemReturn.refund_queue.
+                                 where('line_item_id IS NOT NULL')
+  end
+
+  # TODO: probably we need to implement more efficient refund calculation.
+  def refund_amount_for(item_return)
+    item_return.line_item.price
+  end
+
+  def refund(item_return)
+    item_return.events.refund.create(user: @user,
+                                     refund_amount: refund_amount_for(item_return),
+                                     refund_method: 'Pin')
+  end
+end
