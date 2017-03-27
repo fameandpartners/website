0b543d6bab0f3026236e66cf5ce3f18cd566e9a9
Douglas Sellers <doug@fitorb.com>
2017-03-26 23:13:45 -0700

    Update the dress taxon with the correct products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170324225539) do
+ActiveRecord::Schema.define(:version => 20170326053740) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
