0295d670534042ab89e1ccd614dcee724fafb8cc
Adam Schuld <elgrecode@gmail.com>
2017-03-28 16:49:01 -0700

    [g2ioi] now fix test by using hash without marshap_dump

--- app/presenters/user_cart/cart_product_presenter.rb
+++ app/presenters/user_cart/cart_product_presenter.rb
@@ -7,7 +7,7 @@ module UserCart
       result[:discount] = discount if discount.present?
       result[:image] = image.marshal_dump if image.present?
       result[:size] = size.marshal_dump if size.present?
-      result[:color] = color.marshal_dump if color.present?
+      result[:color] = color if color.present?
 
       result[:customizations] = (customizations || []).map do |t|
         { id: t.id, name: t.name, display_price: t.display_price.to_s }
