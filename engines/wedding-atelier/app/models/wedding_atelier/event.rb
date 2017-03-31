05c7640957024526289f9b0fd88f36edb45825dc
Gustavo Robles <gustavo.robles@magmalabs.io>
2017-03-31 09:27:36 -0600

    undo changes to event model

--- engines/wedding-atelier/app/models/wedding_atelier/event.rb
+++ engines/wedding-atelier/app/models/wedding_atelier/event.rb
@@ -3,9 +3,7 @@ module WeddingAtelier
     belongs_to :owner, class_name: 'Spree::User'
 
     has_many :event_assistants, class_name: 'WeddingAtelier::EventAssistant'
-    has_many :assistants,
-              through: :event_assistants,
-              source: :user
+    has_many :assistants, through: :event_assistants, source: :user
     has_many :dresses, class_name: 'WeddingAtelier::EventDress'
     has_many :invitations
 
