26c5aa8c47ba0b6c1cfe491aa90d755d0daaefaa
Thanh Truong <thanht@fameandpartners.com>
2017-04-06 16:14:38 -0700

    refuse to giveup

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,9 +4,10 @@
 # end
 
 Raven.configure do |config|
-  config.dsn             = "https://#{ENV['SENTRY_PUBLIC']}:#{ENV['SENTRY_SECRET']}@sentry.io/156022"
+  # config.dsn             = "https://#{ENV['SENTRY_PUBLIC']}:#{ENV['SENTRY_SECRET']}@sentry.io/156022"
+  config.dsn             = 'https://6d51d1f989a44c33ad1b7e362f6b124e:b62fad1d2b3d4b998fced7da13d266e0@sentry.io/156022'
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments    = %w(staging production)
+  config.environments    = ['staging']
 end
 
 
