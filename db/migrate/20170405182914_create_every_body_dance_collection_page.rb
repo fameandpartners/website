a4dc5f431c7ee4b1c35371517a7fd9ffe6c27f76
Mateus Moraes <mateusm@fameandpartners.com>
2017-04-06 15:15:27 -0300

    update URL (lowercase) in migration file

--- db/migrate/20170405182914_create_every_body_dance_collection_page.rb
+++ db/migrate/20170405182914_create_every_body_dance_collection_page.rb
@@ -2,7 +2,7 @@ class CreateEveryBodyDanceCollectionPage < ActiveRecord::Migration
   # All values MUST be Strings!
   private def landing_page_properties
     {
-      path:             '/every-BODY-dance',
+      path:             '/every-body-dance',
       template_path:    '/products/collections/show',
       pids:             %w(1362-black 706-black 1338-black-and-white-gingham 1348-navy 1344-black 630-ivory 916-black 1336-navy 1336-pale-blue 1287-black 1348-dark-tan 916-pale-blue 1135-black 493-black 916-dark-nude 1367-pink-and-white-gingham 1375-black 1377-red 1369-black 1328-pretty-pink 1378-black 916-ivory 1341-burgundy 1358-black 968-burgundy 1370-navy 1356-black 1346-navy 1332-pale-pink 968-black 1363-black 1356-dark-tan 1377-pale-pink 1363-white 1329-black-and-white 909-black 1131-champagne 1346-dusty-rose 1286-navy 1330-black 1089-navy).join(','),
       heading:          '#everyBODYdance',
