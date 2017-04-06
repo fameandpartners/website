0e6f5173042f47fde4ea03d68a48776993fd2fa7
Thanh Truong <thanht@fameandpartners.com>
2017-04-06 13:33:37 -0700

    modify for sentry and new relic

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,12 @@
-Raven.configure do |config|
-  config.dsn             = "https://#{ENV['SENTRY_PUBLIC']}:#{ENV['SENTRY_SECRET']}@sentry.io/68181"
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments    = %w(production)
+app_id = '68181'	#production
+if Rails.env.staging?
+  app_id = '156022' #staging, qa1, qa2
 end
+
+Raven.configure do |config|
+  config.dsn             = "https://#{ENV['SENTRY_PUBLIC']}:#{ENV['SENTRY_SECRET']}@sentry.io/#{app_id}"
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.environments    = %w(staging production)
+end
+
+https://6d51d1f989a44c33ad1b7e362f6b124e:b62fad1d2b3d4b998fced7da13d266e0@sentry.io/156022
