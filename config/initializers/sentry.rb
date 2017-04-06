6c607ab81eebcf70f9b694d7dc1aa76532b07e01
Thanh Truong <thanht@fameandpartners.com>
2017-04-06 15:26:40 -0700

    run staging test

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,10 @@
-app_id = '68181'	#production
-if Rails.env.staging?
-  app_id = '156022' #staging, qa1, qa2
-end
+# app_id = '68181'	#production
+# if Rails.env.staging?
+#   app_id = '156022' #staging, qa1, qa2
+# end
 
 Raven.configure do |config|
-  config.dsn             = "https://#{ENV['SENTRY_PUBLIC']}:#{ENV['SENTRY_SECRET']}@sentry.io/#{app_id}"
+  config.dsn             = "https://#{ENV['SENTRY_PUBLIC']}:#{ENV['SENTRY_SECRET']}@sentry.io/156022"
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.environments    = %w(staging production)
 end
