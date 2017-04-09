39e97ea3742606b5fffb7a626b7e3fc3ccd20552
Adam Schuld <elgrecode@gmail.com>
2017-04-08 17:51:20 -0700

    [6xfc3] add to bag from Size Profile and decouple opening of drawer

--- app/assets/javascripts/storefront/actions/PdpActions.js
+++ app/assets/javascripts/storefront/actions/PdpActions.js
@@ -1,15 +1,26 @@
-import {defaultMakingOption} from '../components/PDP/models/MakingOption';
+import { defaultMakingOption } from '../components/PDP/models/MakingOption';
 
 export function customizeDress(customize) {
   return { type: 'CUSTOMIZE_DRESS', customize };
 }
 
+export function toggleDrawer(drawerName) {
+  return { type: 'TOGGLE_PDP_DRAWER', drawerName };
+}
+
+export function addToBagPending(isPending) {
+  return {
+    type: 'ADD_TO_BAG_PENDING',
+    addToBagPending: isPending,
+  };
+}
+
 export function customizeMakingOption(customize) {
   // TODO: this rule is duplicated at the SidePanelFastMaking
-  let isCustomColor = customize.color && customize.color.price > 0;
+  const isCustomColor = customize.color && customize.color.price > 0;
 
   if (isCustomColor) {
-    let makingOption = Object.assign({}, defaultMakingOption, {error: true});
+    const makingOption = Object.assign({}, defaultMakingOption, { error: true });
     customize.makingOption = makingOption;
   }
 
