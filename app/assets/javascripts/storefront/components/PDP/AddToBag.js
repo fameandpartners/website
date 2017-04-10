e83a65801f9efa35503c658bec5c69583ee65a71
Adam Schuld <elgrecode@gmail.com>
2017-04-10 11:07:29 -0700

    [6xfc3] save and add to bag

--- app/assets/javascripts/storefront/components/PDP/AddToBag.js
+++ app/assets/javascripts/storefront/components/PDP/AddToBag.js
@@ -179,4 +179,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddToBag);
+export default connect(mapStateToProps, mapDispatchToProps, null, { withRef: true })(AddToBag);
