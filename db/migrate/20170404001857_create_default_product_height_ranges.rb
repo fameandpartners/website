4dd5446d4e695f4871801a16c097ac05752a512c
Douglas Sellers <doug@fitorb.com>
2017-04-11 21:48:52 -1000

    Updated the migration

--- db/migrate/20170404001857_create_default_product_height_ranges.rb
+++ db/migrate/20170404001857_create_default_product_height_ranges.rb
@@ -1,8 +1,6 @@
 class CreateDefaultProductHeightRanges < ActiveRecord::Migration
   def up
-    metric_six = ProductHeightRangeGroup.new( name: 'default_six_size_metric_group', unit: 'cm' )
-    metric_six.save
-
+    metric_six = ProductHeightRangeGroup.create( name: 'default_six_size_metric_group', unit: 'cm' )
     metric_six.product_height_ranges << ProductHeightRange.new( min: 0,   max: 156, unit: 'cm', map_to: 'length1' )
     metric_six.product_height_ranges << ProductHeightRange.new( min: 157, max: 164, unit: 'cm', map_to: 'length2' )
     metric_six.product_height_ranges << ProductHeightRange.new( min: 165, max: 172, unit: 'cm', map_to: 'length3' )
@@ -11,15 +9,13 @@ class CreateDefaultProductHeightRanges < ActiveRecord::Migration
     metric_six.product_height_ranges << ProductHeightRange.new( min: 187, max: 999, unit: 'cm', map_to: 'length6' )
 
 
-    metric_three = ProductHeightRangeGroup.new( name: 'default_three_size_metric_group', unit: 'cm' )
-    metric_three.save
+    metric_three = ProductHeightRangeGroup.create( name: 'default_three_size_metric_group', unit: 'cm' )
     metric_three.product_height_ranges << ProductHeightRange.new( min: 0,   max: 165, unit: 'cm', map_to: 'petite' )
     metric_three.product_height_ranges << ProductHeightRange.new( min: 166, max: 176, unit: 'cm', map_to: 'standard' )
     metric_three.product_height_ranges << ProductHeightRange.new( min: 177, max: 999, unit: 'cm', map_to: 'tall' )
     
     
-    english_six = ProductHeightRangeGroup.new( name: 'default_six_size_english_group', unit: 'inch' )
-    english_six.save
+    english_six = ProductHeightRangeGroup.create( name: 'default_six_size_english_group', unit: 'inch' )
     english_six.product_height_ranges << ProductHeightRange.new( min: 0,   max: 61,  unit: 'inch', map_to: 'length1' )
     english_six.product_height_ranges << ProductHeightRange.new( min: 62,  max: 64,  unit: 'inch', map_to: 'length2' )
     english_six.product_height_ranges << ProductHeightRange.new( min: 65,  max: 67,  unit: 'inch', map_to: 'length3' )
@@ -27,8 +23,7 @@ class CreateDefaultProductHeightRanges < ActiveRecord::Migration
     english_six.product_height_ranges << ProductHeightRange.new( min: 71,  max: 73,  unit: 'inch', map_to: 'length5' )
     english_six.product_height_ranges << ProductHeightRange.new( min: 74,  max: 999, unit: 'inch', map_to: 'length6' )
 
-    english_three = ProductHeightRangeGroup.new( name: 'default_three_size_english_group', unit: 'inch' )
-    english_three.save
+    english_three = ProductHeightRangeGroup.create( name: 'default_three_size_english_group', unit: 'inch' )
     english_three.product_height_ranges << ProductHeightRange.new( min: 0,   max: 65,   unit: 'inch', map_to: 'petite' )
     english_three.product_height_ranges << ProductHeightRange.new( min: 66,  max: 69,   unit: 'inch', map_to: 'standard' )
     english_three.product_height_ranges << ProductHeightRange.new( min: 70,  max: 999,  unit: 'inch', map_to: 'tall' )
@@ -36,18 +31,8 @@ class CreateDefaultProductHeightRanges < ActiveRecord::Migration
   end
 
   def down
-    delete_if_exists( 'default_six_size_metric_group' )
-    delete_if_exists( 'default_three_size_metric_group' )
-    delete_if_exists( 'default_six_size_english_group' )
-    delete_if_exists( 'default_three_size_english_group' )
+    names = %w(default_six_size_metric_group default_three_size_metric_group default_six_size_english_group default_three_size_english_group)
+    ProductHeightRangeGroup.where(name: names).destroy_all    
   end
-
-  private
-
-  def delete_if_exists( name )
-    group = ProductHeightRangeGroup.find_by_name( name )
-    group.delete if group.present?
-  end
-  
   
 end
