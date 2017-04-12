4a633ae413abbff4914ea65105fd6413eb5b5c9e
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2017-04-12 18:20:05 +0300

    Provide missing steps for acceptance specs

--- spec/support/steps/product_steps.rb
+++ spec/support/steps/product_steps.rb
@@ -2,18 +2,18 @@ module Acceptance
   module ProductSteps
     step 'I select :dress_size size' do |dress_size|
       # a with no href can't be accessed through click_link
-      click_a_with_no_href( 'Size Profile')
+      click_a_with_no_href('Size Profile')
       sleep 0.2 # User interacting with sidebar + animation
-      click_a_with_no_href( dress_size)
+      click_a_with_no_href(dress_size)
       sleep 0.2 # User interacting with sidebar + animation
-      expect(page).to have_selector('.pdp-side-menu', visible: false)
+      expect(page).to have_selector('.pdp-side-menu', visible: true)
     end
 
     step 'I select :skirt_length skirt length' do |skirt_length|
       sleep 0.2 # User interacting with sidebar + animation
-      fill_in 'height-option-cm', :with => skirt_length.downcase
+      find('.height-selection input').set(skirt_length.downcase)
       sleep 0.2 # User interacting with sidebar + animation
-      expect(page).to have_selector('.pdp-side-menu', visible: false)
+      expect(page).to have_selector('.pdp-side-menu', visible: true)
     end
 
     step 'I select the :color_name color' do |color_name|
@@ -27,7 +27,7 @@ module Acceptance
     step 'I save the profile' do
       find(:css,'.size-selection .btn-black').click
     end
-    
+
     step 'I select the express making option checkbox' do
       checkbox = find(:css, '.pdp-side-container-fast-making label')
       checkbox.click
@@ -41,15 +41,15 @@ module Acceptance
       add_to_bag_link = find("a", :text => "ADD TO BAG")
       expect(add_to_bag_link['class']).not_to have_text('btn-lowlight')
     end
- 
+
     step "I add to bag" do
-      find("a", :text => "ADD TO BAG").click      
+      find("a", :text => "ADD TO BAG").click
     end
-    
+
     def click_a_with_no_href( text )
-      find("a", :text => text).click      
+      find("a", :text => text).click
     end
-    
+
   end
 end
 
