041f80845f4d9a3bddb22d8bf2905140d607595a
Douglas Sellers <doug@fitorb.com>
2017-04-11 21:56:18 -1000

    Made it so that we use activerecord more effectively

--- app/models/style_to_product_height_range_group.rb
+++ app/models/style_to_product_height_range_group.rb
@@ -11,10 +11,11 @@ class StyleToProductHeightRangeGroup < ActiveRecord::Base
   end
 
   def self.map_height_values_to_height_name( variant, height_value, height_units )
-    height_groups = self.find_both_for_variant_or_use_default( variant ).find { |product_range_group| product_range_group.unit == height_units }
-
-    height_range = height_groups.product_height_ranges.where( "min <= ? and max >= ?", height_value.to_i, height_value.to_i ).first
-    height_range.map_to
+    find_both_for_variant_or_use_default(variant)
+      .where(unit: height_units)
+      .product_height_ranges.where('min <= ? AND max >= ?', height_value.to_i, height_value.to_i)
+      .pluck(:map_to)
+      .first
   end
   
   
