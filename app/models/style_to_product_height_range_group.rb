af62a7926e495a2a5d8ec3bf7c03a7403f13d40e
Douglas Sellers <doug@fitorb.com>
2017-04-12 00:01:32 -1000

    Made it so that this works again

--- app/models/style_to_product_height_range_group.rb
+++ app/models/style_to_product_height_range_group.rb
@@ -11,11 +11,8 @@ class StyleToProductHeightRangeGroup < ActiveRecord::Base
   end
 
   def self.map_height_values_to_height_name( variant, height_value, height_units )
-    find_both_for_variant_or_use_default(variant)
-      .where(unit: height_units)
-      .product_height_ranges.where('min <= ? AND max >= ?', height_value.to_i, height_value.to_i)
-      .pluck(:map_to)
-      .first
+    height_groups = find_both_for_variant_or_use_default(variant).where(unit: height_units).first
+    height_groups.product_height_ranges.where( "min <= ? and max >= ?", height_value.to_i, height_value.to_i ).pluck(:map_to).first
   end
   
   
