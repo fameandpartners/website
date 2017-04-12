1d743805e53c4fe991e940105e888ea9f767fa4e
Mateus Moraes <mateusm@fameandpartners.com>
2017-04-12 19:53:07 -0300

    add pids list to migration file

--- db/migrate/20170410043635_create_shop_every_body_dance_page.rb
+++ db/migrate/20170410043635_create_shop_every_body_dance_page.rb
@@ -4,7 +4,7 @@ class CreateShopEveryBodyDancePage < ActiveRecord::Migration
     {
       path:             '/shop-every-body-dance',
       template_path:    '/statics/landing_page_shop_every_body_dance',
-      pids:             %w().join(','),
+      pids:             %w(1362-black 630-ivory 1348-navy 1287-black 1363-black 1285-black 1120-black 1356-black 1359-silver 1354-silver 1324-silver 1342-silver 1377-red 930-black 1332-pale-pink 1392-red 1340-black 1343-silver 1378-black 1345-warm-tan 968-black 1341-burgundy 1357-black 1370-navy).join(','),
       heading:          '#everyBODYdance',
       title:            'Body-Positive Evening Dresses and Gowns',
       limit:            '99',
