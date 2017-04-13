cab65fb0836dd53ac0e213c7b06e68ce200b2048
Mateus Moraes <mateusm@fameandpartners.com>
2017-04-13 01:15:17 -0300

    remove remainder “equal pay day” conditions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -189,10 +189,4 @@ module ApplicationHelper
     flash_classes.fetch(flash_type, flash_type.to_s)
   end
 
-  def equal_pay_active?
-    date_start = DateTime.parse('Apr 3 9:00pm -7:00')
-    date_end  = DateTime.parse('Apr 5 12:00pm -7:00')
-    current_site_version.is_usa? && Time.zone.now.between?(date_start, date_end)
-  end
-
 end
