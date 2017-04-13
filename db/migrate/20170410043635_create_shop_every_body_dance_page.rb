73bb1fd7b0ebe3e0d0acb70a8a940e35d1f738fb
Mateus Moraes <mateusm@fameandpartners.com>
2017-04-12 21:17:14 -0300

    remove “unless…” condition from migration file

--- db/migrate/20170410043635_create_shop_every_body_dance_page.rb
+++ db/migrate/20170410043635_create_shop_every_body_dance_page.rb
@@ -13,16 +13,14 @@ class CreateShopEveryBodyDancePage < ActiveRecord::Migration
   end
 
   def up
-    unless Revolution::Page.where(path: landing_page_properties[:path]).exists?
-      page = Revolution::Page.create!(
-        path:          landing_page_properties[:path],
-        template_path: landing_page_properties[:template_path],
-        variables:     { lookbook: true, limit: landing_page_properties[:limit], pids: landing_page_properties[:pids] },
-        publish_from:  1.day.ago
-      )
-      page.translations.create!(locale: 'en-US', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
-      page.translations.create!(locale: 'en-AU', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
-    end
+    page = Revolution::Page.create!(
+      path:          landing_page_properties[:path],
+      template_path: landing_page_properties[:template_path],
+      variables:     { lookbook: true, limit: landing_page_properties[:limit], pids: landing_page_properties[:pids] },
+      publish_from:  1.day.ago
+    )
+    page.translations.create!(locale: 'en-US', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
+    page.translations.create!(locale: 'en-AU', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
   end
 
   def down
