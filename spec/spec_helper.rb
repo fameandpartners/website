b383bae962c8be2f2d9981497a21f2b6418f0ae7
Thanh Truong <thanht@fameandpartners.com>
2017-04-13 16:15:48 -0700

    adjust knapsack

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ require 'knapsack'
 
 # Rails.application.railties.all { |r| r.eager_load! }
 
-knapsack::Adapters::RSpecAdapter.bind
+Knapsack::Adapters::RSpecAdapter.bind
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
