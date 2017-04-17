91428ded67b705110e8ec67d885058ef418b90cd
Mateus Moraes <mateusm@fameandpartners.com>
2017-04-17 18:03:12 -0300

    move custom video player JS as a local function

--- app/assets/javascripts/application_redesign.js
+++ app/assets/javascripts/application_redesign.js
@@ -78,7 +78,6 @@
 //= require partials/helpers/collapse_toggle
 //= require partials/helpers/form_submit_ajax_validation
 //= require partials/helpers/anchor_navigation
-//= require partials/helpers/custom_video_player
 
 //-----------------------------------
 // Show old quiz in popup with iframe
