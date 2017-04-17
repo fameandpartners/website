d4fcde6370c593b17d2a232020a17eaeb5fccc73
Thanh Truong <thanht@fameandpartners.com>
2017-04-17 11:08:22 -0700

    tweak logic

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,10 +4,10 @@
 # end
 
 Raven.configure do |config|
-  app_id = '68181'  #production
+  app_id = '156022' #devs
 
-  if Rails.env.staging?
-    app_id = '156022'
+  if Rails.env.production?
+    app_id = '68181'  #production
   end
   config.dsn             = "https://#{ENV['SENTRY_PUBLIC']}:#{ENV['SENTRY_SECRET']}@sentry.io/#{app_id}"
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
