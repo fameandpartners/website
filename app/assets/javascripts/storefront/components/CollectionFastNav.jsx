1313fa7acfb08acafc677876f1537746728d9614
Nav Samra <navsamra@Navs-MacBook-Pro.local>
2017-04-21 10:49:12 -0700

    add temp data to test data structure for links in FastNav component

--- app/assets/javascripts/storefront/components/CollectionFastNav.jsx
+++ app/assets/javascripts/storefront/components/CollectionFastNav.jsx
@@ -1,7 +1,11 @@
 import React, {Component, PropTypes,} from 'react';
+import sampleData from './tempData/sampleData.js';
 
 export default class CollectionFastNav extends Component {
   render() {
+
+    console.log(sampleData);
+
     return (
       <div className="CollectionFastNav">
         <div className="FilterSort">
---
+++ app/assets/javascripts/storefront/components/tempData/sampleData.js
@@ -0,0 +1,101 @@
+export default {
+  'categories': [
+    {
+      'dresses': {
+        'displayName': 'Dresses',
+        'path': '/dresses',
+        'categoryLength': [
+          {
+            'maxi': {
+              'count': 22,
+              'displayName': 'Maxi',
+              'path': '/long'  // relative
+            }
+          }, {
+            'midi': {
+              'count': 10,
+              'displayName': 'Midi',
+              'path': '/midi'
+            }
+          }, {
+            'mini': {
+              'count': 44,
+              'displayName': 'Mini',
+              'path': '/mini'
+            }
+          }, {
+            'twoPiece': {
+              'count': 22,
+              'displayName': 'Two Piece',
+              'path': '/two-piece'
+            }
+          }
+        ],
+        'categoryEvent': [
+          {
+            'prom': {
+              'count': 10,
+              'displayName': 'Prom',
+              'path': '/prom'
+            }
+          }, {
+            'bridal': {
+              'count': 22,
+              'displayName': 'Bridal',
+              'path': '/bridal'
+            }
+          }, {
+            'bridesmaid': {
+              'count': 43,
+              'displayName': 'Bridesmaid',
+              'path': '/bridesmaid'
+            }
+          }, {
+            'wedding': {
+              'count': 32,
+              'displayName': 'Wedding Guests',
+              'path': '/wedding-guests'
+            }
+          }, {
+            'cocktail': {
+              'count': 34,
+              'displayName': 'Cocktail',
+              'path': '/cocktail'
+            }
+          }, {
+            'graduation': {
+              'count': 56,
+              'displayName': 'Graduation',
+              'path': '/graduation'
+            }
+          }
+        ]
+      }
+    }, {
+      'skirts': {
+        'displayName': 'Skirts',
+        'path': '/skirts'
+      }
+    }, {
+      'jumpsuits': {
+        'displayName': 'Jumpsuits & Rompers',
+        'path': '/dresses/jumpsuit' // actual path on live...
+      }
+    }, {
+      'tops': {
+        'displayName': 'Tops',
+        'path': '/tops'
+      }
+    }, {
+      'pants': {
+        'displayName': 'Pants',
+        'path': '/pants'
+      }
+    }, {
+      'outerwear': {
+        'displayName': 'Outerwear & Jackets',
+        'path': '/outerwear-and-jackets'
+      }
+    }
+  ]
+};
