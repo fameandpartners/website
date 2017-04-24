71bda350dfea1e55328c4a902cf9fcd4ab274abe
Nav Samra <navsamra@Navs-MacBook-Pro.local>
2017-04-23 22:11:36 -0700

    update data structure (again, but should be final / most flexible)

--- app/assets/javascripts/storefront/components/tempData/sampleData.js
+++ app/assets/javascripts/storefront/components/tempData/sampleData.js
@@ -4,60 +4,67 @@ export default {
       'id': 'dresses',
       'displayName': 'Dresses',
       'relativePath': '/dresses',
-      'lengthSubcategories': [
+      'subCategories': [
         {
-          'id': 'maxi',
-          'count': 22,
-          'displayName': 'Maxi',
-          'relativePath': '/long'
+          'label': null,
+          'subItems': [
+            {
+              'id': 'maxi',
+              'count': 22,
+              'displayName': 'Maxi',
+              'relativePath': '/long'
+            }, {
+              'id': 'midi',
+              'count': 10,
+              'displayName': 'Midi',
+              'relativePath': '/midi'
+            }, {
+              'id': 'mini',
+              'count': 44,
+              'displayName': 'Mini',
+              'relativePath': '/mini'
+            }, {
+              'id': 'twoPiece',
+              'count': 22,
+              'displayName': 'Two Piece',
+              'relativePath': '/two-piece'
+            }
+          ]
         }, {
-          'id': 'midi',
-          'count': 10,
-          'displayName': 'Midi',
-          'relativePath': '/midi'
-        }, {
-          'id': 'mini',
-          'count': 44,
-          'displayName': 'Mini',
-          'relativePath': '/mini'
-        }, {
-          'id': 'twoPiece',
-          'count': 22,
-          'displayName': 'Two Piece',
-          'relativePath': '/two-piece'
-        }
-      ],
-      'eventSubcategories': [
-        {
-          'id': 'prom',
-          'count': 10,
-          'displayName': 'Prom',
-          'relativePath': '/prom'
-        }, {
-          'id': 'bridal',
-          'count': 22,
-          'displayName': 'Bridal',
-          'relativePath': '/bridal'
-        }, {
-          'id': 'bridesmaid',
-          'count': 43,
-          'displayName': 'Bridesmaid',
-          'relativePath': '/bridesmaid'
-        }, {
-          'id': 'wedding',
-          'count': 32,
-          'displayName': 'Wedding Guests',
-          'relativePath': '/wedding-guests'
-        }, {
-          'id': 'cocktail',
-          'count': 34,
-          'displayName': 'Cocktail',
-          'relativePath': '/cocktail'
-        }, {
-          'id': 'graduation',
-          'count': 56,
-          'displayName': 'Graduation',
-          'relativePath': '/graduation'
+          'label': 'Event',
+          'subItems': [
+            {
+              'id': 'prom',
+              'count': 10,
+              'displayName': 'Prom',
+              'relativePath': '/prom'
+            }, {
+              'id': 'bridal',
+              'count': 22,
+              'displayName': 'Bridal',
+              'relativePath': '/bridal'
+            }, {
+              'id': 'bridesmaid',
+              'count': 43,
+              'displayName': 'Bridesmaid',
+              'relativePath': '/bridesmaid'
+            }, {
+              'id': 'wedding',
+              'count': 32,
+              'displayName': 'Wedding Guests',
+              'relativePath': '/wedding-guests'
+            }, {
+              'id': 'cocktail',
+              'count': 34,
+              'displayName': 'Cocktail',
+              'relativePath': '/cocktail'
+            }, {
+              'id': 'graduation',
+              'count': 56,
+              'displayName': 'Graduation',
+              'relativePath': '/graduation'
+            }
+          ]
         }
       ]
     }, {
