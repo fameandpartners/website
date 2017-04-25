b6b8cec81e509be429825c3afcec5d8d8444f6b8
Thanh Truong <thanht@fameandpartners.com>
2017-04-24 19:24:30 -0700

    fix broken test

--- app/services/products/selection_options.rb
+++ app/services/products/selection_options.rb
@@ -9,7 +9,7 @@ module Products
     def initialize(options = {})
       @product      = options[:product]
       #put the layers in order for processing
-      @product.layer_cads.sort!{|layer| layer.position}.reverse!
+      @product.layer_cads&.sort!{|layer| layer.position}.reverse!
       @site_version = options[:site_version] || SiteVersion.default
     end
 
