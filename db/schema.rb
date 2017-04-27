a33a77c5305351da0b8c58bb31ad043f6a526f0c
Alberto Renteria Correa <sejeb2001@hotmail.com>
2017-04-28 09:24:43 +1000

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
-ActiveRecord::Schema.define(:version => 20170425023634) do
 
+ActiveRecord::Schema.define(:version => 20170427231827) do
 
   create_table "activities", :force => true do |t|
     t.string   "action"
