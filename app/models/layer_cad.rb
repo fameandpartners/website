9158bd267c0a99d184febcdb6eae0897b162ce95
Douglas Sellers <doug@fitorb.com>
2017-05-01 15:07:55 -0700

    Tried to fix the resizing issue

--- app/models/layer_cad.rb
+++ app/models/layer_cad.rb
@@ -6,24 +6,25 @@ class LayerCad < ActiveRecord::Base
   
   has_attached_file :base_image,
                     :styles => lambda { |a|
+    puts "#{a.instance.width}x#{a.instance.height}#"
                                            { :original => "944x800",
-                                             :web => "-gravity Center -crop #{a.instance.width}x#{a.instance.height}+0+0 +repage" }
+                                             :web => "#{a.instance.width}x#{a.instance.height}#"
+                                           }
                     },
                     :default_style => :web,
                     :url => '/spree/products/:product_id/cads/:id/:style/:basename.:extension',
                     :path => 'spree/products/:product_id/cads/:id/:style/:basename.:extension',
-                    :convert_options => { :all => '-strip -auto-orient' }
-
+                    :convert_options => { :web => '-quality 100 -strip -auto-orient -gravity Center' }
   has_attached_file :layer_image,
                     :styles => lambda { |a|
    
                                            { :original => "944x800",
-                                             :web => "-gravity Center -crop #{a.instance.width}x#{a.instance.height}+0+0 +repage" }
+                                             :web => "#{a.instance.width}x#{a.instance.height}#" }
                      },
                     :default_style => :web,
                     :url => '/spree/products/:product_id/cads/:id/:style/:basename.:extension',
                     :path => 'spree/products/:product_id/cads/:id/:style/:basename.:extension',
-                    :convert_options => { :all => '-strip -auto-orient' }
+                    :convert_options => { :all => '-quality 100 -strip -auto-orient -gravity Center' }
   before_save :rename_files
 
   def rename_files
