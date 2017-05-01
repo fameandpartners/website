dc0284b46c59dbd4c580536de31e7b672199b983
Thanh Truong <thanht@fameandpartners.com>
2017-05-01 10:08:18 -0700

    started rewrite of search query

---
+++ app/services/search/color_variants_es_query.rb
@@ -0,0 +1,95 @@
+module Search
+  class ColorVariantsESQuery
+    require 'elasticsearch/dsl'
+    include Elasticsearch::DSL
+
+    def self.build(options = {})
+      options = HashWithIndifferentAccess.new(options)
+
+
+      # some kind of documentation
+      colors            = options[:color_ids]
+      body_shapes       = options[:body_shapes]
+      taxons            = options[:taxon_ids]
+      exclude_products  = options[:exclude_products]
+      discount          = options[:discount]
+      query_string      = options[:query_string]
+      order             = options[:order]
+      fast_making       = options[:fast_making]
+      limit             = options[:limit].present? ? options[:limit].to_i : 1000
+      offset            = options[:offset].present? ? options[:offset].to_i : 0
+      price_min         = Array.wrap(options[:price_min]).map(&:to_f)
+      price_max         = Array.wrap(options[:price_max]).map(&:to_f)
+      currency          = options[:currency]
+      show_outerwear    = !!options[:show_outerwear]
+      exclude_taxon_ids = options[:exclude_taxon_ids] if query_string.blank?
+
+      product_orderings = self.product_orderings(currency: currency)
+
+
+      product_ordering = product_orderings.fetch(order) do
+        if query_string.present?
+          # Do not apply ordering for searches, let ES order by term relevance.
+          product_orderings['native']
+        else
+          product_orderings['newest']
+        end
+      end
+      binding.pry
+      hash = Elasticsearch::DSL::Search.search do
+        query do
+          bool do
+            must do
+              term 'product.is_deleted' => false
+              term 'product.is_hidden' => false
+            end
+            #should do
+            #  range 'product.available_on' =>
+            #end
+          end
+        end
+      end
+    end
+
+    def self.product_orderings(currency: nil)
+      ProductOrdering.new(currency: currency).all
+    end
+
+    class ProductOrdering
+      attr_accessor :all, :currency
+
+      def initialize(currency: nil)
+        self.currency = currency
+        self.all = {}
+        add_orders
+      end
+
+      def add(name, description, behaviour)
+        all.update(name => {
+          description: description,
+          behaviour: behaviour
+        })
+      end
+
+      def all_as_collection
+        all.map { |key, value| [value[:description], key]  }
+      end
+
+      private
+
+      def add_orders
+        price_behaviour_field = currency.present? ? "prices.#{currency}" : 'product.price'
+
+        add('price_high',     "Price: High to Low",            [price_behaviour_field, 'desc'])
+        add('price_low',      "Price: Low to High",            [price_behaviour_field, 'asc'])
+        add('newest',         "Newest First",                  ['product.created_at', 'desc'])
+        add('oldest',         "Oldest First",                  ['product.created_at', 'asc'])
+        add('fast_delivery',  "Show Fast Delivery First",      ['product.fast_delivery', 'desc'])
+        add('best_sellers',   "Best Sellers",                  ['product.total_sales', 'desc'])
+        add('alpha_asc',      "Name: A-Z",                     ['product.name', 'asc'])
+        add('alpha_desc',     "Name: Z-A",                     ['product.name', 'desc'])
+        add('native',         "Do Not Apply Ordering",         nil)
+      end
+    end
+  end
+end
