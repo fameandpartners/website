038aad638add6e41ed2e1d732e1473cd46000a52
Adam Schuld <elgrecode@gmail.com>
2017-05-08 16:45:16 -0700

    [vtlal] improve styling and punt to @dorothyc

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -29,7 +29,6 @@ Spree::CheckoutController.class_eval do
     end
 
     current_order.updater.update_totals
-binding.pry
     # li_presenters = @order.line_items.map {|li| Orders::LineItemPresenter.new(li)}
     # @order.line_item_presenters = li_presenters
 
