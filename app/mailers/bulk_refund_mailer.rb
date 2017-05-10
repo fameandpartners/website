0ebcb285b1ff1c9a422290d2e859b078a50364e7
Nickolay Kondratenko <devmarkup@gmail.com>
2017-05-10 19:12:49 +0300

    fix bulk refund mailer

--- app/mailers/bulk_refund_mailer.rb
+++ app/mailers/bulk_refund_mailer.rb
@@ -8,8 +8,8 @@ class BulkRefundMailer < ActionMailer::Base
       'bulk_refund_report_email',
       email_to:                    email,
       subject:                     subject,
-      success:                     results[:success].map { |r| r['item_return_id'] },
-      fails:                       results[:error].map { |r| [ r['item_return_id'], r['result']['message'] ] }
+      success:                     results[:success].map { |r| r[:item_return_id] },
+      fails:                       results[:error].map { |r| [ r[:item_return_id], r[:result][:message] ] }
     )
   rescue StandardError => e
     NewRelic::Agent.notice_error(e)
