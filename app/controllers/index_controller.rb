a9ab2089c11b31fd1d94224aa812cd0d7a2e97d3
Nav Samra <navsamra@Navs-MacBook-Pro.local>
2017-05-13 01:50:19 -0700

    integrate category tiles / secondary hero from cache
    - category tiles could still be cleaner, probably best / more readable to use previous CSS and apply to iteratees via switch vs. the overly specific mess I've made now (e.g. !important and not easily grokkable :first / :last of types... fml)

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,9 +2,7 @@ class IndexController < ApplicationController
   layout 'redesign/application'
 
   def show
-    # @test_num = load_rando
-    # @home_page_container = home_page_container_from_contentful
-    @instaTiles = get_contentful_parent_container
+    @home_page_container = get_contentful_parent_container
     @banner      = Spree::BannerBox.big_banner.for_site_version(current_site_version).limit(10)
     @title       = [homepage_title, default_seo_title].join(' ')
     @description = default_meta_description
@@ -12,17 +10,6 @@ class IndexController < ApplicationController
                    Spree::Order.where(number: params[:iec_order_number]).first
                  end
   end
-
-  # def home_page_container_from_contentful
-  #   blah = eval(get_contentful_parent_container)
-  #   get_contentful_parent_container.entries(content_type: 'homePageContainer')[0]
-  # end
-
-  # def load_rando
-  #   Rails.cache.fetch('load-rando-cache-key') do
-  #     rand(1...50)
-  #   end
-  # end
 
   private
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -215,9 +215,9 @@ module ApplicationHelper
   # CMS
   def get_contentful_parent_container
     if (params['developer'] == 'preview')
-      puts '*'*30
-      puts 'Contentful - PREVIEW'
-      puts '*'*30
+      puts '*'*60
+      puts 'In PREVIEW MODE - Fetching unpublished Contentful data...'
+      puts '*'*60
       Rails.cache.delete('contentful-cms-cache-key')
       @contentful_client ||= Contentful::Client.new(
         api_url: 'preview.contentful.com',
@@ -228,10 +228,13 @@ module ApplicationHelper
       )
       create_home_page_container_from_contentful(@contentful_client.entries(content_type: 'homePageContainer')[0])
     else
-      puts '-'*30
-      puts 'Contentful - LIVE'
-      puts '-'*30
+      puts '-'*60
+      puts 'In PRODUCTION Mode - Loading cached Contentful data...'
+      puts '-'*60
       Rails.cache.fetch('contentful-cms-cache-key') do
+        puts '/'*60
+        puts 'NO CACHE - Fetching latest published Contentful data...'
+        puts '/'*60
         @contentful_client ||= Contentful::Client.new(
           access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
           space: ENV['CONTENTFUL_SPACE_ID'],
@@ -243,15 +246,35 @@ module ApplicationHelper
     end
   end
 
-  # currently just doing Instagram Tiles (fix, obv.)
-  def create_home_page_container_from_contentful(data)
-    data.instagram_thumbnails_container.map do |item|
+  def create_home_page_container_from_contentful(parent_container)
+    # binding.pry
+    second_hero = {
+      image: parent_container.secondary_header_container.secondary_header_image.url,
+      mobile_image: parent_container.secondary_header_container.secondary_header_mobile_image.url,
+      path_link: parent_container.secondary_header_container.secondary_header_link
+    }
+
+    category_tiles = parent_container.category_tiles_container.map do |item|
+      {
+        link: item.link,
+        title: item.title_overlay,
+        image: item.image.image_url
+      }
+    end
+
+    ig_tiles = parent_container.instagram_thumbnails_container.map do |item|
       {
         path_link: item.path_link,
         handle: item.handle,
         image: item.image.image_url
       }
     end
+
+    @main_container = {
+      secondary_header: second_hero,
+      category_tiles: category_tiles,
+      instagram_tiles: ig_tiles
+    }
   end
 
 end
