d42413318568f2a08187ba106850f491d0855357
Alberto Renteria Correa <sejeb2001@hotmail.com>
2017-05-17 09:26:22 +1000

    Update Page title

--- db/migrate/20170516070445_create_white_trends_page.rb
+++ db/migrate/20170516070445_create_white_trends_page.rb
@@ -5,7 +5,7 @@ class CreateWhiteTrendsPage < ActiveRecord::Migration
       path:             '/trends-white',
       template_path:    '/products/collections/show',
       pids:             %w(766-white 815-white 1076-white 1080-white 1079-white 1077-white 1083-white 1085-white 1087-white 1088-white 1090-white 1141-white 1143-white 1149-white 1155-white 1168-white 1191-white 1170-white 1247-white 1435-white 1263-white 1438-white 1440-white 1441-white 1445-white 1457-white 1464-white 1466-white 1467-white 1476-white 1480-white 1488-white 1500-white).join(','),
-      heading:          'White Trend',
+      heading:          'Shop White',
       title:            'White Dresses, Tops, and Skirts',
       meta_description: 'Shop white dresses, white tops, and white skirts for summer 2017; all customizable and ethically made.',
       limit:            '99'
