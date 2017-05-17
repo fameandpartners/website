3832c5382d8ca13bd05bffa6d4967e6cbe8fb8fa
Mateus Moraes <mateusm@fameandpartners.com>
2017-05-16 21:29:04 -0300

    add banner image path in migration file

--- db/migrate/20170516070445_create_white_trends_page.rb
+++ db/migrate/20170516070445_create_white_trends_page.rb
@@ -8,6 +8,7 @@ class CreateWhiteTrendsPage < ActiveRecord::Migration
       heading:          'Shop White',
       title:            'White Dresses, Tops, and Skirts',
       meta_description: 'Shop white dresses, white tops, and white skirts for summer 2017; all customizable and ethically made.',
+      banner_image_url: 'https://s3.amazonaws.com/fandp-web-production-marketing/lookbook/trends-white/trends-white-banner.jpg',
       limit:            '99'
     }
   end
@@ -16,7 +17,7 @@ class CreateWhiteTrendsPage < ActiveRecord::Migration
     page = Revolution::Page.create!(
       path:          landing_page_properties[:path],
       template_path: landing_page_properties[:template_path],
-      variables:     { lookbook: true, limit: landing_page_properties[:limit], pids: landing_page_properties[:pids], banner_image_url: landing_page_properties[:banner_image_url] },
+      variables:     { lookbook: true, limit: landing_page_properties[:limit], pids: landing_page_properties[:pids], banner_image_url: landing_page_properties[:banner_image_url], banner_image_url: landing_page_properties[:banner_image_url] },
       publish_from:  1.day.ago
     )
     page.translations.create!(locale: 'en-US', title: landing_page_properties[:title], heading: landing_page_properties[:heading], meta_description: landing_page_properties[:meta_description])
