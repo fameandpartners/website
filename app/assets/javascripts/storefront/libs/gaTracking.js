27f3aa63ecf00ece301df364e57469aefa0ad361
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-05-26 14:53:57 -0700

    copied over gaTracking.js from fwxfh-events-pdp

--- app/assets/javascripts/storefront/libs/gaTracking.js
+++ app/assets/javascripts/storefront/libs/gaTracking.js
@@ -9,17 +9,15 @@ const defaultData = {
   nonInteraction: false,
 };
 
-export function trackEvent(eventData) {
+export function trackEvent(eventData, dynamicStatus, dynamicData) {
   if (isGAAvailable()){
+    dynamicStatus ? eventData.label = dynamicData : ''
     const event = assign({}, defaultData, eventData);
-
     ga('send', 'event', {
       eventCategory: event.category,
       eventAction: event.action,
       eventLabel: event.label,
       nonInteraction: event.nonInteraction,
-    });
+    })
   }
-
-
 }
