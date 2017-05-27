dfbd1ada10800cd5e80518dae92768cb6c158379
Douglas Sellers <doug@fitorb.com>
2017-05-26 22:29:59 -0700

    We are able to iterate over all the ads and adsets

--- lib/facebook/facebook_sync.rb
+++ lib/facebook/facebook_sync.rb
@@ -2,9 +2,14 @@ class FacebookSync
   def initialize( key )
     @access_key = key
     @base_uri = 'https://graph.facebook.com/v2.9'
+    @first_request = true
     @next_token = nil
   end
 
+  def has_more_pages?
+    @first_request || !@next_token.nil?
+  end
+  
   def next_page
     response = begin
                  RestClient.get(generate_uri, accept: :json, accept_encoding: :identity)
@@ -14,14 +19,19 @@ class FacebookSync
     results = JSON.parse( response )
     account_array = results['data']
     account = account_array.find { |account| account['name'] == "Fame & Partners #2" }
-    @next_token = account['campaigns']['paging']['cursors']['after']
+    if( account['campaigns'] && account['campaigns']['paging'] && account['campaigns']['paging']['cursors'] && account['campaigns']['paging']['cursors']['after'] )
+      @next_token = account['campaigns']['paging']['cursors']['after']      
+    else
+      @next_token = nil
+    end
+    @first_request = false
     account
   end
 
   private
   
   def generate_uri
-    if( @next_token.nil? )
+    if( @first_request )
       "#{@base_uri}/me/adaccounts?fields=name,account_status,age,amount_spent,currency,campaigns.filtering([{'field':'campaign.effective_status', 'operator':'IN', 'value':['ACTIVE']}]).limit(5){name,created_time,start_time,stop_time,updated_time,status,recommendations,adsets.filtering([{'field':'adset.effective_status', 'operator':'IN', 'value':['ACTIVE']}]){name,adlabels,adset_schedule,bid_amount,daily_budget,created_time,updated_time,start_time,end_time,optimization_goal,status,targeting,ads.filtering([{'field':'ad.effective_status', 'operator':'IN', 'value':['ACTIVE']}]){name,created_time,updated_time,bid_amount,status,recommendations,creative{body,call_to_action_type,image_url,instagram_permalink_url,link_url,name,status,title,video_id},insights.time_range({'since':'2017-05-25','until':'2017-05-25'}).time_increment(1){clicks,cost_per_action_type,cpc,cpm,cpp,ctr,date_start,date_stop,frequency,reach,relevance_score,social_impressions,spend,total_actions,total_unique_actions,website_ctr,website_clicks},adcreatives{image_url}}}}&access_token=#{@access_key}"
     else
       sprintf( "%s/me/adaccounts?fields=name,account_status,age,amount_spent,currency,campaigns.filtering([{'field':'campaign.effective_status', 'operator':'IN', 'value':['ACTIVE']}]).limit(5).after('%s'){name,created_time,start_time,stop_time,updated_time,status,recommendations,adsets.filtering([{'field':'adset.effective_status', 'operator':'IN', 'value':['ACTIVE']}]){name,adlabels,adset_schedule,bid_amount,daily_budget,created_time,updated_time,start_time,end_time,optimization_goal,status,targeting,ads.filtering([{'field':'ad.effective_status', 'operator':'IN', 'value':['ACTIVE']}]){name,created_time,updated_time,bid_amount,status,recommendations,creative{body,call_to_action_type,image_url,instagram_permalink_url,link_url,name,status,title,video_id},insights.time_range({'since':'2017-05-25','until':'2017-05-25'}).time_increment(1){clicks,cost_per_action_type,cpc,cpm,cpp,ctr,date_start,date_stop,frequency,reach,relevance_score,social_impressions,spend,total_actions,total_unique_actions,website_ctr,website_clicks},adcreatives{image_url}}}}&access_token=%s", @base_uri, @next_token, @access_key )
--- lib/tasks/facebook_ads.rake
+++ lib/tasks/facebook_ads.rake
@@ -7,25 +7,16 @@ namespace :facebook_ads do
     task :sync => :environment do
       access_token = "EAAaDM5wsv7sBAG1cPMRDktA1O4yi9XFu7MvoEPqDuSNQljlaYIX829iCxmlPQAx0ao1vaY6dhZCZCUUakj9ES5MNt4uDu0rLzwJdj68YP003kf40sjtKMNTo5T4dDsZBOjxGLqZBMlDVb2DHcNFGxs9GGlMZASFQZD"
       sync = FacebookSync.new( access_token )
-      account = sync.next_page
-      
-      account['campaigns']['data'].each do |campaign|
-        puts "#{campaign['name']}"
-        campaign['adsets']['data'].each do |adset|
-          puts "\t#{adset['name']} has #{adset['ads']['data'].count} ads"
-        end
-      end
 
-      account = sync.next_page
-      
-      account['campaigns']['data'].each do |campaign|
-        puts "#{campaign['name']}"
-        campaign['adsets']['data'].each do |adset|
-          puts "\t#{adset['name']} has #{adset['ads']['data'].count} ads" unless adset['ads'].nil?
-        end unless( campaign.nil? || campaign['adsets'].nil? )
+      while( sync.has_more_pages? ) do
+        account = sync.next_page
+        account['campaigns']['data'].each do |campaign|
+          puts "#{campaign['name']}"
+          campaign['adsets']['data'].each do |adset|
+            puts "\t#{adset['name']} has #{adset['ads']['data'].count} ads" unless adset['ads'].nil?
+          end unless( campaign.nil? || campaign['adsets'].nil? )
+        end if account['campaigns'] && account['campaigns']['data']
       end
-      
     end
   end
 end
-
