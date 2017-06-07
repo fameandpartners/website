0653aeb7e03cf7ce2a5cec2ae37b78f0423dce8d
Adam Schuld <elgrecode@gmail.com>
2017-06-06 18:35:28 -0700

    creates contentful page in admin

---
+++ engines/admin_ui/app/controllers/admin_ui/content/contentful_controller.rb
@@ -0,0 +1,16 @@
+module AdminUi
+  module Content
+    class ContentfulController < ::AdminUi::ApplicationController
+
+      def index
+      end
+
+      def show
+      end
+
+      def create
+      end
+
+    end
+  end
+end
