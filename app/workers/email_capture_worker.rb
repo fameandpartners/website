cc0c7a58ee00b11ec599cda520d15424edb353b7
Nickolay Kondratenko <devmarkup@gmail.com>
2017-06-07 13:02:40 +0300

    subscribe on user registration

--- app/workers/email_capture_worker.rb
+++ app/workers/email_capture_worker.rb
@@ -4,7 +4,7 @@ class EmailCaptureWorker
   # TODO: EmailCapture needs to be replaced with MailChimpClient engine
   def perform(user_id, params)
     user = Spree::User.find(user_id)
-    EmailCapture.new({ service: :mailchimp }, email: user.email,  newsletter: user.newsletter,
+    EmailCapture.new({ service: configatron.subscription_service }, email: user.email,  newsletter: user.newsletter,
                      first_name: user.first_name, last_name: user.last_name,
                      current_sign_in_ip: params['remote_ip'], landing_page: params['landing_page'],
                      utm_params: params['utm_params'], site_version: params['site_version'],
