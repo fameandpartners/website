102d99c4503f3dc7bffb438db1afcad3a4593282
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-06-06 17:43:41 -0700

    Fixed function for trackEvent

--- app/assets/javascripts/storefront/libs/gaTracking.js
+++ app/assets/javascripts/storefront/libs/gaTracking.js
@@ -9,15 +9,9 @@ const defaultData = {
   nonInteraction: false,
 };
 
-<<<<<<< HEAD
-export function trackEvent(eventData, dynamicStatus, dynamicData) {
-  if (isGAAvailable()){
-    dynamicStatus ? eventData.label = dynamicData : ''
-=======
 export function trackEvent(eventData, dynamicLabelStatus, dynamicLabelData) {
   if (isGAAvailable()){
     dynamicLabelStatus ? eventData.label = dynamicLabelData : ''
->>>>>>> 29fce2d770f582ca396f60acf49719abb8fce26d
     const event = assign({}, defaultData, eventData);
     ga('send', 'event', {
       eventCategory: event.category,
@@ -27,8 +21,4 @@ export function trackEvent(eventData, dynamicLabelStatus, dynamicLabelData) {
       nonInteraction: event.nonInteraction,
     })
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 29fce2d770f582ca396f60acf49719abb8fce26d
