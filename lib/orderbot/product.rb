e4f3dfebf3c34d265a131f2ed32e6dd49405315b
Douglas Sellers <doug@fitorb.com>
2017-06-08 12:49:17 -0700

    Cleaned up a bunch of stuff and did the initial generation of the product hash

--- lib/orderbot/product.rb
+++ lib/orderbot/product.rb
@@ -10,16 +10,31 @@ module Orderbot
 
     def self.find_all
       connection = Orderbot::Connection.new
-#      puts connection.get( '/product_structure.json/' )
-#      puts connection.get( '/products.json/' )
+      puts connection.get( '/products.json/' )
     end
-  end
 
-  private
-  def create_product_hash
-    { reference_product_id: spree_product_id.id,
-      group_id: sync }
-     
+    private
+    def create_product_hash
+      return { reference_product_id: @spree_product.id,
+               group_id: @spree_product.orderbot_product_group.group_id,
+               create_bom: false,
+               create_purchase_unit: false,
+               name: @spree_product.name,
+               description: @spree_product.description,
+               sku: @spree_product.sku,
+               base_price: @spree_product.price,
+               units_of_measure: 'each',
+               unit_of_measure_type_id: 1,
+               weight: 8.0,
+               shipping_units_of_measure_type_id: 1,
+               taxable: false,
+               min_quatity: 1,
+               active: true,
+               is_parent: false,
+               upc: @spree_product.id.to_s
+      }
+      
+    end
   end
   
 end
--- lib/tasks/orderbot.rake
+++ lib/tasks/orderbot.rake
@@ -2,11 +2,18 @@ require 'pathname'
 
 namespace :orderbot do
 
+  namespace :product_structure do
+    desc 'Sync all of the product structure / groups to orderbot'
+    task :sync => :environment do
+      orderbot_product = Orderbot::ProductGroup.sinc
+    end
+  end
+  
   namespace :products do
     desc 'Sync all of the products to orderbot'    
     task :sync => :environment do
       orderbot_product = Orderbot::Product.new( 1380 )
-      orderbot_product.sync
+      orderbot_product.create
     end
   end
 
