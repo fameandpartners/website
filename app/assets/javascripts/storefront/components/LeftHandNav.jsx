d2e67258ad6bbf444071fc600533fa41c63b94b8
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-06-19 13:44:50 -0700

    Added a new component for the left hand navigation

---
+++ app/assets/javascripts/storefront/components/LeftHandNav.jsx
@@ -0,0 +1,17 @@
+import React, {Component, PropTypes,} from 'react';
+
+class LeftHandNav extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Brooooooo</h1>
+            </div>
+        );
+    }
+}
+
+export default LeftHandNav
