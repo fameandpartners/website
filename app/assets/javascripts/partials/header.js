c0a0df081046743ec3f1265ea51d47a5abd56d91
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-06-22 14:41:57 -0700

    Extracted sub nav GA logic

--- app/assets/javascripts/partials/header.js
+++ app/assets/javascripts/partials/header.js
@@ -1,5 +1,8 @@
 'use strict';
 (function ($) {
+  function isGAAvailable() {
+    return typeof window === 'object' && !!window.ga;
+  }
   function close_menu() {
     $('#home-menu .nav-main-menu span, .rect').removeClass('active');
   }
@@ -19,12 +22,18 @@
 
 
   $('.js-search-trigger').on('click', function(e) {
-    window.ga('send', 'event', { 
-      eventCategory: 'Top Nav', 
-      eventAction: 'Search Press', 
-      eventLabel: null, 
-      nonInteraction: false 
-    });
+    
+    if(isGAAvailable()) {
+      window.ga('send', 'event', { 
+        eventCategory: 'Top Nav', 
+        eventAction: 'Search Press', 
+        eventLabel: null, 
+        nonInteraction: false 
+      });
+    }
+    else {
+      console.log("Google analytics not found")
+    }
     if ($(this).parent().hasClass('active')) {
       if ($("#searchForm #searchValue").val() !== '') {
         $("#searchForm").submit();
