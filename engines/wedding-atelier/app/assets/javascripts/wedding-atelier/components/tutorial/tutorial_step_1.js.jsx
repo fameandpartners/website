72f0647e84d4ddb8ddf4fb2c2f9d2d1edca20fb4
Mumo Carlos <mumo.crls@gmail.com>
2017-06-25 13:39:44 -0500

    [omhdu] Add Oxford comma

--- engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/tutorial/tutorial_step_1.js.jsx
+++ engines/wedding-atelier/app/assets/javascripts/wedding-atelier/components/tutorial/tutorial_step_1.js.jsx
@@ -8,7 +8,7 @@ var TutorialStep1 = React.createClass({
           <span>STEP 1</span>
         </div>
         <h1 className='text-center'>Customize your dress</h1>
-        <p className='tutorial__description'>It's up to you! Choose your silhouette, fabric, length, style and fit.</p>
+        <p className='tutorial__description'>It's up to you! Choose your silhouette, fabric, length, style, and fit.</p>
         <input className="btn btn-black full-width" name="commit" defaultValue="next" onClick={this.props.nextStep} />
       </div>
     );
