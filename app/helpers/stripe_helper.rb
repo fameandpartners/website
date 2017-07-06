8249f41b722eeb03c1483e3b6f7d36cfa81636b0
Nav Samra <navsamra@me.com>
2017-07-06 12:01:06 -0700

    pull variables out of .env file (since they're public)

--- app/helpers/stripe_helper.rb
+++ app/helpers/stripe_helper.rb
@@ -1,16 +1,22 @@
 module StripeHelper
   def get_stripe_key()
+
+    stripe_public_us_test = 'pk_test_W2TXpgZbnoebFKeDzNW74xhB'
+    stripe_public_us_prod = 'pk_live_MaBBVDshRhR7V9jk4mfB89UE'
+    stripe_public_au_test = 'pk_test_ic3pWUfqR66brmOJ01TrE5Lf'
+    stripe_public_au_prod = 'pk_live_06GugZv2uL6y6kah0Gx7RtFv'
+
     if Rails.env.production?
       if current_site_version.is_australia?
-        current_stripe_key = ENV.fetch('STRIPE_PUBLIC_AU_PROD')
+        current_stripe_key = stripe_public_au_prod
       else
-        current_stripe_key = ENV.fetch('STRIPE_PUBLIC_US_PROD')
+        current_stripe_key = stripe_public_us_prod
       end
     else
       if current_site_version.is_australia?
-        current_stripe_key = ENV.fetch('STRIPE_PUBLIC_AU_TEST')
+        current_stripe_key = stripe_public_au_test
       else
-        current_stripe_key = ENV.fetch('STRIPE_PUBLIC_US_TEST')
+        current_stripe_key = stripe_public_us_test
       end
     end
   end
