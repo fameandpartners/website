aef96c0d03f24cc7a3d45030dc97f3be30188abd
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-07-18 11:16:49 -0700

    Replaced collect.js with lodash

--- app/assets/javascripts/storefront/libs/getOrderArray.js
+++ app/assets/javascripts/storefront/libs/getOrderArray.js
@@ -1,12 +1,11 @@
-import collect from 'collect.js'
+import _ from 'lodash'
 
 export function getOrderArray(products) {
-	const collection = collect(products);
-	const grouped = collection.groupBy('trackingNumber');
-	let trackingNumberArray = Object.keys(grouped.items)
+	const grouped = _.groupBy(products, p => p.trackingNumber);
+	let trackingNumberArray = Object.keys(grouped)
 	let orderArray = []
 	for (var i = trackingNumberArray.length - 1; i >= 0; i--) {
-	  orderArray.push(grouped.items[trackingNumberArray[i]])
+	  orderArray.push(grouped[trackingNumberArray[i]])
 	}
 	return orderArray
 }
