e09203e0c9b07cbf8de3831e61edaaff28517de7
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-07-25 16:34:21 -0700

    Added the API hookup with the form

--- app/assets/javascripts/storefront/components/guest_checkout/GuestReturns.js
+++ app/assets/javascripts/storefront/components/guest_checkout/GuestReturns.js
@@ -1,45 +1,64 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {Link} from 'react-router'
+import { Link } from 'react-router'
 import Button from '../returns/components/Button'
 import autobind from 'auto-bind';
-
+import axios from 'axios';
 class GuestReturnApp extends Component {
   constructor(props) {
     super(props);
     autobind(this);
     this.state = {
-    	guestEmail: '',
-    	guestOrderID: '',
-    	lookupError: false
+      guestEmail: 'notreal@gmail.com',
+      guestOrderID: 'R833073426',
+      lookupError: false
     };
   }
   checkOrder(e) {
-  	e.preventDefault()
-  	const {guestOrderID, guestEmail} = this.state
-  	console.log(guestOrderID, guestEmail)
+    e.preventDefault()
+    const { guestOrderID, guestEmail } = this.state
+    console.log(guestOrderID, guestEmail)
+    const that = this
+    axios({
+        method: 'get',
+        url: `/order-lookup?id=${guestOrderID}&email=${guestEmail}`,
+        headers: {
+          'Accept': 'application/json',
+        }
+      })
+      .then(function(response) {
+        if(response.data.status) {
+        	that.setState({
+        		lookupError: true
+        	})
+        }
+      })
+      .catch(function(error) {
+        console.log("ERROR")
+        console.log(error)
+      })
   }
   validEmail() {
-  	let {guestEmail} = this.state
-  	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
-  	if(guestEmail.match(mailformat)) {
-  		return true
-  	}
-  	return false
+    let { guestEmail } = this.state
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (guestEmail.match(mailformat)) {
+      return true
+    }
+    return false
   }
   updateEmail(event) {
-  	this.setState({
-  		guestEmail: event.target.value
-  	});
+    this.setState({
+      guestEmail: event.target.value
+    });
   }
   updateOrderNumber(event) {
-  	this.setState({
-  		guestOrderID: event.target.value
-  	});
+    this.setState({
+      guestOrderID: event.target.value
+    });
   }
   render() {
-  	const {guestOrderID, guestEmail, lookupError} = this.state
-  	const validEmailAddress = this.validEmail()
+    const { guestOrderID, guestEmail, lookupError } = this.state
+    const validEmailAddress = this.validEmail()
     return (
     	<div className="grid-center-noGutter GuestReturn__Container">
     		<div className="col-4_md-5_sm-10">
@@ -52,8 +71,7 @@ class GuestReturnApp extends Component {
 				</p>
 				<div className={lookupError ? "error-box" : "u-hide"}>
 					<p>
-						Sorry the order number and/or email you entered are incorrect. 
-						Please check them and enter again. 
+						Sorry the order number and/or email you entered are incorrect. Please check them and enter again. 
 					</p>
 				</div>
 				<form className="guestForm__container" onSubmit={this.checkOrder}>
