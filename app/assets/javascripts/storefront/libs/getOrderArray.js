991d27ae8f451b8eb9480b75d4c27d661cc795e2
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-07-31 15:23:18 -0700

    Cleaned up getOrderArray

--- app/assets/javascripts/storefront/libs/getOrderArray.js
+++ app/assets/javascripts/storefront/libs/getOrderArray.js
@@ -1,9 +1,9 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
 export default function getOrderArray(items) {
-	const grouped = _.groupBy(items, p => p.trackingNumber);
-	let trackingNumberArray = Object.keys(grouped)
-	let orderArray = []
-	trackingNumberArray.map(i => orderArray.push(grouped[trackingNumberArray[i]]))
-	return orderArray 
+  const grouped = _.groupBy(items, p => p.trackingNumber);
+  const trackingNumberArray = Object.keys(grouped);
+  const orderArray = [];
+  trackingNumberArray.map(i => orderArray.push(grouped[trackingNumberArray[i]]));
+  return orderArray;
 }
