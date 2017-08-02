23ca951fec557fab71106f3ca05e4a016ed7173c
Nav Samra <navsamra@me.com>
2017-08-02 14:00:31 -0700

    verify presence of token before assigning (fixes test)

--- app/assets/javascripts/storefront/libs/default-csrf.js
+++ app/assets/javascripts/storefront/libs/default-csrf.js
@@ -1,5 +1,5 @@
 /* global document */
-const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+const csrfToken = document.querySelector('meta[name="csrf-token"]') ? document.querySelector('meta[name="csrf-token"]').content : '';
 const axiosDefaults = require('axios/lib/defaults');
 
 axiosDefaults.headers.common['X-CSRF-Token'] = csrfToken;
