869041e60ecd02f5008a834b5e932aabae0586e2
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-08-03 10:05:33 -0700

    Refactoring setReturnWindow.js

--- app/assets/javascripts/storefront/libs/setReturnWindow.js
+++ app/assets/javascripts/storefront/libs/setReturnWindow.js
@@ -1,19 +1,16 @@
 import moment from 'moment';
 
-export default function setReturnWindow(order) {
+export default function setReturnWindow(orders) {
   const newOrderArray = [];
-  order.map((o) => {
-    const { shipDate } = o;
-    const currentDay = moment();
-    const shipDay = moment(new Date(shipDate));
-    const shipDayArray = moment([[shipDay.format('YYYY')][0], [shipDay.format('M')][0], [shipDay.format('D')][0]]);
-    const currentDayArray = moment([[currentDay.format('YYYY')][0], [currentDay.format('M')][0], [currentDay.format('D')][0]]);
-    const returnEligible = currentDayArray.diff(shipDayArray, 'days') < 0;
-    const newOrderObject = Object.assign({}, o, {
+  orders.map((orderData) => {
+    const { spree_order: spreeOrder } = orderData;
+    const { final_return_by_date: lastDayToReturn } = spreeOrder;
+    const returnEligible = moment(new Date(lastDayToReturn)).unix() > moment().unix();
+    const newOrderObject = Object.assign({}, orderData, {
       returnEligible,
     });
     newOrderArray.push(newOrderObject);
   });
-  console.log(newOrderObject);
+  console.log(newOrderArray);
   return newOrderArray;
 }
