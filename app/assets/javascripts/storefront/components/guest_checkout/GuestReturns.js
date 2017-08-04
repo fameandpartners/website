01b718821f23c474559ac4b9297f7743e7a2cf61
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-08-04 11:03:32 -0700

    Added class to button on guestreturn

--- app/assets/javascripts/storefront/components/guest_checkout/GuestReturns.js
+++ app/assets/javascripts/storefront/components/guest_checkout/GuestReturns.js
@@ -75,7 +75,7 @@ class GuestReturnApp extends Component {
               onChange={this.updateEmail}
               value={guestEmail}
             />
-            <input type="submit" value="Return My Order" />
+            <input type="submit" value="Return My Order" className="GuestReturn--submitButton" />
           </form>
           <div className="grid-noGutter-spaceBetween">
             <div className="col-6_sm-12">
