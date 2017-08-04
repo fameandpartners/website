c81c36b87d559870b4d407376a39ed404f48440e
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-08-04 09:56:03 -0700

    Small UI fixes to StepOne

--- app/assets/javascripts/storefront/components/returns/components/EstimatedRefundTotal.js
+++ app/assets/javascripts/storefront/components/returns/components/EstimatedRefundTotal.js
@@ -6,7 +6,7 @@ const propTypes = {
 
 const EstimatedRefundTotal = ({ returnSubtotal }) => (
   <div className="EstimatedRefundTotal">
-    <div className="grid-center">
+    <div className="grid-center u-margin-bottom-medium">
       <div className="col-12_md-4_sm-10">
         <p
           className="font-sans-serif"
@@ -16,13 +16,14 @@ const EstimatedRefundTotal = ({ returnSubtotal }) => (
         <p className="font-sans-serif">
          Shipping: $0.00
         </p>
-        <hr />
       </div>
     </div>
     <div className="grid-right">
       <div className="col-4_md-12_sm-12">
         <p className="font-sans-serif total">
-           Total estimated refund: ${Number(returnSubtotal).toFixed(2) || 0.00}
+          <span className="u-full-length-border-top u-position-relative">
+             Total estimated refund: ${Number(returnSubtotal).toFixed(2) || 0.00}
+          </span>
         </p>
       </div>
     </div>
