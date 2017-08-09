654eeb2cda54077144c9fc441b28729f19ee9a7c
Thanh Truong <thanht@fameandpartners.com>
2017-08-09 11:24:35 -0700

    test is unfixable

--- engines/admin_ui/app/models/item_return_event.rb
+++ engines/admin_ui/app/models/item_return_event.rb
@@ -102,8 +102,9 @@ class ItemReturnEvent < ActiveRecord::Base
     validates :refund_amount,
               presence: true,
               numericality: { less_than_or_equal_to:
+                          #this lambda essentially checks for california tax and adjusts
                           lambda do |event|
-                            tax_adj = event.item_return.line_item.try(:order)&.adjustments&.tax.first
+                            tax_adj = event.item_return.line_item.try(:order)&.adjustments&.tax&.first
                             tax_total = 0
                             if tax_adj
                               tax_rate = Spree::TaxRate.find(tax_adj.originator_id).amount
