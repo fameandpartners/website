3148b3259e0216ff4da5edddbbf3d73fe2298b6a
Nav Samra <navsamra@me.com>
2017-08-14 12:38:18 -0700

    re-enable side menu

--- app/assets/javascripts/application_returns.js
+++ app/assets/javascripts/application_returns.js
@@ -1,7 +1,20 @@
 //= require libs/typekit.min
 
-// Header
+// TO-DO: clean-up
 
+//= require libs/underscore-min
+
+//= require helpers
+//= require partials/helpers/site_version
+//= require partials/product_collection_image_hover.js.coffee
+
+//= require partials/helpers/shopping_cart
+//= require partials/shopping_bag
+//= require partials/email_newsletter_subscriber
+
+
+// Header
 //= require partials/header
 //= require partials/simplified_nav
+//= require partials/side_menu
 //= require application_bundle
