766b7adf55e3d08c97b4d16b5dfb4a8f28f23a2e
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-08-14 16:58:22 -0700

    Merged wit the latest

--- app/mailers/return_mailer.rb
+++ app/mailers/return_mailer.rb
@@ -1,4 +1,5 @@
 class ReturnMailer < ActionMailer::Base
+<<<<<<< HEAD
 
   def convert_height_units(height_value, height_unit)
     if ( !height_value || !height_unit)
@@ -48,6 +49,8 @@ class ReturnMailer < ActionMailer::Base
     }
   end
 
+=======
+>>>>>>> 494717b74a4be4ad9fc24104ab859a2aa0d89773
   def notify_user(order_return_request)
     user_returns_object = create_formatted_order(order_return_request).as_json
     user = order_return_request.order.user
@@ -60,4 +63,56 @@ class ReturnMailer < ActionMailer::Base
     NewRelic::Agent.notice_error(e)
     Raven.capture_exception(e)
   end
+
+  private
+    def convert_height_units(height_value, height_unit)
+      if ( !height_value || !height_unit)
+        return nil
+      end
+      if (height_unit == 'inch')
+        "#{height_value.to_i / 12}ft #{height_value.to_i % 12}in"
+      else
+        "#{height_value}cm"
+      end
+    end
+    
+    def create_formatted_order(return_request)
+      order = return_request.order
+      user = order.user
+      return_items = return_request.return_request_items
+      billing_address = order.billing_address
+      label_print_link = return_items.first.item_return.item_return_label[:label_url]
+      send_by_date = (return_request.order.projected_delivery_date + 45).strftime("%m/%d/%y")
+      formatted_return_items = return_items.map do |item|
+        {
+          name: item.line_item&.product&.name,
+          size: item.line_item&.cart_item&.size&.presentation,
+          color: item.line_item&.cart_item&.color&.presentation,
+          image: item.line_item&.cart_item&.image&.large,
+          price: item.line_item&.product&.price,
+          height_copy: convert_height_units(item.line_item&.personalization&.height_value, item.line_item&.personalization&.height_unit)
+        }
+      end
+
+      # .sum isn't working for some reason (also need to verify this includes tax / discounts...)
+      total_refund_amount = formatted_return_items.reduce(0) { |sum, item| sum + item[:price] }
+      international_user = order.shipping_address&.country_id != 49
+
+      {
+        "order_number": order.number,
+        "email": user.email,
+        "send_by_date": send_by_date,
+        "label_url": label_print_link,
+        "total_refund": total_refund_amount,
+        "address": {
+          "address_one": billing_address[:address1],
+          "address_two": billing_address[:address2],
+          "city": billing_address[:city],
+          "state": billing_address.state[:abbr],
+          "zipcode": billing_address[:zipcode]
+        },
+        "items": formatted_return_items,
+        "international_user": international_user
+      }
+    end
 end
