42e510d9b8b9461806f3e9452cce1b5f27d67354
dagnar <dagnar@gmail.com>
2017-08-15 11:51:42 -0700

    make a todo note

--- app/mailers/return_mailer.rb
+++ app/mailers/return_mailer.rb
@@ -31,6 +31,7 @@ class ReturnMailer < ActionMailer::Base
       return_items = return_request.return_request_items
       billing_address = order.billing_address
       label_print_link = return_items.first.item_return.item_return_label[:label_url]
+      #todo: need to revisit this next line when we get final delivery date approval
       send_by_date = (return_request.order.delivery_policy.delivery_date + 45).strftime("%m/%d/%y")
       formatted_return_items = return_items.map do |item|
         {
