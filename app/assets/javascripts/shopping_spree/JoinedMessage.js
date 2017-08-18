a3eaf6e82f4d73ada82fa2c9f115ade3f839bd0e
Douglas Sellers <doug@fitorb.com>
2017-08-17 23:42:57 -0700

    Some basic date formatting

--- app/assets/javascripts/shopping_spree/JoinedMessage.js
+++ app/assets/javascripts/shopping_spree/JoinedMessage.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 export default class JoinedMessage extends React.Component
 {
+    constructor( props )
+    {
+        super( props );
+        this.formatDate = this.formatDate.bind( this );
+    }
+
+    formatDate( timeInMilliseconds )
+    {
+        var d = new Date( timeInMilliseconds );
+        return (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + d.getHours() + ":" + d.getMinutes()        
+    }
+    
     render()
     {
         
         return(
                 <li className='joined-message'>
-                {name}
                 <div className='row equal'>
-                <div className="joined-text col-xs-push-2 col-xs-8">{this.props.createdAt} - {this.props.name} joined this shopping spree</div>
+                <div className="joined-text col-xs-push-2 col-xs-8">{this.formatDate(this.props.createdAt)} - {this.props.name} joined this shopping spree</div>
                 </div>
                 </li>
         )
