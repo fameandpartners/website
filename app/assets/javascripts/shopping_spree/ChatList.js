f28cfda83436065eab4d605f675426874c4b756b
Douglas Sellers <doug@fitorb.com>
2017-08-17 22:43:23 -0700

    Made it os that now we don't render certain aspects of the element if the last element was the same owner

--- app/assets/javascripts/shopping_spree/ChatList.js
+++ app/assets/javascripts/shopping_spree/ChatList.js
@@ -8,7 +8,8 @@ export default class ChatList extends React.Component
     constructor( props )
     {
         super( props );
-        this.addChatMessage = this.addChatMessage.bind(this);         
+        this.addChatMessage = this.addChatMessage.bind(this);
+        this.sameOwnerAsLastMessage = this.sameOwnerAsLastMessage.bind( this );
         this.initializeFirebase();
         this.state =
             {
@@ -16,15 +17,28 @@ export default class ChatList extends React.Component
             };
     }
 
+    sameOwnerAsLastMessage( email )
+    {
+        if( this.state.messages.length == 0 )
+        {
+            return false;
+        } else
+        {
+            return this.state.messages[this.state.messages.length - 1].props.email == email;
+        }
+        
+    }
     addChatMessage( data )
     { 
         switch( data.val().type )
         {
             case 'text':
             this.state.messages.push(<TextMessage key={data.key}
-                                     text={data.val().value} 
+                                     text={data.val().value} p
                                      iconNumber={data.val().from.icon}
-                                     name={data.val().from.name}/>)
+                                     name={data.val().from.name}
+                                     email={data.val().from.email}
+                                     sameOwnerAsLastMessage={this.sameOwnerAsLastMessage( data.val().from.email )} />)
             break;
             
             case 'welcome_message':
--- app/assets/javascripts/shopping_spree/TextMessage.js
+++ app/assets/javascripts/shopping_spree/TextMessage.js
@@ -4,15 +4,22 @@ export default class TextMessage extends React.Component
 {
     render()
     {
+        const sameOwner = this.props.sameOwnerAsLastMessage;
+        let name = ""
+        if( !sameOwner )
+        {
+            name = (<div className='row equal'>
+                    <div className='message-name col-xs-push-2 col-xs-10'>
+                    {this.props.name}
+                    </div>
+                    </div>);
+        }
+        
         return(
                 <li className='text-message'>
+                {name}
                 <div className='row equal'>
-                <div className='message-name col-xs-push-2 col-xs-10'>
-                {this.props.name}
-                </div> 
-               </div>
-                <div className='row equal'>
-                <div className={"avatar col-xs-2 " + "avatar-" + this.props.iconNumber }></div>
+                <div className={"avatar col-xs-2 " + (this.props.sameOwnerAsLastMessage ? "" : "avatar-" + this.props.iconNumber ) }></div>
                 <div className="firebase-text col-xs-8">{this.props.text}</div>
                 </div>
                 </li>
@@ -23,5 +30,7 @@ export default class TextMessage extends React.Component
 TextMessage.propTypes = {
     text: React.PropTypes.string.isRequired,
     name: React.PropTypes.string.isRequired,
+    email: React.PropTypes.string.isRequired,
+    sameOwnerAsLastMessage: React.PropTypes.bool.isRequired,
     iconNumber: React.PropTypes.number.isRequired
 }
