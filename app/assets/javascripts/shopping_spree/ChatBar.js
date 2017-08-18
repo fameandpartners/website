f39dedef18ce7e2ca67dd57037d718a98aa7a3c3
Douglas Sellers <doug@fitorb.com>
2017-08-18 16:28:15 -0700

    Styled things a bit better

--- app/assets/javascripts/shopping_spree/ChatBar.js
+++ app/assets/javascripts/shopping_spree/ChatBar.js
@@ -4,9 +4,12 @@ export default class ChatBar extends React.Component
 {
     render()
     {
-        return ( <div className="row chat-bar">
-                 <div className="col-xs-12">
-                 Let\'s Shop
+        return ( <div className="row chat-bar equal">
+                 <div className="col-xs-10 no-right-gutter">
+                 <input className="shoppingSpreeTextInput" type="text"></input>
+                 </div>
+                 <div className="col-xs-2 no-left-gutter">
+                 <a className='center-block btn btn-black btn-lrg'>Send</a>
                  </div>
                  </div> );
     }
