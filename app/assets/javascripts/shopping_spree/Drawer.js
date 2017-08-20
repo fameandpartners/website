ed7b1d1d157adad11d002e3ad13005b4069432ed
Douglas Sellers <doug@fitorb.com>
2017-08-19 22:54:21 -0700

    Made things a bit nastier with the rendering but made it so that things don't get unmounted

--- app/assets/javascripts/shopping_spree/Drawer.js
+++ app/assets/javascripts/shopping_spree/Drawer.js
@@ -11,15 +11,15 @@ export default class Drawer extends React.Component
         super(props);
 
         this.state =
-            {
-                closed: true,
-                display: 'chat',
-                firebaseAPI: props.firebaseAPI,
-                firebaseDatabase: props.firebaseDatabase,
-                firebaseNodeId: props.firebaseNodeId
-                
-                
-            };
+        {
+            closed: true,
+            display: 'chat',
+            firebaseAPI: props.firebaseAPI,
+            firebaseDatabase: props.firebaseDatabase,
+            firebaseNodeId: props.firebaseNodeId
+            
+            
+        };
         this.handleToggle = this.handleToggle.bind(this);
         this.transitionToCart = this.transitionToCart.bind(this);
         this.transitionToChat = this.transitionToChat.bind(this);
@@ -49,54 +49,50 @@ export default class Drawer extends React.Component
     
     render()
     {
-        if( this.state.display === 'cart' )
-        {
-            return (
-                <div className={"shopping-spree-container container"}>
-                <div className="row header vertical-align">
-                <div className="back-to-spree col-xs-2" onClick={this.transitionToChat}>
-                Back to spree
-                </div>
-                </div>
-                </div>
-                )
+        return (
+            <div>
+            <div className={"shopping-spree-container container" + (this.state.display !== 'cart' ? " hidden" : "") }>
+            <div className="row header vertical-align">
+            <div className="back-to-spree col-xs-2" onClick={this.transitionToChat}>
+            Back to spree
+            </div>
+            </div>
+            </div>
             
-        } else
-        {
+            <div className={"shopping-spree-container container " + (this.state.closed ? 'collapsed' : '') + (this.state.display === 'cart' ? " hidden" : "")}>
+            <div className="full-toggle-btn" onClick={this.handleToggle}></div>
+            <div className="row header vertical-align">
+            <div className="col-xs-2">
+            <i className={"toggle-btn " + (this.state.closed ? "closed-caret" : "open-caret")}  onClick={this.handleToggle}></i>
+            </div>
+            <div className="col-xs-8 text-center">Shopping Spree</div>
+            <div className="col-xs-3"><span onClick={this.transitionToCart} className="icon icon-bag"></span></div>
+            </div>
+            <div className="row">
+            <ChatList
+            firebaseAPI={this.state.firebaseAPI}
+            firebaseDatabase={this.state.firebaseDatabase}
+            firebaseNodeId={this.state.firebaseNodeId}
+            />
+            </div>
+            <ChatBar
+            firebaseAPI={this.state.firebaseAPI}
+            firebaseDatabase={this.state.firebaseDatabase}
+            firebaseNodeId={this.state.firebaseNodeId}
+            name='Doug'
+            email='dougs@fameandpartners.com'
+            icon='14'
+            />
+            </div>
+            </div>
+        );
+        
 
-            return (
-                <div className={"shopping-spree-container container " + (this.state.closed ? 'collapsed' : '')}>
-                <div className="full-toggle-btn" onClick={this.handleToggle}></div>
-                <div className="row header vertical-align">
-                <div className="col-xs-2">
-                <i className={"toggle-btn " + (this.state.closed ? "closed-caret" : "open-caret")}  onClick={this.handleToggle}></i>
-                </div>
-                <div className="col-xs-8 text-center">Shopping Spree</div>
-                <div className="col-xs-3"><span onClick={this.transitionToCart} className="icon icon-bag"></span></div>
-                </div>
-                <div className="row">
-                <ChatList
-                firebaseAPI={this.state.firebaseAPI}
-                firebaseDatabase={this.state.firebaseDatabase}
-                firebaseNodeId={this.state.firebaseNodeId}
-                />
-                </div>
-                <ChatBar
-                firebaseAPI={this.state.firebaseAPI}
-                firebaseDatabase={this.state.firebaseDatabase}
-                firebaseNodeId={this.state.firebaseNodeId}
-                name='Doug'
-                email='dougs@fameandpartners.com'
-                icon='14'
-                />
-                </div>
-            );
-        }
     }
 }
 
 Drawer.propTypes = {
-        firebaseAPI: React.PropTypes.string.isRequired,
-        firebaseDatabase: React.PropTypes.string.isRequired,
-        firebaseNodeId: React.PropTypes.string.isRequired
-    }
+    firebaseAPI: React.PropTypes.string.isRequired,
+    firebaseDatabase: React.PropTypes.string.isRequired,
+    firebaseNodeId: React.PropTypes.string.isRequired
+}
