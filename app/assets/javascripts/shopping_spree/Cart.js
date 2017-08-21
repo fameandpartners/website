e77c19d212a966858d471e8a67b168704163480c
Douglas Sellers <doug@fitorb.com>
2017-08-21 11:21:49 -0700

    Made the bag responsive

--- app/assets/javascripts/shopping_spree/Cart.js
+++ app/assets/javascripts/shopping_spree/Cart.js
@@ -10,16 +10,16 @@ export default class ChatList extends FirebaseComponent
     {
         return(
             <div className="row header vertical-align">
-            <div className="back-to-spree col-xs-6" onClick={this.props.transitionToChat}>
+            <div className="back-to-spree col-md-4 col-xs-8" onClick={this.props.transitionToChat}>
             <div className="left-caret"></div>
             <div className="back-to-spree-text shopping-spree-headline">
             Back to spree
             </div>
             </div>
-            <div className="col-xs-9 col-xs-pull-1">
+            <div className="col-xs-6 col-md-4">
             Your Bag
             </div>
-            <div className="col-xs-3 col-xs-pull-1 text-right">
+            <div className="col-xs-4 col-md-4 text-center">
             23% off
             </div>
             </div>
