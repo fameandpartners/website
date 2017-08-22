202687bc6971d50a260abb90197073823ad98763
Douglas Sellers <doug@fitorb.com>
2017-08-22 16:37:10 -0700

    Got the font working correctly

--- app/assets/javascripts/shopping_spree/ShoppingSpreeOnboarding.js
+++ app/assets/javascripts/shopping_spree/ShoppingSpreeOnboarding.js
@@ -8,7 +8,7 @@ export default class ShoppingSpreeOnboarding extends React.Component
 
         this.state = 
             {
-                closed: true
+                closed: false
             };
 
         this.close = this.close.bind(this);
@@ -40,12 +40,16 @@ export default class ShoppingSpreeOnboarding extends React.Component
             <div id="shopping-spree-modal" className="shopping-spree-onboarding modal modal animated bounceIn" role="dialog" aria-hidden="true" style={this.state.closed ? {display: 'none'} : {display: 'block'}}>
               <div id="shopping-spree-modal-content" className="container">
                 <a className="btn-close med" alt="Close" onClick={this.close}></a>              
-                <div className="row">
-                  <div className="col-xs-18 text-right">
-
+                <div className="welcome-headline row">
+                  <div className="col-xs-12 text-center">
+                    Welcome to
                   </div>
                 </div>
-
+                <div className="welcome-headline row">
+                  <div className="col-xs-12 text-center">
+                    Partner Shop.
+                  </div>
+                </div>
               </div>
             </div>                        
         );
