741a56786d206a8945c671a86a2c11a082d59ccf
Douglas Sellers <doug@fitorb.com>
2017-08-22 15:21:40 -0700

    Modal is working a bit better

--- app/assets/javascripts/shopping_spree/ShoppingSpreeOnboarding.js
+++ app/assets/javascripts/shopping_spree/ShoppingSpreeOnboarding.js
@@ -6,8 +6,14 @@ export default class ShoppingSpreeOnboarding extends React.Component
     render()
     {
         return(
-            <div id="shopping-spree-modal-content">
-              Test
+            <div id="shopping-spree-modal-content" className="container">
+              <a className="btn-close med" alt="Close"></a>              
+              <div className="row">
+                <div className="col-xs-18 text-right">
+
+                </div>
+              </div>
+
             </div>
         );
     }   
