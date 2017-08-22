39953128bce473767a0366ce936279e63a544204
Douglas Sellers <doug@fitorb.com>
2017-08-22 15:03:33 -0700

    Made it so that the initial modal is now showing up

--- app/assets/javascripts/shopping_spree/App.js
+++ app/assets/javascripts/shopping_spree/App.js
@@ -2,10 +2,17 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import Drawer from './Drawer'
+import Drawer from './Drawer';
+import ShoppingSpreeOnboarding from './ShoppingSpreeOnboarding';
 
 
+// render(
+//        <Drawer firebaseAPI={window.ShoppingSpreeData.firebaseAPI} firebaseDatabase={window.ShoppingSpreeData.firebaseDatabase} firebaseNodeId={window.ShoppingSpreeData.firebaseNodeId}/>,
+//    document.getElementById('shopping-spree')
+// );
+
+ 
 render(
-        <Drawer firebaseAPI={window.ShoppingSpreeData.firebaseAPI} firebaseDatabase={window.ShoppingSpreeData.firebaseDatabase} firebaseNodeId={window.ShoppingSpreeData.firebaseNodeId}/>,
-    document.getElementById('shopping-spree')
+    <ShoppingSpreeOnboarding firebaseAPI={window.ShoppingSpreeData.firebaseAPI} firebaseDatabase={window.ShoppingSpreeData.firebaseDatabase} firebaseNodeId={window.ShoppingSpreeData.firebaseNodeId}/>,
+    document.getElementById( 'shopping-spree-modal' )
 );
---
+++ app/assets/javascripts/shopping_spree/ShoppingSpreeOnboarding.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ShoppingSpreeOnboarding extends React.Component
+{
+
+    render()
+    {
+        return(
+            <div id="shopping-spree-modal-content">
+              Test
+            </div>
+        );
+    }   
+
+}
