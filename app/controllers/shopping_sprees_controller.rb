e8450c2753948755e8dd17f196d1205099379ea3
Douglas Sellers <doug@fitorb.com>
2017-09-01 15:35:51 -0700

    Made it so that now we correctly set the cookies

--- app/controllers/shopping_sprees_controller.rb
+++ app/controllers/shopping_sprees_controller.rb
@@ -4,6 +4,10 @@ class ShoppingSpreesController < ApplicationController
     if( !params[:email].blank? && !params[:name].blank? )
       shopping_spree = ShoppingSpree.create
       to_return = shopping_spree.join( params[:email], params[:name ] )
+      cookies[:shopping_spree_name] = params[:name]
+      cookies[:shopping_spree_email] = params[:email]
+      cookies[:shopping_spree_id] = shopping_spree.shopping_spree_id
+      
       render json: (to_return.merge( { id: shopping_spree.shopping_spree_id } ) ).to_json
     end
   end
