cdfc0c002ed06f66dedb6601b3b3da7626afaf74
Douglas Sellers <doug@fitorb.com>
2017-09-05 16:39:36 -0700

    Made the modal look decent on desktop

--- app/assets/javascripts/shopping_spree/ShareModal.js
+++ app/assets/javascripts/shopping_spree/ShareModal.js
@@ -16,21 +16,21 @@ export default class ShareModal extends React.Component
 
               <div className='shopping-spree shopping-spree-share-modal container'>
                 <div className="row">
-                  <div className="modal-headline text-center col-xs-10 col-xs-push-1 col-lg-2 col-lg-push-5 no-gutter-mobile">
+                  <div className="modal-headline text-center col-xs-10 col-xs-push-1 col-md-10 col-md-push-1 no-gutter-mobile">
                     Copy and share this link to start shopping with your friends!
                   </div>
                 </div>
                 <div className="row equal">
-                  <div className="col-xs-7 no-right-gutter">
+                  <div className="col-xs-7 col-md-4 col-md-push-3 no-right-gutter">
                     <input className="form-control input-lg" type="text"></input>
                   </div>
-                  <div className="col-xs-5 no-left-gutter">
+                  <div className="col-xs-5 col-md-2 col-md-push-3 no-left-gutter">
                     <a className='btn btn-black btn-block no-horizontal-padding'>Copy Link</a>
                   </div>
                 </div>
                 <div id="start-button" className="row">
-                  <div  className="col-xs-12 col-lg-2 col-lg-push-5 no-gutter-mobile">
-                    <a className="btn btn-md btn-black btn-block">Start Shopping Spree</a>
+                  <div  className="col-xs-12 col-lg-3 col-lg-push-4 no-gutter-mobile">
+                    <a className="center-button-text btn btn-md btn-black btn-block">Start Shopping Spree</a>
                   </div>
                 </div>
               </div>
