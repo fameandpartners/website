388ebc76ca22c41e4aba94ce78f6bacbbed2ea3d
Thanh Truong <thanht@fameandpartners.com>
2017-09-05 17:17:05 -0700

    thanhauth is working

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class UserSessionsController < Devise::SessionsController
+    class UserSessionsController < ApplicationController #< Devise::SessionsController
       include SslRequirement
       helper 'spree/users', 'spree/base'
       include Spree::Core::ControllerHelpers::Auth
@@ -10,23 +10,40 @@ module Api
       ssl_required :new, :create, :destroy
 
       respond_to :json
-      skip_before_filter :verify_authenticity_token
+      # skip_before_filter :verify_authenticity_token
 
       def create
-        authenticate_spree_user!
+        binding.pry
+        ensure_params_exist
+        # authenticate_spree_user!
 
-        if spree_user_signed_in?
-          respond_with spree_current_user
-          # respond_with spree_current_user.to_json(:only => [:email, :sign_in_count, :first_name, :last_name])
-        else
-          warden.custom_failure!
-          render :json => {success: false, message: "Error with your login or password"}, status: 401
+        resource = Spree::User.find_for_database_authentication(:login => params["spree_user"]["email"])
+        return invalid_login_attempt unless resource
+
+        if resource.valid_password?(params["spree_user"]["password"])
+          sign_in("spree_user", resource)
+          respond_with resource
+          return
         end
+
+        invalid_login_attempt
       end
 
       def destroy
-        # env['warden'].logout(:spree_user) #this might work if below fails
-        sign_out(resource_name)
+        session.clear
+        render :json => {:success=>true}, status: 200
+      end
+
+      private
+
+      def ensure_params_exist
+        return unless params[:spree_user].blank?
+        render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
+      end
+
+      def invalid_login_attempt
+        warden.custom_failure!
+        render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
       end
 
     end
