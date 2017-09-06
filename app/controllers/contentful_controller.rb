0e7ac63c1885a6ccb04f0d1fcb88352300fb7790
Mateus Moraes <mateusm@fameandpartners.com>
2017-09-05 21:06:29 -0300

    [fix] move variables into @landing_page_container conditional

--- app/controllers/contentful_controller.rb
+++ app/controllers/contentful_controller.rb
@@ -14,10 +14,11 @@ class ContentfulController < ApplicationController
     current_contently = Contentful::Version.fetch_payload(params['developer'] == 'preview')
 
     @landing_page_container = current_contently[request.path]
-    landing_page_specific_site_version = @landing_page_container[:site_version]
-    landing_page_redirect_to_url = @landing_page_container[:site_version_url_to_redirect]
 
     if @landing_page_container
+      landing_page_specific_site_version = @landing_page_container[:site_version]
+      landing_page_redirect_to_url = @landing_page_container[:site_version_url_to_redirect]
+
       # Check if the domain is either AU or US and compare with the flag
       if (landing_page_specific_site_version == 'all' || current_site_version[:permalink] == landing_page_specific_site_version)
         get_all_pids
