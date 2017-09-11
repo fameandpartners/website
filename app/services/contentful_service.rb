0742ca7ca59082142014ff7f69c37966f48113ad
Mateus Moraes <mateusm@fameandpartners.com>
2017-09-11 14:31:31 -0300

    load mobile image in headers

--- app/services/contentful_service.rb
+++ app/services/contentful_service.rb
@@ -134,6 +134,8 @@ module Contentful
       id = large_container.id
 
       fetched_lg_container = @contentful_client.entries('sys.id' => id)[0]
+      desktop_image = (fetched_lg_container.respond_to? :image) ? fetched_lg_container.image.url : nil
+      mobile_image = (fetched_lg_container.respond_to? :mobile_image) ? fetched_lg_container.mobile_image.url : desktop_image
       image_caption = (fetched_lg_container.respond_to? :image_caption) ? fetched_lg_container.image_caption : nil
       image_caption_color = (fetched_lg_container.respond_to? :image_caption_color) ? fetched_lg_container.image_caption_color : 'white'
       image_caption_url = (fetched_lg_container.respond_to? :image_caption_url) ? fetched_lg_container.image_caption_url : nil
@@ -142,9 +144,9 @@ module Contentful
 
       if (fetched_lg_container.content_type.id == 'ITEM--lg')
         {
-          image: fetched_lg_container.image.url,
-          # mobile_image: fetched_lg_container.mobile_image.url,
           overlay_pids: fetched_lg_container.overlay_pids,
+          image: desktop_image,
+          mobile_image: mobile_image,
           image_caption: image_caption,
           image_caption_color: image_caption_color,
           image_caption_url: image_caption_url,
@@ -233,11 +235,14 @@ module Contentful
           full_width_content: full_width_content
         }
       elsif (main_header_container.content_type.id == 'HEADER--xl-editorial')
+        desktop_image = (main_header_container.respond_to? :image) ? main_header_container.image.url : nil
+        mobile_image = (main_header_container.respond_to? :mobile_image) ? main_header_container.mobile_image.url : desktop_image
+
         {
           id: main_header_container.content_type.id,
-          image: main_header_container.image.url,
-          # mobile_image: main_header_container.mobile_image.url,
           overlay_pids: main_header_container.overlay_pids
+          image: desktop_image,
+          mobile_image: mobile_image,
         }
       end
     end
