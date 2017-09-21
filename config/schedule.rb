820993475abfb83b4fb90ad072b47cefe30325fa
Thanh Truong <thanht@fameandpartners.com>
2017-09-21 15:02:42 -0700

    remove bad sidekiq calls

--- config/schedule.rb
+++ config/schedule.rb
@@ -33,7 +33,7 @@ every(30.minutes) { rake 'bergen:workers:verify_style_masters' }
 every(30.minutes) { rake 'bergen:workers:update_tracking_numbers' }
 every(30.minutes) { rake 'bergen:workers:create_asns' }
 every(3.hours)    { rake 'bergen:workers:receive_asns' }
-every(2.hours)    { runner 'Facebook::FacebookSync.delay.sync_last_28_days' }
+# every(2.hours)    { runner 'Facebook::FacebookSync.delay.sync_last_28_days' }
 
 # Next Logistics scheduled tasks
 
