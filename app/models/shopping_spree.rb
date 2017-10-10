6b48b00fad3351517129b1b6f03dc0f721e04c5f
Douglas Sellers <doug@fitorb.com>
2017-10-10 10:59:59 -0700

    Merged in cherry-pick

--- app/models/shopping_spree.rb
+++ app/models/shopping_spree.rb
@@ -37,6 +37,38 @@ class ShoppingSpree
     post_chat_message( { type: 'joined', email: email, icon: icon, name: name } )
   end
 
+  def post_dress_message( email, name, icon, product )
+    product.extend( PathBuildersHelper )
+    color = product.basic_colors.first
+    
+    post_chat_message(
+      {
+        type: 'share_dress',
+        value:
+          {
+            name: product.name,
+            price: product.price.to_i,
+            product_id: product.id,
+            url: "/dresses/#{product.descriptive_url( product )}",
+            color: {
+              id: color.id,
+              name: color.name,
+              presentation: color.presentation,
+              price: 0
+            },
+            image: "https://d1msb7dh8kb0o9.cloudfront.net/spree/products/35873/original/fprv1026p-black_and_white_spot-front.jpg?1494461867",
+            description: product.description,
+          },
+        from:
+          {
+            name: email,
+            email: name,
+            icon: icon,
+          }
+      }
+    )
+  end
+  
   def post_text_message( email, icon, name, text )
     post_chat_message( { type: 'text',
                          from:
@@ -54,6 +86,12 @@ class ShoppingSpree
   def post_welcome_messages
     post_text_message( 'help@fameandpartners.com', '20', 'Fame Bot', 'You can post items here and chat with your friends' )
     post_text_message( 'help@fameandpartners.com', '20', 'Fame Bot', 'Here''s something to get you started!' )
+    
+    post_dress_message( 'help@fameandpartners.com',
+                        '20',
+                        'Fame Bot',
+                        Spree::Product.where( 'name like ?', '%Maritza%' ).first
+                      ) 
   end
   
 end
