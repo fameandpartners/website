59a34ac9965fc60165f9e2b24b4b0455ac6fbeaa
Adam <elgrecode@gmail.com>
2017-10-09 18:43:11 -0700

    git subrepo clone git@github.com:fameandpartners/shopping-spree.git master
    subrepo:
      subdir:   "master"
      merged:   "39bdec8e1"
    upstream:
      origin:   "git@github.com:fameandpartners/shopping-spree.git"
      branch:   "master"
      commit:   "39bdec8e1"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ master/js/Cart.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import * as firebase from 'firebase';
+
+import FirebaseComponent from './FirebaseComponent';
+import CartItem from './CartItem';
+
+export default class Cart extends FirebaseComponent {
+  constructor(props) {
+    super(props);
+
+    this.state =
+    {
+      discount: '0%',
+      totalInSharedCart: 0,
+      totalInMyCart: 0,
+      myItems: [],
+      totalOff: 0,
+
+    };
+    this.addToCart = this.addToCart.bind(this);
+    this.recalculateDiscount = this.recalculateDiscount.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addToCart(data) {
+    const { totalInSharedCart } = this.state;
+    this.setState({
+      totalInSharedCart: totalInSharedCart + Math.round(parseFloat(data.val().dress.price)),
+    });
+
+    if (data.val().entry_for.email === this.props.email) {
+      this.setState({
+        myItems: this.state.myItems.concat([
+          <CartItem
+            key={data.key}
+            firebaseKey={data.key}
+            dress={data.val().dress}
+            delete={this.deleteItem}
+          />]),
+        totalInMyCart: this.state.totalInMyCart + Math.round(data.val().dress.price),
+      });
+    }
+
+    this.recalculateDiscount();
+  }
+
+  deleteItem(firebaseKey) {
+    let index = -1;
+    const { myItems } = this.state;
+    // for (let i = 0; i < this.state.myItems.length && index === -1; i++) {
+    //   if (this.state.myItems[i].props.firebaseKey === firebaseKey) {
+    //     index = i;
+    //   }
+    // }
+    index = myItems.filter((item, i) => this.state.myItems[i].props.firebaseKey === firebaseKey);
+
+
+    this.state.myItems.splice(index, 1);
+    this.setState(
+      {
+        myItems: this.state.myItems,
+      },
+        );
+
+    this.cartDB.child(firebaseKey).remove();
+  }
+
+  recalculateDiscount() {
+    const discount = this.calculateDiscount(this.state.totalInSharedCart);
+
+    this.setState(
+      {
+        discount: `${discount}%`,
+        totalOff: (discount / 100.0) * this.state.totalInMyCart,
+      },
+        );
+  }
+
+  startListeningToFirebase() {
+    super.connectToFirebase();
+    this.cartDB = firebase.apps[0].database().ref(`${this.props.firebaseNodeId}/cart`);
+    this.cartDB.on('child_added', this.addToCart);
+  }
+
+
+  stopListeningToFirebase() {
+    this.cartDB.off('child_added', this.addToCart);
+  }
+
+  componentWillMount() {
+    this.startListeningToFirebase();
+  }
+
+  componentWillUnmount() {
+    this.stopListeningToFirebase();
+  }
+
+  render() {
+    return (
+      <div className="shopping-spree-cart">
+        <div className="row header vertical-align">
+          <div className="back-to-spree col-md-4 col-xs-8" onClick={this.props.transitionToChat}>
+            <div className="left-caret" />
+            <div className="back-to-spree-text shopping-spree-headline">
+                    Back to spree
+                  </div>
+          </div>
+          <div className="col-xs-6 col-md-4">
+                  Your Bag
+                </div>
+          <div className="col-xs-4 col-md-4 text-center">
+            {this.state.discount} off
+                </div>
+        </div>
+        <div className="row">
+          <div className="no-left-gutter col-xs-push-1 col-xs-4">
+                  Subtotal
+                </div>
+          <div className="no-right-gutter col-xs-push-1 col-xs-6 text-right">
+                  ${this.state.totalInMyCart}.00
+                </div>
+        </div>
+
+        <div className="row">
+          <div className="no-left-gutter col-xs-push-1 col-xs-4">{this.state.discount} off
+                </div>
+          <div className="no-right-gutter col-xs-push-1 col-xs-6 text-right">
+                  ${this.state.totalOff.toFixed(2)}
+          </div>
+        </div>
+        <div className="row">
+          <div className="no-left-gutter col-xs-push-1 col-xs-4">
+            <strong>Total</strong>
+          </div>
+          <div className="no-right-gutter col-xs-push-1 col-xs-6 text-right">
+            <strong>${(this.state.totalInMyCart - this.state.totalOff).toFixed(2)}</strong>
+          </div>
+        </div>
+
+        <div className="row checkout-btn">
+          <div
+            className="no-right-gutter no-left-gutter col-xs-push-1 col-xs-10"
+          >
+            <a className="center-block btn btn-black btn-lrg">
+                Checkout
+              </a>
+          </div>
+        </div>
+        <div className="shopping-spree-contents">
+          <div className="row">
+            <div className="col-xs-18">
+              <ul className="cart-item-list">
+                {this.state.myItems}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Cart.propTypes = {
+  firebaseAPI: React.PropTypes.string.isRequired,
+  firebaseDatabase: React.PropTypes.string.isRequired,
+  firebaseNodeId: React.PropTypes.string.isRequired,
+  name: React.PropTypes.string.isRequired,
+  email: React.PropTypes.string.isRequired,
+};
---
+++ master/js/ChatList.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import * as firebase from 'firebase';
+
+import TextMessage from './TextMessage';
+import JoinedMessage from './JoinedMessage';
+import DressMessage from './DressMessage';
+import FirebaseComponent from './FirebaseComponent';
+
+export default class ChatList extends FirebaseComponent {
+  constructor(props) {
+    super(props);
+    this.addChatMessage = this.addChatMessage.bind(this);
+    this.sameOwnerAsLastMessage = this.sameOwnerAsLastMessage.bind(this);
+    this.scrollToBottom = this.scrollToBottom.bind(this);
+    this.state =
+    {
+      messages: [],
+      updateCount: 0,
+    };
+  }
+
+  sameOwnerAsLastMessage(email) {
+    if (this.state.messages.length === 0) {
+      return false;
+    }
+    return this.state.messages[this.state.messages.length - 1].props.email === email;
+  }
+  addChatMessage(data) {
+    switch (data.val().type) {
+      case 'text':
+        this.setState(
+          {
+            messages:
+                    this.state.messages.concat([<TextMessage
+                      key={data.key}
+                      text={data.val().value}
+                      iconNumber={parseInt(data.val().from.icon, 10)}
+                      name={data.val().from.name}
+                      email={data.val().from.email}
+                      sameOwnerAsLastMessage={this.sameOwnerAsLastMessage(data.val().from.email)}
+                    />]),
+            updateCount: this.state.updateCount + 1,
+          },
+            );
+        break;
+
+      case 'welcome_message':
+        break;
+
+      case 'share_dress':
+        this.setState(
+          {
+            messages:
+                    this.state.messages.concat([<DressMessage
+                      key={data.key}
+                      iconNumber={data.val().from.icon}
+                      name={data.val().from.name}
+                      email={data.val().from.email}
+                      sameOwnerAsLastMessage={this.sameOwnerAsLastMessage(data.val().from.email)}
+                      dress={data.val().value}
+                      showAddToCartModal={this.props.showAddToCartModal}
+                    />]),
+            updateCount: this.state.updateCount + 1,
+
+          },
+            );
+        break;
+
+      case 'joined':
+        this.setState(
+          {
+            messages:
+                    this.state.messages.concat(
+                      [
+                        <JoinedMessage
+                          key={data.key}
+                          name={data.val().name}
+                          email={data.val().email}
+                          createdAt={data.val().created_at}
+                        />,
+                      ],
+                    ),
+            updateCount: this.state.updateCount + 1,
+
+          },
+            );
+
+        break;
+
+
+      default:
+        console.log(`unknown card type: ${data.val().type}`);
+    }
+  }
+
+  scrollToBottom() {
+    this.bottomOfChat.scrollIntoView({ behavior: 'instant', block: 'end' });
+  }
+
+  startListeningToFirebase() {
+    super.connectToFirebase();
+
+    this.chatsDB = firebase.apps[0].database().ref(`${this.props.firebaseNodeId}/chats`);
+    this.chatsDB.on('child_added', this.addChatMessage);
+  }
+
+  stopListeningToFirebase() {
+    this.chatsDB.off('child_added', this.addChatMessage);
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  componentWillMount() {
+    this.startListeningToFirebase();
+  }
+
+  componentWillUnmount() {
+    this.stopListeningToFirebase();
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  exit() {
+    console.log('exit');
+  }
+  render() {
+    return (
+      <div className="chat-list">
+        <div className="row chat-header">
+          <div className="col-xs-4 header-name">
+            {this.props.name}
+          </div>
+          <div
+            onClick={this.props.doneShoppingSpree}
+            className="col-xs-1 col-xs-push-3"
+            role="button"
+          >
+            Exit
+          </div>
+          <div
+            onClick={this.props.showShareModal}
+            className="col-xs-4 col-xs-push-3 text-right"
+            role="button"
+          >
+            Invite other friends!
+          </div>
+        </div>
+        <div className="row">
+          <div className="chat-content">
+            <ul>
+              {this.state.messages}
+            </ul>
+            <div
+              style={{ float: 'left', clear: 'both' }}
+              ref={(el) => { this.bottomOfChat = el; }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+ChatList.propTypes = {
+  firebaseAPI: React.PropTypes.string.isRequired,
+  firebaseDatabase: React.PropTypes.string.isRequired,
+  firebaseNodeId: React.PropTypes.string.isRequired,
+  showAddToCartModal: React.PropTypes.func.isRequired,
+  name: React.PropTypes.string.isRequired,
+  doneShoppingSpree: React.PropTypes.func.isRequired,
+  showShareModal: React.PropTypes.func.isRequired,
+};
