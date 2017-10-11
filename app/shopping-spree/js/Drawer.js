5dcdd41e743d61772bc7a9847ad9cd7701be21c5
Douglas Sellers <doug@fitorb.com>
2017-10-10 17:56:05 -0700

    git subrepo pull app/shopping-spree
    subrepo:
      subdir:   "app/shopping-spree"
      merged:   "cc92b3f80"
    upstream:
      origin:   "git@github.com:fameandpartners/shopping-spree.git"
      branch:   "master"
      commit:   "cc92b3f80"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "d1a3f56"

--- app/shopping-spree/js/Drawer.js
+++ app/shopping-spree/js/Drawer.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import ChatList from './ChatList';
 import ChatBar from './ChatBar';
--- app/shopping-spree/js/FirebaseComponent.js
+++ app/shopping-spree/js/FirebaseComponent.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import * as firebase from 'firebase';
 
