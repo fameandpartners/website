0392f53b89ae6fb1dfaaa0b337d67a5d9ba82994
Douglas Sellers <doug@fitorb.com>
2017-10-12 10:42:09 -0700

    git subrepo pull (merge) app/shopping-spree
    subrepo:
      subdir:   "app/shopping-spree"
      merged:   "713a1728d"
    upstream:
      origin:   "git@github.com:fameandpartners/shopping-spree.git"
      branch:   "master"
      commit:   "713a1728d"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "d1a3f56"

--- app/shopping-spree/js/AddToCartModal.js
+++ app/shopping-spree/js/AddToCartModal.js
@@ -83,7 +83,6 @@ export default class AddToCartModal extends FirebaseComponent
     createFirebaseCartItem()
     {
         let newMessage = this.cartDB.push();
-        console.log( this.props.dress );
         newMessage.set( { created_at: firebase.database.ServerValue.TIMESTAMP,
                           dress:
                           {
@@ -95,6 +94,7 @@ export default class AddToCartModal extends FirebaseComponent
                               name: this.props.dress['name'],
                               price: this.props.dress['price'],
                               product_id: this.props.dress['product_id'],
+                              product_variant_id: this.props.dress['product_variant_id'],
                               url: this.props.dress['url']
                           },
                           entry_for:
