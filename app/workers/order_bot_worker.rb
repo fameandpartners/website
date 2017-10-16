153a632bce807193635e450bb3f8c3ae595b2771
dmkrtchyan <davidm@fameandpartners.com>
2017-10-16 11:16:11 -0700

    use default of 25 retries

--- app/workers/order_bot_worker.rb
+++ app/workers/order_bot_worker.rb
@@ -2,8 +2,6 @@ class OrderBotWorker
   include Spree::OrderBotHelper
   include Sidekiq::Worker
 
-  sidekiq_options :retry => 5
-
   def perform(order_id)
    	order = Spree::Order.find(order_id)
     create_new_order_by_factory(order)
