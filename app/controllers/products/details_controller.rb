e250746c1196863512adbf8242a517e5b85d4192
Matthew Whittaker <mwhitt89@gmail.com>
2017-10-23 16:46:28 -0700

    add json endpoint for product details

--- app/controllers/products/details_controller.rb
+++ app/controllers/products/details_controller.rb
@@ -1,11 +1,75 @@
 class Products::DetailsController < Products::BaseController
   include Marketing::Gtm::Controller::Product
 
+  before_filter :setup_product, only: [:show, :show_json]
+
   layout 'custom_experience/application'
 
   def show
+
+    #TODO: Possibly no longer needed??
+
     @zopim_opt_out = true
 
+    # Set SEO properties
+    # Drop anything after the first period(.) and newline
+    title(@product.meta_title, default_seo_title)
+    description(@product.meta_description)
+
+    append_gtm_product(product_presenter: @product)
+
+    # @partial_hash = Rails.cache.fetch(env["ORIGINAL_FULLPATH"]+current_site_version.name, expires_in: 14.hours) do
+    #   #let's makey object for nodepdp
+    #   pdp_obj = {
+    #     paths: env["REQUEST_URI"],  #todo: work this out with adam
+    #     product: @product,
+    #     discount: @product_discount,
+    #     images: @product.all_images,
+    #     sizeChart: @product.size_chart_data,
+    #     siteVersion: @current_site_version.name,
+    #     svgSpritePath: "/assets/svg/sprite.svg",
+    #     flags: {
+    #       afterpay: Features.active?(:afterpay),
+    #       fastMaking: !Features.active?(:getitquick_unavailable)
+    #     }
+    #   }
+
+    #   begin
+    #     resp = RestClient.post "#{configatron.node_pdp_url}/pdp", {'data' => pdp_obj}.to_json, {content_type: :json}
+    #     JSON.parse(resp)
+    #   rescue Exception => e
+    #     Raven.capture_exception(e, response: resp)
+    #     NewRelic::Agent.notice_error(e, response: resp)
+    #     throw e
+    #   end
+    # end
+
+    render :show, status: pdp_status
+  end
+
+  def show_json
+    # TODO: Cache this json response
+    # TODO: Add test coverage
+
+    pdp_obj = {
+      paths: env["REQUEST_URI"],  #todo: work this out with adam
+      product: @product,
+      discount: @product_discount,
+      images: @product.all_images,
+      sizeChart: @product.size_chart_data,
+      siteVersion: @current_site_version.name,
+      svgSpritePath: "/assets/svg/sprite.svg",
+      flags: {
+        afterpay: Features.active?(:afterpay),
+        fastMaking: !Features.active?(:getitquick_unavailable)
+      }
+    }
+    render json: pdp_obj
+  end
+
+  private
+
+  def setup_product
     @product = Products::DetailsResource.new(
       site_version: current_site_version,
       slug:         params[:product_slug],
@@ -41,13 +105,6 @@ class Products::DetailsController < Products::BaseController
 
     @product.use_auto_discount!(current_promotion.discount) if current_promotion
 
-    # Set SEO properties
-    # Drop anything after the first period(.) and newline
-    title(@product.meta_title, default_seo_title)
-    description(@product.meta_description)
-
-    append_gtm_product(product_presenter: @product)
-
     if @product.fit
       @product.fit = @product.fit.gsub(" Height", "Height")
       @product.fit = @product.fit.gsub("Height", ", Height")
@@ -56,37 +113,10 @@ class Products::DetailsController < Products::BaseController
       @product.fit = @product.fit.gsub(" Waist","Waist")
       @product.fit = @product.fit.gsub("Waist",", Waist")
     end
-
-    @partial_hash = Rails.cache.fetch(env["ORIGINAL_FULLPATH"]+current_site_version.name, expires_in: 14.hours) do
-      #let's makey object for nodepdp
-      pdp_obj = {
-        paths: env["REQUEST_URI"],  #todo: work this out with adam
-        product: @product,
-        discount: @product_discount,
-        images: @product.all_images,
-        sizeChart: @product.size_chart_data,
-        siteVersion: @current_site_version.name,
-        svgSpritePath: "/assets/svg/sprite.svg",
-        flags: {
-          afterpay: Features.active?(:afterpay),
-          fastMaking: !Features.active?(:getitquick_unavailable)
-        }
-      }
-
-      begin
-        resp = RestClient.post "#{configatron.node_pdp_url}/pdp", {'data' => pdp_obj}.to_json, {content_type: :json}
-        JSON.parse(resp)
-      rescue Exception => e
-        Raven.capture_exception(e, response: resp)
-        NewRelic::Agent.notice_error(e, response: resp)
-        throw e
-      end
-    end
-
-    render :show, status: pdp_status
   end
 
-  private def pdp_status
+
+  def pdp_status
     @product.is_active ? :ok : :not_found
   end
 end
--- app/middleware/webpack_proxy.rb
+++ app/middleware/webpack_proxy.rb
@@ -2,7 +2,7 @@ require 'rack/proxy'
 class WebpackProxy < Rack::Proxy
   def perform_request(env)
     request = Rack::Request.new(env)
-    if request.path =~ /dresses\/dresses-|static\/js/
+    if request.path =~ /dresses\/dress-|static\/js/
       env['HTTP_HOST'] = 'localhost:3030'
       env['REQUEST_PATH'] = request.fullpath
       super(env)
