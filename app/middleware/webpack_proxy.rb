f12e167b357b4dd0610b6b6e8ceaaacad95490d3
Matthew Whittaker <mwhitt89@gmail.com>
2017-10-23 15:24:53 -0700

    add middleware to proxy react app

---
+++ app/middleware/webpack_proxy.rb
@@ -0,0 +1,13 @@
+require 'rack/proxy'
+class WebpackProxy < Rack::Proxy
+  def perform_request(env)
+    request = Rack::Request.new(env)
+    if request.path =~ /dresses\/dresses-/
+      env['HTTP_HOST'] = 'localhost:3030'
+      env['REQUEST_PATH'] = request.fullpath
+      super(env)
+    else
+      @app.call(env)
+    end
+  end
+end
