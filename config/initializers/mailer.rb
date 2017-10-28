f735fc165d7e7c396dc261856a3b383e4a7be468
Thanh Truong <thanht@fameandpartners.com>
2017-10-28 15:54:22 -0700

    remove mailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,12 +1,12 @@
-if Rails.env.staging? || Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings   = {
-    address:              ENV['SMTP_HOST'],
-    port:                 ENV['SMTP_PORT'],
-    domain:               configatron.mailgun.mailbox.domain,
-    user_name:            configatron.mailgun.mailbox.username,
-    password:             configatron.mailgun.mailbox.password,
-    authentication:       ENV['SMTP_AUTHENTICATION'].to_sym,
-    enable_starttls_auto: ENV['SMTP_STARTTLS']
-  }
-end
+# if Rails.env.staging? || Rails.env.production?
+#   ActionMailer::Base.delivery_method = :smtp
+#   ActionMailer::Base.smtp_settings   = {
+#     address:              ENV['SMTP_HOST'],
+#     port:                 ENV['SMTP_PORT'],
+#     domain:               configatron.mailgun.mailbox.domain,
+#     user_name:            configatron.mailgun.mailbox.username,
+#     password:             configatron.mailgun.mailbox.password,
+#     authentication:       ENV['SMTP_AUTHENTICATION'].to_sym,
+#     enable_starttls_auto: ENV['SMTP_STARTTLS']
+#   }
+# end
