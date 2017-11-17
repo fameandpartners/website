a4824bd4c17ed09e2e492f42f18f8ca40cf60fd1
dmkrtchyan <davidm@fameandpartners.com>
2017-11-17 12:00:28 -0800

    Fix tests

--- app/controllers/concerns/webpack.rb
+++ app/controllers/concerns/webpack.rb
@@ -3,7 +3,7 @@ module Concerns
   extend ActiveSupport::Concern
 
   def webpack_assets
-    Rails.cache.fetch(env["ORIGINAL_FULLPATH"]+current_site_version.name, expires_in: 14.hours) do
+    Rails.cache.fetch(env["ORIGINAL_FULLPATH"].to_s+current_site_version.name, expires_in: 14.hours) do
       begin
         resp = RestClient.get "#{configatron.node_pdp_url}/webpack/asset-manifest"
         JSON.parse(resp)
