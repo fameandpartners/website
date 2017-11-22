4e11fedba68c0a0acee6e9362ede747874f170f8
dmkrtchyan <davidm@fameandpartners.com>
2017-11-21 16:30:25 -0800

    Fix broken tests

--- spec/factories/spree/products.rb
+++ spec/factories/spree/products.rb
@@ -8,7 +8,6 @@ FactoryGirl.define do
     available_on  { rand(100).days.ago.utc }
     permalink     { name.downcase.gsub(/\s/, '_') }
     customizations  "[{\"customisation_value\":{\"created_at\":\"2017-11-21T15:24:51.797-08:00\",\"customisation_type\":\"fabric\",\"id\":1,\"image_content_type\":null,\"image_file_name\":null,\"image_file_size\":null,\"name\":\"make-skirt-ankle-length-1\",\"point_of_view\":\"front\",\"position\":1,\"presentation\":\"Make Skirt Ankle Length 1\",\"price\":\"19.5\",\"product_id\":1,\"updated_at\":\"2017-11-21T15:24:51.797-08:00\"}},{\"customisation_value\":{\"created_at\":\"2017-11-21T15:24:51.803-08:00\",\"customisation_type\":\"cut\",\"id\":2,\"image_content_type\":null,\"image_file_name\":null,\"image_file_size\":null,\"name\":\"make-skirt-ankle-length-2\",\"point_of_view\":\"front\",\"position\":2,\"presentation\":\"Make Skirt Ankle Length 2\",\"price\":\"14.3\",\"product_id\":1,\"updated_at\":\"2017-11-21T15:24:51.803-08:00\"}}]"
-    making_options { build(:product_making_option) }
 
     factory :dress_with_variants do |f|
       after(:create) do |dress, _evaluator|
