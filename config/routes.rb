9363aecd463132448ab91676cfbbbfc18ef012e8
Mateus Moraes <mateusm@fameandpartners.com>
2017-12-27 16:13:27 -0200

    redirect /wedding-atelier to bridesmaid teaser page

--- config/routes.rb
+++ config/routes.rb
@@ -317,10 +317,9 @@ FameAndPartners::Application.routes.draw do
     get '/trends-gingham-stripe' => 'products/collections#show', :permalink => 'gingham-stripe-trend', :as => :gingham_stripe_trend_page
 
     # Wedding Atelier App - Landing page
-    get '/wedding-atelier' => 'statics#wedding_atelier_app', as: :wedding_atelier_app_landing_page
+    get '/wedding-atelier', to: redirect('/coming-soon-custom-bridesmaid-dresses'), as: :wedding_atelier_app_landing_page
     # Redirection in case of misspelling
-    get '/weddings-atelier', to: redirect('/wedding-atelier')
-
+    get '/weddings-atelier', to: redirect('/coming-soon-custom-bridesmaid-dresses')
 
     # Casual Summer Styles Collection Page
     get '/casual-summer-styles' => 'products/collections#show', :permalink => 'casual-summer-styles', :as => :casual_summer_styles_page
