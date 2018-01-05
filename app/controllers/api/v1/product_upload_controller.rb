3eca05471f916b9e477492f39ab124711b97616c
dmkrtchyan <davidm@fameandpartners.com>
2018-01-05 11:52:35 -0800

    upload controller clean up

--- app/controllers/api/v1/product_upload_controller.rb
+++ app/controllers/api/v1/product_upload_controller.rb
@@ -6,13 +6,10 @@ module Api
       respond_to :json
 
       def upload
-      	product = create_or_update_products(request.body.string)
+        product = create_or_update_products(request.body.string)
         respond_with product.to_json
       end
 
-
-    end
-
-
+    end 
   end
 end
