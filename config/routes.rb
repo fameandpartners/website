3895e19504a9cdefef0c2efe4368614123ebe5da
Nav Samra <navsamra@me.com>
2018-01-10 20:09:57 -0800

    add bridesmaids results route and page

--- config/routes.rb
+++ config/routes.rb
@@ -630,6 +630,7 @@ FameAndPartners::Application.routes.draw do
 
   scope '/bridesmaids' do
     get '/' => 'products/bridesmaids#index'
+    get '/dresses' => 'products/bridesmaids#show'
   end
 
 
