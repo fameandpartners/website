3b8ec9010c1fc429f1a53c8f25c51a4fcacd16f1
Thanh Truong <thanht@fameandpartners.com>
2018-01-11 15:06:22 -0800

    need the actual controller

--- app/controllers/api/v1/fabric_swatches_controller.rb
+++ app/controllers/api/v1/fabric_swatches_controller.rb
@@ -10,19 +10,22 @@ module Api
       skip_before_filter :verify_authenticity_token
 
       def index
-        binding.pry
-        prd = Spree::Product.find_by_name('Fabric Swatch - Heavy Georgette')
-        output = prd.variants.map do |swatch_variant|
-          {
-            variant_id: swatch_variant.id,
-            product_id: swatch_variant.product.id,
-            sku: swatch_variant.sku,
-            color_name: swatch_variant.dress_color.presentation,
-            color_id: swatch_variant.dress_color.id,
-          }
-        end
+        colors = Rails.cache.fetch('fabric_swatches_heavy') do
+          prd = Spree::Product.find_by_name('Fabric Swatch - Heavy Georgette')
 
-        respond_with output.to_json
+          prd.variants.map do |swatch_variant|
+            {
+              variant_id: swatch_variant.id,
+              product_id: swatch_variant.product.id,
+              sku: swatch_variant.sku,
+              color_name: swatch_variant.dress_color.presentation,
+              color_id: swatch_variant.dress_color.id,
+              color_hex: swatch_variant.dress_color.value,
+              price: swatch_variant.prices.first.amount
+            }
+          end
+        end
+        respond_with colors.to_json
       end
 
 
