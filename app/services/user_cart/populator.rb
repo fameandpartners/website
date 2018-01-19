a840c6ca6ba8d236643e6f8f09b1317e8ccb29aa
dmkrtchyan <davidm@fameandpartners.com>
2018-01-18 17:52:33 -0800

    remove to _i

--- app/services/user_cart/populator.rb
+++ app/services/user_cart/populator.rb
@@ -80,6 +80,7 @@ class Populator
 
     def add_personalized_product
       personalization = build_personalization
+      binding.pry
       if personalization.valid?
         add_product_to_cart
         line_item.customizations = product_customizations.to_json
@@ -191,8 +192,9 @@ class Populator
       @product_customizations ||= begin
         customizations = []
         Array.wrap(product_attributes[:customizations_ids]).compact.each do |id|
+          binding.pry
           next if id.blank?
-          customization = JSON.parse(product.customizations).detect { |customisation_value| customisation_value['customisation_value']['id'] == id.to_i }
+          customization = JSON.parse(product.customizations).detect { |customisation_value| customisation_value['customisation_value']['id'] == id }
           if customization.blank?
             raise Errors::ProductOptionNotAvailable.new("product customization ##{ id } not available")
           else
