dd759c7363412046811e3a48dcc7ba0073df4198
dmkrtchyan <davidm@fameandpartners.com>
2018-01-22 12:59:00 -0800

    Handle missing params

--- app/controllers/api/v1/bridesmaid_controller.rb
+++ app/controllers/api/v1/bridesmaid_controller.rb
@@ -5,39 +5,64 @@ module Api
       respond_to :json
 
       def index
-        customized_products = CustomizationVisualization.where("lower(length) = ? AND lower(silhouette) = ? AND lower(neckline) in (?) AND lower(render_urls)c @> ? ",
-                                                               params[:selectedLength].downcase, params[:selectedSilhouette].downcase, params[:selectedTopDetails].downcase, [{color:  params[:selectedColor].downcase}].to_json)
-                                                        .order('Length(customization_ids)')  #gets base most customization 
-        
-        customized_products = customized_products.uniq_by{ |x| x.product_id.to_s + x.neckline } #only present one of each product and neckline combo
+        if !validate_params([:selectedColor, :selectedTopDetails, :selectedLength, :selectedSilhouette], params)
+          respond_with nil, status: :not_acceptable
+        else
+          customized_products = CustomizationVisualization.where("lower(length) = ? AND lower(silhouette) = ? AND lower(neckline) in (?) AND lower(render_urls)c @> ? ",
+                                                                 params[:selectedLength].downcase, params[:selectedSilhouette].downcase, params[:selectedTopDetails].downcase, [{color:  params[:selectedColor].downcase}].to_json)
+                                                          .order('Length(customization_ids)')  #gets base most customization 
+          
+          customized_products = customized_products.uniq_by{ |x| x.product_id.to_s + x.neckline } #only present one of each product and neckline combo
 
-        res = setup_collection(customized_products, params[:selectedColor])
-        respond_with res
+          res = setup_collection(customized_products, params[:selectedColor])
+          respond_with res
+        end
       end
 
       def incompatabilities
-        customized_products = CustomizationVisualization.where("customization_ids = ? AND product_id = ?",
-                                                               params[:customization_ids].sort.join('_'), params[:product_id])
-        
-        customized_product = customized_products.select{ |x| x.length.downcase ==  params[:length].downcase }.first
+        if !validate_params([:customization_ids, :product_id, :length], params)
+          respond_with nil, status: :not_acceptable
+        else
 
-        product_length_custs = JSON.parse(customized_product.product.customizations).select{ |x| x['customisation_value']['group'] == 'Lengths' }
-        
-        lengths = customized_products.map { |x| "change-to-#{x.length.downcase}" }
-        
-        compatible_lengths = product_length_custs.select{ |x| lengths.include?(x['customisation_value']['name']) }
-        
+          customized_products = CustomizationVisualization.where("customization_ids = ? AND product_id = ?",
+                                                                 params[:customization_ids].sort.join('_'), params[:product_id])
+          
+          customized_product = customized_products.select{ |x| x.length.downcase ==  params[:length].downcase }.first
 
-        res = {}
-        res[:id] = customized_product.id
-        res[:compatible_lengths] = compatible_lengths.map{ |x| x['customisation_value']['id']}
-        res[:incompatible_ids] = customized_product.incompatible_ids.split(',').reject { |x| res[:compatible_lengths].include?(x) }
+          if customized_product.nil?
+            respond_with nil, status: :not_found
 
-        respond_with res
+          else
+
+            product_length_custs = JSON.parse(customized_product.product.customizations).select{ |x| x['customisation_value']['group'] == 'Lengths' }
+            
+            lengths = customized_products.map { |x| "change-to-#{x.length.downcase}" }
+            
+            compatible_lengths = product_length_custs.select{ |x| lengths.include?(x['customisation_value']['name']) }
+            
+
+            res = {}
+            res[:id] = customized_product.id
+            res[:compatible_lengths] = compatible_lengths.map{ |x| x['customisation_value']['id']}
+            res[:incompatible_ids] = customized_product.incompatible_ids.split(',').reject { |x| res[:compatible_lengths].include?(x) }
+
+            respond_with res
+            
+          end
+        end
 
       end
 
       private
+
+      def validate_params(required_params, passed_params)
+        required_params.each do |rp|
+          if !passed_params.has_key?(rp)
+            return false #TODO: Make this cooler when we upgrade to rails 4
+          end
+        end
+        return true
+      end
 
       def setup_collection(customized_products, color)
         collection = []
