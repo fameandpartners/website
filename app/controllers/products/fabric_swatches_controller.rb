9068261b6a827778c3c2be06414e35da95f6a069
Thanh Truong <thanht@fameandpartners.com>
2018-01-22 15:36:30 -0800

    minor cleanup

--- app/controllers/products/fabric_swatches_controller.rb
+++ app/controllers/products/fabric_swatches_controller.rb
@@ -1,5 +1,6 @@
 class Products::FabricSwatchesController < Products::BaseController
   include Marketing::Gtm::Controller::Collection
+  include ProductsHelper
   respond_to :html
 
   layout 'custom_experience/application'
@@ -10,21 +11,7 @@ class Products::FabricSwatchesController < Products::BaseController
                 :redirect_site_version
 
   def index
-    @swatches = Rails.cache.fetch('fabric_swatches_heavy') do
-      prd = Spree::Product.find_by_name('Fabric Swatch - Heavy Georgette')
-
-      prd.variants.map do |swatch_variant|
-        {
-          variant_id: swatch_variant.id,
-          product_id: swatch_variant.product.id,
-          sku: swatch_variant.sku,
-          color_name: swatch_variant.dress_color.presentation,
-          color_id: swatch_variant.dress_color.id,
-          color_hex: swatch_variant.dress_color.value,
-          price: swatch_variant.prices.first.amount
-        }
-      end
-    end
+    @swatches = fabric_swatch_colors
   end
 
   private
