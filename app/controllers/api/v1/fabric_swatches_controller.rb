7327c84d3ca0e53658d089daeb6fe360a4713349
Thanh Truong <thanht@fameandpartners.com>
2018-01-22 11:08:18 -0800

    connect color filters to real values

--- app/controllers/api/v1/fabric_swatches_controller.rb
+++ app/controllers/api/v1/fabric_swatches_controller.rb
@@ -2,7 +2,9 @@ module Api
   module V1
     class FabricSwatchesController < ApplicationController
       include SslRequirement
+
       helper 'spree/users', 'spree/base'
+      include ProductsHelper
       include Spree::Core::ControllerHelpers::Auth
 
       ssl_required :new, :create, :destroy
@@ -10,21 +12,7 @@ module Api
       skip_before_filter :verify_authenticity_token
 
       def index
-        colors = Rails.cache.fetch('fabric_swatches_heavy') do
-          prd = Spree::Product.find_by_name('Fabric Swatch - Heavy Georgette')
-
-          prd.variants.map do |swatch_variant|
-            {
-              variant_id: swatch_variant.id,
-              product_id: swatch_variant.product.id,
-              sku: swatch_variant.sku,
-              color_name: swatch_variant.dress_color.presentation,
-              color_id: swatch_variant.dress_color.id,
-              color_hex: swatch_variant.dress_color.value,
-              price: swatch_variant.prices.first.amount
-            }
-          end
-        end
+        colors = fabric_swatch_colors
         respond_with colors.to_json
       end
 
