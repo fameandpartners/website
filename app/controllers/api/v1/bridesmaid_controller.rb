d2632da038b9473bbe803406ea160bfc34de64a0
dmkrtchyan <davidm@fameandpartners.com>
2018-01-22 17:07:03 -0800

    Fix filters for bridesmaids

--- app/controllers/api/v1/bridesmaid_controller.rb
+++ app/controllers/api/v1/bridesmaid_controller.rb
@@ -5,11 +5,12 @@ module Api
       respond_to :json
 
       def index
+        binding.pry
         if !validate_params([:selectedColor, :selectedTopDetails, :selectedLength, :selectedSilhouette], params)
           respond_with nil, status: :not_acceptable
         else
-          customized_products = CustomizationVisualization.where("lower(length) = ? AND lower(silhouette) = ? AND lower(neckline) in (?) AND lower(render_urls)c @> ? ",
-                                                                 params[:selectedLength].downcase, params[:selectedSilhouette].downcase, params[:selectedTopDetails].downcase, [{color:  params[:selectedColor].downcase}].to_json)
+          customized_products = CustomizationVisualization.where("lower(length) = ? AND lower(silhouette) = ? AND lower(neckline) in (?) AND render_urls @> ? ",
+                                                                 params[:selectedLength].downcase, params[:selectedSilhouette].downcase, params[:selectedTopDetails].map { |x| x.downcase }, [{color:  params[:selectedColor]}].to_json)
                                                           .order('Length(customization_ids)')  #gets base most customization 
           
           customized_products = customized_products.uniq_by{ |x| x.product_id.to_s + x.neckline } #only present one of each product and neckline combo
@@ -26,8 +27,14 @@ module Api
 
           customized_products = CustomizationVisualization.where("customization_ids = ? AND product_id = ?",
                                                                  params[:customization_ids].sort.join('_'), params[:product_id])
-          
-          customized_product = customized_products.select{ |x| x.length.downcase ==  params[:length].downcase }.first
+
+
+          if params[:length].downcase == 'micro-mini'
+            customized_product = customized_products.select{ |x| x.length.downcase ==  params[:length].downcase  ||  x.length.downcase == 'cheeky' }.first
+
+          else
+            customized_product = customized_products.select{ |x| x.length.downcase ==  params[:length].downcase }.first
+          end
 
           if customized_product.nil?
             respond_with nil, status: :not_found
@@ -46,8 +53,10 @@ module Api
             res[:compatible_lengths] = compatible_lengths.map{ |x| x['customisation_value']['id']}
             res[:incompatible_ids] = customized_product.incompatible_ids.split(',').reject { |x| res[:compatible_lengths].include?(x) }
 
+            @swatch_colors = fabric_swatch_colors.to_json
+
             respond_with res
-            
+
           end
         end
 
@@ -70,7 +79,7 @@ module Api
           product = cp.product
           collection << { 
                     id: cp.id,
-                    product_name: product.name, #TODO: Need to do this per dorothy's suggestion
+                    product_name: "#{cp.length} Length #{cp.silhouette} Dress with #{cp.neckline} #{cp.neckline.include?('Neckline') ? '' : Neckline}", # product.name, #TODO: Need to do this per dorothy's suggestion
                     color_count: product.colors.count,
                     customization_count: JSON.parse(product.customizations).count,
                     price: product.master.price_in(current_currency.upcase).attributes,
