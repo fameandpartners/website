7b702ce42f9ec124c5be5a6d73c4caafd553950b
dmkrtchyan <davidm@fameandpartners.com>
2018-01-29 23:04:28 -0800

    two more postgres

--- db/migrate/20180105234451_change_render_urls_to_jsonb.rb
+++ db/migrate/20180105234451_change_render_urls_to_jsonb.rb
@@ -1,5 +1,6 @@
 class ChangeRenderUrlsToJsonb < ActiveRecord::Migration
   def change
-  		change_column :customization_visualizations, :render_urls, :jsonb, using: 'column_name::jsonb'
+  		change_column :customization_visualizations, :render_urls, 'jsonb USING CAST(render_urls as jsonb)'
+  end
 	end
 end
--- db/migrate/20180118062620_add_lengths_to_products.rb
+++ db/migrate/20180118062620_add_lengths_to_products.rb
@@ -1,5 +1,4 @@
 class AddLengthsToProducts < ActiveRecord::Migration
   def change
-  	add_column :spree_products, :lengths, :jsonb, using: 'column_name::jsonb'
-  end
+  	add_column :spree_products, :lengths, 'jsonb USING CAST(lengths as jsonb)'
 end
