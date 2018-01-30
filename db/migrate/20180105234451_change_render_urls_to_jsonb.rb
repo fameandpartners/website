eae5b7bc5d3bb3a54311e61bf2a18a14be9c29ff
dmkrtchyan <davidm@fameandpartners.com>
2018-01-29 23:35:32 -0800

    other end

--- db/migrate/20180105234451_change_render_urls_to_jsonb.rb
+++ db/migrate/20180105234451_change_render_urls_to_jsonb.rb
@@ -2,5 +2,4 @@ class ChangeRenderUrlsToJsonb < ActiveRecord::Migration
   def change
   		change_column :customization_visualizations, :render_urls, 'jsonb USING CAST(render_urls as jsonb)'
   end
-	end
 end
