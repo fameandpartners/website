c3107d8457ea29ce561115ca1af1fe7568482e15
dmkrtchyan <davidm@fameandpartners.com>
2018-02-07 14:09:42 -0800

    fix conflict

--- app/presenters/user_cart/cart_product_presenter.rb
+++ app/presenters/user_cart/cart_product_presenter.rb
@@ -3,31 +3,6 @@ module UserCart
     include ApplicationHelper
     include BridesmaidHelper  
 
-<<<<<<< HEAD
-
-    COLOR_MAP = {
-      'Bright Turquoise' => '0000',
-      'Pale Blue' => '0001',
-      'Blush' =>'0002',
-      'Guava' => '0003',
-      'Burgundy' => '0004',
-      'Champagne' => '0005',
-      'Ivory' => '0006',
-      'Lilac' => '0007',
-      'Mint' => '0008',
-      'Pale Grey' => '0009',
-      'Pale Pink' => '0010',
-      'Peach' => '0011',
-      'Red' => '0012',
-      'Royal Blue' => '0013',
-      'Black' => '0014',
-      'Sage Green' => '0015',
-      'Berry'=> '0016',
-      'Navy' => '0017',
-    }
-
-=======
->>>>>>> origin/swatch-bugs
     def  serialize
       result = self.marshal_dump.clone
       result[:price] = price.marshal_dump
@@ -83,20 +58,7 @@ module UserCart
 
     def generate_image
       if brides_maid
-<<<<<<< HEAD
-        customization_ids = customizations.reject{|y| y['customisation_value']['id'][0].downcase == 'l'}.map{|x| x['customisation_value']['id']}.sort.join('-')
-        customization_ids = customization_ids.blank? ? 'default' : customization_ids
-        return {
-                :id => 0,
-          :position => 0,
-          :original => "http://d1h7wjzwtdym94.cloudfront.net/renders/composites/#{sku.downcase}/142x142/#{customization_ids}-#{length.downcase}-front-#{COLOR_MAP[color[:presentation]]}.png",
-             :large => "http://d1h7wjzwtdym94.cloudfront.net/renders/composites/#{sku.downcase}/142x142/#{customization_ids}-#{length.downcase}-front-#{COLOR_MAP[color[:presentation]]}.png",
-            :xlarge => "http://d1h7wjzwtdym94.cloudfront.net/renders/composites/#{sku.downcase}/800x800/#{customization_ids}-#{length.downcase}-front-#{COLOR_MAP[color[:presentation]]}.png",
-             :small => "http://d1h7wjzwtdym94.cloudfront.net/renders/composites/#{sku.downcase}/142x142/#{customization_ids}-#{length.downcase}-front-#{COLOR_MAP[color[:presentation]]}.png" 
-        }
-=======
         return BridesmaidHelper.generate_image(customizations, length, sku, color)
->>>>>>> origin/swatch-bugs
       else
         return image.marshal_dump if image.present?
       end
