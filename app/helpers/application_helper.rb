bc3876c874ee94cc92e839fbecb50136927dea78
Mateus Moraes <mateusm@fameandpartners.com>
2018-02-28 16:38:57 -0300

    create helper for link_to_if with a content block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -232,4 +232,12 @@ module ApplicationHelper
     end
   end
 
+  def link_to_if_with_block condition, options, html_options={}, &block
+    if condition
+      link_to options, html_options, &block
+    else
+      capture &block
+    end
+  end
+
 end
