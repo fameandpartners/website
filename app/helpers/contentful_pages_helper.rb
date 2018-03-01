a71250e720445c76fc15c768fa1fd5d58d7aa8cc
Mateus Moraes <mateusm@fameandpartners.com>
2018-03-01 00:32:49 -0300

    fix conditionals related to editorial tile pids

--- app/helpers/contentful_pages_helper.rb
+++ app/helpers/contentful_pages_helper.rb
@@ -6,18 +6,18 @@ module ContentfulPagesHelper
       pid_item = contents[:editorial_tile_pid][0]
       split_pid = pid_item.split('-', 2)
       pid_info = @collection.products.select do |item|
-        if item.color.present?
+        if item.color.present? && item.color.name.present?
           item.id == split_pid[0].to_i && item.color.name == split_pid[1]
         else
           nil
         end
       end
 
-      if pid_info
+      if pid_info.present?
         collection_product_path(pid_info[0], color: pid_info[0].color.try(:name))
       end
     else
-      if contents[:bottom_caption] && contents[:bottom_caption_url]
+      if contents[:bottom_caption].present? && contents[:bottom_caption_url].present?
         contents[:bottom_caption_url]
       end
     end
@@ -34,7 +34,7 @@ module ContentfulPagesHelper
         current_color = split_pid[1]
 
         present_product = @collection.products.select do |item|
-          if item.color.present?
+          if item.color.present? && item.color.name.present?
             item.id == current_id && item.color.name == current_color
           else
             nil
