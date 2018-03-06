33fe8596afe25b24052e310ea8a3622797610b74
dmkrtchyan <davidm@fameandpartners.com>
2018-03-06 14:04:06 -0800

    Modify image generation logic in revolution for fabrics

--- engines/revolution/app/services/revolution/product_service.rb
+++ engines/revolution/app/services/revolution/product_service.rb
@@ -47,14 +47,14 @@ module Revolution
         if p.present? && !p.hidden
           colour_name = colours[params[:offset].to_i + i]
 
-          images = collection_images(p, colour_name)
-
           price = p.site_price_for(site_version)
           fabric = Fabric.where(:name => colour_name).first
           if fabric.nil?
             color = Spree::OptionValue.where(:name => colour_name).first
+            images = collection_images_by_fabric(p, fabric)
           else
             color = fabric.option_value
+            images = collection_images(p, colour_name)
           end
 
           Products::Presenter.new(
@@ -74,6 +74,13 @@ module Revolution
 
     def collection_images(product, colour_name)
       images = product.images.find_all { |i| i.attachment_file_name.downcase.include?(colour_name.gsub('-', '_')) && i.attachment_file_name.downcase.include?('crop') }
+
+      images.sort_by { |i| i.position }.collect { |i| i.attachment.url(:large) }
+    end
+
+    def collection_images_by_fabric(product, fabric)
+      fabric_product = product.fabric_products.detect {|x| x.fabric_id == fabric.id}
+      images = product.images.find_all { |i| i.viewable_id == fabric_product.id && i.attachment_file_name.downcase.include?('crop') }
 
       images.sort_by { |i| i.position }.collect { |i| i.attachment.url(:large) }
     end
