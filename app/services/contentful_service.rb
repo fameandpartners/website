e384eb4452b34efc3675ec5e41a1cda36e044180
Mateus Moraes <mateusm@fameandpartners.com>
2018-03-22 14:22:52 -0300

    add linking capability to headers containing “ITEM——lg”

--- app/services/contentful_service.rb
+++ app/services/contentful_service.rb
@@ -343,6 +343,7 @@ module Contentful
         mobile_image = (main_header_container.respond_to? :mobile_image) ? main_header_container.mobile_image.url : desktop_image
         desktop_video = (main_header_container.respond_to? :video_desktop) ? main_header_container.video_desktop.url : nil
         mobile_video = (main_header_container.respond_to? :video_mobile) ? main_header_container.video_mobile.url : nil
+        tile_url = (main_header_container.respond_to? :tile_url) ? main_header_container.tile_url : nil
         live_text = (main_header_container.respond_to? :live_text) ? main_header_container.live_text : nil
         live_text_color = (main_header_container.respond_to? :live_text_color) ? main_header_container.live_text_color : '#fff'
         full_width_content = (main_header_container.respond_to? :full_width_content) ? main_header_container.full_width_content.sort.join(',').downcase : nil
@@ -362,6 +363,7 @@ module Contentful
           mobile_image: mobile_image,
           video: desktop_video,
           mobile_video: mobile_video,
+          tile_url: tile_url,
           overlay_pids: overlay_pids,
           live_text: live_text,
           live_text_color: live_text_color
