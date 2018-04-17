385284664c05cb2722b7ad4e33d71e632b31d1ce
Andreas Thurn <AndreasThurn@gmx.de>
2018-04-17 17:47:15 +1000

    make sure unicorn config won't be used in production

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,19 +1,24 @@
+# This config file is only for development, it will be overwritten as part of the deployment
+raise "dev config file used in non dev environment" unless ENV.fetch('RAILS_ENV', 'development') == 'development'
+
 # set path to application
 app_dir = File.expand_path("../..", __FILE__)
 working_directory app_dir
 
 # Set unicorn options
-worker_processes 2
+worker_processes 3
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
-timeout 30
+timeout 60
 
 # Set up socket location
 listen "127.0.0.1:3000", :tcp_nopush => true
 
 # Logging
 logger Logger.new($stdout)
+
+check_client_connection false
 
 
 # Set master PID location
