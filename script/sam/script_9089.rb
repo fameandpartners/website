5185ace6b899cae5457814022b2e830cc3c2b98b
Sam Weerasinghe <oystersauce8@gmail.com>
2018-05-10 11:50:14 -0700

    Fix position field of images /2
      { :product => 'Valerie' , :color => 'black' },
      { :product => 'Summer Angel' , :color => 'pale-blue' },
      { :product => 'Dragon Eyes Lace' , :color => 'burgundy' },
      { :product => 'Agata Dress' , :color => 'cobalt-blue' },
      { :product => 'Zephyra Dress' , :color => 'dark-tan' },
      { :product => 'Antares' , :color => 'red' },
      { :product => 'The Pisces' , :color => 'black' },
      { :product => 'The Axis' , :color => 'pretty-pink' },
      { :product => 'The Celestine' , :color => 'black' },
      { :product => 'The Equinox' , :color => 'black' },
      { :product => 'The Wilton Dress' , :color => 'black' },
      { :product => 'The Vista Dress' , :color => 'black' },
      { :product => 'The Bluth Dress' , :color => 'navy' },
      { :product => 'The Diaz Dress' , :color => 'lemon' },
      { :product => 'The Micah Dress' , :color => 'red-stretch-crepe' },
      { :product => 'The Gower Dress' , :color => 'dark-tan-matte-satin' },
      { :product => 'The Zarita Dress' , :color => 'navy-matte-satin' },
      { :product => 'The Lara Dress' , :color => 'lemon-heavy-georgette' },
      { :product => 'The Vionna Dress' , :color => 'red-heavy-georgette' },
      { :product => 'The Cynthia Dress' , :color => 'red-heavy-georgette' },
      { :product => 'The Nancy Dress' , :color => 'ivory-raised-spot-georgette' },
      { :product => 'The Sylvan Dress' , :color => 'ivory-raised-spot-georgette' },
      { :product => 'The Ollie Dress' , :color => 'burgundy-light-georgette' },
      { :product => 'The Maeva Dress' , :color => 'blue-violet-line' },
      { :product => 'The Gable Dress' , :color => 'ice-grey-linen' },
      { :product => 'The Babette Dress' , :color => 'black-heavy-stretch-linen' },
      { :product => 'The Rori Dress ' , :color => 'seasame-heavy-stretch-linen' },
      { :product => 'The Marisol Dress ' , :color => 'ivory-heavy-stretch-linen' },
      { :product => 'The Riley Dress' , :color => 'ivory-cotton-poplin-stripe' },
      { :product => 'The Brighton Dress',  :color => 'cornflower-blue-heavy-georgette' },
    
      { :product => 'The Brighton Dress',  :color => 'milano-floral-pale-blue-light-georgette' },
      { :product => 'The Brighton Dress',  :color => 'milano-floral-red-light-georgette' },
      { :product => 'The Tiffany Dress ' , :color => 'red-heavy-georgette' },
      { :product => 'The Tiffany Dress ' , :color => 'lemon-heavy-georgette' },
      { :product => 'The Tiffany Dress ' , :color => 'daint-floral-blue-light-georgette' },
      { :product => 'The Tiffany Dress ' , :color => 'ivory-raised-spot-georgette' },
      { :product => 'The Tiffany Dress ' , :color => 'black-heavy-georgette' },

--- script/sam/script_9089.rb
+++ script/sam/script_9089.rb
@@ -1,13 +1,36 @@
-puts "\n\n\n\n"
-print "Larissa "
-gets
-a = Spree::Product.find_by_name("Larissa").images.find_by_position('6')
-b = Spree::Product.find_by_name("Larissa").images.find_by_position('7')
-a.update_column(:position, 7)
-b.update_column(:position, 6)
-puts "fixed"
+tiles = [
+  { :product => 'The Brighton Dress',  :color => 'cornflower-blue-heavy-georgette' },
+  { :product => 'The Brighton Dress',  :color => 'milano-floral-pale-blue-light-georgette' },
+  { :product => 'The Brighton Dress',  :color => 'milano-floral-red-light-georgette' },
+  { :product => 'The Tiffany Dress ' , :color => 'red-heavy-georgette' },
+  { :product => 'The Tiffany Dress ' , :color => 'lemon-heavy-georgette' },
+  { :product => 'The Tiffany Dress ' , :color => 'daint-floral-blue-light-georgette' },
+  { :product => 'The Tiffany Dress ' , :color => 'ivory-raised-spot-georgette' },
+  { :product => 'The Tiffany Dress ' , :color => 'black-heavy-georgette' },
 
-# Similar logic for.
-#   The Callais Dress
-#   Summer Angel
-# i think.
+]
+
+tiles.each do |tile|
+  puts "\n\n\n\n"
+  puts "#{tile[:product]} #{tile[:color]}"
+  all_images = Spree::Product.find_by_name(tile[:product]).images
+  images = all_images.select { |x| x.viewable.fabric.name==tile[:color] }
+  images.each { |img| puts "  #{img.position} #{img.attachment_file_name} #{img.viewable.fabric.name}" }
+  position_of_front_crop = images.select { |img| img.attachment_file_name=~/-front-crop.jpg/ }.first.position
+  position_of_crop = images.select { |img| img.attachment_file_name=~/[^t]-crop.jpg/ }.first.position
+  faulty = position_of_crop < position_of_front_crop
+  if faulty
+    puts "  Will fix"
+  else
+    puts "  Looks ok"
+  end
+  gets
+
+  if faulty
+    a = images.select { |x| x.position==position_of_crop }.first
+    b = images.select { |x| x.position==position_of_front_crop }.first
+    a.update_column(:position, position_of_front_crop)
+    b.update_column(:position, position_of_crop)
+  end
+  puts "\n\n\n"
+end
