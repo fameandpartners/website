962edbc51e9a72c9e297a4c35123e1d91a136c7a
Mateus Moraes <mateusm@fameandpartners.com>
2018-07-19 19:14:22 -0300

    add /shop-micro-floral route again

--- config/routes.rb
+++ config/routes.rb
@@ -241,6 +241,9 @@ FameAndPartners::Application.routes.draw do
     # Linen category page
     get '/shop-linen' => 'products/collections#show', :permalink => 'shop-linen', :as => :linen_category_page
 
+    # Micro Floral category page
+    get '/shop-micro-floral' => 'products/collections#show', :permalink => 'shop-micro-floral', :as => :micro_floral_category_page
+
     # The Anti-Fast Fashion Shop (2.0 Collection) Landing page
     # get '/the-anti-fast-fashion-shop'   => 'products/collections#show', :permalink => 'the-anti-fast-fashion-shop', :as => :the_anti_fast_fashion_shop_landing_page
 
