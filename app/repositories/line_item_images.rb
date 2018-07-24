5b202578976299188efafbea261c2cfd82740b89
Andreas Thurn <AndreasThurn@gmx.de>
2018-07-24 16:17:29 +1000

    remove wedding atelier

--- app/repositories/line_item_images.rb
+++ app/repositories/line_item_images.rb
@@ -1,7 +1,5 @@
 module Repositories
   class LineItemImages
-    WEDDING_ATELIER_TAXONOMY_NAME = 'Wedding Atelier'.freeze
-
     attr_reader :line_item
 
     def initialize(line_item:)
@@ -10,48 +8,18 @@ module Repositories
 
     # @return [Repositories::Images::Template]
     def read(color_id: nil, cropped: true)
-      if product_from_wedding_atelier?
-        wedding_attrs = image_for_wedding_atelier
-        Images::Template.new(wedding_attrs)
-      else
-        image_ostruct = Repositories::ProductImages.new(product: product).read(
-          color_id: color_id,
-          cropped: cropped,
-          product_customizations: line_item.personalization&.customization_values,
-          fabric: line_item.fabric
-        )
-        Images::Template.new(image_ostruct)
-      end
+      image_ostruct = Repositories::ProductImages.new(product: product).read(
+        color_id: color_id,
+        cropped: cropped,
+        product_customizations: line_item.personalization&.customization_values,
+        fabric: line_item.fabric
+      )
+      Images::Template.new(image_ostruct)
     end
 
     private
-
-    # TODO: related to "primitive obsession" reported at the `WeddingAtelier::EventDress` model. This conversion shouldn't be here
-    def image_for_wedding_atelier
-      customization_values = line_item&.personalization&.customization_values
-      images = WeddingAtelier::EventDress.new({
-        product_id: line_item.product.id,
-        color_id:   line_item&.personalization&.color_id,
-        fit_id:     customization_values&.select{ |x| x['customisation_value']['customisation_type'] == 'fit' }.first&.dig('customisation_value','id'),
-        style_id:   customization_values&.select{ |x| x['customisation_value']['customisation_type'] == 'style' }.first&.dig('customisation_value','id'),
-        fabric_id:  customization_values&.select{ |x| x['customisation_value']['customisation_type'] == 'fabric' }.first&.dig('customisation_value','id'),
-        length_id:  customization_values&.select{ |x| x['customisation_value']['customisation_type'] == 'length' }.first&.dig('customisation_value','id')
-      }).images
-
-      {
-        original: images.dig(:real, :large)&.first,
-        large:    images.dig(:front, :normal),
-        xlarge:   images.dig(:front, :large),
-        small:    images.dig(:front, :moodboard)
-      }
-    end
-
     def product
       line_item.product
-    end
-
-    def product_from_wedding_atelier?
-      product.taxons.joins(:taxonomy).exists?(['spree_taxonomies.name = ?', WEDDING_ATELIER_TAXONOMY_NAME])
     end
   end
 end
