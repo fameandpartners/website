fe0ef8a80c0ccbeaa3fb3052a0c985944b213fd6
Andreas Thurn <AndreasThurn@gmx.de>
2018-07-24 16:20:34 +1000

    remove wedding atelier engine

--- engines/wedding-atelier/app/models/spree/line_item_decorator.rb
+++
@@ -1,3 +0,0 @@
-Spree::LineItem.class_eval do
-  scope :for_wedding_atelier, -> { joins(product: {taxons: :taxonomy}).where('spree_taxonomies.name = ?', 'Wedding Atelier') }
-end
--- engines/wedding-atelier/db/migrate/20170127005811_add_event_id_and_inviter_id_to_invitations.rb
+++
@@ -1,6 +0,0 @@
-class AddEventIdAndInviterIdToInvitations < ActiveRecord::Migration
-  def change
-    add_column :wedding_atelier_invitations, :event_id, :integer
-    add_column :wedding_atelier_invitations, :inviter_id, :integer
-  end
-end
