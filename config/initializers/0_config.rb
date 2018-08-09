001ee5dc3a8e18ed43795dbbf64b3864fed74476
Andreas Thurn <AndreasThurn@gmx.de>
2018-08-09 14:53:08 +1000

    update routing

--- config/initializers/0_config.rb
+++ config/initializers/0_config.rb
@@ -94,8 +94,8 @@ configatron.micro_influencer_email_address='qa@fameandpartners.com'
 configatron.node_pdp_url = ENV['NODE_CONTENT_URL']
 
 configatron.fame_webclient_url = ENV['FAME_WEBCLIENT_URL']
-configatron.fame_webclient_always_regex = /^(\/static|\/dresses-new|\/_webclient)/
-configatron.fame_webclient_regex = /^(\/size-guide|\/bridesmaids-swatches|\/bridesmaids-search|\/dresses\/(custom-dress|dress-.*-[0-9]*))/
+configatron.fame_webclient_always_regex = /^(\/static|\/dresses-new|\/_webclient|\/dress-system|\/dresses\/custom-dress-FPG|\/dresses\/dress-FPG)/
+configatron.fame_webclient_regex = /^(\/size-guide|\/dresses\/(custom-dress|dress-.*-[0-9]*))/
 configatron.product_render_url = ENV['PRODUCT_RENDER_URL']
 
 
