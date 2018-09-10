8baa4ee67ad5fa5d250953df31a7536bdab7cf26
Danny Sok <danny@hellohuman.com.au>
2018-09-10 15:05:44 +1000

    Add /account to forward route

--- config/initializers/0_config.rb
+++ config/initializers/0_config.rb
@@ -94,7 +94,7 @@ configatron.micro_influencer_email_address='qa@fameandpartners.com'
 configatron.node_pdp_url = ENV['NODE_CONTENT_URL']
 
 configatron.fame_webclient_url = ENV['FAME_WEBCLIENT_URL']
-configatron.fame_webclient_always_regex = /^(\/static|\/dresses-new|\/_webclient|\/custom-clothes|\/dress-system|\/dresses\/custom-dress-FPG|\/dresses\/dress-FPG)/
+configatron.fame_webclient_always_regex = /^(\/static|\/dresses-new|\/_webclient|\/custom-clothes|\/dress-system|\/dresses\/custom-dress-FPG|\/dresses\/dress-FPG|\/account)/
 configatron.fame_webclient_pdp_regex = /^(\/dresses\/(custom-dress|dress-.*-[0-9]*))/
 configatron.fame_webclient_content_regex = /^(\/size-guide|\/about|\/$)/
 configatron.product_render_url = ENV['PRODUCT_RENDER_URL']
