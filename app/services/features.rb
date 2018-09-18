0aafb2523703f798594d85be222178bfdae250e9
Andreas Thurn <AndreasThurn@gmx.de>
2018-09-18 12:04:33 +1000

    add feature flag

--- app/services/features.rb
+++ app/services/features.rb
@@ -34,6 +34,7 @@ module Features
     refulfill:                     "Check new orders coming in against inventory.",
     batching:                      "Batch line items into styles for production.",
     new_pdp:                       "Enables new PDP page that was introduced by the bridesmaids project",
+    new_category_page:             "Enables new category and search page",
     new_content:                   "Enables new content pages that were introduced by the bridesmaids project",
     new_navigation:                "Enables new navigation that was introduced by the bridesmaids project"
   }
