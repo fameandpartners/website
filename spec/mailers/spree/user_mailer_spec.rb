56cefdb5cd3a68b979ef63b00725e2207486a005
Danny Sok <danny@hellohuman.com.au>
2018-09-18 13:45:15 +1000

    Fix user_mailer spec

--- spec/mailers/spree/user_mailer_spec.rb
+++ spec/mailers/spree/user_mailer_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe Spree::UserMailer do
   context "#reset_password_instructions" do
     let(:user) { build(:spree_user) }
-    let(:mail) { Spree::UserMailer.reset_password_instructions(user) }
+    let(:expected_attributes) { { password_reset_link: '/account/forgot-password/token/' } }
 
-    it "render content without error" do
-      expect(mail.body.encoded).not_to be_empty
+    it "sends data to customer io correctly" do
+      expect_any_instance_of(Marketing::CustomerIOEventTracker).to receive(:track).with(user, 'account_password_reset', expected_attributes)
+      described_class.reset_password_instructions(user)
     end
   end
 end
