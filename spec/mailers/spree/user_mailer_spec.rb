d6e1fec573c64971ecd77557715ed36e2a237583
Danny Sok <danny@hellohuman.com.au>
2018-09-20 16:27:25 +1000

    Feature flag the user_mailer spec

--- spec/mailers/spree/user_mailer_spec.rb
+++ spec/mailers/spree/user_mailer_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
 describe Spree::UserMailer do
-  context "#reset_password_instructions" do
-    let(:user) { build(:spree_user) }
-    let(:expected_attributes) { { password_reset_link: '/account/forgot-password/token/' } }
+  if Features.active?(:new_account)
+    context "#reset_password_instructions" do
+      let(:user) { build(:spree_user) }
+      let(:expected_attributes) { { password_reset_link: '/account/forgot-password/token/' } }
 
-    it "sends data to customer io correctly" do
-      expect_any_instance_of(Marketing::CustomerIOEventTracker).to receive(:track).with(user, 'account_password_reset', expected_attributes)
-      described_class.reset_password_instructions(user)
+      it "sends data to customer io correctly" do
+        expect_any_instance_of(Marketing::CustomerIOEventTracker).to receive(:track).with(user, 'account_password_reset', expected_attributes)
+        described_class.reset_password_instructions(user)
+      end
+    end
+  else
+    context "#reset_password_instructions" do
+      let(:user) { build(:spree_user) }
+      let(:mail) { Spree::UserMailer.reset_password_instructions(user) }
+  
+      it "render content without error" do
+        expect(mail.body.encoded).not_to be_empty
+      end
     end
   end
 end
