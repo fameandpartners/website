8e0facbac612b25dcc1a3872182c588e221b323b
Danny Sok <danny@hellohuman.com.au>
2018-10-02 09:13:44 +1000

    Forgot to commit serialiser update

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-    attributes :email, :is_admin, :first_name, :last_name, :id
+    attributes :email, :is_admin, :first_name, :last_name, :id, :current_sign_in_at, :last_sign_in_at
 
     def is_admin
         object.admin?
