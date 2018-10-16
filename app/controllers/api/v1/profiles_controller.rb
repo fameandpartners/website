dff8f738c0a491617245e269290046c615231976
Danny Sok <danny@hellohuman.com.au>
2018-10-16 11:46:35 +1100

    Speed up func, ensure user still signed in

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,8 +1,14 @@
 module Api
   module V1
     class ProfilesController < Users::BaseController
+      include SslRequirement
+      helper 'spree/users', 'spree/base'
+      include Spree::Core::ControllerHelpers::Auth
+
       respond_to :json
       skip_before_filter :verify_authenticity_token
+
+      ssl_required :update
 
       def update
         @user = Spree::User.find_for_database_authentication(:email => params[:profile][:old_email])
@@ -10,15 +16,19 @@ module Api
           old_email = @user.email
 
           if @user.update_profile(params[:profile])
-            EmailCapture.new({ service: :mailchimp }, email: @user.email,
-                                        previous_email: old_email, first_name: @user.first_name,
-                                        last_name: @user.last_name, current_sign_in_ip: request.remote_ip,
-                                        landing_page: session[:landing_page], utm_params: session[:utm_params],
-                                        site_version: current_site_version.name, form_name: 'account_update').capture
             if old_email != @user.email
+              EmailCapture.new({ service: :mailchimp }, email: @user.email,
+                previous_email: old_email, first_name: @user.first_name,
+                last_name: @user.last_name, current_sign_in_ip: request.remote_ip,
+                landing_page: session[:landing_page], utm_params: session[:utm_params],
+                site_version: current_site_version.name, form_name: 'account_update').capture
+
               update_user_orders
             end
             
+            sign_in("spree_user", @user)
+            @user.remember_me!
+
             respond_with @user
             return
           end
