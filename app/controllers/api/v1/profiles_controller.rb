12fe4e341b6330667e5c50b3065688000d354b95
Danny Sok <danny@hellohuman.com.au>
2018-10-18 11:28:40 +1100

    Ensure the profile is being updated by the current logged in user

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -11,26 +11,28 @@ module Api
       ssl_required :update
 
       def update
-        @user = Spree::User.find_for_database_authentication(:email => params[:profile][:old_email])
-        if @user.present?
-          old_email = @user.email
+        if spree_user_signed_in?
+          @user = Spree::User.find_for_database_authentication(:email => params[:profile][:old_email])
+          if @user.present? and @user.email == spree_current_user.email
+            old_email = @user.email
 
-          if @user.update_profile(params[:profile])
-            if old_email != @user.email
-              EmailCapture.new({ service: :mailchimp }, email: @user.email,
-                previous_email: old_email, first_name: @user.first_name,
-                last_name: @user.last_name, current_sign_in_ip: request.remote_ip,
-                landing_page: session[:landing_page], utm_params: session[:utm_params],
-                site_version: current_site_version.name, form_name: 'account_update').capture
+            if @user.update_profile(params[:profile])
+              if old_email != @user.email
+                EmailCapture.new({ service: :mailchimp }, email: @user.email,
+                  previous_email: old_email, first_name: @user.first_name,
+                  last_name: @user.last_name, current_sign_in_ip: request.remote_ip,
+                  landing_page: session[:landing_page], utm_params: session[:utm_params],
+                  site_version: current_site_version.name, form_name: 'account_update').capture
 
-              update_user_orders
+                update_user_orders
+              end
+              
+              sign_in("spree_user", @user)
+              @user.remember_me!
+
+              respond_with @user
+              return
             end
-            
-            sign_in("spree_user", @user)
-            @user.remember_me!
-
-            respond_with @user
-            return
           end
         end
 
