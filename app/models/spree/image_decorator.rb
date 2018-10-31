d30970e4dc7369d986b986151458dcbb8e72ac01
Andreas Thurn <AndreasThurn@gmx.de>
2018-10-31 13:43:16 +1100

    update image resizing settings

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -14,6 +14,43 @@ Spree::Image.class_eval do
     small: '704x704>',
     xsmall: '528x528>',
     xxsmall: '352x352>',
+
     product: '704x704>',
+
+    webp_xxxlarge: ['4048x4048>', :webp],
+    webp_xxlarge: ['2816x2816>', :webp], 
+    webp_xlarge: ['2024x2024>', :webp],
+    webp_large: ['1408x1408>', :webp],
+    webp_medium: ['1056x1056>', :webp], 
+    webp_small: ['704x704>', :webp],
+    webp_xsmall: ['528x528>', :webp],
+    webp_xxsmall: ['352x352>', :webp],
+  }
+
+  self.attachment_definitions[:attachment][:convert_options] = {
+    :xxxlarge => '-sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB',
+    :xxlarge => '-sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB',
+    :xlarge => '-sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB',
+    :large => '-sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB',
+    :medium => '-sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB',
+    :small => '-sampling-factor 4:2:0 -strip -quality 95 -interlace JPEG -colorspace sRGB',
+    :xsmall => '-sampling-factor 4:2:0 -strip -quality 95 -interlace JPEG -colorspace sRGB',
+    :xxsmall => '-sampling-factor 4:2:0 -strip -quality 95 -interlace JPEG -colorspace sRGB',
+
+    :product => '-sampling-factor 4:2:0 -strip -quality 95 -interlace JPEG -colorspace sRGB',
+
+
+    :webp_xxxlarge => '-define webp:lossless=true -strip',
+    :webp_xxlarge => '-define webp:lossless=true -strip',
+    :webp_xlarge => '-define webp:lossless=true -strip',
+    :webp_large => '-define webp:lossless=true -strip',
+    :webp_medium => '-define webp:lossless=true -strip',
+    :webp_small => '-define webp:lossless=true -strip',
+    :webp_xsmall => '-define webp:lossless=true -strip',
+    :webp_xxsmall => '-define webp:lossless=true -strip',
+  }
+
+  self.attachment_definitions[:attachment][:fog_file] = {
+    cache_control: 'public, max-age=31536000'
   }
 end
