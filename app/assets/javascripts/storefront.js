f12aea2be7b38b3407229ca1534021c5ccde3dc2
Danny Sok <danny@hellohuman.com.au>
2018-11-28 13:57:51 +1100

    Remove application_bundle requirement

--- app/assets/javascripts/storefront.js
+++ app/assets/javascripts/storefront.js
@@ -2,10 +2,3 @@
 // Old JS files, note we should not be introducing any new code here.
 //---------
 //= require application_redesign
-
-//---------
-// Modern JS, note this is entry file that has been browserified already,
-// don't modifie this file directly, all source files are located in
-// the storefront folder.
-//---------
-//= require application_bundle
