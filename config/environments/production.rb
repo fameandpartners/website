ef9cb0dca7f7a63d3cf1255bdae3f90b2eb1138d
Andreas Thurn <AndreasThurn@gmx.de>
2018-12-05 15:38:17 +1100

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ FameAndPartners::Application.configure do
   config.action_controller.asset_host = ENV['RAILS_ASSET_HOST']
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(application_redesign.css html5.js ie.css)
+  config.assets.precompile += %w(application_redesign.css application_redesign.js html5.js ie.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
