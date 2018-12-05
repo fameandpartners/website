8ecc73b619c550d9e9234c0b46ebbec949ca6d33
Andreas Thurn <AndreasThurn@gmx.de>
2018-12-05 15:53:43 +1100

    remove env vars

--- config/initializers/twitter.rb
+++
@@ -1,4 +0,0 @@
-configatron.twitter.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-configatron.twitter.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-configatron.twitter.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-configatron.twitter.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
