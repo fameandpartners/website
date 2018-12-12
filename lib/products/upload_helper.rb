3f453d54616f45e9bd3960e38b0d3a09390a6367
Andreas Thurn <AndreasThurn@gmx.de>
2018-12-12 15:09:39 +1100

    use worker to process images

--- lib/products/upload_helper.rb
+++ lib/products/upload_helper.rb
@@ -58,15 +58,7 @@ module Products
         curation.save!
 
         c[:media].each_with_index do |(image, p)| 
-          image_url = "https://s3.amazonaws.com/fame-on-body-images-dev/#{image}"
-          file_name = File.basename(image)
-          attachment = open(image_url)
-
-          spree_image = Spree::Image.find_or_create_by_viewable_type_and_viewable_id_and_attachment_file_name('Curation', curation.id, file_name)
-          spree_image.attachment = attachment
-          spree_image.attachment_file_name = file_name
-          spree_image.position = p
-          spree_image.save!
+          AddCurationImageWorker.perform_async(image, p, curation.id)
         end
       end
     end
