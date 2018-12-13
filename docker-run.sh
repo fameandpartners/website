8861faa6e8dcc133d30607d2a5a0783cc7c428cc
Danny Sok <danny@pixelbru.sh>
2018-12-13 20:17:15 +1100

    script updates to daemonize stuff

--- docker-run.sh
+++ docker-run.sh
@@ -1,51 +1,64 @@
 #!/usr/bin/env bash
+
+# Exit if error
 set -e
 
-# TODO:Cleanup, move things to functions
+############ Output ############
+log_date_format='%Y-%m-%d %H:%M:%S'
+function info() { echo $(green)[$(date +"$log_date_format")][I]  $*$(normal); }
+
+function normal  { tput sgr0;    }
+function green   { tput setaf 2; }
 
 function kill_pid() {
-  echo "Attempting to kill for PID: $1"
+  info "Attempting to kill for PID: $1"
   if [ -f $1 ]; then
     kill $(cat $1)
-    echo "Deleting: $1"
+    info "Deleting: $1"
     rm $1
   fi
 }
 
 function mv_if_exists() {
-  echo "mv_if_exists: $1 $2"
+  info "mv_if_exists: $1 $2"
 
   if [ -f $1 ]; then
     if [ -f $2 ]; then
-      echo "Both found, removing old: $1 $2"
+      info "Both found, removing old: $1 $2"
       rm $2
     fi
 
-    echo "Moving $1 to $2"
+    info "Moving $1 to $2"
     mv $1 $2
   fi
 }
 
+function rm_if_exists() {
+  info "rm_if_exists: $1"
+
+  if [ -f $1 ]; then
+    info "Found: $1, now deleting"
+    rm $1
+  fi
+}
+
+function mkdir_if_not_exists() {
+  info "mkdir_if_not_exists: $1 $2"
+
+  if [ ! -d $1 ]; then
+    info "Dir does not exist... creating"
+    mkdir $1
+  elif [ -n "$2" ]; then
+    info "Dir already exists... clearing"
+    rm -rf $1/*
+  fi
+}
+
 # Clear out and creater required folders
-if [ ! -d "/app/tmp" ]; then
-  mkdir /app/tmp
-fi
-
-if [ ! -d "/app/tmp/pids" ]; then
-  mkdir /app/tmp/pids
-fi
-
-if [ ! -d "/app/tmp/logs" ]; then
-  mkdir /app/tmp/logs
-else
-  rm -rf /app/tmp/logs/*
-fi
-
-if [ ! -d "/app/logs" ]; then
-  mkdir /app/logs
-else
-  rm -rf /app/logs/*
-fi
+mkdir_if_not_exists "/app/tmp"
+mkdir_if_not_exists "/app/tmp/pids" 
+mkdir_if_not_exists "/app/tmp/logs" y
+mkdir_if_not_exists "/app/logs" y
 
 # Test for redis
 # if [ -z "${REDIS_IP}" ]; then
@@ -63,7 +76,7 @@ if [ "${RAILS_TYPE}" == "worker" ]; then
 
   mv_if_exists /app/config/sidekiq_production.yml /app/config/sidekiq.yml
 
-  echo "Running sidekiq"
+  info "Running sidekiq"
   cmd="/app/bin/sidekiq -C /app/config/sidekiq.yml -e ${RAILS_ENV}"
 
 else
@@ -81,25 +94,33 @@ else
 
   # Run db commands
   if psql -U $DBUSER -h $DBHOST -lqt | cut -d \| -f 1 | grep -qw $DBNAME; then
-      # database exists
-      # $? is 0
+    # database exists
+    # $? is 0
+    info "DB Already exists, running migration"
     bundle exec rake db:migrate
   else
-      # ruh-roh
-      # $? is 1
+    # ruh-roh
+    # $? is 1
+    info "DB missing, running create"
     bundle exec rake db:create db:schema:load db:migrate --trace
 
     #  Clear cache
-    bundle exec rake cache:clear
+    info "Running cache:clear in background daemon"
+    nohup bundle exec rake cache:clear &
   fi
 
   if [ -n "${ASSET_SYNC_ENABLE}" ]; then
-    bundle exec rake assets:sync
+    info "Running assets:sync in background daemon"
+    nohup bundle exec rake assets:sync &
   fi
 
-  echo "Running web"
+  info "Running web"
   cmd="/app/bin/unicorn -c /app/config/unicorn.rb -E ${RAILS_ENV}"
+
+  # Setup log redirection
+  ln -sf /dev/stdout "/app/log/${RAILS_ENV}.log"
 
 fi
 
+info "Executing: bundle exec $cmd"
 exec bundle exec "$cmd"
