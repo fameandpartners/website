b95c59604fdd613a3c98bc42b1e4c2c52f876c81
Danny Sok <danny@pixelbru.sh>
2018-12-13 13:56:27 +1100

    test :all domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -10,7 +10,7 @@ COOKIE_STORE_DOMAINS = [
 
 FameAndPartners::Application.config.session_store :cookie_store, {
   key: "_fame_and_partners_#{ Rails.env.to_s }_session",
-  domain: COOKIE_STORE_DOMAINS
+  domain: :all
 }
 
 # Use the database for sessions instead of the cookie-based default,
