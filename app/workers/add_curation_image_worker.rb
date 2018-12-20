025e3e6b917500bc5459fce405da12494bbb411c
Andreas Thurn <AndreasThurn@gmx.de>
2018-12-20 16:57:34 +1100

    dont duplicate images when reimporting

--- app/workers/add_curation_image_worker.rb
+++ app/workers/add_curation_image_worker.rb
@@ -3,7 +3,7 @@ class AddCurationImageWorker
 
   def perform(image_url, position, curation_id)
     Dir.mktmpdir do |dir|
-      file_name = URI(image_url).path.gsub('/', '~').gsub(/^~/, '')
+      file_name = URI(image_url).path.gsub('/', '~').gsub(/^~/, '').gsub('~', '_')
 
       attachment_tmp = open(image_url)
       attachment = File.new(File.join(dir, file_name), 'w+')
@@ -12,7 +12,7 @@ class AddCurationImageWorker
       attachment.flush
       attachment.seek(0)
 
-      spree_image = Spree::Image.find_or_create_by_viewable_type_and_viewable_id_and_attachment_file_name('Curation', curation_id, file_name)
+      spree_image = Spree::Image.find_or_initialize_by_viewable_type_and_viewable_id_and_attachment_file_name('Curation', curation_id, file_name)
       spree_image.attachment = attachment
       spree_image.position = position
       spree_image.save!
