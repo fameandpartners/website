0cd4edea47267a9d0c50d2c8678b90930cf78a29
Andreas Thurn <AndreasThurn@gmx.de>
2019-01-11 10:37:59 +1100

    show error messages when updating resources

--- app/controllers/spree/admin/customisation_values_controller.rb
+++ app/controllers/spree/admin/customisation_values_controller.rb
@@ -7,22 +7,6 @@ module Spree
         respond_with(collection)
       end
 
-      def update #TODO: Need to address this
-        customisation_value = @product.customisation_values.find(params[:id])
-        customisation_value.update_attributes(
-          params[:customisation_value].reverse_merge(incompatible_ids: [])
-        )
-        respond_to do |format|
-          format.html do
-            flash[:success] = "Customisation set been updated"
-            redirect_to admin_product_customisation_values_url(@product)
-          end
-          format.js do
-            render nothing: true
-          end
-        end
-      end
-
       private
 
       def model_class
