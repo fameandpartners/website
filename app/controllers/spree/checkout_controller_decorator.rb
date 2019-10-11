fd05abe5ac9b5a12d20ca8a6df2e6890e66857af
liuliming <758185271@qq.com>
2019-10-11 14:25:24 +0800

    1.add quadpay code

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,7 +2,7 @@ Spree::CheckoutController.class_eval do
 
   before_filter :before_masterpass
   skip_before_filter :check_registration
-
+  #before_action :confirm_quad_pay, only: [:update]
   before_filter def switch_views_version
     prepend_view_path Rails.root.join('app/views/checkout/v1')
   end
@@ -277,6 +277,32 @@ Spree::CheckoutController.class_eval do
   end
 
   private
+   #add from quadpay master
+  def confirm_quad_pay
+    return unless (params[:state] == 'payment') && params[:order] && params[:order][:payments_attributes]
+
+    payment_method = Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
+    if payment_method && payment_method.kind_of?(Spree::BillingIntegration::QuadPayCheckout)
+      if qp_order = payment_method.create_order(@order)
+        if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+          @order.temporary_address = !params[:save_user_address]
+          if payment = @order.payments.valid.first
+            payment.update(response_code: qp_order['token'])
+            payment.quad_pay_orders.create(
+              qp_order_id: qp_order['orderId'],
+              qp_order_token: qp_order['token']
+            )
+          end
+          redirect_to qp_order['redirectUrl']
+        else
+          render :edit
+        end
+      else
+        flash[:error] = Spree.t(:quad_pay_checkout_error)
+        redirect_to checkout_state_path('payment')
+      end
+    end
+  end
 
   def object_params
     # For payment step, filter order parameters to produce the expected nested attributes for a single payment and its source, discarding attributes for payment methods other than the one selected
