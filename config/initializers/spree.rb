1a38055b2817709f880e2bd4074e82528db4d16e
heixuxu <kurterharris@163.com>
2019-10-22 02:52:02 -0400

    the quadpay base for linux

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -12,7 +12,7 @@ Spree.config do |config|
   config.allow_ssl_in_production = !!ENV.fetch('SPREE_SSL_IN_PRODUCTION') { false }
   config.allow_ssl_in_staging = false
   config.allow_ssl_in_development_and_test = false
-#Features.active?(:force_sitewide_ssl)
+  #Features.active?(:force_sitewide_ssl)
 
   config.products_per_page = 1000 # disable pagination at all
   config.allow_backorders = false # allow order items out of stock - we have 90% items by order
