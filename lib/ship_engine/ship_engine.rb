ee1a092410682fe2ec17994001969c15dcb39c6f
liulimingTuhua <liuliming@graphicchina.com>
2020-05-28 15:02:10 +0800

    1.修改环境变量

--- lib/ship_engine/ship_engine.rb
+++ lib/ship_engine/ship_engine.rb
@@ -54,7 +54,7 @@ module ShipEngine
       puts "SSSSSSSS-------fetch_shipping_label_from_api--------SSSSSSSS"
       url = URI(ENV['SHIPENGINE_API'])
       https = Net::HTTP.new(url.host, url.port);
-      https.use_ssl = ENV['SHIPENGINE_USE_SSL']
+      https.use_ssl = ENV['SHIPENGINE_USE_SSL'] == true
 
       request = Net::HTTP::Post.new(url)
       request["Host"] = ENV['SHIPENGINE_HOST']
