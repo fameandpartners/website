ad32124b5f52d9de93d93ef49cd7b8b91ac98c7b
yeguangping <yeguangping@graphicchina.com>
2020-06-09 10:35:33 +0800

    remove newgistics tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -42,11 +42,11 @@ every(15.minutes) { rake 'data:refulfill_items' }
 #every(1.hour) { rake 'data:groom_batches'}
 
 # Newgistics scheduled tasks
-every(1.day) {
-  rake 'newgistics:upload_product_list' #Master file
-  rake 'newgistics:upload_return_list'  #External Orders file
-  rake 'newgistics:update_item_returns' #API for Inbound Returns
-}
+#every(1.day) {
+  #rake 'newgistics:upload_product_list' #Master file
+  #rake 'newgistics:upload_return_list'  #External Orders file
+  #rake 'newgistics:update_item_returns' #API for Inbound Returns
+#}
 # every(10.minutes) {
 #   rake 'newgistics:upload_return_list'  #External Orders file
 # }
