72f7b7c72e7f4f4e6dbe3135243ff6459684ee86
liulimingTuhua <liuliming@graphicchina.com>
2020-06-24 11:48:42 +0800

    password email

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -29,7 +29,7 @@ class Spree::UserMailer < ActionMailer::Base
         user,
         'account_password_reset',
         password_reset_link: @edit_password_reset_url,
-        email:           user.email,
+        email:           user.email
       )
     else
       # Slim file
